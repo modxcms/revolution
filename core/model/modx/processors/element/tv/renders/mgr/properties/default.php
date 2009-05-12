c13cadb4da39b6b38e120b232d12861010953ff2
Shaun McCormick <shaun@modx.com>
2009-05-12 13:52:13 +0000

    - Fixed bug in default property TV render

--- core/model/modx/processors/element/tv/renders/mgr/properties/default.php
+++ core/model/modx/processors/element/tv/renders/mgr/properties/default.php
@@ -3,7 +3,4 @@
  * @package modx
  * @subpackage processors.element.tv.renders.mgr.properties
  */
-
-// handles image fields using htmlarea image manager
-$modx->smarty->assign('base_url',$this->xpdo->getOption('base_url'));
 return $modx->smarty->fetch('element/tv/renders/properties/default.tpl');
