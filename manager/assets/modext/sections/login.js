00496213487594e0b05e944373bca887c7b50b2e
Shaun McCormick <shaun@modx.com>
2009-05-28 19:14:54 +0000

    - Fix to login page JS

--- manager/assets/modext/sections/login.js
+++ manager/assets/modext/sections/login.js
@@ -2,6 +2,7 @@ Ext.onReady(function() {
     if (top.frames.length !== 0) {
         top.location=self.document.location;
     }
+    MODx.load({ xtype: 'modx-page-login' });
 });
 var loginHandler = function(opt,s,r) {
     r = Ext.decode(r.responseText);
@@ -20,17 +21,17 @@ var doLogin = function() {
  * @xtype modx-page-login
  */
 MODx.page.Login = function(config) {
-	config = config || {};
-	Ext.applyIf(config,{
-	   components: [{
-	       xtype: 'modx-panel-login'
-	       ,renderTo: 'modx-panel-login'
-	   }]
-	});
-	MODx.page.Login.superclass.constructor.call(this,config);
+    config = config || {};
+    Ext.applyIf(config,{
+       components: [{
+           xtype: 'modx-panel-login'
+           ,renderTo: 'modx-panel-login'
+       }]
+    });
+    MODx.page.Login.superclass.constructor.call(this,config);
 };
 Ext.extend(MODx.page.Login,MODx.Component,{
-	
+    
 });
 Ext.reg('modx-page-login',MODx.page.Login);
 
@@ -41,75 +42,75 @@ Ext.reg('modx-page-login',MODx.page.Login);
  * @xtype modx-panel-login
  */
 MODx.panel.Login = function(config) {
-	config = config || {};
-	Ext.applyIf(config,{
-	   title: _('login')
-	   ,url: CONNECTORS_URL+'security/login.php'
-	   ,baseParams: {
-	   	
-	   }
-	   ,border: true
-	   ,frame: true
-	   ,collapsible: false
-	   ,labelAlign: 'right'
-	   ,buttonAlign: 'right'
-	   ,defaults: { 
-	       labelSeparator: ''
+    config = config || {};
+    Ext.applyIf(config,{
+       title: _('login')
+       ,url: CONNECTORS_URL+'security/login.php'
+       ,baseParams: {
+        
        }
-	   ,items: [{
-	       html: '<h2>'+SITE_NAME+'</h2>'
-	       ,border: false
-	   },{
-	       html: _('login_message')+'<br /><br />'
-	       ,border: false
-	   },{
-	       xtype: 'hidden'
-	       ,name: 'login_context'
+       ,border: true
+       ,frame: true
+       ,collapsible: false
+       ,labelAlign: 'right'
+       ,buttonAlign: 'right'
+       ,defaults: { 
+           labelSeparator: ''
+       }
+       ,items: [{
+           html: '<h2>'+SITE_NAME+'</h2>'
+           ,border: false
+       },{
+           html: _('login_message')+'<br /><br />'
+           ,border: false
+       },{
+           xtype: 'hidden'
+           ,name: 'login_context'
            ,id: 'modx-login-context'
-	       ,value: 'mgr'
-	   },{
-	       xtype: 'textfield'
-	       ,fieldLabel: _('login_username')
-	       ,name: 'username'
-	       ,el: 'modx-login-username'
+           ,value: 'mgr'
+       },{
+           xtype: 'textfield'
+           ,fieldLabel: _('login_username')
+           ,name: 'username'
+           ,el: 'modx-login-username'
            ,id: 'modx-login-username'
-	   },{
-	       xtype: 'textfield'
-	       ,fieldLabel: _('login_password')
-	       ,name: 'password'
-	       ,el: 'modx-login-password'
+       },{
+           xtype: 'textfield'
+           ,fieldLabel: _('login_password')
+           ,name: 'password'
+           ,el: 'modx-login-password'
            ,id: 'modx-login-password'
-	       ,inputType: 'password'
-	   },{
-	       xtype: 'checkbox'
-	       ,boxLabel: _('remember_username')
-	       ,name: 'rememberme'
-	       ,el: 'modx-login-rememberme'
+           ,inputType: 'password'
+       },{
+           xtype: 'checkbox'
+           ,boxLabel: _('remember_username')
+           ,name: 'rememberme'
+           ,el: 'modx-login-rememberme'
            ,id: 'modx-login-rememberme'
            ,inputValue: true
-	   },{
-	   	   html: onManagerLoginFormRender
-	   }]
-	   ,buttons: [{
-	       text: _('login_button')
+       },{
+           html: onManagerLoginFormRender
+       }]
+       ,buttons: [{
+           text: _('login_button')
            ,id: 'modx-login-button'
-	       ,handler: this.submit
-	       ,scope: this
-	   }]
+           ,handler: this.submit
+           ,scope: this
+       }]
        ,renderTo: 'modx-login-form'
-	   ,listeners: {
-	   	   'success': {fn:function(o) {
-	   	   	   location.href = (o.result.object.id !== undefined) ? './index.php?id=' + o.result.object.id : './';
-	   	   },scope:this}
-	   }
-	   ,keys: [{
+       ,listeners: {
+           'success': {fn:function(o) {
+               location.href = (o.result.object.id !== undefined) ? './index.php?id=' + o.result.object.id : './';
+           },scope:this}
+       }
+       ,keys: [{
             key: Ext.EventObject.ENTER,
             fn: this.submit
             ,scope: this
         }]
 
-	});
-	MODx.panel.Login.superclass.constructor.call(this,config);
+    });
+    MODx.panel.Login.superclass.constructor.call(this,config);
 };
 Ext.extend(MODx.panel.Login,MODx.FormPanel);
 Ext.reg('modx-panel-login',MODx.panel.Login);
