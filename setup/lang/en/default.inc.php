c7762abcf905359510f066fd785394d9cfd3894e
Shaun McCormick <shaun@modx.com>
2009-10-20 17:26:15 +0000

    - Fixing setup text for memory limit checks

--- setup/lang/en/default.inc.php
+++ setup/lang/en/default.inc.php
@@ -201,8 +201,8 @@ $_lang['test_dependencies'] = 'Checking PHP for zlib dependency: ';
 $_lang['test_dependencies_fail_zlib'] = 'Your PHP installation does not have the "zlib" extension installed. This extension is necessary for MODx to run. Please enable it to continue.';
 $_lang['test_directory_exists'] = 'Checking if <span class="mono">%s</span> directory exists: ';
 $_lang['test_directory_writable'] = 'Checking if <span class="mono">%s</span> directory is writable: ';
-$_lang['test_memory_limit'] = 'Checking if memory limit is set to at least 32M: ';
-$_lang['test_memory_limit_fail'] = 'MODx found your memory_limit setting to be below the recommended setting of 32M. MODx attempted to set the memory_limit to 32M, but was unsuccessful. Please set the memory_limit setting in your php.ini file to 32M or higher before proceeding.';
+$_lang['test_memory_limit'] = 'Checking if memory limit is set to at least 24M: ';
+$_lang['test_memory_limit_fail'] = 'MODx found your memory_limit setting to be below the recommended setting of 24M. MODx attempted to set the memory_limit to 24M, but was unsuccessful. Please set the memory_limit setting in your php.ini file to 24M or higher before proceeding.';
 $_lang['test_php_version_fail'] = 'You are running on PHP %s, and MODx Revolution requires PHP 4.3.0 or later';
 $_lang['test_php_version_sn'] = 'While MODx will work on your PHP version (%s), usage of MODx on this version is not recommended. Your version of PHP is vulnerable to numerous security holes. Please upgrade to PHP version is 4.3.11 or higher, which patches these holes. It is recommended you upgrade to this version for the security of your own website.';
 $_lang['test_php_version_start'] = 'Checking PHP version:';
@@ -212,5 +212,5 @@ $_lang['test_table_prefix_inuse'] = 'Table prefix is already in use in this data
 $_lang['test_table_prefix_inuse_desc'] = 'Setup couldn\'t install into the selected database, as it already contains tables with the prefix you specified. Please choose a new table_prefix, and run Setup again.';
 $_lang['test_table_prefix_nf'] = 'Table prefix does not exist in this database!';
 $_lang['test_table_prefix_nf_desc'] = 'Setup couldn\'t install into the selected database, as it does not contain existing tables with the prefix you specified to be upgraded. Please choose an existing table_prefix, and run Setup again.';
-$_lang['test_zip_memory_limit'] = 'Checking if memory limit is set to at least 64M for zip extensions: ';
-$_lang['test_zip_memory_limit_fail'] = 'MODx found your memory_limit setting to be below the recommended setting of 64M. MODx attempted to set the memory_limit to 64M, but was unsuccessful. Please set the memory_limit setting in your php.ini file to 64M or higher before proceeding, so that the zip extensions can work properly.';
+$_lang['test_zip_memory_limit'] = 'Checking if memory limit is set to at least 24M for zip extensions: ';
+$_lang['test_zip_memory_limit_fail'] = 'MODx found your memory_limit setting to be below the recommended setting of 24M. MODx attempted to set the memory_limit to 24M, but was unsuccessful. Please set the memory_limit setting in your php.ini file to 24M or higher before proceeding, so that the zip extensions can work properly.';
