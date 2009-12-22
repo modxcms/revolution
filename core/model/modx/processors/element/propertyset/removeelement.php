18920892be4d44139c939496061ac5e79ba9eb2d
Shaun McCormick <shaun@modx.com>
2009-12-22 21:54:51 +0000

    - [#MODX-1532] Fixed bug in permissions reference when trying to remove element from property set

--- core/model/modx/processors/element/propertyset/removeelement.php
+++ core/model/modx/processors/element/propertyset/removeelement.php
@@ -5,7 +5,7 @@
  * @package modx
  * @subpackage processors.element.propertyset
  */
-if (!$modx->hasPermission('delete')) return $modx->error->failure($modx->lexicon('permission_denied'));
+if (!$modx->hasPermission('remove')) return $modx->error->failure($modx->lexicon('permission_denied'));
 $modx->lexicon->load('propertyset','element');
 
 if (!isset($_POST['element_class']) || !isset($_POST['element'])) {
