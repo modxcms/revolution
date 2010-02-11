5f897813991e8baaf755d5849a923c384f4c084e
Shaun McCormick <shaun@modx.com>
2010-02-11 05:32:21 +0000

    - [#MODX-1606] Fixed bug in modRestCurlClient class due to encoded ampersand

--- core/model/modx/rest/modrestcurlclient.class.php
+++ core/model/modx/rest/modrestcurlclient.class.php
@@ -27,7 +27,8 @@ class modRestCurlClient extends modRestClient {
                 curl_setopt($ch,CURLOPT_POSTFIELDS,$params);
                 break;
         }
-        curl_setopt($ch, CURLOPT_URL, $host.$path);
+        $url = str_replace('&amp;', '&', $host.$path);
+        curl_setopt($ch, CURLOPT_URL,$url);
 
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_HEADER, 0);
