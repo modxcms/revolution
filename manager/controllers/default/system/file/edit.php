5a294d8b705e6b14d9f5a4b79ac6a3bb4c7adc5f
Shaun McCormick <shaun@modx.com>
2010-02-25 14:19:45 +0000

    - [#MODX-1675] Added fix for windows paths on Edit File panel

--- manager/controllers/default/system/file/edit.php
+++ manager/controllers/default/system/file/edit.php
@@ -6,8 +6,10 @@
  * @subpackage manager.system.file
  */
 if (!$modx->hasPermission('file_manager')) return $modx->error->failure($modx->lexicon('access_denied'));
+if (empty($_GET['file'])) return $modx->error->failure($modx->lexicon('file_err_nf'));
 
+$filename = preg_replace('#([\\\\]+|/{2,})#', '/',$_GET['file']);
 $modx->regClientStartupScript($modx->getOption('manager_url').'assets/modext/sections/system/file/edit.js');
-$modx->regClientStartupHTMLBlock('<script type="text/javascript">Ext.onReady(function() {MODx.load({xtype: "modx-page-file-edit",file: "'.$_GET['file'].'"});});</script>');
+$modx->regClientStartupHTMLBlock('<script type="text/javascript">Ext.onReady(function() {MODx.load({xtype: "modx-page-file-edit",file: "'.$filename.'"});});</script>');
 
 return $modx->smarty->fetch('system/file/edit.tpl');
