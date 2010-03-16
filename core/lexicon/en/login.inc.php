10dd7f6f74475db216987e12f36477de3f8f48c3
Shaun McCormick <shaun@modx.com>
2010-03-16 15:59:12 +0000

    [LEXICON RELOAD REQUIRED] - Tweaked CSS in login panel - Updated copyright data in lexicon entries - Fixed path issue in css compression command in build.xml

--- core/lexicon/en/login.inc.php
+++ core/lexicon/en/login.inc.php
@@ -14,13 +14,13 @@ $_lang['login_blocked_time'] = 'You are not allowed to login at this time. Pleas
 $_lang['login_blocked_too_many_attempts'] = 'You have been blocked due to too many failed login attempts.';
 $_lang['login_button'] = 'Login';
 $_lang['login_cannot_locate_account'] = 'That account could not be located.  Check the username and re-type the password to try again.';
-$_lang['login_copyright'] = '&copy; 2005-2009 by the <a href="http://modxcms.com/" target="_blank" style="text-decoration: none;">MODx Team</a>. <strong>MODx</strong>&trade; is licensed under the GPLv2 or later.';
+$_lang['login_copyright'] = '&copy; 2005-2010 by the <a href="http://modxcms.com/" target="_blank">MODx Team</a>. <strong>MODx</strong>&trade; is licensed under the GPLv2 or later.';
 $_lang['login_email_label'] = 'Account Email:';
 $_lang['login_err_unknown'] = 'An unknown error occurred while trying to log in.';
 $_lang['login_forget_your_login'] = 'Forget your Login?';
 $_lang['login_hostname_error'] = 'Your hostname does not point back to your IP address.';
 $_lang['login_message'] = 'Please enter your login credentials to start your Manager session. Your username and password are case-sensitive, so please enter them carefully!';
-$_lang['login_modx_support'] = '<p>Supported By:</p><p style="font-size: x-small;"><a href="http://www.modxcms.com/forums/" target="_blank" style="text-decoration: none;"><strong>The MODx Community</strong></a></p>';
+$_lang['login_modx_support'] = '<p>Supported By:</p><p><a href="http://www.modxcms.com/forums/" target="_blank"><strong>The MODx Community</strong></a></p>';
 $_lang['login_password'] = 'Password: ';
 $_lang['login_password_reset_act_sent'] = 'Password Reset Activation email sent. Please check your inbox.';
 $_lang['login_remember'] = 'Remember me';
