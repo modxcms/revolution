9688736558a98e0f450ebbc23e806b8f0ece1989
Shaun McCormick <shaun@modx.com>
2010-03-17 18:38:42 +0000

    - Sort Template Variables on the Template editing page by name

--- core/model/modx/processors/element/template/tv/getlist.php
+++ core/model/modx/processors/element/template/tv/getlist.php
@@ -22,8 +22,8 @@ $modx->lexicon->load('template');
 $isLimit = !empty($scriptProperties['limit']);
 $start = $modx->getOption('start',$scriptProperties,0);
 $limit = $modx->getOption('limit',$scriptProperties,20);
-$sort = $modx->getOption('sort',$scriptProperties,'rank');
 $sortAlias = $modx->getOption('sort',$scriptProperties,'modTemplateVar');
+$sort = $modx->getOption('sort',$scriptProperties,'name');
 $dir = $modx->getOption('dir',$scriptProperties,'ASC');
 $template = $modx->getOption('template',$scriptProperties,false);
 
