ed029655d8c7ad78ef8578eaadabe855320934f1
Shaun McCormick <shaun@modx.com>
2010-04-12 20:10:31 +0000

    [LEXICON RELOAD REQUIRED] - [#MODX-1789] Added extra checks in Package Management to make sure that the correct directories are created before using it. Will now prevent usage of PM if those directories do not exist or are not writable.

--- manager/controllers/default/workspaces/index.php
+++ manager/controllers/default/workspaces/index.php
@@ -7,6 +7,50 @@
  */
 if (!$modx->hasPermission('workspaces')) return $modx->error->failure($modx->lexicon('access_denied'));
 
+/* ensure directories for Package Management are created */
+$cacheManager = $modx->getCacheManager();
+$directoryOptions = array(
+    'new_folder_permissions' => $modx->getOption('new_folder_permissions',null,0775),
+);
+
+$errors = array();
+/* create assets/ */
+$assetsPath = $modx->getOption('base_path').'assets/';
+if (!is_dir($assetsPath)) {
+    $cacheManager->writeTree($assetsPath,$directoryOptions);
+}
+if (!is_dir($assetsPath) || !is_writable($assetsPath)) {
+    $errors['assets_not_created'] = $modx->lexicon('dir_err_assets',array('path' => $assetsPath));
+}
+unset($assetsPath);
+
+/* create assets/components/ */
+$assetsCompPath = $modx->getOption('base_path').'assets/components/';
+if (!is_dir($assetsCompPath)) {
+    $cacheManager->writeTree($assetsCompPath,$directoryOptions);
+}
+if (!is_dir($assetsCompPath) || !is_writable($assetsCompPath)) {
+    $errors['assets_comp_not_created'] = $modx->lexicon('dir_err_assets_comp',array('path' => $assetsCompPath));
+}
+unset($assetsCompPath);
+
+/* create core/components/ */
+$coreCompPath = $modx->getOption('core_path').'components/';
+if (!is_dir($coreCompPath)) {
+    $cacheManager->writeTree($coreCompPath,$directoryOptions);
+}
+if (!is_dir($coreCompPath) || !is_writable($coreCompPath)) {
+    $errors['core_comp_not_created'] = $modx->lexicon('dir_err_core_comp',array('path' => $coreCompPath));
+}
+unset($coreCompPath);
+
+
+if (!empty($errors)) {
+    $modx->smarty->assign('errors',$errors);  
+    return $modx->smarty->fetch('workspaces/error.tpl');
+}
+
+
 /* get default provider */
 $provider = $modx->getObject('transport.modTransportProvider',array(
     'name' => 'modxcms.com',
