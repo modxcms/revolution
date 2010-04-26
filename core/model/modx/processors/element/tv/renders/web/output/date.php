3bed1bc371b1cdbfde240377941a2472c78632d3
Jason Coward <jason@modx.com>
2010-04-26 14:47:02 +0000

    - [#MODX-1897] Fix Date TemplateVar web output render error in PHP 5.3 due to use of ereg()

--- core/model/modx/processors/element/tv/renders/web/output/date.php
+++ core/model/modx/processors/element/tv/renders/web/output/date.php
@@ -6,14 +6,7 @@
 
 
 $value= $this->parseInput($value);
-// Check for MySQL style date - Adam Crownoble 8/3/2005
-$date_match= '^([0-9]{2})-([0-9]{2})-([0-9]{4})\ ([0-9]{2}):([0-9]{2}):([0-9]{2})$';
-$matches= array ();
-if (strpos($value, '-') !== false && ereg($date_match, $value, $matches)) {
-    $timestamp= mktime($matches[4], $matches[5], $matches[6], $matches[2], $matches[1], $matches[3]);
-} else { // If it's not a MySQL style date, then use strtotime to figure out the date
-    $timestamp= strtotime($value);
-}
+$timestamp= strtotime($value);
 $p= $params['format'] ? $params['format'] : "%A %d, %B %Y";
 $o= strftime($p, $timestamp);
 
