378b9189557f4285782e486b8691ffe8556ca390
Shaun McCormick <shaun@modx.com>
2010-06-01 16:43:15 +0000

    - [#MODX-1904] Fixed bug with phx placeholders in modTranslate095 class

--- core/model/modx/modtranslate095.class.php
+++ core/model/modx/modtranslate095.class.php
@@ -25,25 +25,33 @@
  * @package modx
  */
 class modTranslate095 {
-    var $modx= null;
-    var $parser= null;
+    public $modx= null;
+    public $parser= null;
 
-    function modTranslate095(& $modx) {
-        $this->__construct($modx);
-    }
-    function __construct(& $modx) {
-        $this->modx= & $modx;
+    function __construct(modX &$modx) {
+        $this->modx = &$modx;
+        $this->preTranslationSearch= array('[*','[~','[+','[!');
+        $this->preTranslationReplace= array('#trans->[*','#trans->[~','#trans->[+','#trans->[!');
+        $this->tagTranslation= array (
+            '[[++' => array ('[(', ')]', '++'),
+            '[[$' => array ('{{', '}}', '$'),
+            '[[*' => array ('#trans->[*', '*]', '*'),
+            '[[~' => array ('#trans->[~', '~]', '~'),
+            '[[+' => array ('#trans->[+', '+]', '+'),
+            '[[!' => array ('#trans->[!', '!]', '!'),
+        );
     }
 
-    function getParser() {
+    public function getParser() {
         if (!is_object($this->parser) || !($this->parser instanceof modParser095)) {
             $this->parser= & $this->modx->getService('parser095', 'modParser095');
         }
         return $this->parser;
     }
 
-    function translateSite($save= false, $classes= null, $files= array (), $toFile= false) {
-        $parser= $this->getParser();
+    public function translateSite($save= false, $classes= null, $files= array (), $toFile= false) {
+        $parser = $this->getParser();
+        $parser->tagTranslation = $this->tagTranslation;
         if ($classes === null) {
             $classes= array (
                 'modResource' => array ('content', 'pagetitle', 'longtitle', 'description', 'menutitle', 'introtext'),
@@ -65,8 +73,10 @@ class modTranslate095 {
             if ($resources) {
                 foreach ($resources as $resource) {
                     foreach ($fields as $field) {
-                        if ($content= $resource->get($field)) {
+                        $content= $resource->get($field);
+                        if ($content) {
                             echo "[BEGIN TRANSLATING FIELD] {$field}\n";
+                            $content = str_replace($this->preTranslationSearch, $this->preTranslationReplace, $content);
                             while ($parser->translate($content, array(), true)) {
                                 $resource->set($field, $content);
                             }
@@ -94,26 +104,28 @@ class modTranslate095 {
         }
     }
 
-    function translateFiles($save= false, $files= array()) {
+    public function translateFiles($save= false, $files= array()) {
         $output= '';
         if (is_array($files) && !empty ($files)) {
             $parser= $this->getParser();
+            $parser->tagTranslation=$this->tagTranslation;
             $cacheManager= $this->modx->getCacheManager();
 
-            $output.= "Processing files: " . print_r($files, true) . "\n\n\n";
+            $output .= "Processing files: " . print_r($files, true) . "\n\n\n";
             ob_start();
             foreach ($files as $file) {
                 if (file_exists($file)) {
                     echo "[BEGIN TRANSLATING FILE] {$file}\n";
                     $content= file_get_contents($file);
                     if ($content !== false) {
+                        $content=str_replace($this->preTranslationSearch,$this->preTranslationReplace,$content);
                         while ($parser->translate($content, array(), true)) {}
                         if ($save) $cacheManager->writeFile($file, $content);
                     }
                     echo "[END TRANSLATING FILE] {$file}\n";
                 }
             }
-            $output.= ob_get_contents();
+            $output .= ob_get_contents();
             ob_end_clean();
         }
         return $output;
