1125e2db97e782e7f4521848948ddb806ee5a9ac
Shaun McCormick <shaun@modx.com>
2010-06-22 15:07:29 +0000

    - [#MODX-1612] Added Create Menu context menu on root node for menus tree

--- core/model/modx/processors/system/menu/getnodes.php
+++ core/model/modx/processors/system/menu/getnodes.php
@@ -17,6 +17,8 @@ $modx->lexicon->load('action','menu','topmenu');
 $isLimit = !empty($scriptProperties['limit']);
 $start = $modx->getOption('start',$scriptProperties,0);
 $limit = $modx->getOption('limit',$scriptProperties,10);
+$sort = $modx->getOption('sort',$scriptProperties,'menuindex');
+$dir = $modx->getOption('dir',$scriptProperties,'ASC');
 $id = $modx->getOption('id',$scriptProperties,'');
 
 $id = str_replace('n_','',$id);
@@ -25,15 +27,15 @@ if (empty($id)) $id = '';
 $c = $modx->newQuery('modMenu');
 $c->leftJoin('modMenu','Children');
 $c->leftJoin('modAction','Action');
-$c->select('
-    `modMenu`.*,
-    `Action`.`controller` AS `controller`,
-    COUNT(`Children`.`text`) AS `childrenCount`
-');
+$c->select(array(
+    'modMenu.*',
+    'Action.controller',
+));
+$c->select('COUNT(`Children`.`text`) AS `childrenCount`');
 $c->where(array(
     'modMenu.parent' => $id,
 ));
-$c->sortby('modMenu.menuindex','ASC');
+$c->sortby($sort,$dir);
 $c->groupby('modMenu.text');
 if ($isLimit) $c->limit($limit,$start);
 $menus = $modx->getCollection('modMenu',$c);
@@ -42,44 +44,25 @@ $list = array();
 foreach ($menus as $menu) {
     $controller = $menu->get('controller');
     if (empty($controller)) $controller = '';
+        if (strlen($controller) > 1 && substr($controller,strlen($controller)-4,strlen($controller)) != '.php') {
+            if (!file_exists($modx->getOption('manager_path').'controllers/'.$controller.'.php')) {
+                $controller .= '/index.php';
+                $controller = strtr($controller,'//','/');
+        } else {
+            $controller .= '.php';
+        }
+    }
+    $text = $modx->lexicon($menu->get('text'));
 
-	if (strlen($controller) > 1 && substr($controller,strlen($controller)-4,strlen($controller)) != '.php') {
-		if (!file_exists($modx->getOption('manager_path').'controllers/'.$controller.'.php')) {
-			$controller .= '/index.php';
-			$controller = strtr($controller,'//','/');
-		} else {
-			$controller .= '.php';
-		}
-	}
-	$text = $modx->lexicon($menu->get('text'));
-
-    $contextMenu = array();
-    $contextMenu[] = array(
-        'text' => $modx->lexicon('menu_update'),
-        'handler' => 'function(itm,e) { this.updateMenu(itm,e); }',
-    );
-    $contextMenu[] = '-';
-    $contextMenu[] = array(
-        'text' => $modx->lexicon('action_place_here'),
-        'handler' => 'function(itm,e) { this.createMenu(itm,e); }',
-    );
-    $contextMenu[] = '-';
-    $contextMenu[] = array(
-        'text' => $modx->lexicon('menu_remove'),
-        'handler' => 'function(itm,e) { this.removeMenu(itm,e); }',
-    );
-
-	$list[] = array(
-		'text' => $text.($controller != '' ? ' <i>('.$controller.')</i>' : ''),
-		'id' => 'n_'.$menu->get('text'),
+    $list[] = array(
+        'text' => $text.($controller != '' ? ' <i>('.$controller.')</i>' : ''),
+        'id' => 'n_'.$menu->get('text'),
         'cls' => 'icon-menu',
-		'type' => 'menu',
+        'type' => 'menu',
         'pk' => $menu->get('text'),
-		'leaf' => false,
-        //'expandable' => $menu->get('childrenCount') <= 0 ? true : false,
+        'leaf' => false,
         'data' => $menu->toArray(),
-		'menu' => array('items' => $contextMenu),
-	);
+    );
 }
 
 return $this->toJSON($list);
