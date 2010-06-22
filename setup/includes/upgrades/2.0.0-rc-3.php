32062ee36eabc29478dd99f8b2b57e56f3542292
Shaun McCormick <shaun@modx.com>
2010-06-22 15:14:11 +0000

    [REBUILD/UPGRADE REQUIRED] - [#MODX-2032] Fixed topic varchar length issue with UTF-8 installs

--- setup/includes/upgrades/2.0.0-rc-3.php
+++ setup/includes/upgrades/2.0.0-rc-3.php
@@ -14,3 +14,10 @@ if (!empty($classes)) {
     $this->createTable($classes);
 }
 unset($classes);
+
+/* adjust entry table [#MODX-2032] */
+$class = 'modLexiconEntry';
+$table = $this->install->xpdo->getTableName($class);
+$description = sprintf($this->install->lexicon['change_column'],'topic topic','topic topic',$table);
+$sql = "ALTER TABLE {$table} CHANGE `topic` `topic` VARCHAR(228) NOT NULL DEFAULT 'default'";
+$this->processResults($class,$description,$sql);
