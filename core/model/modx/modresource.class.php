76589d5bec7c862ef61d7a4931e173de83141c9c
Shaun McCormick <shaun@modx.com>
2010-07-08 21:36:23 +0000

    - [#MODX-2091] Ensure duplicate maintains published status

--- core/model/modx/modresource.class.php
+++ core/model/modx/modresource.class.php
@@ -550,9 +550,8 @@ class modResource extends modAccessibleSimpleObject {
         $newResource->set('deleted',false);
         $newResource->set('deletedon',0);
         $newResource->set('deletedby',0);
-        $newResource->set('publishedon',0);
-        $newResource->set('publishedby',0);
-        $newResource->set('published',false);
+        $newResource->set('publishedon',time());
+        $newResource->set('publishedby',$this->xpdo->user->get('id'));
         
         if (!$newResource->save()) {
             return $this->xpdo->lexicon('resource_err_duplicate');
