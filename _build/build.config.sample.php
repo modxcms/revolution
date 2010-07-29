412e4d23f9314d8d91b99cd6128ad061b3001986
Jason Coward <jason@modx.com>
2010-07-29 10:10:43 -0600

    Refactor build for git
    Getting rid of the svn Ant tasks, converting export to use git command, .gitignore configuration, etc.

--- _build/build.config.sample.php
+++ _build/build.config.sample.php
@@ -1,10 +1,7 @@
 <?php
 // define the MODX path constants necessary for core installation
 define('MODX_CORE_PATH', dirname(dirname(__FILE__)) . '/core/');
-define('MODX_BASE_PATH', dirname(dirname(__FILE__)) . '/');
-define('MODX_MANAGER_PATH', MODX_BASE_PATH . 'manager/');
-define('MODX_CONNECTORS_PATH', MODX_BASE_PATH . 'connectors/');
-define('MODX_ASSETS_PATH', MODX_BASE_PATH . 'assets/');
+define('MODX_CONFIG_KEY', 'config');
 
 // define the connection variables
 define('XPDO_DSN', 'mysql:host=localhost;dbname=modx;charset=utf8');
