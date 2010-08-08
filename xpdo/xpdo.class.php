bf22a32af4f4ebf0ee6bd1868167624d2a4fe500
Jason Coward <jason@modx.com>
2010-08-08 15:26:58 -0600

    Add xPDO::literal() method for SQL literal strings
    Provides a method for adding SQL literal strings (unescaped and unquoted) to xPDOCriteria. Can also be used to validate those literals or perform other operations in the future.

--- xpdo/xpdo.class.php
+++ xpdo/xpdo.class.php
@@ -1463,6 +1463,17 @@ class xPDO {
     }
 
 	/**
+	 * Use to insert a literal string into a SQL query without escaping or quoting.
+	 *
+	 * @param string $string A string to return as a literanl, unescaped and unquoted.
+	 * @return string The string with any escape or quote characters trimmed.
+	 */
+	public function literal($string) {
+		$string = trim($string, $this->_escapeChar . $this->_quoteChar);
+		return $string;
+	}
+
+    /**
      * Adds the table prefix, and optionally database name, to a given table.
      *
      * @param string $baseTableName The table name as specified in the object
