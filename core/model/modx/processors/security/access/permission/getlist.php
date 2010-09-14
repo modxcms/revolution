7c4efab09673c7af793f50626818e3916f5886aa
Shaun McCormick <shaun@modxcms.com>
2010-09-14 08:44:20 -0500

    Added an editable dropdown for Permissions tab when editing an Access Policy for easier addition of Permissions

---
+++ core/model/modx/processors/security/access/permission/getlist.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * @package modx
+ * @subpackage processors.security.permission
+ */
+if (!$modx->hasPermission('access_permissions')) return $modx->error->failure($modx->lexicon('permission_denied'));
+$modx->lexicon->load('access','permissions');
+
+/* setup default properties */
+$isLimit = !empty($scriptProperties['limit']);
+$start = $modx->getOption('start',$scriptProperties,0);
+$limit = $modx->getOption('limit',$scriptProperties,10);
+$sort = $modx->getOption('sort',$scriptProperties,'');
+$dir = $modx->getOption('dir',$scriptProperties,'ASC');
+
+/* build query */
+$c = $modx->newQuery('modAccessPermission');
+$c->select(array(
+    'modAccessPermission.id',
+    'modAccessPermission.name',
+    'modAccessPermission.description',
+    'Policy.lexicon',
+));
+$c->leftJoin('modAccessPolicy','Policy');
+$c->query['DISTINCT'] = 'DISTINCT';
+$count = $modx->getCount('modAccessPermission',$c);
+$c->groupby('modAccessPermission.name');
+$c->sortby('modAccessPermission.name','ASC');
+if ($isLimit) {
+    $c->limit($limit,$start);
+}
+$permissions = $modx->getCollection('modAccessPermission',$c);
+
+/* iterate */
+$list = array();
+foreach ($permissions as $permission) {
+    $permissionArray = $permission->get(array('name','description','lexicon'));
+
+    if (!empty($permissionArray['lexicon'])) {
+        if (strpos($permissionArray['lexicon'],':') !== false) {
+            $modx->lexicon->load('en:'.$permissionArray['lexicon']);
+        } else {
+            $modx->lexicon->load('en:core:'.$permissionArray['lexicon']);
+        }
+        $desc = $modx->lexicon($desc);
+    }
+    $permissionArray['description'] = $modx->lexicon($permissionArray['description']);
+    $list[] = $permissionArray;
+}
+
+return $this->outputArray($list,$count);
