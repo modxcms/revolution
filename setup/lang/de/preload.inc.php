a88ea37f66ece683b98520dafb2f3d8f21cd8da0
Shaun McCormick <shaun@modxcms.com>
2010-09-14 09:01:28 -0500

    [#MODX-2325] Updated placeholders in setup lexicons for french/german languages

--- setup/lang/de/preload.inc.php
+++ setup/lang/de/preload.inc.php
@@ -5,7 +5,7 @@
  * @package setup
  * @subpackage lexicon
  */
-$_lang['preload_err_cache'] = 'Stellen Sie sicher, dass das Verzeichnis %scache existiert und für PHP beschreibbar ist.';
+$_lang['preload_err_cache'] = 'Stellen Sie sicher, dass das Verzeichnis [[+path]]cache existiert und für PHP beschreibbar ist.';
 $_lang['preload_err_core_path'] = 'Stellen Sie sicher, dass Sie einen gültigen MODX_CORE_PATH in der Datei setup/includes/config.core.php angegeben haben; diese Konstante muss auf einen funktionierenden MODx-Core zeigen.';
 $_lang['preload_err_mysql'] = 'MODx benötigt die mysql-Extension, wenn PHP ohne natives PDO verwendet wird, aber diese scheint nicht geladen zu sein.';
 $_lang['preload_err_pdo'] = 'MODx benötigt die PDO-Extension, wenn natives PDO vewendet wird, aber diese scheint nicht geladen zu sein.';
