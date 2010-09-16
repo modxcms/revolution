8d079504c8593e753703e96715ea25d033267b43
Shaun McCormick <shaun@modxcms.com>
2010-09-16 09:10:51 -0500

    Merging from 2.0 branch

--- setup/lang/ja/preload.inc.php
+++ setup/lang/ja/preload.inc.php
@@ -6,7 +6,7 @@
  * @subpackage lexicon
  * @author KUROI Enogu http://twitter.com/enogu
  */
-$_lang['preload_err_cache'] = '%scache ディレクトリーをPHPプロセスから書き込める状態にしてください。';
+$_lang['preload_err_cache'] = '[[+path]]cache ディレクトリーをPHPプロセスから書き込める状態にしてください。';
 $_lang['preload_err_core_path'] = 'setup/includes/config.core.php を開き、MODX_CORE_PATHの値を修正してください。このパラメーターはcoreディレクトリーの場所を指している必要があります。';
 $_lang['preload_err_mysql'] = 'MySQL拡張モジュールが発見できませんでした。';
 $_lang['preload_err_pdo'] = 'PDO拡張モジュールが発見できませんでした。';
