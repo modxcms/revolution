6f9ec5bcf32f2d54c379132d9700e2f24b447b52
Lauri L <lauri@laanepere.ee>
2010-09-17 00:05:52 +0800

    Translated core/lexicon/et/about.inc.php

---
+++ core/lexicon/et/about.inc.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * About Estonian lexicon topic
+ *
+ * @language et
+ * @package modx
+ * @subpackage lexicon
+ */
+$_lang['about_msg'] = 'MODx <a href="http://modxcms.com/" target="_blank">PHP Application Framework ja Sisuhaldussüsteem</a> on <a href="http://opensource.org/licenses/gpl-2.0.php">GNU Public License v2</a> litsentsi all ja (vastavalt Teie valikule) ükskõik millise hilisema versiooni all.';
+$_lang['about_title'] = 'MODx-ist';
+$_lang['credits'] = 'Tänud';
+$_lang['credits_mysql'] = 'MODx kasutab <a href="http://www.mysql.com" target="_blank">MySQL</a>.';
+$_lang['credits_php'] = 'MODx kasutab <a href="http://www.php.net" target="_blank">PHP</a>.';
+$_lang['credits_sencha'] = 'MODx kasutab <a href="http://www.sencha.com/" target="_blank">Sencha (ExtJS)</a> oma tava-manageri kasutajaliideses.';
+$_lang['credits_smarty'] = 'MODx kasutab <a href="http://www.smarty.net/" target="_blank">Smarty</a> oma tava-manageri kasutajaliideses.';
+$_lang['credits_xpdo'] = 'MODx kasutab andmebaasi mapperit (ORM) <a href="http://www.xpdo.org/" target="_blank">xPDO</a>.';
+$_lang['help_msg'] = '<p>Tasuta kogukondliku abi saab <a href="http://modxcms.com/forums/" target="_blank">külastades MODx foorumeid</a>. Sammuti on olemas ka täienev <a href="http://docs.modxcms.com/display/revolution" target="_blank">MODx Revolution-i dokumentatsioon</a>.</p><p>Vigade (bugs) teatamiseks palun kasutage <a href="http://bugs.modxcms.com/" target="_blank">MODx\'s JIRA Issue Tracker</a>-it.</p>';
+$_lang['help_title'] = 'Abi';
