934829676d35d554f48423c17bf521547973169a
Lauri L <lauri@laanepere.ee>
2010-09-19 17:30:00 +0800

    Translated content_type and context

---
+++ core/lexicon/et/content_type.inc.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Content Type Estonian lexicon topic
+ *
+ * @language et
+ * @package modx
+ * @subpackage lexicon
+ */
+$_lang['binary'] = 'Binaarne';
+$_lang['binary_desc'] = 'On see binaar või text/ascii fail?';
+$_lang['content_type'] = 'Content Type';
+$_lang['content_type_desc'] = 'Content Type-id on spetsiifilised failisüsteemi tüübid sinu ressurssidele. Siin võid neid muuta ja sammuti siduda neid faili laienditega.';
+$_lang['content_type_err_nf'] = 'Content type-i ei leitud.';
+$_lang['content_type_err_nfs'] = 'Content type-i ID-ga [[+id]] ei leitud.';
+$_lang['content_type_err_ns'] = 'Content type-i ei olnud määratud.';
+$_lang['content_type_err_ns_name'] = 'Palun määrake korrektne nimi content type-ile.';
+$_lang['content_type_err_create'] = 'Tekkis viga content type-i loomisel.';
+$_lang['content_type_err_remove'] = 'Tekkis viga content type-i eemaldamisel.';
+$_lang['content_type_err_save'] = 'Tekkis viga content type-i salvestamisel.';
+$_lang['content_type_new'] = 'Uus Content Type';
+$_lang['content_type_remove'] = 'Eemalda Content Type';
+$_lang['content_type_remove_confirm'] = 'Oled kindel, et soovid selle content type-i eemaldada?';
+$_lang['content_types'] = 'Content Type-id';
+$_lang['file_extensions'] = 'Faili laiendid';
