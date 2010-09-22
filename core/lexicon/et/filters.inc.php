2dc958ccb704f4672b0f9438912e00c6aa9489f5
Lauri L <lauri@laanepere.ee>
2010-09-22 17:31:13 +0800

    Translated file and filters

---
+++ core/lexicon/et/filters.inc.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Custom Filters Estonian lexicon topic
+ *
+ * @language et
+ * @package modx
+ * @subpackage lexicon
+ */
+$_lang['ago'] = '[[+time]] tagasi';
+$_lang['ago_day'] = '[[+time]] päev';
+$_lang['ago_days'] = '[[+time]] päeva';
+$_lang['ago_hour'] = '[[+time]] tund';
+$_lang['ago_hours'] = '[[+time]] tundi';
+$_lang['ago_minutes'] = '[[+time]] minutit';
+$_lang['ago_month'] = '[[+time]] kuu';
+$_lang['ago_months'] = '[[+time]] kuud';
+$_lang['ago_seconds'] = '[[+time]] sek';
+$_lang['ago_week'] = '[[+time]] nädal';
+$_lang['ago_weeks'] = '[[+time]] nädalat';
+$_lang['ago_year'] = '[[+time]] aasta';
+$_lang['ago_years'] = '[[+time]] aastat';
+$_lang['today_at'] = 'täna [[+time]]';
+$_lang['yesterday_at'] = 'eile [[+time]]';
