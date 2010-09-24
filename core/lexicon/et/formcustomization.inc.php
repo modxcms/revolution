18ceafe0c2afe290a8226496078fd80885812ad9
Lauri L <lauri@laanepere.ee>
2010-09-24 17:28:05 +0800

    Translated formcustomization and import

---
+++ core/lexicon/et/formcustomization.inc.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Form Customization Estonian lexicon topic
+ *
+ * @language et
+ * @package modx
+ * @subpackage lexicon
+ */
+$_lang['action'] = 'Tegevus';
+$_lang['action_desc'] = 'Tegevus millele see reegel kehtib.';
+$_lang['activate'] = 'Aktiveeri';
+$_lang['constraint'] = 'Constraint';
+$_lang['constraint_class'] = 'Constraint Class';
+$_lang['constraint_class_desc'] = 'Valikuline. Kui määratud koos Constraint Väljaga ja Constraint valikutega kasutab see reegel constrainte, mis määratud.';
+$_lang['constraint_desc'] = 'Valikuline. Constraint Välja väärtus, mille vastu kontrollida.';
+$_lang['constraint_field'] = 'Constraint Väli';
+$_lang['constraint_field_desc'] = 'Valikuline. Väli, mille kontrollimisel seda constrainti tuleks kasutada.';
+$_lang['containing_panel'] = 'Containimise Paneel';
+$_lang['containing_panel_desc'] = 'Contain vorm Paneeli ID milles väli asub. See on mõnikord vajalik kindlatele reeglitele, nii süsteem teab milline vorm või paneel on.';
+$_lang['deactivate'] = 'Deaktiveeri';
+$_lang['field'] = 'Väli';
+$_lang['field_desc'] = 'See on väli mid mõjutada. See võib olla ka tab või TV. Kui see on TV. siis palun määra sellises formaadis: "tv#", kus " on TV ID.';
+$_lang['field_default'] = 'Välja Vaikimisi Väärtus';
+$_lang['field_label'] = 'Välja nimi';
+$_lang['field_visible'] = 'Väli Nähtav';
+$_lang['form_customization_msg'] = 'Siin on nimekiri pragu kasutuses olevatest reeglitest. Rohkem informatsiooni Reeglite ja Vormide kohandamise kohta leiab <a href="http://svn.modxcms.com/docs/display/revolution/Form+Customization" target="_blank">siit</a>. Palun panetähele, et ebakorrketsed Reeglid võivad põhjustada probleeme sinu MODx Revolution installatsiooniga. Mitteaktiivsed Reeglid on hallid.';
+$_lang['form_rules'] = 'Vormi reeglid';
+$_lang['rule'] = 'Reegel';
+$_lang['rule_create'] = 'Loo Reegel';
+$_lang['rule_desc'] = 'Reegli tüüp, mis sellel väljal kasutatakse.';
+$_lang['rule_description_desc'] = 'Valikuline. Reegli kirjeldus.';
+$_lang['rule_err_ae'] = 'Reegel juba eksisteerib selle välja jaoks.';
+$_lang['rule_err_duplicate'] = 'Tekkis viga selle Reegli dubleerimisel.';
+$_lang['rule_err_nf'] = 'Reeglit ei leitud.';
+$_lang['rule_err_ns'] = 'Reeglit ei olnud määratud.';
+$_lang['rule_err_remove'] = 'Tekkis viga reegi eemaldamisel.';
+$_lang['rule_err_save'] = 'Tekkis viga reegli salvestamisel.';
+$_lang['rule_remove'] = 'Eemalda Reegel';
+$_lang['rule_remove_confirm'] = 'Oled kindel, et soovid eemalda selle Reegli?';
+$_lang['rule_remove_multiple'] = 'Eemalda Mitu Reeglit';
+$_lang['rule_remove_multiple_confirm'] = 'Oled kindel, et soovid eemaldada need reeglid? See tegevus on pöördumatu.';
+$_lang['rule_update'] = 'Uuenda reeglit';
+$_lang['rule_value_desc'] = 'Väärtus, mis määratakse Reeglis.';
+$_lang['rules'] = 'Reeglid';
+$_lang['tab_title'] = 'Tabi Tiitel';
+$_lang['tab_new'] = 'Uus Tab';
+$_lang['tab_visible'] = 'Tab Nähtav';
+$_lang['tv_default'] = 'TV Vaikimisi Väärtus';
+$_lang['tv_label'] = 'TV Nimi';
+$_lang['tv_move'] = 'Liiguta TV Tab-i';
+$_lang['tv_visible'] = 'TV Nähtav';
+$_lang['usergroup'] = 'Kasutaja Grupp';
+$_lang['usergroup_desc'] = 'Valikuline. Kui määratud, kehtib see reegel ainult kasutajatele kes on selles määratud Kasutaja Grupis.';
