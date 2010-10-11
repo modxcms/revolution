55c133a546d096afc62276eaceec6135fe2ae884
Shaun McCormick <shaun@modxcms.com>
2010-10-11 08:42:40 -0500

    [#MODX-2408] Fixed issue where sorting was not available for description field on search page

--- manager/assets/modext/widgets/modx.panel.search.js
+++ manager/assets/modext/widgets/modx.panel.search.js
@@ -161,18 +161,21 @@ MODx.grid.Search = function(config) {
         },{
             header: _('description')
             ,dataIndex: 'description'
+            ,sortable: true
         },{
             header: _('published')
             ,dataIndex: 'published'
             ,width: 30
             ,editor: { xtype: 'combo-boolean' ,renderer: 'boolean' }
             ,editable: false
+            ,sortable: true
         },{
             header: _('deleted')
             ,dataIndex: 'deleted'
             ,width: 30
             ,editor: { xtype: 'combo-boolean' ,renderer: 'boolean' }
             ,editable: false
+            ,sortable: true
         }]
     });
     MODx.grid.Search.superclass.constructor.call(this,config);
