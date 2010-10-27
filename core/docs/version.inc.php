d0968c595668da1a17c4184c6640e2e8e146cc40
Jason Coward <jason@modx.com>
2010-10-27 11:03:02 -0600

    Bump version for next planned release: v2.0.5-pl

--- core/docs/version.inc.php
+++ core/docs/version.inc.php
@@ -2,8 +2,8 @@
 $v= array ();
 $v['version']= '2'; // Current version.
 $v['major_version']= '0'; // Current major version.
-$v['minor_version']= '4'; // Current minor version.
-$v['patch_level']= 'pl2'; // Current patch level.
+$v['minor_version']= '5'; // Current minor version.
+$v['patch_level']= 'dev'; // Current patch level.
 $v['code_name']= 'Revolution'; // Current codename.
 $v['full_version']= $v['version'] . ($v['major_version'] ? ".{$v['major_version']}" : ".0") . ($v['minor_version'] ? ".{$v['minor_version']}" : ".0") . ($v['patch_level'] ? "-{$v['patch_level']}" : "");
 $v['full_appname']= 'MODx' . ($v['code_name'] ? " {$v['code_name']} " : " ") . $v['full_version'];
