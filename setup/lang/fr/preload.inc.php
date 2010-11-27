9f622cf83ef4690783d6e4d041059f59c7dccfe6
Romain <romain@meltingmedia.net>
2010-11-27 16:01:16 +0100

    translated new setup strings in french

--- setup/lang/fr/preload.inc.php
+++ setup/lang/fr/preload.inc.php
@@ -7,6 +7,4 @@
  */
 $_lang['preload_err_cache'] = 'Veuillez vérifier que le répertoire [[+path]]cache existe et est accessible en écriture par le processus PHP.';
 $_lang['preload_err_core_path'] = 'Veuillez vérifier que le MODX_CORE_PATH est correct dans le fichier setup/includes/config.core.php; celui-ci doit pointer vers une installation de MODx valide.';
-$_lang['preload_err_mysql'] = 'MODx requiert l\'extension mysql quand PHP est utilisé sans PDO en natif et il semblerait que celle-ci ne soit pas chargée.';
 $_lang['preload_err_pdo'] = 'MODx requiert l\'extension PDO quand PDO est utilisé en natif et il semblerait que celle-ci ne soit pas chargée.';
-$_lang['preload_err_pdo_mysql'] = 'MODx requiert le pilote pdo_mysql quand PDO est utilisé en natif et il semblerait que celui-ci ne soit pas chargé.';
