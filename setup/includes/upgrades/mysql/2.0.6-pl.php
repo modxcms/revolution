bacc611b92183832ca0bdd0dde28ddd21afcedd8
Shaun McCormick <shaun@modx.com>
2010-12-15 16:40:00 -0600

    [ReUp] [#3082] Fix Form Customization issue where cacheable and ID fields not able to be hidden/altered

---
+++ setup/includes/upgrades/mysql/2.0.6-pl.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Specific upgrades for Revolution 2.0.6
+ *
+ * @package setup
+ * @subpackage upgrades
+ */
+/* handle new class creation */
+$classes = array(
+);
+if (!empty($classes)) {
+    $this->createTable($classes);
+}
+unset($classes);
+
