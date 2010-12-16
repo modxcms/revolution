325e08b240f56edbce5a3b57327f49ca8f8e709b
Shaun McCormick <shaun@modx.com>
2010-12-16 09:07:18 -0600

    [#3091] Add missing specific dom ID to profile change password panel

--- manager/assets/modext/sections/security/profile/update.js
+++ manager/assets/modext/sections/security/profile/update.js
@@ -37,10 +37,12 @@ MODx.panel.Profile = function(config) {
             ,anchor: '100%'
         },MODx.getPageStructure([{
             xtype: 'modx-panel-profile-update'
+            ,id: 'modx-panel-profile-update'
             ,user: config.user
             ,preventRender: true
         },{
             xtype: 'modx-panel-profile-password-change'
+            ,id: 'modx-panel-profile-password-change'
             ,user: config.user
             ,preventRender: true
         },{
