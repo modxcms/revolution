b6329ad6c686d68fe71d09f7f9634685d3a5114f
Shaun McCormick <shaun@modx.com>
2011-01-14 09:22:01 -0600

    - [#3422], [#3374], [#3197] Fix issue with filemanager_url and Image/File TVs and their relative end result URLs

--- core/model/modx/processors/element/tv/renders/mgr/input/image.php
+++ core/model/modx/processors/element/tv/renders/mgr/input/image.php
@@ -8,17 +8,18 @@ $this->xpdo->lexicon->load('tv_widget');
 $modx->getService('fileHandler','modFileHandler', '', array('context' => $this->xpdo->context->get('key')));
 
 $isRelativeBaseUrl = $this->xpdo->getOption('filemanager_path_relative',null,true);
-if ($isRelativeBaseUrl) {
-    $basePath = $this->xpdo->fileHandler->getBasePath(false);
-    $baseUrl = $this->xpdo->fileHandler->getBaseUrl(true);
-    $value = $this->get('value');
-    if ($baseUrl != '/') {
-        $value = str_replace($baseUrl,'',$value);
-    }
-    $value = ltrim($value,'/');
-    $this->set('value',$value);
-    $this->xpdo->smarty->assign('tv',$this);
+
+/* strip out filemanager_url from value to get relativeValue */
+$basePath = $this->xpdo->fileHandler->getBasePath(false);
+$baseUrl = $this->xpdo->fileHandler->getBaseUrl(true);
+$value = $this->get('value');
+if ($baseUrl != '/') {
+    $value = str_replace($baseUrl,'',$value);
 }
+$value = ltrim($value,'/');    
+
+$this->set('relativeValue',$value);
+$this->xpdo->smarty->assign('tv',$this);
 
 // handles image fields using htmlarea image manager
 $this->xpdo->smarty->assign('base_url',$this->xpdo->getOption('base_url'));
