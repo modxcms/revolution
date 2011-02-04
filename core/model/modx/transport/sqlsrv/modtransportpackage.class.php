7efca3002955e08b0b2e0f597c0d7b5d56d886b9
Mike Schell <mike@webprogramming.ca>
2011-02-04 15:08:41 -0500

    Refactor package version query
    Query moved to new mothods in driver classes as
     modTransportPackage_mysql::listPackageVersions()
     modTransportPackage_sqlsrv::listPackageVersions()

--- core/model/modx/transport/sqlsrv/modtransportpackage.class.php
+++ core/model/modx/transport/sqlsrv/modtransportpackage.class.php
@@ -35,4 +35,28 @@ class modTransportPackage_sqlsrv extends modTransportPackage {
         $result['collection'] = $modx->getCollection('transport.modTransportPackage',$c);
         return $result;
     }
+
+    public static function listPackageVersions(modX &$modx, $criteria, $limit = 0, $offset = 0) {
+        $result = array('collection' => array(), 'total' => 0);
+        $c = $modx->newQuery('transport.modTransportPackage');
+        $c->select($modx->getSelectColumns('transport.modTransportPackage','modTransportPackage'));
+        $c->select(array('Provider.name AS provider_name'));
+        $c->leftJoin('transport.modTransportProvider','Provider');
+        $c->where($criteria);
+        $result['total'] = $modx->getCount('modTransportPackage',$c);
+        $c->sortby('modTransportPackage.version_major', 'DESC');
+        $c->sortby('modTransportPackage.version_minor', 'DESC');
+        $c->sortby('modTransportPackage.version_patch', 'DESC');
+        $c->sortby("CASE WHEN modTransportPackage.release = '' OR modTransportPackage.release = 'ga' OR modTransportPackage.release = 'pl'
+            THEN 'z'
+            ELSE modTransportPackage.release
+          END",'DESC');
+        $c->sortby('modTransportPackage.release_index', 'DESC');
+        if((int)$limit > 0) {
+            $c->limit((int)$limit, (int)$offset);
+        }
+        $result['collection'] = $modx->getCollection('transport.modTransportPackage',$c);
+        return $result;
+    }
+
 }
