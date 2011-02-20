5c2e5debc6072c13f72aef4e5d737031001bf06e
Jason Coward <jason@modx.com>
2011-02-20 15:40:35 -0700

    Bump version for 2.0.8-pl hotfix release

--- core/docs/version.inc.php
+++ core/docs/version.inc.php
@@ -2,7 +2,7 @@
 $v= array ();
 $v['version']= '2'; // Current version.
 $v['major_version']= '0'; // Current major version.
-$v['minor_version']= '7'; // Current minor version.
+$v['minor_version']= '8'; // Current minor version.
 $v['patch_level']= 'pl'; // Current patch level.
 $v['code_name']= 'Revolution'; // Current codename.
 $v['distro']= '@git@';
