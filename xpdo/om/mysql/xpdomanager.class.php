6c8f46bc4ec214ce6013a649843fa44759ce33a7
Jason Coward <jason@modx.com>
2011-03-09 14:52:47 -0700

    Fix bug in FIRST and AFTER options in xPDOManager_mysql->addField()
    The options were being appended to the SQL without a space causing SQL syntax errors in MySQL.

--- xpdo/om/mysql/xpdomanager.class.php
+++ xpdo/om/mysql/xpdomanager.class.php
@@ -266,9 +266,9 @@ class xPDOManager_mysql extends xPDOManager {
                 if (!empty($colDef)) {
                     $sql = "ALTER TABLE {$this->xpdo->getTableName($className)} ADD COLUMN {$this->xpdo->escape($name)} {$colDef}";
                     if (isset($options['first']) && !empty($options['first'])) {
-                        $sql .= "FIRST";
+                        $sql .= " FIRST";
                     } elseif (isset($options['after']) && array_key_exists($options['after'], $meta)) {
-                        $sql .= "AFTER {$this->xpdo->escape($options['after'])}";
+                        $sql .= " AFTER {$this->xpdo->escape($options['after'])}";
                     }
                     if ($this->xpdo->exec($sql)) {
                         $result = true;
