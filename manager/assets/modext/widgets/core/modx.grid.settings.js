0d05c06be2f4bcc5ea8ad99ad117d9cebd4e19b7
Shaun McCormick <shaun@modx.com>
2011-03-31 15:25:53 -0500

    Fix issue with settings.grids and grouping

--- manager/assets/modext/widgets/core/modx.grid.settings.js
+++ manager/assets/modext/widgets/core/modx.grid.settings.js
@@ -135,6 +135,7 @@ MODx.grid.SettingsGrid = function(config) {
         ,groupBy: 'area_text'
         ,singleText: _('setting')
         ,pluralText: _('settings')
+        ,sortBy: 'key'
         ,plugins: this.exp
         ,primaryKey: 'key'
         ,autosave: true
