b28488353d604c65649c5dd44a778ca1a295056d
Shaun McCormick <shaun@modx.com>
2011-04-04 09:31:00 -0500

    [#4270] Fix issue where images could not be removed when using a custom basePath TV

--- core/model/modx/processors/browser/file/remove.php
+++ core/model/modx/processors/browser/file/remove.php
@@ -14,6 +14,7 @@ $modx->lexicon->load('file');
 
 if (empty($scriptProperties['file'])) return $modx->error->failure($modx->lexicon('file_err_ns'));
 
+
 /* get working context */
 $wctx = isset($scriptProperties['wctx']) && !empty($scriptProperties['wctx']) ? $scriptProperties['wctx'] : '';
 if (!empty($wctx)) {
@@ -27,16 +28,23 @@ if (!empty($wctx)) {
 
 $modx->getService('fileHandler','modFileHandler', '', array('context' => $workingContext->get('key')));
 
-/* in case rootVisible is true */
-$file = str_replace('root/','',$scriptProperties['file']);
-$file = str_replace('undefined/','',$file);
-
-/* create modFile object */
-$root = $modx->fileHandler->getBasePath(false);
-if ($workingContext->getOption('filemanager_path_relative',true)) {
-    $root = $workingContext->getOption('base_path','').$root;
+/* get base paths and sanitize incoming paths */
+$dir = $modx->fileHandler->sanitizePath($scriptProperties['file']);
+if (empty($scriptProperties['basePath'])) {
+    $root = $modx->fileHandler->getBasePath();
+    if ($workingContext->getOption('filemanager_path_relative',true)) {
+        $root = $workingContext->getOption('base_path','').$root;
+    }
+} else {
+    $root = $scriptProperties['basePath'];
+    if (!empty($scriptProperties['basePathRelative'])) {
+        $root = $workingContext->getOption('base_path').$root;
+    }
 }
-$file = $modx->fileHandler->make($root.$file);
+$fullPath = $root.ltrim($dir,'/');
+if (!file_exists($fullPath)) return $modx->error->failure($modx->lexicon('file_folder_err_ns').': '.$fullPath);
+
+$file = $modx->fileHandler->make($fullPath);
 
 /* verify file exists and is writable */
 if (!$file->exists()) {
