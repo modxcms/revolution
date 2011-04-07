49c96cc4726e8045ed5bb2e4a2a3fee5cd8f94f7
Jan Tezner <jan.tezner@gmail.com>
2011-04-07 06:38:50 +0200

    Update of czech translation

--- core/lexicon/cs/about.inc.php
+++ core/lexicon/cs/about.inc.php
@@ -7,12 +7,12 @@
  * @subpackage lexicon
  *
  * @author modxcms.cz
- * @updated 2010-07-17
+ * @updated 2011-04-06
  */
 $_lang['about_msg'] = 'MODX je <a href="http://modxcms.com/what-is-modx.html" target="_blank">PHP aplikační framework a správce obsahu (CMS)</a> licencovaný pod <a href="http://opensource.org/licenses/gpl-2.0.php">GNU Public License v2</a> a každou pozdější verzí (dle vaší volby).';
 $_lang['about_title'] = 'O MODX';
 $_lang['credits'] = 'Kdo za tím stojí';
-$_lang['credits_mysql'] = 'MODX běží na databázi MySQL!';
+$_lang['credits_database'] = 'MODX běží na databázi <a href="http://www.mysql.com/" target="_blank">MySQL</a> nebo <a href="http://www.microsoft.com/sqlserver/" target="_blank">Microsoft SQL Server</a>';
 $_lang['credits_php'] = 'MODX běží na PHP!';
 $_lang['credits_sencha'] = 'MODX používá <a href="http://www.sencha.com/" target="_blank">Sencha (ExtJS)</a> ve výchozím manager UI.';
 $_lang['credits_smarty'] = 'MODX používá <a href="http://www.smarty.net/" target="_blank">Smarty</a> ve výchozím manager UI.';
