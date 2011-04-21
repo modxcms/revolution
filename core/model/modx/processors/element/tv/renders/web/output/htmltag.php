895777c24ecbcb62ba3b334aaf89f5d40d1d4d2e
splittingred <shaun@modxcms.com>
2011-04-21 11:32:53 -0500

    [#4373] Fix issue with htmltag TV output render and empty values

--- core/model/modx/processors/element/tv/renders/web/output/htmltag.php
+++ core/model/modx/processors/element/tv/renders/web/output/htmltag.php
@@ -7,6 +7,7 @@
 $value= $this->parseInput($value, "||", "array");
 $tagid = $params['tagid'];
 $tagname = ($params['tagname'])? $params['tagname']:'div';
+$o = '';
 /* Loop through a list of tags */
 for ($i = 0; $i < count($value); $i++) {
     $tagvalue = is_array($value[$i]) ? implode(' ', $value[$i]) : $value[$i];
@@ -27,5 +28,16 @@ for ($i = 0; $i < count($value); $i++) {
     /* Output the HTML Tag */
     $o .= '<'.$tagname.rtrim($attributes).'>'.$tagvalue.'</'.$tagname.'>';
 }
+if (empty($o)) {
+    $attributes = '';
+    $attr = array(
+        'class' => $params['class'],
+        'style' => $params['style'],
+    );
+    foreach ($attr as $k => $v) $attributes.= ($v ? ' '.$k.'="'.$v.'"' : '');
+    $attributes .= ' '.$params['attrib']; /* add extra */
 
+    /* Output the HTML Tag */
+    $o .= '<'.$tagname.rtrim($attributes).'>'.$tagvalue.'</'.$tagname.'>';
+}
 return $o;
