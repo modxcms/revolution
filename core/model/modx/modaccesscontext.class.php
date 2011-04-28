f8a7d1be1834b7b3554065a48632bc9d00f23621
Jason Coward <jason@modx.com>
2011-04-28 16:00:14 -0600

    Have modAccess*::loadAttributes() check access_*_enabled settings

--- core/model/modx/modaccesscontext.class.php
+++ core/model/modx/modaccesscontext.class.php
@@ -8,46 +8,52 @@ class modAccessContext extends modAccess {
         if (empty($context)) {
             $context = $modx->context->get('key');
         }
-        $accessTable = $modx->getTableName('modAccessContext');
-        $policyTable = $modx->getTableName('modAccessPolicy');
-        $memberTable = $modx->getTableName('modUserGroupMember');
-        $memberRoleTable = $modx->getTableName('modUserGroupRole');
-        if ($userId > 0) {
-            $sql = "SELECT acl.target, acl.principal, mr.authority, acl.policy, p.data FROM {$accessTable} acl " .
-                    "LEFT JOIN {$policyTable} p ON p.id = acl.policy " .
-                    "JOIN {$memberTable} mug ON acl.principal_class = 'modUserGroup' " .
-                    "AND mug.member = :principal " .
-                    "AND mug.user_group = acl.principal " .
-                    "JOIN {$memberRoleTable} mr ON mr.id = mug.role " .
-                    "AND mr.authority <= acl.authority " .
-                    "ORDER BY acl.target, acl.principal, mr.authority, acl.policy";
-            $bindings = array(
-                ':principal' => $userId
-            );
-            $query = new xPDOCriteria($modx, $sql, $bindings);
-            if ($query->stmt && $query->stmt->execute()) {
-                while ($row = $query->stmt->fetch(PDO::FETCH_ASSOC)) {
-                    $attributes[$row['target']][] = array(
-                        'principal' => $row['principal'],
-                        'authority' => $row['authority'],
-                        'policy' => $row['data'] ? $modx->fromJSON($row['data'], true) : array(),
-                    );
+        $enabled = (boolean) $modx->getOption('access_context_enabled', null, true);
+        if ($context !== $modx->context->get('key') && $modx->getContext($context)) {
+            $enabled = (boolean) $modx->contexts[$context]->getOption('access_context_enabled', $enabled);
+        }
+        if ($enabled) {
+            $accessTable = $modx->getTableName('modAccessContext');
+            $policyTable = $modx->getTableName('modAccessPolicy');
+            $memberTable = $modx->getTableName('modUserGroupMember');
+            $memberRoleTable = $modx->getTableName('modUserGroupRole');
+            if ($userId > 0) {
+                $sql = "SELECT acl.target, acl.principal, mr.authority, acl.policy, p.data FROM {$accessTable} acl " .
+                        "LEFT JOIN {$policyTable} p ON p.id = acl.policy " .
+                        "JOIN {$memberTable} mug ON acl.principal_class = 'modUserGroup' " .
+                        "AND mug.member = :principal " .
+                        "AND mug.user_group = acl.principal " .
+                        "JOIN {$memberRoleTable} mr ON mr.id = mug.role " .
+                        "AND mr.authority <= acl.authority " .
+                        "ORDER BY acl.target, acl.principal, mr.authority, acl.policy";
+                $bindings = array(
+                    ':principal' => $userId
+                );
+                $query = new xPDOCriteria($modx, $sql, $bindings);
+                if ($query->stmt && $query->stmt->execute()) {
+                    while ($row = $query->stmt->fetch(PDO::FETCH_ASSOC)) {
+                        $attributes[$row['target']][] = array(
+                            'principal' => $row['principal'],
+                            'authority' => $row['authority'],
+                            'policy' => $row['data'] ? $modx->fromJSON($row['data'], true) : array(),
+                        );
+                    }
                 }
-            }
-        } else {
-            $sql = "SELECT acl.target, acl.principal, 0 AS authority, acl.policy, p.data FROM {$accessTable} acl " .
-                    "LEFT JOIN {$policyTable} p ON p.id = acl.policy " .
-                    "WHERE acl.principal_class = 'modUserGroup' " .
-                    "AND acl.principal = 0 " .
-                    "ORDER BY acl.target, acl.principal, acl.authority, acl.policy";
-            $query = new xPDOCriteria($modx, $sql);
-            if ($query->stmt && $query->stmt->execute()) {
-                while ($row = $query->stmt->fetch(PDO::FETCH_ASSOC)) {
-                    $attributes[$row['target']][] = array(
-                        'principal' => 0,
-                        'authority' => $row['authority'],
-                        'policy' => $row['data'] ? $modx->fromJSON($row['data'], true) : array(),
-                    );
+            } else {
+                $sql = "SELECT acl.target, acl.principal, 0 AS authority, acl.policy, p.data FROM {$accessTable} acl " .
+                        "LEFT JOIN {$policyTable} p ON p.id = acl.policy " .
+                        "WHERE acl.principal_class = 'modUserGroup' " .
+                        "AND acl.principal = 0 " .
+                        "ORDER BY acl.target, acl.principal, acl.authority, acl.policy";
+                $query = new xPDOCriteria($modx, $sql);
+                if ($query->stmt && $query->stmt->execute()) {
+                    while ($row = $query->stmt->fetch(PDO::FETCH_ASSOC)) {
+                        $attributes[$row['target']][] = array(
+                            'principal' => 0,
+                            'authority' => $row['authority'],
+                            'policy' => $row['data'] ? $modx->fromJSON($row['data'], true) : array(),
+                        );
+                    }
                 }
             }
         }
