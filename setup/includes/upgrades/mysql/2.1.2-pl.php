7288ed3035b92c600eb355da81ccdd05b1e8524e
Jason Coward <jason@modx.com>
2011-06-17 15:09:26 -0600

    [#5072] Add missing primary key index to modEvent

---
+++ setup/includes/upgrades/mysql/2.1.2-pl.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Specific upgrades for Revolution 2.1.2-pl
+ *
+ * @package setup
+ * @subpackage upgrades
+ */
+/* handle new class creation */
+$classes = array(
+);
+if (!empty($classes)) {
+    $this->createTable($classes);
+}
+unset($classes);
+
+/* [#5072] add missing primary key on modEvent */
+$class = 'modEvent';
+$table = $this->install->xpdo->getTableName($class);
+$description = $this->install->lexicon('drop_index',array('index' => 'name','table' => $table));
+$this->processResults($class, $description, array($modx->manager, 'removeIndex'), array($class, 'name'));
+
+$description = $this->install->lexicon('change_default_value',array('column' => 'name', 'value'=>'NULL', 'table' => $table));
+$this->processResults($class, $description, array($modx->manager, 'alterField'), array($class, 'name'));
+
+$description = $this->install->lexicon('add_index',array('index' => 'PRIMARY','table' => $table));
+$this->processResults($class, $description, array($modx->manager, 'addIndex'), array($class, 'PRIMARY'));
