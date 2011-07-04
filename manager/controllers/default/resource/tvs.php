a1cebd1db56d47e771db20c6fa8b8e0aca90ab4c
Adam Lundrigan <adamlundrigan@cdli.ca>
2011-07-04 13:41:08 -0230

    Bug #5164 Fatal error when TV uses non-existent category ID

--- manager/controllers/default/resource/tvs.php
+++ manager/controllers/default/resource/tvs.php
@@ -120,12 +120,14 @@ if ($templateId && ($template = $modx->getObject('modTemplate', $templateId))) {
 $tvCounts = array();
 $finalCategories = array();
 foreach ($categories as $n => $category) {
-    $category->hidden = empty($category->tvCount) ? true : false;
-    $ct = count($category->tvs);
-    if ($ct > 0) {
-        $finalCategories[$category->get('id')] = $category;
-        $tvCounts[$n] = $ct;
-    }
+    if ($category instanceof modCategory) {
+        $category->hidden = empty($category->tvCount) ? true : false;
+        $ct = count($category->tvs);
+        if ($ct > 0) {
+            $finalCategories[$category->get('id')] = $category;
+            $tvCounts[$n] = $ct;
+        }
+    }    
 }
 $onResourceTVFormRender = $modx->invokeEvent('OnResourceTVFormRender',array(
     'categories' => &$finalCategories,
