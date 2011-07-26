fa42a703e5842f61bece3476968d9bf27d1b258c
Shaun McCormick <shaun@modx.com>
2011-07-26 17:24:32 -0500

    Fixed some issues with layouts, styling, and modManagerController tweaks

--- manager/assets/modext/sections/welcome.js
+++ manager/assets/modext/sections/welcome.js
@@ -12,10 +12,6 @@ MODx.page.Welcome = function(config) {
         components: [{
             xtype: 'modx-panel-welcome'
             ,renderTo: 'modx-panel-welcome-div'
-            ,displayConfigCheck: config.displayConfigCheck
-            ,user: MODx.user.id
-            ,newsEnabled: config.newsEnabled
-            ,securityEnabled: config.securityEnabled
         }]
     });
     MODx.page.Welcome.superclass.constructor.call(this,config);
