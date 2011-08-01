af7e07f134b2ba2505141ca878dd3e2f49e3e180
Shaun McCormick <shaun@modx.com>
2011-08-01 14:47:14 -0500

    Add base tests for modTemplate/modTemplateVar

--- _build/test/Tests/Model/Element/modTemplateTest.php
+++ _build/test/Tests/Model/Element/modTemplateTest.php
@@ -31,4 +31,73 @@
  * @group modElement
  * @group modTemplate
  */
-class modTemplateTest extends MODxTestCase {}
+class modTemplateTest extends MODxTestCase {
+    /** @var modTemplate $template */
+    public $template;
+
+    public function setUp() {
+        parent::setUp();
+        $this->template = $this->modx->newObject('modTemplate');
+        $this->template->fromArray(array(
+            'id' => 12345,
+            'templatename' => 'Unit Test Template',
+            'description' => 'A template for unit testing.',
+            'content' => '<p>Hello, [[+name]]!</p>',
+            'category' => 0,
+            'locked' => 0,
+        ),'',true,true);
+        $this->template->setProperties(array('name' => 'John'));
+        $this->template->setCacheable(false);
+    }
+    public function tearDown() {
+        parent::tearDown();
+        $this->template = null;
+    }
+
+    /**
+     * @return void
+     */
+    public function testGetContent() {
+        $this->assertEquals($this->template->get('content'),$this->template->getContent());
+    }
+
+    /**
+     * @param string $content
+     * @dataProvider providerSetContent
+     * @depends testGetContent
+     */
+    public function testSetContent($content) {
+        $this->template->setContent($content);
+        $this->assertEquals($content,$this->template->get('content'));
+    }
+    /**
+     * @return array
+     */
+    public function providerSetContent() {
+        return array(
+            array('Test content.'),
+        );
+    }
+
+    /**
+     * @param string $expected
+     * @param array $properties
+     * @param null|string $content
+     * @dataProvider providerProcess
+     */
+    public function testProcess($expected,array $properties = array(),$content = null) {
+        $result = $this->template->process($properties,$content);
+        $this->assertEquals($expected,$result);
+    }
+    /**
+     * @return array
+     */
+    public function providerProcess() {
+        return array(
+            array('<p>Hello, John!</p>'),
+            array('<p>Hello, Mark!</p>',array('name' => 'Mark')),
+            array('<p>Having fun.</p>',array(),'<p>Having fun.</p>'),
+            array('<p>Test 1</p>',array('number' => 1),'<p>Test [[+number]]</p>'),
+        );
+    }
+}
--- _build/test/Tests/Model/Element/modTemplateVarTest.php
+++ _build/test/Tests/Model/Element/modTemplateVarTest.php
@@ -29,7 +29,76 @@
  * @group Model
  * @group Element
  * @group modElement
- * @group modTemplate
  * @group modTemplateVar
  */
-class modTemplateVarTest extends MODxTestCase {}
+class modTemplateVarTest extends MODxTestCase {
+    /** @var modTemplateVar $tv */
+    public $tv;
+
+    public function setUp() {
+        parent::setUp();
+        $this->tv = $this->modx->newObject('modTemplateVar');
+        $this->tv->fromArray(array(
+            'id' => 12345,
+            'name' => 'Unit Test Template Var',
+            'caption' => 'UTTV',
+            'description' => 'A template variable for unit testing.',
+            'default_text' => '<p>Hello, [[+name]]!</p>',
+            'category' => 0,
+            'locked' => 0,
+        ),'',true,true);
+        $this->tv->setProperties(array('name' => 'John'));
+        $this->tv->setCacheable(false);
+    }
+    public function tearDown() {
+        parent::tearDown();
+        $this->tv = null;
+    }
+
+    /**
+     * @return void
+     */
+    public function testGetContent() {
+        $this->assertEquals($this->tv->get('default_text'),$this->tv->getContent());
+    }
+
+    /**
+     * @param string $content
+     * @dataProvider providerSetContent
+     * @depends testGetContent
+     */
+    public function testSetContent($content) {
+        $this->tv->setContent($content);
+        $this->assertEquals($content,$this->tv->get('default_text'));
+    }
+    /**
+     * @return array
+     */
+    public function providerSetContent() {
+        return array(
+            array('Test content.'),
+        );
+    }
+
+    /**
+     * @param string $expected
+     * @param array $properties
+     * @param null|string $content
+     * @dataProvider providerProcess
+     */
+    public function testProcess($expected,array $properties = array(),$content = null) {
+        $result = $this->tv->process($properties,$content);
+        $this->assertEquals($expected,$result);
+    }
+    /**
+     * @return array
+     */
+    public function providerProcess() {
+        return array(
+            array('<p>Hello, John!</p>'),
+            array('<p>Hello, Mark!</p>',array('name' => 'Mark')),
+            //array('<p>Having fun.</p>',array(),'<p>Having fun.</p>'),
+            //array('<p>Test 1</p>',array('number' => 1),'<p>Test [[+number]]</p>'),
+        );
+    }
+}
