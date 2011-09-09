c3df755eee362e8cb5b8703b3a0ff763435a69a6
Shaun McCormick <shaun@modx.com>
2011-09-09 10:06:25 -0500

    [#5671] Add :toPlaceholder, :cssToHead, :htmlToHead, :htmlToBottom, :jsToHead, :jsToBottom output filters

--- _build/test/Tests/Model/Filters/modOutputFilterTest.php
+++ _build/test/Tests/Model/Filters/modOutputFilterTest.php
@@ -22,7 +22,7 @@
  * @package modx-test
  */
 /**
- * Tests related to the modInputFilter class.
+ * Tests related to the modOutputFilter class, including testing of core output filters.
  *
  * @package modx-test
  * @subpackage modx
@@ -31,7 +31,169 @@
  * @group modOutputFilter
  */
 class modOutputFilterTest extends MODxTestCase {
-    public function testExample() {
-        $this->assertTrue(true);
+    /** @var modPlaceholderTag $tag */
+    public $tag;
+
+    public function setUp() {
+        parent::setUp();
+        $this->modx->getParser();
+        $this->tag = new modPlaceholderTag($this->modx);
+        $this->tag->setCacheable(false);
+        $this->tag->set('name','utp');
+    }
+
+    /**
+     * Test :cssToHead filter that adds CSS to HEAD of a page
+     * 
+     * @param string $value
+     * @param boolean $addTag
+     * @dataProvider providerCssToHead
+     */
+    public function testCssToHead($value,$addTag) {
+        $this->modx->setPlaceholder('utp',$value);
+        $this->tag->set('name','utp:cssToHead');
+        $this->tag->process();
+        if ($addTag) {
+            $value = '<link rel="stylesheet" href="'.$value.'" type="text/css" />';
+        }
+        $this->assertContains($value,$this->modx->sjscripts);
+        unset($this->modx->sjscripts[$value]);
+    }
+    /**
+     * @return array
+     */
+    public function providerCssToHead() {
+        return array(
+            array('assets/css/style.css',true),
+            array('<link rel="stylesheet" href="assets/css/style.css" type="text/css" />',false),
+        );
+    }
+
+    /**
+     * Test :htmlToHead filter that adds HTML to the HEAD of a page
+     * 
+     * @param string $value
+     * @dataProvider providerHtmlToHead
+     */
+    public function testHtmlToHead($value) {
+        $this->modx->setPlaceholder('utp',$value);
+        $this->tag->set('name','utp:htmlToHead');
+        $this->tag->process();
+        $this->assertContains($value,$this->modx->sjscripts);
+        unset($this->modx->sjscripts[$value]);
+    }
+    /**
+     * @return array
+     */
+    public function providerHtmlToHead() {
+        return array(
+            array('<style>'),
+        );
+    }
+
+    /**
+     * Test :htmlToBottom filter that adds HTML to the bottom of a page
+     *
+     * @param string $value
+     * @dataProvider providerHtmlToBottom
+     */
+    public function testHtmlToBottom($value) {
+        $this->modx->setPlaceholder('utp',$value);
+        $this->tag->set('name','utp:htmlToBottom');
+        $this->tag->process();
+        $this->assertContains($value,$this->modx->jscripts);
+        unset($this->modx->jscripts[$value]);
+    }
+    /**
+     * @return array
+     */
+    public function providerHtmlToBottom() {
+        return array(
+            array('<footer>'),
+        );
+    }
+
+    /**
+     * Test :jsToBottom filter that adds JS to the bottom of a page
+     *
+     * @param string $value
+     * @param boolean $addTag
+     * @param boolean $plainText
+     * @dataProvider providerJsToBottom
+     */
+    public function testJsToBottom($value,$addTag = false,$plainText = false) {
+        $this->modx->setPlaceholder('utp',$value);
+        $this->tag->set('name','utp:jsToBottom=`'.($plainText ? 1 : 0).'`');
+        $this->tag->process();
+        if ($addTag) {
+            $value = '<script type="text/javascript" src="'.$value.'"></script>';
+        }
+        $this->assertContains($value,$this->modx->jscripts);
+        unset($this->modx->jscripts[$value]);
+    }
+    /**
+     * @return array
+     */
+    public function providerJsToBottom() {
+        return array(
+            array('assets/js/script.js',true,false),
+            array('<script type="text/javascript" src="assets/js/script2.js"></script>',false,false),
+            array('assets/js/script3.js',false,true),
+        );
+    }
+
+    /**
+     * Test :jsToHead filter that adds JS to the HEAD of a page
+     *
+     * @param string $value
+     * @param boolean $addTag
+     * @param boolean $plainText
+     * @dataProvider providerJsToHead
+     */
+    public function testJsToHead($value,$addTag = false,$plainText = false) {
+        $this->modx->setPlaceholder('utp',$value);
+        $this->tag->set('name','utp:jsToHead=`'.($plainText ? 1 : 0).'`');
+        $this->tag->process();
+        if ($addTag) {
+            $value = '<script type="text/javascript" src="'.$value.'"></script>';
+        }
+        $this->assertContains($value,$this->modx->sjscripts);
+        unset($this->modx->sjscripts[$value]);
+    }
+    /**
+     * @return array
+     */
+    public function providerJsToHead() {
+        return array(
+            array('assets/js/hscript.js',true,false),
+            array('<script type="text/javascript" src="assets/js/hscript2.js"></script>',false,false),
+            array('assets/js/hscript3.js',false,true),
+        );
+    }
+
+    /**
+     * Test :toPlaceholder=`phName` filter
+     * 
+     * @param string $toPlaceholder
+     * @param mixed $value
+     * @dataProvider providerToPlaceholder
+     */
+    public function testToPlaceholder($toPlaceholder,$value) {
+        $this->modx->setPlaceholder('utp',$value);
+        $this->tag->set('name','utp:toPlaceholder=`'.$toPlaceholder.'`');
+        $this->tag->process();
+        $this->assertArrayHasKey($toPlaceholder,$this->modx->placeholders);
+        if (isset($this->modx->placeholders[$toPlaceholder])) {
+            $this->assertEquals($value,$this->modx->placeholders[$toPlaceholder]);
+        }
+    }
+    /**
+     * @return array
+     */
+    public function providerToPlaceholder() {
+        return array(
+            array('myPlaceholder','Test'),
+            array('emptyPlaceholder',''),
+        );
     }
 }
--- core/model/modx/filters/modoutputfilter.class.php
+++ core/model/modx/filters/modoutputfilter.class.php
@@ -544,6 +544,37 @@ class modOutputFilter {
                             $output = urldecode($output);
                             break;
 
+                        case '':
+                            /*
+                            :csstohead $modx->regClientCSS($m_val);
+:htmltobottom $modx->regClientHTMLBlock($m_val);
+:htmltohead $modx->regClientStartupHTMLBlock($m_val);
+:jslinktobottom $modx->regClientScript($m_val)
+*/
+
+                            break;
+                        case 'toPlaceholder':
+                            $this->modx->toPlaceholder($m_val,$output);
+                            break;
+                        case 'cssToHead':
+                            $this->modx->regClientCSS($output);
+                            break;
+                        case 'htmlToHead':
+                            $this->modx->regClientStartupHTMLBlock($output);
+                            break;
+                        case 'htmlToBottom':
+                            $this->modx->regClientHTMLBlock($output);
+                            break;
+                        case 'jsToHead':
+                            if (empty($m_val)) $m_val = false;
+                            $this->modx->regClientStartupScript($output,$m_val);
+                            break;
+                        case 'jsToBottom':
+                            if (empty($m_val)) $m_val = false;
+                            $this->modx->regClientScript($output,$m_val);
+                            break;
+
+
                         /* Default, custom modifier (run snippet with modifier name) */
                         default:
                             /*@todo Possibility to only look for snippet names prefixed with 'filter:' */
