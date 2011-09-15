15b2699fe678643012864e199f754ce5bdaf501d
Jason Coward <jason@modx.com>
2011-09-15 12:28:06 -0600

    Add showNone option to source/getlist processor

--- core/model/modx/processors/source/getlist.php
+++ core/model/modx/processors/source/getlist.php
@@ -1,9 +1,7 @@
 <?php
 /**
- * Gets a list of dashboards
+ * Gets a list of Media Sources
  *
- * @param string $username (optional) Will filter the grid by searching for this
- * username
  * @param integer $start (optional) The record to start at. Defaults to 0.
  * @param integer $limit (optional) The number of records to limit to. Defaults
  * to 10.
@@ -15,7 +13,7 @@
  * @var modProcessor $this
  * 
  * @package modx
- * @subpackage processors.security.user
+ * @subpackage processors.source
  */
 if (!$modx->hasPermission('source_view')) {
     return $modx->error->failure($modx->lexicon('permission_denied'));
@@ -28,8 +26,9 @@ $start = $modx->getOption('start',$scriptProperties,0);
 $limit = $modx->getOption('limit',$scriptProperties,10);
 $sort = $modx->getOption('sort',$scriptProperties,'name');
 $dir = $modx->getOption('dir',$scriptProperties,'ASC');
+$showNone = $modx->getOption('showNone',$scriptProperties,false);
 
-/* query for users */
+/* query for sources */
 $c = $modx->newQuery('sources.modMediaSource');
 if (!empty($scriptProperties['query'])) {
     $c->where(array('modMediaSource.name:LIKE' => '%'.$scriptProperties['query'].'%'));
@@ -45,8 +44,15 @@ $canEdit = $modx->hasPermission('source_edit');
 $canSave = $modx->hasPermission('source_save');
 $canRemove = $modx->hasPermission('source_delete');
 
-/* iterate through users */
+/* iterate through sources */
 $list = array();
+if ($showNone) {
+    $list[] = array(
+        'id' => 0,
+        'name' => $modx->lexicon('none'),
+        'description' => $modx->lexicon('none'),
+    );
+}
 /** @var modMediaSource $source */
 foreach ($sources as $source) {
     if (!$source->checkPolicy('list')) continue;
