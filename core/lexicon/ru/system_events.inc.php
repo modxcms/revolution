eda488444c13395ac6a55e04ecaef065bd3e5872
yama <yamamoto@kyms.ne.jp>
2011-09-28 21:58:28 +0900

    不要な言語ファイルを削除

--- core/lexicon/ru/system_events.inc.php
+++
@@ -1,6 +0,0 @@
-<?php
-$_lang['clear'] = 'Очистить';
-$_lang['error_log'] = 'Журнал ошибок';
-$_lang['error_log_desc'] = 'Журнал ошибок MODX Revolution:';
-$_lang['system_events'] = 'Системные события';
-$_lang['priority'] = 'Приоритетность';
