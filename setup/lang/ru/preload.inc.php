eceb953d8ad4a8c2f148ad906b911cb20bf49679
yama <yamamoto@kyms.ne.jp>
2011-09-28 22:02:06 +0900

    不要な言語ファイルを削除

--- setup/lang/ru/preload.inc.php
+++
@@ -1,4 +0,0 @@
-<?php
-$_lang['preload_err_cache'] = 'Удостоверьтесь, что каталог [[+path]] существует и доступен для записи процессами PHP.';
-$_lang['preload_err_core_path'] = 'Удостоверьтесь, что в файле  setup/includes/config.core.php правильно определена переменная MODX_CORE_PATH. Она должна указывать на ядро MODx.';
-$_lang['preload_err_pdo'] = 'MODx требует  PHP расширение PDO при использовании PHP без родного PDO и оно не загружено.';
