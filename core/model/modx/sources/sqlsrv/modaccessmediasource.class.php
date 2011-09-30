261c9d32ee6a8aaf2346d5a099818eceb26b58dc
Jason Coward <jason@modx.com>
2011-09-30 10:04:22 -0600

    Add missing modAccessMediaSource object to sqlsrv schema

---
+++ core/model/modx/sources/sqlsrv/modaccessmediasource.class.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @package modx
+ * @subpackage sources.sqlsrv
+ */
+require_once (dirname(dirname(__FILE__)) . '/modaccessmediasource.class.php');
+/**
+ * @package modx
+ * @subpackage sources.sqlsrv
+ */
+class modAccessMediaSource_sqlsrv extends modAccessMediaSource {
+}
