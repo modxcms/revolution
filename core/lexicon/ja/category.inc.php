4cce80cf282008950db24e77fe8c9af9f4db2cfe
yama <yamamoto@kyms.ne.jp>
2011-10-08 21:52:26 +0900

    Japanese core-lexicon files new entry merged.

--- core/lexicon/ja/category.inc.php
+++ core/lexicon/ja/category.inc.php
@@ -12,6 +12,7 @@
 $_lang['categories'] = 'カテゴリー';
 $_lang['category_confirm_delete'] = 'カテゴリーを削除しますか？このカテゴリーに属している全てのエレメントは「カテゴリーなし」に分類されます。';
 $_lang['category_create'] = 'カテゴリーを作成';
+$_lang['category_err_ae'] = 'A category already exists with that name. Please try a different name.';
 $_lang['category_err_create'] = 'カテゴリーの作成中にエラーが発生しました。';
 $_lang['category_err_not_found'] = 'カテゴリーが見つかりませんでした。';
 $_lang['category_err_nf'] = 'カテゴリーが見つかりませんでした。.';
