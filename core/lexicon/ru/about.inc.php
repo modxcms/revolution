a4675be0384f9c99af8682a1d5426631fee696ea
yama <yamamoto@kyms.ne.jp>
2011-10-10 11:52:25 +0900

    Russian lexicon files entry merged. (for Revolution 2.2)

--- core/lexicon/ru/about.inc.php
+++ core/lexicon/ru/about.inc.php
@@ -1,11 +1,11 @@
 <?php
-$_lang['about_msg'] = 'MODx - это <a href="http://modxcms.com/what-is-modx.html" target="_blank">система для разработки и управления веб-сайтами</a>, распространяемая по лицензии <a href="http://opensource.org/licenses/gpl-2.0.php">GNU Public License v2</a> и по более поздним версиям этой лицензии.';
+$_lang['about_msg'] = 'MODX - это <a href="http://modx.com" target="_blank">система для разработки и управления веб-сайтами</a>, распространяемая по лицензии <a href="http://opensource.org/licenses/gpl-2.0.php">GNU Public License v2</a> и по более поздним версиям этой лицензии.';
 $_lang['about_title'] = 'О MODX';
-$_lang['credits'] = 'Команда MODx';
-$_lang['credits_mysql'] = 'MODx использует базу данных MySQL!';
-$_lang['credits_php'] = 'MODx написан на <a href="http://www.php.net" target="_blank">PHP</a>!';
-$_lang['credits_sencha'] = 'MODx использует <a href="http://www.sencha.com/" target="_blank">Sencha (ExtJS)</a>.';
-$_lang['credits_smarty'] = 'MODx использует <a href="http://www.smarty.net/" target="_blank">Smarty</a>.';
-$_lang['credits_xpdo'] = 'MODx использует технологию преобразования объектов в реляционные модели баз данных <a href="http://www.xpdo.org/" target="_blank">xPDO</a>.';
-$_lang['help_msg'] = '<p>Вы можете получить поддержку от сообщества <a href="http://modxcms.com/forums" target="_blank">на форумах MODx</a>. Советуем почитать <a href="http://svn.modxcms.com/docs/display/revolution" target="_blank">документацию MODx Revolution</a>.</p><p>Если Вы нашли ошибку, то пожалуйста сообщите нам <a href="http://bugs.modx.com/projects/revo" target="_blank">MODx\'s JIRA Issue Tracker</a>.</p>';
+$_lang['credits'] = 'Команда MODX';
+$_lang['credits_database'] = 'MODX is powered by <a href="http://www.mysql.com/" target="_blank">MySQL</a> or <a href="http://www.microsoft.com/sqlserver/" target="_blank">Microsoft SQL Server</a>';
+$_lang['credits_php'] = 'MODX написан на <a href="http://www.php.net" target="_blank">PHP</a>!';
+$_lang['credits_sencha'] = 'MODX использует <a href="http://www.sencha.com/" target="_blank">Sencha (ExtJS)</a>.';
+$_lang['credits_smarty'] = 'MODX использует <a href="http://www.smarty.net/" target="_blank">Smarty</a>.';
+$_lang['credits_xpdo'] = 'MODX использует технологию преобразования объектов в реляционные модели баз данных <a href="http://www.xpdo.org/" target="_blank">xPDO</a>.';
+$_lang['help_msg'] = '<p>Вы можете получить поддержку от сообщества <a href=""http://forums.modx.com" target="_blank">на форумах MODX</a>. Советуем почитать <a href="http://rtfm.modx.com/display/revolution20/Home" target="_blank">документацию MODX Revolution</a>.</p><p>Если Вы нашли ошибку, то пожалуйста сообщите нам <a href="http://bugs.modx.com/projects/revo" target="_blank">MODX\'s JIRA Issue Tracker</a>.</p>';
 $_lang['help_title'] = 'Помощь';
