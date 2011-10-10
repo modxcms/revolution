cc769845d59d3a8f611f52c38f4f5ed6a6db3cf3
yama <yamamoto@kyms.ne.jp>
2011-10-10 13:55:46 +0900

    Russian lexicon some issue

--- core/lexicon/ru/about.inc.php
+++ core/lexicon/ru/about.inc.php
@@ -7,5 +7,5 @@ $_lang['credits_php'] = 'MODX написан на <a href="http://www.php.net" t
 $_lang['credits_sencha'] = 'MODX использует <a href="http://www.sencha.com/" target="_blank">Sencha (ExtJS)</a>.';
 $_lang['credits_smarty'] = 'MODX использует <a href="http://www.smarty.net/" target="_blank">Smarty</a>.';
 $_lang['credits_xpdo'] = 'MODX использует технологию преобразования объектов в реляционные модели баз данных <a href="http://www.xpdo.org/" target="_blank">xPDO</a>.';
-$_lang['help_msg'] = '<p>Вы можете получить поддержку от сообщества <a href=""http://forums.modx.com" target="_blank">на форумах MODX</a>. Советуем почитать <a href="http://rtfm.modx.com/display/revolution20/Home" target="_blank">документацию MODX Revolution</a>.</p><p>Если Вы нашли ошибку, то пожалуйста сообщите нам <a href="http://bugs.modx.com/projects/revo" target="_blank">MODX\'s JIRA Issue Tracker</a>.</p>';
+$_lang['help_msg'] = '<p>Вы можете получить поддержку от сообщества <a href="http://forums.modx.com" target="_blank">на форумах MODX</a>. Советуем почитать <a href="http://rtfm.modx.com/display/revolution20/Home" target="_blank">документацию MODX Revolution</a>.</p><p>Если Вы нашли ошибку, то пожалуйста сообщите нам <a href="http://bugs.modx.com/projects/revo" target="_blank">MODX\'s JIRA Issue Tracker</a>.</p>';
 $_lang['help_title'] = 'Помощь';
