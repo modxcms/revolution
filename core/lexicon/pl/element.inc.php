a89e8c0c5bfa4198c23410bf334696f580efeb39
yama <yamamoto@kyms.ne.jp>
2011-10-11 09:48:24 +0900

    Polish lexicon new topics merged(For Revolution 2.2)

--- core/lexicon/pl/element.inc.php
+++ core/lexicon/pl/element.inc.php
@@ -5,66 +5,30 @@
 * @package modx
 * @subpackage lexicon
 */
-
-#$_lang['element'] = 'Element';
 $_lang['element'] = 'Element';
-
-#$_lang['element_err_nf'] = 'Element not found.';
 $_lang['element_err_nf'] = 'Element not found.';
-
-#$_lang['element_err_ns'] = 'Element not specified.';
 $_lang['element_err_ns'] = 'Element not specified.';
-
-#$_lang['quick_create'] = 'Quick Create';
+$_lang['is_static'] = 'Is Static';
+$_lang['is_static_msg'] = 'Indicates if the element source is stored in an external file.';
 $_lang['quick_create'] = 'Quick Create';
-
-#$_lang['quick_create_chunk'] = 'Quick Create Chunk';
 $_lang['quick_create_chunk'] = 'Quick Create Chunk';
-
-#$_lang['quick_create_plugin'] = 'Quick Create Plugin';
 $_lang['quick_create_plugin'] = 'Quick Create Plugin';
-
-#$_lang['quick_create_snippet'] = 'Quick Create Snippet';
 $_lang['quick_create_snippet'] = 'Quick Create Snippet';
-
-#$_lang['quick_create_template'] = 'Quick Create Template';
 $_lang['quick_create_template'] = 'Quick Create Template';
-
-#$_lang['quick_create_tv'] = 'Quick Create TV';
 $_lang['quick_create_tv'] = 'Quick Create TV';
-
-#$_lang['quick_update_chunk'] = 'Quick Update Chunk';
 $_lang['quick_update_chunk'] = 'Quick Update Chunk';
-
-#$_lang['quick_update_plugin'] = 'Quick Update Plugin';
 $_lang['quick_update_plugin'] = 'Quick Update Plugin';
-
-#$_lang['quick_update_snippet'] = 'Quick Update Snippet';
 $_lang['quick_update_snippet'] = 'Quick Update Snippet';
-
-#$_lang['quick_update_template'] = 'Quick Update Template';
 $_lang['quick_update_template'] = 'Quick Update Template';
-
-#$_lang['quick_update_tv'] = 'Quick Update TV';
 $_lang['quick_update_tv'] = 'Quick Update TV';
-
-#$_lang['properties_export'] = 'Export Properties';
 $_lang['properties_export'] = 'Export Properties';
-
-#$_lang['properties_import'] = 'Import Properties';
 $_lang['properties_import'] = 'Import Properties';
-
-#$_lang['tv_elements'] = 'Input Option Values';
+$_lang['static_file'] = 'Static File';
+$_lang['static_file_msg'] = 'An external file location where the element source is stored.';
+$_lang['static_source'] = 'Media Source for Static File';
+$_lang['static_source_msg'] = 'MODX will use this Media Source as a base path when looking for the Static Element file. Using "None" will assume you are specifying an absolute path.';
 $_lang['tv_elements'] = 'Input Option Values';
-
-#$_lang['tv_default'] = 'Default Value';
 $_lang['tv_default'] = 'Default Value';
-
-#$_lang['tv_type'] = 'Input Type';
 $_lang['tv_type'] = 'Input Type';
-
-#$_lang['tv_output_type'] = 'Output Type';
 $_lang['tv_output_type'] = 'Output Type';
-
-#$_lang['tv_output_type_properties'] = 'Output Type Properties';
 $_lang['tv_output_type_properties'] = 'Output Type Properties';
