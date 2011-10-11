0211ccbc5fc42b9a23ce8dfcad0f1527a6459dc6
yama <yamamoto@kyms.ne.jp>
2011-10-11 10:50:29 +0900

    Merged Italian lexicon topics for setup

--- setup/lang/it/preload.inc.php
+++ setup/lang/it/preload.inc.php
@@ -6,5 +6,5 @@
  * @subpackage lexicon
  */
 $_lang['preload_err_cache'] = 'Assicurati che la directory [[+path]]cache esista e sia scrivibile da PHP.';
-$_lang['preload_err_core_path'] = 'Assicurati di aver specificato correttamente il parametro MODX_CORE_PATH nel tuo file setup/includes/config.core.php; questo deve puntare a un core MODx funzionante.';
-$_lang['preload_err_pdo'] = 'MODx richiede l\'estensione PDO quando PDO nativo viene usato e non sembra essere stato caricato.';
+$_lang['preload_err_core_path'] = 'Assicurati di aver specificato correttamente il parametro MODX_CORE_PATH nel tuo file setup/includes/config.core.php; questo deve puntare a un core MODX funzionante.';
+$_lang['preload_err_pdo'] = 'MODX richiede l\'estensione PDO quando PDO nativo viene usato e non sembra essere stato caricato.';
