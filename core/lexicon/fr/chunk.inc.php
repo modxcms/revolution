8e10bc038313e9398e17042433e48d2621d8b3af
yama <yamamoto@kyms.ne.jp>
2011-10-11 09:18:00 +0900

    French lexicon new topics merged(For Revolution 2.2)

--- core/lexicon/fr/chunk.inc.php
+++ core/lexicon/fr/chunk.inc.php
@@ -7,6 +7,9 @@
  * @subpackage lexicon
  */
 $_lang['chunk'] = 'Chunk';
+$_lang['chunk_desc_category'] = 'The Category this Chunk belongs in.';
+$_lang['chunk_desc_description'] = 'A short description of this chunk.';
+$_lang['chunk_desc_name'] = 'The name of this chunk. It will be available using the [[$nameOfChunk]] tags.';
 $_lang['chunk_code'] = 'Code source du chunk (html)';
 $_lang['chunk_desc'] = 'Description';
 $_lang['chunk_delete_confirm'] = 'Voulez-vous vraiment supprimer ce chunk?';
