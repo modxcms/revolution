17317dac1300229efdfbbc38ce8f77cdecd7787a
yama <yamamoto@kyms.ne.jp>
2011-10-11 11:54:00 +0900

    CRLF->LF

--- core/lexicon/it/login.inc.php
+++ core/lexicon/it/login.inc.php
@@ -14,7 +14,7 @@ $_lang['login_blocked_time'] = 'Non sei autorizzato ad accedere in questo moment
 $_lang['login_blocked_too_many_attempts'] = 'Sei stato bloccato dopo troppi tentativi di accesso falliti.';
 $_lang['login_button'] = 'Accedi';
 $_lang['login_cannot_locate_account'] = 'Account sconosciuto. Controlla il nome utente e  ri-digita la password per riprovare.';
-$_lang['login_copyright'] = '&copy; 2005-2011 di <a href="http://modx.com/about/" target="_blank">MODX, LLC</a>. MODX&trade; Revolution è rilasciato sotto licenza GPLv2 o  successive';
+$_lang['login_copyright'] = '&copy; 2005-2011 by <a href="http://modx.com/about/" target="_blank">MODX, LLC</a>. MODX&trade; Revolution is licensed under the GPLv2 or later.';
 $_lang['login_email_label'] = 'Account Email:';
 $_lang['login_err_unknown'] = 'Errore sconosciuto durante il tentativo di accesso.';
 $_lang['login_forget_your_login'] = 'Dati dimenticati?';
