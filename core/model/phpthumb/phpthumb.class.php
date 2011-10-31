dc532233737007639a1fccbe9899333e2a37362a
Shaun McCormick <shaun@modx.com>
2011-10-31 08:03:09 -0500

    Fix issue with phpThumb and zoom crop

--- core/model/phpthumb/phpthumb.class.php
+++ core/model/phpthumb/phpthumb.class.php
@@ -1363,7 +1363,7 @@ class phpthumb {
 								$commandline .= ' -'.$IMresizeParameter.' x'.$thumbnailH;
 							}
 
-							switch (strtoupper($this->far)) {
+							switch (strtoupper($this->zc)) {
 								case 'T':
 									$commandline .= ' -gravity north';
 									break;
