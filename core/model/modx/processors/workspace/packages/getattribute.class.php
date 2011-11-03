149ab2074c3b1ba5f7e76b2eec750fe63efe5629
Shaun McCormick <shaun@modx.com>
2011-11-03 10:18:44 -0500

    Convert  somepackage management processors to class-based

---
+++ core/model/modx/processors/workspace/packages/getattribute.class.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Gets an attribute of a package
+ *
+ * @param string $signature The signature of the package
+ * @param string $attr The attribute to select
+ *
+ * @package modx
+ * @subpackage processors.workspace.packages
+ */
+class modPackageGetAttributeProcessor extends modProcessor {
+    /** @var modTransportPackage $package */
+    public $package;
+    /** @var xPDOTransport $transport */
+    public $transport;
+    
+    public function checkPermissions() {
+        return $this->modx->hasPermission('packages');
+    }
+    public function getLanguageTopics() {
+        return array('workspace');
+    }
+
+    public function initialize() {
+        $signature = $this->getProperty('signature');
+        if (empty($signature)) return $this->modx->lexicon('package_err_ns');
+        $this->package = $this->modx->getObject('transport.modTransportPackage',$signature);
+        if (empty($this->package)) return $this->modx->lexicon('package_err_nf');
+
+        $this->transport = $this->package->getTransport();
+        if (!$this->transport) {
+            return $this->modx->lexicon('package_err_nf');
+        }
+        return true;
+    }
+
+    public function process() {
+        $attributes = array();
+        $attributesToGet = explode(',',$this->getProperty('attributes',''));
+        foreach ($attributesToGet as $attribute) {
+            $attributes[$attribute] = $this->transport->getAttribute($attribute);
+
+            /* if setup options, include setup file */
+            if ($attribute == 'setup-options') {
+                @ob_start();
+                $options = $this->package->toArray();
+                $options[xPDOTransport::PACKAGE_ACTION] = empty($this->package->installed)
+                    ? xPDOTransport::ACTION_INSTALL
+                    : xPDOTransport::ACTION_UPGRADE;
+                $attributeFile = $this->modx->getOption('core_path').'packages/'.$this->package->signature.'/'.$attribute.'.php';
+                if (file_exists($attributeFile) && $attribute != '') {
+                    $attributes['setup-options'] = include $attributeFile;
+                }
+                @ob_end_clean();
+            }
+        }
+        return $this->success('',$attributes);
+    }
+}
+return 'modPackageGetAttributeProcessor';
