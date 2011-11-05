e7dda5c62752c61c8bfe9587dd8f0aa004b1b864
Jan-Christoph Ihrens <ihrens@cc-services.de>
2011-11-05 13:34:41 +0100

    Translated the new phrases to German
    Signed-off-by: Jan-Christoph Ihrens <ihrens@cc-services.de>

--- core/lexicon/de/file.inc.php
+++ core/lexicon/de/file.inc.php
@@ -11,10 +11,13 @@
  *
  * MODX Revolution translated to German by Jan-Christoph Ihrens (enigmatic_user, enigma@lunamail.de)
  */
+$_lang['directory'] = 'Verzeichnis';
 $_lang['file_confirm_remove'] = 'Sind Sie sicher, dass Sie diese Datei löschen möchten? Das könnte möglicherweise dazu führen, dass Ihre Website ganz oder teilweise nicht mehr funktioniert.';
+$_lang['file_create'] = 'Datei erstellen';
 $_lang['file_deleted'] = 'Erfolgreich!';
 $_lang['file_delete_file'] = 'Datei löschen';
 $_lang['file_delete_folder'] = 'Ordner löschen';
+$_lang['file_download'] = 'Datei herunterladen';
 $_lang['file_download_file'] = 'Datei herunterladen';
 $_lang['file_download_unzip'] = 'Datei entpacken';
 $_lang['file_edit'] = 'Datei bearbeiten';
