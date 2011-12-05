4e634a67dd50624624ef0ebe454014ca90c6a938
Jason Coward <jason@modx.com>
2011-12-05 10:35:00 -0700

    [#6197] Refresh old and new context caches when moving Resource
    When changing parent of a Resource to a new context, the old context was not being refreshed properly

--- core/model/modx/processors/resource/update.class.php
+++ core/model/modx/processors/resource/update.class.php
@@ -691,11 +691,15 @@ class modResourceUpdateProcessor extends modObjectUpdateProcessor {
         $syncSite = $this->getProperty('syncsite',false);
         $clearCache = $this->getProperty('clearCache',false);
         if (!empty($syncSite) || !empty($clearCache)) {
+            $contexts = array($this->object->get('context_key'));
+            if (!empty($this->oldContext)) {
+                $contexts[] = $this->oldContext->get('key');
+            }
             $this->modx->cacheManager->refresh(array(
                 'db' => array(),
-                'auto_publish' => array('contexts' => array($this->object->get('context_key'))),
-                'context_settings' => array('contexts' => array($this->object->get('context_key'))),
-                'resource' => array('contexts' => array($this->object->get('context_key'))),
+                'auto_publish' => array('contexts' => $contexts),
+                'context_settings' => array('contexts' => $contexts),
+                'resource' => array('contexts' => $contexts),
             ));
         }
     }
