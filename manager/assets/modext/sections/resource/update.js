acd76ea9376c5d4f1a23c3ffb13c7b9ffae1ab31
Shaun McCormick <shaun@modx.com>
2011-12-12 09:49:08 -0600

    [#6302] Fix issue where save button was disabled for template changed resources

--- manager/assets/modext/sections/resource/update.js
+++ manager/assets/modext/sections/resource/update.js
@@ -115,7 +115,7 @@ Ext.extend(MODx.page.UpdateResource,MODx.Component,{
                 process: 'update'
                 ,text: _('save')
                 ,method: 'remote'
-                ,checkDirty: cfg.richtext || MODx.request.activeSave == 1 ? false : true
+                ,checkDirty: cfg.richtext || MODx.request.reload || MODx.request.activeSave == 1 ? false : true
                 ,keys: [{
                     key: MODx.config.keymap_save || 's'
                     ,ctrl: true
