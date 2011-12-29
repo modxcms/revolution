ffce43bdbbd0462617a6b2f9162e43bd6bc0bbd3
Romain <romain@meltingmedia.net>
2011-12-29 22:24:25 +0100

    Updating French lexicon files

--- core/lexicon/fr/system_events.inc.php
+++ core/lexicon/fr/system_events.inc.php
@@ -8,6 +8,8 @@
  */
 $_lang['clear'] = 'Effacer';
 $_lang['error_log'] = 'Log d\'erreurs';
-$_lang['error_log_desc'] = 'Voici le fichier d\'erreurs de MODX Revolution:';
+$_lang['error_log_desc'] = 'Voici le fichier de log d\'erreurs de MODX Revolution :';
+$_lang['error_log_download'] = 'Télécharger le fichier de log d\'erreurs ([[+size]])';
+$_lang['error_log_too_large'] = 'Le fichier de log d\'erreur de <em>[[+name]]</em> est trop volumineux pour être visualisé. Vous pouvez le télécharger grâce au bouton ci-dessous.';
 $_lang['system_events'] = 'Événements système';
 $_lang['priority'] = 'Priorité';
