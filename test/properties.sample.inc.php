bc34823760c66bfeafba35ce1dc61955d9474710
Jason Coward <jason@modx.com>
2012-01-17 10:51:42 -0700

    Add xPDO::getInstance() for singleton support
    - Get instances by a unique identifier
    - Force recreate instances by unique identifier
    - Update config of existing instances

--- test/properties.sample.inc.php
+++ test/properties.sample.inc.php
@@ -27,13 +27,25 @@ $properties['mysql_string_dsn_nodb']= 'mysql:host=localhost;charset=utf8';
 $properties['mysql_string_dsn_error']= 'mysql:host= nonesuchhost;dbname=nonesuchdb';
 $properties['mysql_string_username']= '';
 $properties['mysql_string_password']= '';
+$properties['mysql_array_driverOptions']= array();
 $properties['mysql_array_options']= array(
-    xPDO::OPT_CACHE_PATH => $properties['xpdo_test_path'] . 'cache/',
+    xPDO::OPT_CACHE_PATH => $properties['xpdo_test_path'] .'cache/',
     xPDO::OPT_HYDRATE_FIELDS => true,
     xPDO::OPT_HYDRATE_RELATED_OBJECTS => true,
     xPDO::OPT_HYDRATE_ADHOC_FIELDS => true,
+    xPDO::OPT_CONN_INIT => array(xPDO::OPT_CONN_MUTABLE => true),
+    xPDO::OPT_CONNECTIONS => array(
+        array(
+            'dsn' => $properties['mysql_string_dsn_test'],
+            'username' => $properties['mysql_string_username'],
+            'password' => $properties['mysql_string_password'],
+            'options' => array(
+                xPDO::OPT_CONN_MUTABLE => true,
+            ),
+            'driverOptions' => $properties['mysql_array_driverOptions'],
+        ),
+    ),
 );
-$properties['mysql_array_driverOptions']= array();
 
 /* sqlite */
 $properties['sqlite_string_dsn_test']= 'sqlite:' . $properties['xpdo_test_path'] . 'db/xpdotest';
@@ -41,13 +53,25 @@ $properties['sqlite_string_dsn_nodb']= 'sqlite::memory:';
 $properties['sqlite_string_dsn_error']= 'sqlite:db/';
 $properties['sqlite_string_username']= '';
 $properties['sqlite_string_password']= '';
+$properties['sqlite_array_driverOptions']= array();
 $properties['sqlite_array_options']= array(
     xPDO::OPT_CACHE_PATH => $properties['xpdo_test_path'] . 'cache/',
     xPDO::OPT_HYDRATE_FIELDS => true,
     xPDO::OPT_HYDRATE_RELATED_OBJECTS => true,
     xPDO::OPT_HYDRATE_ADHOC_FIELDS => true,
+    xPDO::OPT_CONN_INIT => array(xPDO::OPT_CONN_MUTABLE => true),
+    xPDO::OPT_CONNECTIONS => array(
+        array(
+            'dsn' => $properties['sqlite_string_dsn_test'],
+            'username' => $properties['sqlite_string_username'],
+            'password' => $properties['sqlite_string_password'],
+            'options' => array(
+                xPDO::OPT_CONN_MUTABLE => true,
+            ),
+            'driverOptions' => $properties['sqlite_array_driverOptions'],
+        ),
+    ),
 );
-$properties['sqlite_array_driverOptions']= array();
 
 /* sqlsrv */
 $properties['sqlsrv_string_dsn_test']= 'sqlsrv:server=(local);database=xpdo_test';
@@ -55,14 +79,27 @@ $properties['sqlsrv_string_dsn_nodb']= 'sqlsrv:server=(local)';
 $properties['sqlsrv_string_dsn_error']= 'sqlsrv:server=xyz;123';
 $properties['sqlsrv_string_username']= '';
 $properties['sqlsrv_string_password']= '';
+$properties['sqlsrv_array_driverOptions']= array(/*PDO::SQLSRV_ATTR_DIRECT_QUERY => false*/);
 $properties['sqlsrv_array_options']= array(
     xPDO::OPT_CACHE_PATH => $properties['xpdo_test_path'] . 'cache/',
     xPDO::OPT_HYDRATE_FIELDS => true,
     xPDO::OPT_HYDRATE_RELATED_OBJECTS => true,
     xPDO::OPT_HYDRATE_ADHOC_FIELDS => true,
+    xPDO::OPT_CONNECTIONS => array(
+        array(
+            'dsn' => $properties['sqlsrv_string_dsn_test'],
+            'username' => $properties['sqlsrv_string_username'],
+            'password' => $properties['sqlsrv_string_password'],
+            'options' => array(
+                xPDO::OPT_CONN_MUTABLE => true,
+            ),
+            'driverOptions' => $properties['sqlsrv_array_driverOptions'],
+        ),
+    ),
 );
-$properties['sqlsrv_array_driverOptions']= array(/*PDO::SQLSRV_ATTR_DIRECT_QUERY => false*/);
 
 /* PHPUnit test config */
 $properties['xpdo_driver']= 'mysql';
 $properties['logLevel']= xPDO::LOG_LEVEL_INFO;
+$properties['logTarget']= XPDO_CLI_MODE ? 'ECHO' : 'HTML';
+//$properties['debug']= -1;
