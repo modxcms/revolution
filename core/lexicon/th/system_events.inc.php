35313f4da0602523c0cef32a1c9b6455fabaf880
kittipongint <kittipongint@hotmail.com>
2012-02-22 19:10:37 +0700

    Translate additional lexicon file and Correct all in Thai translation

--- core/lexicon/th/system_events.inc.php
+++ core/lexicon/th/system_events.inc.php
@@ -1,16 +1,17 @@
-<?php
+﻿<?php
 /**
  * System Events Thai lexicon topic
  *
  * @language th
  * @package modx
  * @subpackage lexicon
- 
-  * @author Mr.Kittipong Intaboot COE#18,KKU
- * @updated 2010-07-21
+ * by Mr.Kittipong Intaboot COE#18,KKU (@kittipongint)
+ * updated 29/01/2012
  */
 $_lang['clear'] = 'ล้าง';
 $_lang['error_log'] = 'บันทึกข้อผิดพลาด';
 $_lang['error_log_desc'] = 'นี่คือบันทึกข้อผิดพลาดสำหรับ MODX Revolution:';
+$_lang['error_log_download'] = 'ดาวน์โหลดบันทึกข้อผิดพลาด ([[+size]])';
+$_lang['error_log_too_large'] = 'บันทึกข้อผิดพลาด <em>[[+name]]</em> ใหญ่เกินไปที่จะดู คุณสามารถดาวน์โหลดมันผ่านทางปุ่มข้างล่าง';
 $_lang['system_events'] = 'เหตุการณ์ของระบบ';
 $_lang['priority'] = 'การจัดลำดับความสำคัญ';
