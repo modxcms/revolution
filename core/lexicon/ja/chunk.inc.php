f9d055eab8f6ea73e4d64fdbb992cff6b7f26d17
yama <yamamoto@kyms.ne.jp>
2012-05-03 21:29:09 +0900

    Update Japanese lexicon topics

--- core/lexicon/ja/chunk.inc.php
+++ core/lexicon/ja/chunk.inc.php
@@ -1,18 +1,22 @@
 <?php
 /**
- * Chunk Japanese lexicon topic
- *
- * @language ja
- * @package modx
- * @subpackage lexicon
- * @author Nick http://smallworld.west-tokyo.com
- * @author shimojo http://www.priqia.com/
- * @author yamamoto http://kyms.jp
- */
+* Chunk Japanese lexicon topic
+*
+* @language ja
+* @package modx
+* @subpackage lexicon
+* @author enogu http://www.kuroienogu.net/
+* @author honda http://kogus.org
+* @author Nick http://smallworld.west-tokyo.com
+* @author shimojo http://www.priqia.com/
+* @author yamamoto http://kyms.jp
+* @author enogu http://www.kuroienogu.net/
+* @author honda http://kogus.org
+*/
 $_lang['chunk'] = 'チャンク';
-$_lang['chunk_desc_category'] = 'The Category this Chunk belongs in.';
-$_lang['chunk_desc_description'] = 'A short description of this chunk.';
-$_lang['chunk_desc_name'] = 'The name of this chunk. It will be available using the [[$nameOfChunk]] tags.';
+$_lang['chunk_desc_category'] = 'このチャンクを所属させるカテゴリーを選択します。';
+$_lang['chunk_desc_description'] = 'チャンクの説明文を設定します。';
+$_lang['chunk_desc_name'] = 'チャンクの名前を設定します。この値はタグ名として使用されます。';
 $_lang['chunk_code'] = 'チャンク コード (html)';
 $_lang['chunk_desc'] = '説明';
 $_lang['chunk_delete_confirm'] = 'このチャンクを本当に削除しますか？';
--- core/lexicon/ja/content_type.inc.php
+++ core/lexicon/ja/content_type.inc.php
@@ -8,22 +8,23 @@
  * @author Nick http://smallworld.west-tokyo.com
  * @author shimojo http://www.priqia.com/
  * @author yamamoto http://kyms.jp
+ * @author honda http://kogus.org
  */
 $_lang['binary'] = 'バイナリ';
-$_lang['binary_desc'] = 'ファイルはバイナリ、もしくはテキストのどちらですか？';
-$_lang['content_type'] = 'コンテンツタイプ';
-$_lang['content_type_desc'] = 'コンテンツタイプはリソースの具体的なファイルシステムタイプです。ここではそれを管理するだけでなく、ファイル拡張子と関連付けることができます。';
-$_lang['content_type_err_nf'] = 'コンテンツタイプが見つかりません。';
-$_lang['content_type_err_nfs'] = 'ID [[+id]] のコンテンツタイプが見つかりません。';
-$_lang['content_type_err_ns'] = 'コンテンツタイプが指定されていません。';
-$_lang['content_type_err_ns_name'] = 'コンテンツタイプの有効な名前を指定してください。';
-$_lang['content_type_err_create'] = 'コンテンツタイプの作成中にエラーが発生しました。';
-$_lang['content_type_err_in_use'] = 'You cannot remove this Content Type, as it is currently in use by Resources in MODX. Please re-assign the Content Types of those Resources before proceeding.';
-$_lang['content_type_err_remove'] = 'コンテンツタイプの削除中にエラーが発生しました。';
-$_lang['content_type_err_save'] = 'コンテンツタイプの保存中にエラーが発生しました。';
-$_lang['content_type_new'] = '新しいコンテンツタイプ';
-$_lang['content_type_remove'] = 'コンテンツタイプの削除';
-$_lang['content_type_remove_confirm'] = '本当にこのコンテンツタイプを削除しますか？';
-$_lang['content_types'] = 'コンテンツタイプ';
+$_lang['binary_desc'] = 'ファイルはバイナリデータですか？';
+$_lang['content_type'] = 'コンテントタイプ';
+$_lang['content_type_desc'] = 'コンテントタイプはリソースの具体的なファイルシステムタイプです。ここではその管理だけでなく、ファイル拡張子との関連付けも可能です。';
+$_lang['content_type_err_nf'] = 'コンテントタイプが見つかりません。';
+$_lang['content_type_err_nfs'] = 'ID [[+id]] のコンテントタイプが見つかりません。';
+$_lang['content_type_err_ns'] = 'コンテントタイプが指定されていません。';
+$_lang['content_type_err_ns_name'] = 'コンテントタイプの有効な名前を指定してください。';
+$_lang['content_type_err_create'] = 'コンテントタイプの作成中にエラーが発生しました。';
+$_lang['content_type_err_in_use'] = 'このコンテントタイプは、リソースが現在使用しているため削除できません。使用しているリソースに別のコンテントタイプを割り当ててから、再度実行してください。';
+$_lang['content_type_err_remove'] = 'コンテントタイプの削除中にエラーが発生しました。';
+$_lang['content_type_err_save'] = 'コンテントタイプの保存中にエラーが発生しました。';
+$_lang['content_type_new'] = '新しいコンテントタイプ';
+$_lang['content_type_remove'] = 'コンテントタイプの削除';
+$_lang['content_type_remove_confirm'] = 'このコンテントタイプを削除してよろしいですか？';
+$_lang['content_types'] = 'コンテントタイプ';
 $_lang['file_extensions'] = 'ファイル拡張子';
-$_lang['file_extensions_desc'] = 'This is the specified file extension for this Content Type.';
+$_lang['file_extensions_desc'] = 'このコンテントタイプに関連付ける拡張子を指定します。';
