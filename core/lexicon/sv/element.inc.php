b7830f78458471c53bdb6e288b51f960ad69ad91
Pontus Ågren <pont@github>
2012-09-19 00:01:14 +0200

    Latest updates to swedish language files

--- core/lexicon/sv/element.inc.php
+++ core/lexicon/sv/element.inc.php
@@ -10,6 +10,7 @@ $_lang['element'] = 'Element';
 $_lang['element_err_nf'] = 'Elementet kunde inte hittas.';
 $_lang['element_err_ns'] = 'Element ej angivet.';
 $_lang['element_static_source_immutable'] = 'Den statiska fil som är angiven som elementkälla är inte skrivbar! Du kan inte redigera innehållet i detta element i hanteraren.';
+$_lang['element_static_source_protected_invalid'] = 'Du kan inte peka ditt element mot MODX konfigurationsmapp. Den mappen är skyddad och går inte att nå.';
 $_lang['is_static'] = 'Är statisk';
 $_lang['is_static_msg'] = 'Indikerar om elementkällan är lagrad i en extern fil.';
 $_lang['quick_create'] = 'Snabbskapa';
