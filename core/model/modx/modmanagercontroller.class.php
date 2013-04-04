9d46c4f8acacf5f158f2e1f0a381915464c2bc9f
Romain <romain@melting-media.com>
2013-04-04 18:07:43 +0200

    [9238] - See http://tracker.modx.com/issues/9238 (quick) proposal for issue 9238 : displaying the whole manager UI instead of a raw blank page with access denied when user performs an action he does not have access to

--- core/model/modx/modmanagercontroller.class.php
+++ core/model/modx/modmanagercontroller.class.php
@@ -5,7 +5,7 @@
 /**
  * Abstract class for manager controllers. Not to be initialized directly; must be extended by the implementing
  * controller.
- * 
+ *
  * @package modx
  */
 abstract class modManagerController {
@@ -67,7 +67,7 @@ abstract class modManagerController {
     /**
      * Return the proper instance of the derived class. This can be used to override how the manager loads a controller
      * class; for example, when handling derivative classes with class_key settings.
-     * 
+     *
      * @static
      * @param modX $modx A reference to the modX object.
      * @param string $className The name of the class that is being requested.
@@ -112,12 +112,27 @@ abstract class modManagerController {
 
     /**
      * Render the controller.
-     * 
+     *
      * @return string
      */
     public function render() {
         if (!$this->checkPermissions()) {
-            return $this->modx->error->failure($this->modx->lexicon('access_denied'));
+            $this->registerBaseScripts();
+            /* register CSS/JS */
+            $this->registerCssJs();
+
+            $this->content = '';
+            if ($this->loadHeader) {
+                $this->content .= $this->getHeader();
+            }
+
+            $this->content .= $this->modx->lexicon('access_denied');
+
+            if ($this->loadFooter) {
+                $this->content .= $this->getFooter();
+            }
+
+            return $this->content;
         }
 
         $this->modx->invokeEvent('OnBeforeManagerPageInit',array(
@@ -280,7 +295,7 @@ abstract class modManagerController {
 
     /**
      * Set a failure on this controller. This will return the error message.
-     * 
+     *
      * @param string $message
      * @return void
      */
@@ -292,7 +307,7 @@ abstract class modManagerController {
     /**
      * Load the path to this controller's template's directory. Only override this if you want to override default
      * behavior; otherwise, overriding getTemplatesPath is preferred.
-     * 
+     *
      * @return string
      */
     public function loadTemplatesPath() {
@@ -308,7 +323,7 @@ abstract class modManagerController {
 
     /**
      * Set the possible template paths for this controller
-     * 
+     *
      * @param array $paths
      * @return void
      */
@@ -319,7 +334,7 @@ abstract class modManagerController {
     /**
      * Load an array of possible paths to this controller's directory. Only override this if you want to override
      * default behavior; otherwise, overriding getControllersPath is preferred.
-     * 
+     *
      * @return array
      */
     public function loadControllersPath() {
@@ -355,11 +370,11 @@ abstract class modManagerController {
         }
         return $paths;
     }
-    
+
     /**
      * Get an array of possible paths to this controller's template's directory.
      * Override this to point to a custom directory.
-     * 
+     *
      * @param bool $coreOnly Ensure that it grabs the path from the core namespace only.
      * @return array|string
      */
@@ -380,7 +395,7 @@ abstract class modManagerController {
 
     /**
      * Do permission checking in this method. Returning false will present a "permission denied" message.
-     * 
+     *
      * @abstract
      * @return boolean
      */
@@ -397,7 +412,7 @@ abstract class modManagerController {
 
     /**
      * Return a string to set as the controller's page title.
-     * 
+     *
      * @abstract
      * @return string
      */
@@ -419,7 +434,7 @@ abstract class modManagerController {
 
     /**
      * Specify an array of language topics to load for this controller
-     * 
+     *
      * @return array
      */
     public function getLanguageTopics() {
@@ -431,7 +446,7 @@ abstract class modManagerController {
      * @return void
      */
     public function firePostRenderEvents() {}
-    
+
     /**
      * Fire any pre-render events for the controller
      * @return void
@@ -440,7 +455,7 @@ abstract class modManagerController {
 
     /**
      * Get the page header for the controller.
-     * 
+     *
      * @return string
      */
     public function getHeader() {
@@ -487,7 +502,7 @@ abstract class modManagerController {
             $externals[] = $managerUrl.'assets/modext/widgets/element/modx.tree.element.js';
             $externals[] = $managerUrl.'assets/modext/widgets/system/modx.tree.directory.js';
             $externals[] = $managerUrl.'assets/modext/core/modx.view.js';
-            
+
             $siteId = $this->modx->user->getUserToken('mgr');
 
             $externals[] = $managerUrl.'assets/modext/core/modx.layout.js';
@@ -497,7 +512,7 @@ abstract class modManagerController {
             $compressJsInGroups = (boolean)$this->modx->getOption('compress_js_groups',null,false);
             $this->modx->setOption('compress_js',$compressJs);
             $this->modx->setOption('compress_js_groups',$compressJsInGroups);
-            
+
             if (!empty($compressJs) && empty($compressJsInGroups)) {
                 if (!empty($externals)) {
                     $minDir = $this->modx->getOption('manager_url',null,MODX_MANAGER_URL).'min/';
@@ -562,7 +577,7 @@ abstract class modManagerController {
         }
         return $obj;
     }
-    
+
     /**
      * Grabs a stripped version of modx to prevent caching of JS after upgrades
      *
@@ -669,7 +684,7 @@ abstract class modManagerController {
             }
         }
 
-        
+
         $this->modx->smarty->assign('cssjs',$cssjs);
     }
 
@@ -809,7 +824,7 @@ abstract class modManagerController {
 
     /**
      * Load the working context for this controller.
-     * 
+     *
      * @return modContext|string
      */
     public function loadWorkingContext() {
@@ -906,7 +921,7 @@ abstract class modExtraManagerController extends modManagerController {
      * @return string The string title of the page
      */
     public function getPageTitle() { return ''; }
-    
+
     /**
      * Loads any page-specific CSS/JS for the controller
      * @return void
