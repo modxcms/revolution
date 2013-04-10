e9290e979866467ad2e1a229c29cae28ef270204
Romain <romain@melting-media.com>
2013-04-10 02:28:15 +0200

    [9809] - See http://tracker.modx.com/issues/9809 Remove empty ULs in main menu
    (cherry picked from commit bd763cd76f0d246d274fd7eac390614410c4906e)

--- manager/controllers/default/header.php
+++ manager/controllers/default/header.php
@@ -84,6 +84,8 @@ function _modProcessMenus(modX &$modx,&$output,$menus,&$childrenCt,$showDescript
         $childrenCt++;
     }
 }
+$emptySub = '<ul class="modx-subsubnav">'."\n".'</ul>'."\n";
+$output = str_replace($emptySub, '', $output);
 $this->setPlaceholder('navb',$output);
 
 /* assign logged in text and link */
