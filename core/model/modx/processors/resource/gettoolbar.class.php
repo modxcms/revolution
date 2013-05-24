282fe6c801278b6efd365dd3277f35ae213a8f6f
JP DeVries <johnpaul.devries@gmail.com>
2013-05-24 14:10:43 -0700

    New Tree Icons
    Blame ExtJS for all the excessive markup, but hey it looks good

--- core/model/modx/processors/resource/gettoolbar.class.php
+++ core/model/modx/processors/resource/gettoolbar.class.php
@@ -18,12 +18,12 @@ class modResourceGetToolbarProcessor extends modProcessor {
         $p = $this->modx->getOption('manager_url').'templates/default/images/restyle/icons/';
         $items = array();
         $items[] = array(
-            'icon' => $p.'arrow_down.png',
+            'html' => '<tbody class="x-btn-small x-btn-icon-small-left"><tr><td class="x-btn-tl"><i>&nbsp;</i></td><td class="x-btn-tc"></td><td class="x-btn-tr"><i>&nbsp;</i></td></tr><tr><td class="x-btn-ml"><i>&nbsp;</i></td><td class="x-btn-mc"><button type="button" id="ext-gen115" class=" x-btn-text" style=" position: relative; padding: 0 !important; text-align: center;"><i class="icon-level-down" style=" font-size: 1.4em; text-align: center; padding-left: 3px;">&nbsp;</i></button></td><td class="x-btn-mr"><i>&nbsp;</i></td></tr><tr><td class="x-btn-bl"><i>&nbsp;</i></td><td class="x-btn-bc"></td><td class="x-btn-br"><i>&nbsp;</i></td></tr></tbody>',
             'tooltip' => $this->modx->lexicon('expand_tree'),
             'handler' => 'this.expandAll',
         );
         $items[] = array(
-            'icon' => $p.'arrow_up.png',
+            'html' => '<tbody class="x-btn-small x-btn-icon-small-left"><tr><td class="x-btn-tl"><i>&nbsp;</i></td><td class="x-btn-tc"></td><td class="x-btn-tr"><i>&nbsp;</i></td></tr><tr><td class="x-btn-ml"><i>&nbsp;</i></td><td class="x-btn-mc"><button type="button" id="ext-gen115" class=" x-btn-text" style=" position: relative; padding: 0 !important; text-align: center;"><i class="icon-level-up" style=" font-size: 1.4em; text-align: center; padding-left: 3px;">&nbsp;</i></button></td><td class="x-btn-mr"><i>&nbsp;</i></td></tr><tr><td class="x-btn-bl"><i>&nbsp;</i></td><td class="x-btn-bc"></td><td class="x-btn-br"><i>&nbsp;</i></td></tr></tbody>',
             'tooltip' => $this->modx->lexicon('collapse_tree'),
             'handler' => 'this.collapseAll',
         );
@@ -31,28 +31,28 @@ class modResourceGetToolbarProcessor extends modProcessor {
         $context = '&context_key=' . $this->modx->getOption('default_context');
         if ($this->modx->hasPermission('new_document')) {
             $items[] = array(
-                'icon' => $p.'folder_page_add.png',
+                'html' => '<tbody class="x-btn-small x-btn-icon-small-left"><tr><td class="x-btn-tl"><i>&nbsp;</i></td><td class="x-btn-tc"></td><td class="x-btn-tr"><i>&nbsp;</i></td></tr><tr><td class="x-btn-ml"><i>&nbsp;</i></td><td class="x-btn-mc"><button type="button" id="ext-gen115" class=" x-btn-text" style=" position: relative; padding: 0 !important; text-align: center;"><i class="icon-file" style=" font-size: 1.4em; text-align: center; padding-left: 3px;">&nbsp;</i></button></td><td class="x-btn-mr"><i>&nbsp;</i></td></tr><tr><td class="x-btn-bl"><i>&nbsp;</i></td><td class="x-btn-bc"></td><td class="x-btn-br"><i>&nbsp;</i></td></tr></tbody>',
                 'tooltip' => $this->modx->lexicon('document_new'),
                 'handler' => 'new Function("this.redirect(\"index.php?a=resource/create'. $context .'\");");',
             );
         }
         if ($this->modx->hasPermission('new_weblink')) {
             $items[] = array(
-                'icon' => $p.'page_white_link.png',
+                'html' => '<tbody class="x-btn-small x-btn-icon-small-left"><tr><td class="x-btn-tl"><i>&nbsp;</i></td><td class="x-btn-tc"></td><td class="x-btn-tr"><i>&nbsp;</i></td></tr><tr><td class="x-btn-ml"><i>&nbsp;</i></td><td class="x-btn-mc"><button type="button" id="ext-gen115" class=" x-btn-text" style=" position: relative; padding: 0 !important; text-align: center;"><i class="icon-link" style=" font-size: 1.4em; text-align: center; padding-left: 2px;">&nbsp;</i></button></td><td class="x-btn-mr"><i>&nbsp;</i></td></tr><tr><td class="x-btn-bl"><i>&nbsp;</i></td><td class="x-btn-bc"></td><td class="x-btn-br"><i>&nbsp;</i></td></tr></tbody>',
                 'tooltip' => $this->modx->lexicon('add_weblink'),
                 'handler' => 'new Function("this.redirect(\"index.php?a=resource/create&class_key=modWebLink'. $context .'\");");',
             );
         }
         if ($this->modx->hasPermission('new_symlink')) {
             $items[] = array(
-                'icon' => $p.'page_white_copy.png',
+                'html' => '<tbody class="x-btn-small x-btn-icon-small-left"><tr><td class="x-btn-tl"><i>&nbsp;</i></td><td class="x-btn-tc"></td><td class="x-btn-tr"><i>&nbsp;</i></td></tr><tr><td class="x-btn-ml"><i>&nbsp;</i></td><td class="x-btn-mc"><button type="button" id="ext-gen115" class=" x-btn-text" style=" position: relative; padding: 0 !important; text-align: center;"><i class="icon-copy" style=" font-size: 1.4em; text-align: center; padding-left: 1px;">&nbsp;</i></button></td><td class="x-btn-mr"><i>&nbsp;</i></td></tr><tr><td class="x-btn-bl"><i>&nbsp;</i></td><td class="x-btn-bc"></td><td class="x-btn-br"><i>&nbsp;</i></td></tr></tbody>',
                 'tooltip' => $this->modx->lexicon('add_symlink'),
                 'handler' => 'new Function("this.redirect(\"index.php?a=resource/create&class_key=modSymLink'. $context .'\");");',
             );
         }
         if ($this->modx->hasPermission('new_static_resource')) {
             $items[] = array(
-                'icon' => $p.'page_white_gear.png',
+                'html' => '<tbody class="x-btn-small x-btn-icon-small-left"><tr><td class="x-btn-tl"><i>&nbsp;</i></td><td class="x-btn-tc"></td><td class="x-btn-tr"><i>&nbsp;</i></td></tr><tr><td class="x-btn-ml"><i>&nbsp;</i></td><td class="x-btn-mc"><button type="button" id="ext-gen115" class=" x-btn-text" style=" position: relative; padding: 0 !important; text-align: center;"><i class="icon-file-alt" style=" font-size: 1.4em; text-align: center; padding-left: 3px;">&nbsp;</i></button></td><td class="x-btn-mr"><i>&nbsp;</i></td></tr><tr><td class="x-btn-bl"><i>&nbsp;</i></td><td class="x-btn-bc"></td><td class="x-btn-br"><i>&nbsp;</i></td></tr></tbody>',
                 'tooltip' => $this->modx->lexicon('static_resource_new'),
                 'handler' => 'new Function("this.redirect(\"index.php?a=resource/create&class_key=modStaticResource'. $context .'\");");',
             );
@@ -61,19 +61,19 @@ class modResourceGetToolbarProcessor extends modProcessor {
         $items[] = '-';
 
         $items[] = array(
-            'icon' => $p.'refresh.png',
+            'html' => '<tbody class="x-btn-small x-btn-icon-small-left"><tr><td class="x-btn-tl"><i>&nbsp;</i></td><td class="x-btn-tc"></td><td class="x-btn-tr"><i>&nbsp;</i></td></tr><tr><td class="x-btn-ml"><i>&nbsp;</i></td><td class="x-btn-mc"><button type="button" id="ext-gen115" class=" x-btn-text" style=" position: relative; padding: 0 !important; text-align: center;"><i class="icon-refresh" style=" font-size: 1.4em; text-align: center; padding-left: 2px;">&nbsp;</i></button></td><td class="x-btn-mr"><i>&nbsp;</i></td></tr><tr><td class="x-btn-bl"><i>&nbsp;</i></td><td class="x-btn-bc"></td><td class="x-btn-br"><i>&nbsp;</i></td></tr></tbody>',
             'tooltip' => $this->modx->lexicon('refresh_tree'),
             'handler' => 'this.refresh',
         );
         $items[] = array(
-            'icon' => $p.'unzip.gif',
+            'html' => '<tbody class="x-btn-small x-btn-icon-small-left"><tr><td class="x-btn-tl"><i>&nbsp;</i></td><td class="x-btn-tc"></td><td class="x-btn-tr"><i>&nbsp;</i></td></tr><tr><td class="x-btn-ml"><i>&nbsp;</i></td><td class="x-btn-mc"><button type="button" id="ext-gen115" class=" x-btn-text" style=" position: relative; padding: 0 !important; text-align: center;"><i class="icon-sort" style=" font-size: 1.4em; text-align: center; padding-left: 2px;">&nbsp;</i></button></td><td class="x-btn-mr"><i>&nbsp;</i></td></tr><tr><td class="x-btn-bl"><i>&nbsp;</i></td><td class="x-btn-bc"></td><td class="x-btn-br"><i>&nbsp;</i></td></tr></tbody>',
             'tooltip' => $this->modx->lexicon('show_sort_options'),
             'handler' => 'this.showFilter',
         );
         if ($this->modx->hasPermission('purge_deleted')) {
             $items[] = '-';
             $items[] = array(
-                'icon' => $p.'trash.png',
+                'html' => '<tbody class="x-btn-small x-btn-icon-small-left"><tr><td class="x-btn-tl"><i>&nbsp;</i></td><td class="x-btn-tc"></td><td class="x-btn-tr"><i>&nbsp;</i></td></tr><tr><td class="x-btn-ml"><i>&nbsp;</i></td><td class="x-btn-mc"><button type="button" id="ext-gen115" class=" x-btn-text" style=" position: relative; padding: 0 !important; text-align: center;"><i class="icon-trash" style=" font-size: 1.4em; text-align: center; padding-left: 3px;">&nbsp;</i></button></td><td class="x-btn-mr"><i>&nbsp;</i></td></tr><tr><td class="x-btn-bl"><i>&nbsp;</i></td><td class="x-btn-bc"></td><td class="x-btn-br"><i>&nbsp;</i></td></tr></tbody>',
                 'tooltip' => $this->modx->lexicon('empty_recycle_bin'),
                 'handler' => 'this.emptyRecycleBin',
             );
