4f9f356cd76562504c62119fe6abf7395efd81c2
Ryan Thrash <ryan@modx.com>
2013-06-20 12:09:19 -0500

    Initial UI flattening thanks to Christian Seele

--- manager/assets/modext/core/modx.layout.js
+++ manager/assets/modext/core/modx.layout.js
@@ -62,7 +62,7 @@ MODx.Layout = function(config){
             xtype: 'box'
             ,region: 'north'
             ,applyTo: 'modx-header'
-            ,height: 34
+            ,height: 55
         },{
              region: 'west'
             ,applyTo: 'modx-leftbar'
