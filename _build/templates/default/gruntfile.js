da2e1270e13c0ba083515696d3a4b216aa13fd1a
JP DeVries <johnpaul.devries@gmail.com>
2013-09-04 23:15:10 -0700

    Beautifying gruntfile

--- _build/templates/default/gruntfile.js
+++ _build/templates/default/gruntfile.js
@@ -1,183 +1,145 @@
 module.exports = function(grunt) {
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-	/*notify:{
-		watch: {
-			options: {
-				title: 'Done Grunting!',
-				message: 'Sass complete'
-			}
-		}
-	},*/
-	dirs: { /* just defining some properties */
-		lib: './lib',
-		scss: './sass/'//,
-		//js: 'js/'
-	},
-	bower: {
-		install: {
-			options: {
-				targetDir: './lib'
-			}
-		}
-	},
-	copy: {
-		main: {
-			files: [
-				{expand:true,filter: 'isFile',cwd:'../../../manager/templates/default/css/',src:'raw.css', dest: './sass/'}
-			]
-		}
-	},
-	rename: { /* move files */
-		bourbon: {
-			src: './lib/bourbon/',
-			dest: './sass/',
-			force:true
+	// Project configuration.
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		dirs: { /* just defining some properties */
+			lib: './lib',
+			scss: './sass/'
 		},
-		raw: {
-			src: './sass/raw.css',
-			dest: './sass/_raw.scss'
-		}
-	},
-	asciify:{
-		dontEdit: {
-			options:{
-				font:'larry3d'
+		bower: {
+			install: {
+				options: {
+					targetDir: './lib'
+				}
+			}
+		},
+		copy: {
+			main: {
+				files: [{
+					expand: true,
+					filter: 'isFile',
+					cwd: '../../../manager/templates/default/css/',
+					src: 'raw.css',
+					dest: './sass/'
+				}]
+			}
+		},
+		rename: { /* move files */
+			bourbon: {
+				src: './lib/bourbon/',
+				dest: './sass/',
+				force: true
 			},
-			text: 'do not edit'
-		}
-	},
-	  csso: {
-	    compress: {
-	      options: {
-		    report:'gzip',
-	        banner: '/*!\n <%= asciify_dontEdit %> \n see _build/templates/default/README.md\n*/\n'
-	      },
-	      files: {
-	        '../../../manager/templates/default/css/index.css': '../../../manager/templates/default/css/index.css',
-	        '../../../manager/templates/default/css/login.css': '../../../manager/templates/default/css/login.css'
-	      }
-	    }
-	  },
-	/*concat: { /* concatenate javascript 
-		script: {
-			files: {
-				'assets/js/main.dev.js' : ['assets/js/plugins.js','assets/js/lib/requirejs.js','assets/js/main.js'] 
+			raw: {
+				src: './sass/raw.css',
+				dest: './sass/_raw.scss'
+			}
+		},
+		asciify: {
+			dontEdit: {
+				options: {
+					font: 'larry3d'
+				},
+				text: 'do not edit'
+			}
+		},
+		csso: {
+			compress: {
+				options: {
+					report: 'gzip',
+					banner: '/*!\n <%= asciify_dontEdit %> \n see _build/templates/default/README.md\n*/\n'
+				},
+				files: {
+					'../../../manager/templates/default/css/index.css': '../../../manager/templates/default/css/index.css',
+					'../../../manager/templates/default/css/login.css': '../../../manager/templates/default/css/login.css'
+				}
+			}
+		},
+		sass: {
+			dist: {
+				options: {
+					style: 'compressed',
+					compass: false
+				},
+				files: {
+					'../../../manager/templates/default/css/index.css': 'sass/index.scss',
+					'../../../manager/templates/default/css/login.css': 'sass/login.scss'
+				}
+			},
+			dev: {
+				options: {
+					style: 'expanded',
+					compass: false,
+				},
+				files: {
+					'../../../manager/templates/default/css/index.css': 'sass/index.scss',
+					'../../../manager/templates/default/css/login.css': 'sass/login.scss'
+				}
+			}
+		},
+		autoprefixer: { /* this expands the css so it needs to get compressed with csso afterwards */
+			options: {
+				// Task-specific options go here.
+			},
+
+			// just prefix the specified file
+			index: {
+				options: {},
+				src: '../../../manager/templates/default/css/index.css',
+				dest: '../../../manager/templates/default/css/index.css'
+			},
+			login: {
+				options: {},
+				src: '../../../manager/templates/default/css/login.css',
+				dest: '../../../manager/templates/default/css/login.css'
+			}
+		},
+		watch: { /* trigger tasks on save */
+			options: {
+				livereload: true
+			},
+			scss: {
+				files: './sass/*',
+				tasks: ['sass:dist', 'autoprefixer', 'asciify', 'csso', 'growl:sass']
+			}
+		},
+		clean: { /* take out the trash */
+			prebuild: ['./sass/bourbon']
+		},
+		growl: {
+			sass: {
+				message: "Sass files created.",
+				title: "grunt"
+			},
+			build: {
+				title: "grunt",
+				message: "Build complete."
+			},
+			prefixes: {
+				title: "grunt",
+				message: "CSS prefixes added."
+			},
+			watch: {
+				title: "grunt",
+				message: "Watching. Grunt has its eye on you."
 			}
 		}
-	},*/
-    /*uglify: { /* minify javascript 
-      options: {
-        banner: ''
-      },
-      build: {
-        //src: 'src/<%= pkg.name %>.js',
-        //dest: 'build/<%= pkg.name %>.min.js'
-		src: 'assets/js/main.dev.js',
-		dest: 'assets/js/main-min.js'
-      }
-    },*/
-  sass: {                             
-    dist: {                           
-      options: {                      
-        style: 'compressed',
-        compass: false
-      },
-      files: {                        
-'../../../manager/templates/default/css/index.css' : 'sass/index.scss',
-'../../../manager/templates/default/css/login.css' : 'sass/login.scss'
-      }
-    },
-    dev: {                           
-      options: {                     
-        style: 'expanded',
-        compass:false,
-      },
-      files: {                        
-'../../../manager/templates/default/css/index.css' : 'sass/index.scss',
-'../../../manager/templates/default/css/login.css' : 'sass/login.scss'
-      }
-    }
-  },
-  autoprefixer: { /* this expands the css so it needs to get compressed with csso afterwards */
-    options: {
-      // Task-specific options go here.
-    },
+	});
 
-    // just prefix the specified file
-    index: {
-      options: {
-      },
-      src: '../../../manager/templates/default/css/index.css',
-      dest: '../../../manager/templates/default/css/index.css'
-    },
-    login: {
-      options: {
-      },
-      src: '../../../manager/templates/default/css/login.css',
-      dest: '../../../manager/templates/default/css/login.css'
-    }
-  },
-	watch: { /* trigger tasks on save */
-		options: {
-			livereload: true
-		},
-		scss: {
-			files: './sass/*',
-			tasks: ['sass:dist','autoprefixer','asciify','csso','growl:sass']
-		},
-		/*script: {
-			files: [ 'assets/js/main.js', 'assets/js/plugins.js' ],
-			tasks: [ 'concat:script','uglify' ]
-		},*/
-		/*uglify: {
-			files: 'assets/js/main.js',
-			tasks: [ 'uglify' ]
-		}*/
-	},
-	clean: { /* take out the trash */
-		prebuild: ['./sass/bourbon'] 
-	},
-	growl:{
-		sass : {
-			message : "Sass files created.",
-			title : "grunt"
-		},
-		build : {
-			title : "grunt",
-			message : "Build complete."
-		},
-		prefixes : {
-			title : "grunt",
-			message : "CSS prefixes added."
-		},
-		watch : {
-			title : "grunt",
-			message : "Watching. Grunt has its eye on you."
-		}
-	}
-  });
+	grunt.loadNpmTasks('grunt-bower-task');
+	grunt.loadNpmTasks('grunt-rename');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-growl');
+	grunt.loadNpmTasks('grunt-asciify');
+	grunt.loadNpmTasks('grunt-autoprefixer');
+	grunt.loadNpmTasks('grunt-csso');
 
-  grunt.loadNpmTasks( 'grunt-bower-task' );
-  //grunt.renameTask( 'bower', 'bowerInstall' );
-  //grunt.loadNpmTasks( 'grunt-bower' );
-  grunt.loadNpmTasks( 'grunt-rename' );
-  grunt.loadNpmTasks('grunt-contrib-sass');
-  grunt.loadNpmTasks( 'grunt-contrib-watch' );
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  //grunt.loadNpmTasks( 'grunt-contrib-concat' );
-  //grunt.loadNpmTasks( 'grunt-contrib-uglify' );
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-growl');
-  grunt.loadNpmTasks('grunt-asciify');
-  grunt.loadNpmTasks('grunt-autoprefixer');
-  grunt.loadNpmTasks('grunt-csso');
-  //grunt.loadNpmTasks( 'grunt-notify' );
 
-  // Tasks
-
-  grunt.registerTask('default', ['sass:dist','autoprefixer','growl:prefixes','growl:sass','asciify','csso','growl:watch','watch']);
-  grunt.registerTask('build', ['clean:prebuild','bower','copy','rename','sass:dist','autoprefixer','growl:prefixes','growl:sass','asciify','csso']);
-  grunt.registerTask('expand',['sass:dev','autoprefixer','growl:prefixes','growl:sass']);
+	// Tasks
+	grunt.registerTask('default', ['sass:dist', 'autoprefixer', 'growl:prefixes', 'growl:sass', 'asciify', 'csso', 'growl:watch', 'watch']);
+	grunt.registerTask('build', ['clean:prebuild', 'bower', 'copy', 'rename', 'sass:dist', 'autoprefixer', 'growl:prefixes', 'growl:sass', 'asciify', 'csso']);
+	grunt.registerTask('expand', ['sass:dev', 'autoprefixer', 'growl:prefixes', 'growl:sass']);
 };
