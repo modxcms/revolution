69c017e9d9749e5f1dd746a1803a4c0d10d61196
Daniel Miguel de Melo <daniel@taghouse.com.br>
2013-09-06 13:02:17 -0300

    Adding basic lexicon files for Brazilian Portuguese
    (cherry picked from commit a189e6326414c0b461c7ffe33b65666e1ce0e984)

---
+++ core/lexicon/pt-br/category.inc.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Category Brazilian Portuguese lexicon topic
+ *
+ * @language br
+ * @package modx
+ * @subpackage lexicon
+ */
+$_lang['categories'] = 'Categorias';
+$_lang['category_confirm_delete'] = 'Você tem certeza que deseja remover esta categoria? Todos elementos nesta categoria serão movidos para "não categorizado".';
+$_lang['category_create'] = 'Criar Categoria';
+$_lang['category_err_create'] = 'Um erro ocorreu enquanto tentava criar a categoria.';
+$_lang['category_err_not_found'] = 'Categoria não encontrada!';
+$_lang['category_err_nf'] = 'Categoria não encontrada.';
+$_lang['category_err_nfs'] = 'Categoria não encontrada com a chave: %s';
+$_lang['category_err_ns'] = 'Categoria não especificada.';
+$_lang['category_err_ns_name'] = 'Por favor especifique um nome válido para categoria.';
+$_lang['category_err_remove'] = 'An error occurred while trying to remove the category.';
+$_lang['category_err_save'] = 'An error occurred while trying to save the category.';
+$_lang['category_existing'] = 'Existing Category';
+$_lang['category_heading'] = 'Category';
+$_lang['category_msg'] = 'Here you can view and edit all resources grouped by category.';
+$_lang['category_no_chunks'] = 'Sem Chunks para esta categoria.';
+$_lang['category_no_plugins'] = 'Sem Plugins para esta categoria.';
+$_lang['category_no_snippets'] = 'Sem Snippets para esta categoria.';
+$_lang['category_no_templates'] = 'Sem Templates para esta categoria.';
+$_lang['category_no_template_variables'] = 'Sem Variáveis de Template para esta categoria.';
+$_lang['category_rename'] = 'Renomear Categoria';
+$_lang['category_remove'] = 'Remover Categoria';
