718bff9580a97c76ab8a4066fa20419bafc08776
JP DeVries <johnpaul.devries@gmail.com>
2013-09-27 20:33:34 -0700

    Color Logo and Swatches
    Switching to Colored Logo
    Loosing Logo Type
    Adding Green to Save button (and site swatches)

--- manager/assets/modext/widgets/core/modx.searchbar.js
+++ manager/assets/modext/widgets/core/modx.searchbar.js
@@ -15,8 +15,8 @@ MODx.SearchBar = function(config) {
         ,minChars: 1
         ,displayField: 'name'
         ,valueField: 'action'
-        ,width: 174
-        ,maxWidth: 300
+        ,width: 250
+        ,maxWidth: 250
         ,itemSelector: '.section'
         ,tpl: new Ext.XTemplate(
             '<tpl for=".">',
