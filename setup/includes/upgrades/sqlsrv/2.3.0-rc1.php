e3de1ff116645ad91c0fc3c12021f3dff2fa2f34
Jason Coward <jason@opengeek.com>
2013-10-01 20:21:54 -0600

    Add 2.2.10-pl db upgrades to 2.3.0-rc1 db upgrade

--- setup/includes/upgrades/sqlsrv/2.3.0-rc1.php
+++ setup/includes/upgrades/sqlsrv/2.3.0-rc1.php
@@ -70,3 +70,15 @@ if ($object) {
     $object->set('value', implode(',', $exploded));
     $object->save();
 }
+
+/* Update modTransportPackage version_* and release_index fields (from 2.2.10-pl) */
+$class = 'transport.modTransportPackage';
+$table = $modx->getTableName($class);
+$description = $this->install->lexicon('modify_column',array('column' => 'version_major', 'old' => 'tinyint', 'new' => 'smallint', 'table' => $table));
+$this->processResults($class, $description, array($modx->manager, 'alterField'), array($class, 'version_major'));
+$description = $this->install->lexicon('modify_column',array('column' => 'version_minor', 'old' => 'tinyint', 'new' => 'smallint', 'table' => $table));
+$this->processResults($class, $description, array($modx->manager, 'alterField'), array($class, 'version_minor'));
+$description = $this->install->lexicon('modify_column',array('column' => 'version_patch', 'old' => 'tinyint', 'new' => 'smallint', 'table' => $table));
+$this->processResults($class, $description, array($modx->manager, 'alterField'), array($class, 'version_patch'));
+$description = $this->install->lexicon('modify_column',array('column' => 'release_index', 'old' => 'tinyint', 'new' => 'smallint', 'table' => $table));
+$this->processResults($class, $description, array($modx->manager, 'alterField'), array($class, 'release_index'));
