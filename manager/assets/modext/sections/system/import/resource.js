50cbc0a15a263494900fa238f42345c782fc3a34
Garry Nutting <garry@modx.com>
2013-11-09 18:35:33 +0000

    Fix broken processor action for Import Resources screen

--- manager/assets/modext/sections/system/import/resource.js
+++ manager/assets/modext/sections/system/import/resource.js
@@ -3,7 +3,7 @@ MODx.page.ImportResource = function(config) {
     Ext.applyIf(config,{
         formpanel: 'modx-panel-import-resources'
         ,buttons: [{
-            process: 'import', text: _('import_resources'), method: 'remote'
+            process: 'system/import/html', text: _('import_resources'), method: 'remote'
         },{
             process: 'cancel', text: _('cancel'), params: {a:'welcome'}
         }]
