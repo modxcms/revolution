e151e2bc572b4298fbcdf14497cea652e2df0217
Garry Nutting <garry@modx.com>
2013-11-11 16:40:23 +0000

    Fix inline grid save action and resource tree refreshing

--- manager/assets/modext/widgets/security/modx.grid.user.recent.resource.js
+++ manager/assets/modext/widgets/security/modx.grid.user.recent.resource.js
@@ -1,6 +1,6 @@
 /**
  * Loads a grid of all the resources a user has recently edited.
- * 
+ *
  * @class MODx.grid.RecentlyEditedResourcesByUser
  * @extends MODx.grid.Grid
  * @param {Object} config An object of options.
@@ -15,6 +15,8 @@ MODx.grid.RecentlyEditedResourcesByUser = function(config) {
             action: 'security/user/getRecentlyEditedResources'
             ,user: config.user
         }
+        ,autosave: true
+        ,save_action: 'resource/updatefromgrid'
         ,pageSize: 10
         ,fields: ['id','pagetitle','description','editedon','deleted','published','menu']
         ,columns: [{
@@ -33,13 +35,21 @@ MODx.grid.RecentlyEditedResourcesByUser = function(config) {
             ,editor: { xtype: 'combo-boolean' ,renderer: 'boolean' }
         }]
         ,paging: true
-        ,autosave: true
+        ,listeners: {
+            'afteredit': {fn: this.refresh, scope: this}
+        }
     });
     MODx.grid.RecentlyEditedResourcesByUser.superclass.constructor.call(this,config);
 };
-Ext.extend(MODx.grid.RecentlyEditedResourcesByUser,MODx.grid.Grid,{    
+Ext.extend(MODx.grid.RecentlyEditedResourcesByUser,MODx.grid.Grid,{
     preview: function() {
         window.open(MODx.config.base_url+'index.php?id='+this.menu.record.id);
+    }
+    ,refresh: function() {
+        var tree = Ext.getCmp('modx-resource-tree');
+        if (tree && tree.rendered) {
+            tree.refresh();
+        }
     }
 });
 Ext.reg('modx-grid-user-recent-resource',MODx.grid.RecentlyEditedResourcesByUser);
