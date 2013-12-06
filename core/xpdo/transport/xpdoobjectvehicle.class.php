a6c280a56203998f0a116869a91915f7cc0536d3
Romain <romain@melting-media.com>
2013-12-06 19:20:30 +0100

    Proposal for silent failure validation

--- core/xpdo/transport/xpdoobjectvehicle.class.php
+++ core/xpdo/transport/xpdoobjectvehicle.class.php
@@ -229,7 +229,7 @@ class xPDOObjectVehicle extends xPDOVehicle {
                     if ($transport->xpdo->getDebug() === true) $transport->xpdo->log(xPDO::LOG_LEVEL_DEBUG, 'Could not resolve vehicle: ' . print_r($vOptions, true));
                 }
             } else {
-                $transport->xpdo->log(xPDO::LOG_LEVEL_ERROR, "Could not validate vehicle object of class {$vClass}; criteria: " . print_r($criteria, true));
+                //$transport->xpdo->log(xPDO::LOG_LEVEL_ERROR, "Could not validate vehicle object of class {$vClass}; criteria: " . print_r($criteria, true));
                 if ($transport->xpdo->getDebug() === true) $transport->xpdo->log(xPDO::LOG_LEVEL_DEBUG, 'Could not validate vehicle object: ' . print_r($vOptions, true));
             }
         } else {
