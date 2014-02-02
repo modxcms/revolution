86206255f97355ff9496b92157e53bb9adb56910
Jason Coward <jason@opengeek.com>
2014-02-01 17:38:11 -0700

    Improved "prevent navigation" alert by tracking field changes instead of using flawed isDirty() check.
    The FormPanel.isDirty() check does a raw comparison between the original value and the current value of each of the fields. When using a rich text editor, there could be changes in the current content value of a resource due to changes in formatting (editors forcing a <p> root block) or encoding (& being turned into &amp;) which would result in the navigation popup. While yes, there are changes, no, the user did not make them so they should not be bothered with a popup.
    
    The commit changes from looking at FormPanel.isDirty() to tracking field changes through the changeField event and a local flag.
    
    This commit also resolves an issue where changing templates would show the warning during the reload.
    
    (cherry picked from commit 5f24bb7fa025e4ae9c07d903e13d1c53b0c2fd41)
    
    Conflicts:
    	manager/assets/modext/widgets/resource/modx.panel.resource.js

--- manager/assets/modext/widgets/resource/modx.panel.resource.js
+++ manager/assets/modext/widgets/resource/modx.panel.resource.js
@@ -19,6 +19,7 @@ MODx.panel.Resource = function(config) {
             ,'success': {fn:this.success,scope:this}
             ,'failure': {fn:this.failure,scope:this}
             ,'beforeSubmit': {fn:this.beforeSubmit,scope:this}
+            ,'fieldChange': {fn:this.onFieldChange,scope:this}
         }
     });
     MODx.panel.Resource.superclass.constructor.call(this,config);
@@ -36,6 +37,7 @@ Ext.extend(MODx.panel.Resource,MODx.FormPanel,{
     ,classLexiconKey: 'document'
     ,rteElements: 'ta'
     ,rteLoaded: false
+    ,warnUnsavedChanges: false
     ,setup: function() {
         if (!this.initialized) {
             this.getForm().setValues(this.config.record);
@@ -66,7 +68,7 @@ Ext.extend(MODx.panel.Resource,MODx.FormPanel,{
             if (MODx.config.confirm_navigation == 1) {
                 var panel = this;
                 window.onbeforeunload = function() {
-                    if (panel.isDirty()) return _('unsaved_changes');
+                    if (panel.warnUnsavedChanges) return _('unsaved_changes');
                 };
             }
 
@@ -129,6 +131,7 @@ Ext.extend(MODx.panel.Resource,MODx.FormPanel,{
         });
     }
     ,success: function(o) {
+        this.warnUnsavedChanges = false;
         var g = Ext.getCmp('modx-grid-resource-security');
         if (g) { g.getStore().commitChanges(); }
         var t = Ext.getCmp('modx-resource-tree');
@@ -159,6 +162,7 @@ Ext.extend(MODx.panel.Resource,MODx.FormPanel,{
         }
     }
     ,failure: function(o) {
+        this.warnUnsavedChanges = true;
         if(this.getForm().baseParams.action == 'resource/create') {
             var btn = Ext.getCmp('modx-abtn-save');
             if (btn) { btn.enable(); }
@@ -184,6 +188,7 @@ Ext.extend(MODx.panel.Resource,MODx.FormPanel,{
                     var nt = t.getValue();
                     var f = Ext.getCmp('modx-page-update-resource');
                     f.config.action = 'resource/reload';
+                    this.warnUnsavedChanges = false;
                     MODx.activePage.submitForm({
                         success: {fn:function(r) {
                             MODx.loadPage(r.result.object.action, 'id='+r.result.object.id+'&reload='+r.result.object.reload + '&class_key='+ r.result.object.class_key);
@@ -199,6 +204,12 @@ Ext.extend(MODx.panel.Resource,MODx.FormPanel,{
             },this);
         }
     }
+    ,onFieldChange: function() {
+        if (this.isReady || MODx.request.reload) {
+            this.warnUnsavedChanges = true;
+        }
+    }
+
     ,cleanupEditor: function() {
         if (MODx.onSaveEditor) {
             var fld = Ext.getCmp('ta');
