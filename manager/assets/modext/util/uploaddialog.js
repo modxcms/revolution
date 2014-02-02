0fdc647531f24e188b30c4cab4dcac9807cf32df
Mike Reid <mike@pixelchutes.com>
2014-02-02 10:30:50 -0700

    Added support for browsers not supporting the File API (IE < 10)

--- manager/assets/modext/util/uploaddialog.js
+++ manager/assets/modext/util/uploaddialog.js
@@ -975,9 +975,11 @@ Ext.extend(Ext.ux.UploadDialog.Dialog, Ext.Window,{
     input_file.dom.disabled = true;
     
     var store = this.grid_panel.getStore();
+    var filesApi = document.getElementById(input_file.id).files;
+    var filename = (typeof filesApi != 'undefined') ? filesApi[0].name : input_file.dom.value.replace("C:\\fakepath\\", "");
     store.add(new Ext.ux.UploadDialog.FileRecord({
           state: Ext.ux.UploadDialog.FileRecord.STATE_QUEUE
-          ,filename: document.getElementById(input_file.id).files[0].name
+          ,filename: filename
           ,note: this.i18n.note_queued_to_upload
           ,input_element: input_file
     }));
