cd0358d475a1cba4bfc494785a847349c970684e
Romain <romain@melting-media.com>
2014-02-07 09:51:00 +0100

    Allow FormPanel into dashboard widgets + Load Recently edited resources assets only when the widget is enabled

--- manager/controllers/default/dashboard/widget.grid-rer.php
+++ manager/controllers/default/dashboard/widget.grid-rer.php
@@ -5,13 +5,14 @@
  */
 /**
  * Renders a grid of recently edited resources by the active user
- * 
+ *
  * @package modx
  * @subpackage dashboard
  */
 class modDashboardWidgetRecentlyEditedResources extends modDashboardWidgetInterface {
     public function render() {
-        $this->modx->controller->addHtml('<script type="text/javascript">Ext.onReady(function() {
+        $this->controller->addJavascript($this->modx->getOption('manager_url').'assets/modext/widgets/security/modx.grid.user.recent.resource.js');
+        $this->controller->addHtml('<script type="text/javascript">Ext.onReady(function() {
     MODx.load({
         xtype: "modx-grid-user-recent-resource"
         ,user: "'.$this->modx->user->get('id').'"
