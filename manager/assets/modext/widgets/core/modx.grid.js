056463ca787f39014565e6ac000882500344d279
Romain <romain@melting-media.com>
2014-03-12 11:22:46 +0100

    Update according to comments with @TheBoxer

--- manager/assets/modext/widgets/core/modx.grid.js
+++ manager/assets/modext/widgets/core/modx.grid.js
@@ -98,14 +98,14 @@ MODx.grid.Grid = function(config) {
         this.getBottomToolbar().bind(this.store);
     }
 
-    var storeParams = {
-        start: config.pageStart || 0
-    };
-    if (config.paging) {
-        storeParams.limit = config.hasOwnProperty('pageSize') ? config.pageSize : (parseInt(MODx.config.default_per_page) || 20);
+    if (!config.paging && !config.hasOwnProperty('pageSize')) {
+        config.pageSize = 0;
     }
     this.getStore().load({
-        params: storeParams
+        params: {
+            start: config.pageStart || 0
+            ,limit: config.hasOwnProperty('pageSize') ? config.pageSize : (parseInt(MODx.config.default_per_page) || 20)
+        }
     });
     this.getStore().on('exception',this.onStoreException,this);
     this.config = config;
