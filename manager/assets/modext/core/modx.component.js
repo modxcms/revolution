modxcms/revolution

commit aa0cab2856efbf13b4c0598e97456f076f5e3b6c

Removed hard coded "welcome" action

manager/assets/modext/core/modx.component.js
@@ -288,9 +288,11 @@ Ext.extend(MODx.toolbar.ActionButtons,Ext.Toolbar,{
             } else {
                 Ext.Msg.alert(_('error'),_('correct_errors'));
             }
-        } else { /* if just doing a URL redirect */
-            Ext.applyIf(itm.params || {},o.baseParams || {});
-            MODx.loadPage('?'+Ext.urlEncode(itm.params));
+        } else {
+            // if just doing a URL redirect
+            var params = itm.params || {};
+            Ext.applyIf(params, o.baseParams || {});
+            MODx.loadPage('?' + Ext.urlEncode(params));
         }
         return false;
     }

manager/assets/modext/sections/element/chunk/create.js
@@ -1,20 +1,15 @@
 /**
  * Loads the chunk create page
- * 
+ *
  * @class MODx.page.CreateChunk
  * @extends MODx.Component
  * @param {Object} config An object of config properties
  * @xtype modx-page-chunk-create
  */
 MODx.page.CreateChunk = function(config) {
-	config = config || {};	
+	config = config || {};
 	Ext.applyIf(config,{
 		formpanel: 'modx-panel-chunk'
-        ,actions: {
-            'new': 'element/chunk/create'
-            ,'edit': 'element/chunk/update'
-            ,'cancel': 'welcome'
-        }
         ,buttons: [{
             process: 'element/chunk/create'
             ,text: _('save')
@@ -26,9 +21,7 @@ MODx.page.CreateChunk = function(config) {
                 ,ctrl: true
             }]
         },'-',{
-            process: 'welcome'
-            ,text: _('cancel')
-            ,params: {a:'welcome'}
+            text: _('cancel')
         }/*,'-',{
             text: _('help_ex')
             ,handler: MODx.loadHelpPane

manager/assets/modext/sections/element/chunk/update.js
@@ -10,11 +10,6 @@ MODx.page.UpdateChunk = function(config) {
 	config = config || {};
 	Ext.applyIf(config,{
 	    formpanel: 'modx-panel-chunk'
-        ,actions: {
-            'new': 'element/chunk/create'
-            ,'edit': 'element/chunk/update'
-            ,'cancel': 'welcome'
-        }
         ,buttons: [{
             process: 'element/chunk/update'
             ,text: _('save')
@@ -29,9 +24,7 @@ MODx.page.UpdateChunk = function(config) {
             ,handler: this.duplicate
             ,scope: this
         },'-',{
-            process: 'welcome'
-            ,text: _('cancel')
-            ,params: {a:'welcome'}
+            text: _('cancel')
         }/*,'-',{
             text: _('help_ex')
             ,handler: MODx.loadHelpPane

manager/assets/modext/sections/element/plugin/create.js
@@ -1,6 +1,6 @@
 /**
  * Loads the create plugin page
- * 
+ *
  * @class MODx.page.CreatePlugin
  * @extends MODx.Component
  * @param {Object} config An object of config properties
@@ -10,11 +10,6 @@ MODx.page.CreatePlugin = function(config) {
     config = config || {};
     Ext.applyIf(config,{
         formpanel: 'modx-panel-plugin'
-        ,actions: {
-            'new': 'element/plugin/create'
-            ,'edit': 'element/plugin/update'
-            ,'cancel': 'welcome'
-        }
         ,buttons: [{
             process: 'element/plugin/create'
             ,text: _('save')
@@ -26,9 +21,7 @@ MODx.page.CreatePlugin = function(config) {
                 ,ctrl: true
             }]
         },'-',{
-            process: 'welcome'
-            ,text: _('cancel')
-            ,params: {a:'welcome'}
+            text: _('cancel')
         },'-',{
             text: _('help_ex')
             ,handler: MODx.loadHelpPane

manager/assets/modext/sections/element/plugin/update.js
@@ -10,11 +10,6 @@ MODx.page.UpdatePlugin = function(config) {
     config = config || {};
     Ext.applyIf(config,{
         formpanel: 'modx-panel-plugin'
-        ,actions: {
-            'new': 'element/plugin/create'
-            ,'edit': 'element/plugin/update'
-            ,'cancel': 'welcome'
-        }
         ,buttons: [{
             process: 'element/plugin/update'
             ,text: _('save')
@@ -29,9 +24,7 @@ MODx.page.UpdatePlugin = function(config) {
             ,handler: this.duplicate
             ,scope: this
         },'-',{
-            process: 'welcome'
-            ,text: _('cancel')
-            ,params: {a:'welcome'}
+            text: _('cancel')
         }/*,'-',{
             text: _('help_ex')
             ,handler: MODx.loadHelpPane

manager/assets/modext/sections/element/snippet/create.js
@@ -1,6 +1,6 @@
 /**
  * Loads the create snippet page
- * 
+ *
  * @class MODx.page.CreateSnippet
  * @extends MODx.Component
  * @param {Object} config An object of config properties
@@ -10,11 +10,6 @@ MODx.page.CreateSnippet = function(config) {
     config = config || {};
     Ext.applyIf(config,{
         formpanel: 'modx-panel-snippet'
-        ,actions: {
-            'new': 'element/snippet/create'
-            ,'edit': 'element/snippet/update'
-            ,'cancel': 'welcome'
-        }
         ,buttons: [{
             process: 'element/snippet/create'
             ,text: _('save')
@@ -26,9 +21,7 @@ MODx.page.CreateSnippet = function(config) {
                 ,ctrl: true
             }]
         },'-',{
-            process: 'welcome'
-            ,text: _('cancel')
-            ,params: {a:'welcome'}
+            text: _('cancel')
         }/*,'-',{
             text: _('help_ex')
             ,handler: MODx.loadHelpPane

manager/assets/modext/sections/element/snippet/update.js
@@ -10,11 +10,6 @@ MODx.page.UpdateSnippet = function(config) {
     config = config || {};
     Ext.applyIf(config,{
         formpanel: 'modx-panel-snippet'
-        ,actions: {
-            'new': 'element/snippet/create'
-            ,'edit': 'element/snippet/update'
-            ,'cancel': 'welcome'
-        }
         ,buttons: [{
             process: 'element/snippet/update'
             ,text: _('save')
@@ -29,9 +24,7 @@ MODx.page.UpdateSnippet = function(config) {
             ,handler: this.duplicate
             ,scope: this
         },'-',{
-            process: 'welcome'
-            ,text: _('cancel')
-            ,params:{a:'welcome'}
+            text: _('cancel')
         }/*,'-',{
             text: _('help_ex')
             ,handler: MODx.loadHelpPane

manager/assets/modext/sections/element/template/create.js
@@ -1,6 +1,6 @@
 /**
  * Loads the create template page
- * 
+ *
  * @class MODx.page.CreateTemplate
  * @extends MODx.Component
  * @param {Object} config An object of config properties
@@ -10,11 +10,6 @@ MODx.page.CreateTemplate = function(config) {
     config = config || {};
     Ext.applyIf(config,{
         formpanel: 'modx-panel-template'
-        ,actions: {
-            'new': 'element/template/create'
-            ,'edit': 'element/template/update'
-            ,'cancel': 'welcome'
-        }
         ,buttons: [{
             process: 'element/template/create'
             ,text: _('save')
@@ -26,9 +21,7 @@ MODx.page.CreateTemplate = function(config) {
                 ,ctrl: true
             }]
         },'-',{
-            process: 'welcome'
-            ,text: _('cancel')
-            ,params: {a:'welcome'}
+            text: _('cancel')
         }/*,'-',{
             text: _('help_ex')
             ,handler: MODx.loadHelpPane

commit 6a570906800e22a876487837f3e20db9b27ab7be

Fix issue introduced in aa0cab2856efbf13b4c0598e97456f076f5e3b6c

manager/assets/modext/core/modx.component.js
@@ -316,7 +316,15 @@ Ext.extend(MODx.toolbar.ActionButtons,Ext.Toolbar,{
             if (MODx.request.parent) { itm.params.parent = MODx.request.parent; }
             if (MODx.request.context_key) { itm.params.context_key = MODx.request.context_key; }
             url = Ext.urlEncode(itm.params);
-            MODx.loadPage(o.actions.edit, url);
+            var action;
+            if (o.actions && o.actions.edit) {
+                // If an edit action is given, use it (BC)
+                action = o.actions.edit;
+            } else {
+                // Else assume we want the 'update' controller
+                action = itm.process.replace('create', 'update');
+            }
+            MODx.loadPage(action, url);
 
         } else if (process === 'delete') {
             itm.params.a = o.actions.cancel;