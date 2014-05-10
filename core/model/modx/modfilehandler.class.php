c79bb1f0dfdd9a02eadff0638e6bcabb55720739
exside <luk@exside.ch>
2014-05-10 03:21:59 +0200

    modFileHandler: Added method unpack() to extract ZIP files

--- core/model/modx/modfilehandler.class.php
+++ core/model/modx/modfilehandler.class.php
@@ -111,7 +111,7 @@ class modFileHandler {
      * @return string The sanitized path
      */
     public function sanitizePath($path) {
-        return preg_replace(array('/\.*[\/|\\\]/i', '/[\/|\\\]+/i'), array('/', '/'), $path);
+        return preg_replace(array("/\.*[\/|\\\]/i", "/[\/|\\\]+/i"), array('/', '/'), $path);
     }
 
     /**
@@ -195,11 +195,13 @@ abstract class modFileSystemResource {
     }
 
     /**
-     * Get the path of the fs resource.
-     * @return string The path of the fs resource
+     * Parses a string mode into octal format
+     *
+     * @param string $mode The octal to parse
+     * @return octal The new mode in octal format
      */
-    public function getPath() {
-        return $this->path;
+    protected function parseMode($mode = '') {
+        return octdec($mode);
     }
 
     /**
@@ -297,28 +299,11 @@ abstract class modFileSystemResource {
     }
 
     /**
-     * Renames the file/folder
-     *
-     * @param string $newPath The new path for the fs resource
-     * @return boolean True if successful
+     * Get the path of the fs resource.
+     * @return string The path of the fs resource
      */
-    public function rename($newPath) {
-        $newPath = $this->fileHandler->sanitizePath($newPath);
-
-        if (!$this->isWritable()) return false;
-        if (file_exists($newPath)) return false;
-
-        return @rename($this->path, $newPath);
-    }
-
-    /**
-     * Parses a string mode into octal format
-     *
-     * @param string $mode The octal to parse
-     * @return octal The new mode in octal format
-     */
-    protected function parseMode($mode = '') {
-        return octdec($mode);
+    public function getPath() {
+        return $this->path;
     }
 
     /**
@@ -336,6 +321,21 @@ abstract class modFileSystemResource {
 
         $directory = $this->fileHandler->make($ppath,array(),'modDirectory');
         return $directory;
+    }
+
+    /**
+     * Renames the file/folder
+     *
+     * @param string $newPath The new path for the fs resource
+     * @return boolean True if successful
+     */
+    public function rename($newPath) {
+        $newPath = $this->fileHandler->sanitizePath($newPath);
+
+        if (!$this->isWritable()) return false;
+        if (file_exists($newPath)) return false;
+
+        return @rename($this->path, $newPath);
     }
 }
 
@@ -361,27 +361,6 @@ class modFile extends modFileSystemResource {
     }
 
     /**
-     * Actually create the file on the file system
-     *
-     * @param string $content The content of the file to write
-     * @param string $mode The perms to write with the file
-     * @return boolean True if successful
-     */
-    public function create($content = '', $mode = 'w+') {
-        if ($this->exists()) return false;
-        $result = false;
-
-        $fp = @fopen($this->path, 'w+');
-        if ($fp) {
-            @fwrite($fp, $content);
-            @fclose($fp);
-
-            $result = file_exists($this->path);
-        }
-        return $result;
-    }
-
-    /**
      * Temporarly set (but not save) the content of the file
      * @param string $content The content
      */
@@ -396,39 +375,6 @@ class modFile extends modFileSystemResource {
      */
     public function getContents() {
         return @file_get_contents($this->path);
-    }
-
-    /**
-     * Alias for save()
-     *
-     * @see modDirectory::write
-     * @param string $content
-     * @param string $mode
-     * @return boolean
-     */
-    public function write($content = null, $mode = 'w+') {
-        return $this->save($content, $mode);
-    }
-
-    /**
-     * Writes the content of the modFile object to the actual file.
-     *
-     * @param string $content Optional. If not using setContent, this will set
-     * the content to write.
-     * @param string $mode The mode in which to write
-     * @return boolean The result of the fwrite
-     */
-    public function save($content = null, $mode = 'w+') {
-        if ($content !== null) $this->content = $content;
-        $result = false;
-
-        $fp = @fopen($this->path, $mode);
-        if ($fp) {
-            $result = @fwrite($fp, $this->content);
-            @fclose($fp);
-        }
-
-        return $result;
     }
 
     /**
@@ -479,6 +425,60 @@ class modFile extends modFileSystemResource {
     }
 
     /**
+     * Actually create the file on the file system
+     *
+     * @param string $content The content of the file to write
+     * @param string $mode The perms to write with the file
+     * @return boolean True if successful
+     */
+    public function create($content = '', $mode = 'w+') {
+        if ($this->exists()) return false;
+        $result = false;
+
+        $fp = @fopen($this->path, 'w+');
+        if ($fp) {
+            @fwrite($fp, $content);
+            @fclose($fp);
+
+            $result = file_exists($this->path);
+        }
+        return $result;
+    }
+
+    /**
+     * Alias for save()
+     *
+     * @see modDirectory::write
+     * @param string $content
+     * @param string $mode
+     * @return boolean
+     */
+    public function write($content = null, $mode = 'w+') {
+        return $this->save($content, $mode);
+    }
+
+    /**
+     * Writes the content of the modFile object to the actual file.
+     *
+     * @param string $content Optional. If not using setContent, this will set
+     * the content to write.
+     * @param string $mode The mode in which to write
+     * @return boolean The result of the fwrite
+     */
+    public function save($content = null, $mode = 'w+') {
+        if ($content !== null) $this->content = $content;
+        $result = false;
+
+        $fp = @fopen($this->path, $mode);
+        if ($fp) {
+            $result = @fwrite($fp, $this->content);
+            @fclose($fp);
+        }
+
+        return $result;
+    }
+
+    /**
      * Deletes the file from the filesystem
      *
      * @return boolean True if successful
@@ -486,6 +486,99 @@ class modFile extends modFileSystemResource {
     public function remove() {
         if (!$this->exists()) return false;
         return @unlink($this->path);
+    }
+
+    /**
+     * Unpack a zip archive to a specified location.
+     *
+     * @uses compression.xPDOZip OR compression.PclZip
+     *
+     * @param string $this->getPath() An absolute file system location to a valid zip archive.
+     * @param string $to A file system location to extract the contents of the archive to.
+     * @param array $options an array of optional options, primarily for the xPDOZip class
+     * @return array|string|boolean An array of unpacked files, a string in case of cli functions or false on failure.
+     */
+    public function unpack($to = '', $options = array()) {
+
+        $results = false;
+
+        // extract the archive in the same directory if no $to path is specified
+        if (empty($to)) {
+            $to = $this->getParentDirectory(true);
+        }
+
+        if (class_exists('ZipArchive') && $this->fileHandler->modx->getService('zip', 'compression.xPDOZip', XPDO_CORE_PATH, array_merge($options, array('file' => $this->path)))) {
+
+            $results = $this->fileHandler->modx->zip->unpack($to);
+            $this->fileHandler->modx->zip->close();
+        } else if (class_exists('PclZip') || include_once(XPDO_CORE_PATH . 'compression/pclzip.lib.php')) {
+
+            $archive = new PclZip($this->path);
+
+            if ($archive) {
+                $results = array_map('current', $archive->extract(PCLZIP_OPT_PATH, $to));
+            }
+        } else if (is_callable('zip_open')) {
+
+            $archive = zip_open($this->path);
+
+            if (!is_dir($to)) {
+                $this->fileHandler->modx->cacheManager->writeTree($to);
+            }
+
+            while ($zip_entry = zip_read($archive)) {
+                $filename = zip_entry_name($zip_entry);
+                $target = $to . DIRECTORY_SEPARATOR . substr($filename, 0, strrpos($filename, '/'));
+                $filesize = zip_entry_filesize($zip_entry);
+
+                if (file_exists($target) || mkdir($target)) {
+                    $results[] = $to . DIRECTORY_SEPARATOR . $filename;
+
+                    if ($filesize > 0) {
+                        $contents = zip_entry_read($zip_entry, $filesize);
+                        file_put_contents($to . DIRECTORY_SEPARATOR . $filename, $contents);
+                    }
+                }
+            }
+        } else {
+            // check for windows systems running PHP, as they do not seem to have a native unzip functionality
+            if (strtoupper(substr(PHP_OS, 0, 3)) !== 'WIN') {
+                $sysfns = array(
+                    'shell_exec' => false,
+                    'system' => false,
+                    'exec' => false,
+                );
+
+                $disabled = array_map('trim', explode(',', ini_get('disable_functions')));
+
+                foreach ($sysfns as $func => $value) {
+                    $sysfns[$func] = !in_array($func, $disabled);
+                }
+
+                $sysfns = array_filter($sysfns); // remove not available system functions
+
+                if (!empty($sysfns)) {
+                    $sysfn = key($sysfns); // get the first available system function
+                    $command = implode(' ', array(
+                        'unzip',
+                        $this->path
+                    ));
+                    
+                    // sanitize and escape command
+                    $command = escapeshellcmd(preg_replace('#(\s){2,}#is', ' ', str_replace(array("\n", "'"), array('', '"'), $command)));
+
+                    if (!is_dir($to)) {
+                        $this->fileHandler->modx->cacheManager->writeTree($to);
+                    }
+
+                    chdir($to); // switch to target directory
+
+                    $results = $sysfn($command); // execute command
+                }
+            }
+        }
+
+        return $results;
     }
 }
 
@@ -495,6 +588,17 @@ class modFile extends modFileSystemResource {
  * @package modx
  */
 class modDirectory extends modFileSystemResource {
+    /**
+     * @see modFileSystemResource::parseMode
+     *
+     * @param string $mode
+     * @return boolean
+     */
+    protected function parseMode($mode = '') {
+        if (empty($mode)) $mode = $this->fileHandler->context->getOption('new_folder_permissions', '0755', $this->fileHandler->config);
+        return parent::parseMode($mode);
+    }
+
     /**
      * Actually creates the new directory on the file system.
      *
@@ -511,17 +615,6 @@ class modDirectory extends modFileSystemResource {
         return $this->fileHandler->modx->cacheManager->writeTree($this->path,array(
             'new_folder_permissions' => $mode,
         ));
-    }
-
-    /**
-     * @see modFileSystemResource::parseMode
-     *
-     * @param string $mode
-     * @return boolean
-     */
-    protected function parseMode($mode = '') {
-        if (empty($mode)) $mode = $this->fileHandler->context->getOption('new_folder_permissions', '0755', $this->fileHandler->config);
-        return parent::parseMode($mode);
     }
 
     /**
