e12861df2aab91eef17a23ae3c181fc9b78adb4c
Gildas NOEL <g.noel@ackwa.fr>
2014-06-06 17:46:17 +0200

    Minor corrections

--- core/lexicon/fr/tv_widget.inc.php
+++ core/lexicon/fr/tv_widget.inc.php
@@ -29,7 +29,7 @@ $_lang['combo_stackitems_desc'] = 'Les éléments sont empilés (1 par ligne). N
 $_lang['combo_title'] = 'Entête de liste';
 $_lang['combo_title_desc'] = 'Un élément d\'entête contenant ce texte est créé et ajouté en haut de la liste.';
 $_lang['combo_typeahead'] = 'Activer l\'auto-complétion';
-$_lang['combo_typeahead_desc'] = 'Complète et sélectionne automatiquement le texte saisi, suivant le délais indiqué (délais d\'auto-complétion) et si une valeur existante correspond (désactivé par défaut).';
+$_lang['combo_typeahead_desc'] = 'Complète et sélectionne automatiquement le texte saisi, suivant le délai indiqué (délais d\'auto-complétion) et si une valeur existante correspond (désactivé par défaut).';
 $_lang['combo_typeahead_delay'] = 'Délais d\'auto-complétion';
 $_lang['combo_typeahead_delay_desc'] = 'Temps d\'attente, en millisecondes, avant que l\'auto-complétion ne soit affichée (si activée). 250 par défaut.';
 $_lang['date'] = 'Date';
@@ -45,7 +45,7 @@ $_lang['disabled_dates_desc'] = 'Liste de chaînes de dates, séparées par des 
 - commence par (utilise si vous utilisez des années courtes): ^03-08<br />
 - désactiver tous les jours de mars 2006: 03-..-2006<br />
 - désactiver tous les jours de mars: ^03<br />
-Notez que le format des dates inclues dans la liste doit exactement correspondre au format configuré. Pour supporter les expressions régulières, si vous utilisez un format de date contenant un ".", vous devrez echapper le point lorsque vous restreindrez les dates.';
+Notez que le format des dates dans la liste doit exactement correspondre au format configuré. Pour supporter les expressions régulières, si vous utilisez un format de date contenant un ".", vous devrez "échapper" le point lorsque vous restreindrez les dates.';
 $_lang['disabled_days'] = 'Jours désactivés';
 $_lang['disabled_days_desc'] = 'Liste de jours, séparés par des virgules, à désactiver, basé sur 0 (nul par défaut). Quelques exemples : <br />
 - désactiver dimanche et samedi: 0,6<br />
@@ -94,7 +94,7 @@ $_lang['radio_columns_desc'] = 'Nombre de colonnes dans lesquelles les boutons r
 $_lang['rawtext'] = 'Texte brut (déprécié)';
 $_lang['rawtextarea'] = 'Zone de texte brut (déprécié)';
 $_lang['required'] = 'Optionnel';
-$_lang['required_desc'] = 'Si non est sélectionné, MODX n\'autorisera pas l\'utilisateur à enregistrer la ressource jusqu\'à ce qu\'une valeur valide et non nulle soit entrée.';
+$_lang['required_desc'] = 'Si la valeur non est sélectionnée, MODX n\'autorisera pas l\'utilisateur à enregistrer la ressource jusqu\'à ce qu\'une valeur valide et non nulle soit entrée.';
 $_lang['resourcelist'] = 'Liste de ressources';
 $_lang['resourcelist_depth'] = 'Profondeur';
 $_lang['resourcelist_depth_desc'] = 'Niveaux de profondeur jusqu\'à laquelle chercher pour obtenir la liste de ressources. 10 niveaux par défaut.';
