2b228456aec0e0fb9ab5be2639b363efa9d35aba
exside <luk@exside.ch>
2014-07-23 17:02:40 +0200

    Fix user settings update / delete

--- manager/assets/modext/widgets/security/modx.grid.user.settings.js
+++ manager/assets/modext/widgets/security/modx.grid.user.settings.js
@@ -37,5 +37,51 @@ MODx.grid.UserSettings = function(config) {
     });
     MODx.grid.UserSettings.superclass.constructor.call(this,config);
 };
-Ext.extend(MODx.grid.UserSettings,MODx.grid.SettingsGrid);
+Ext.extend(MODx.grid.UserSettings,MODx.grid.SettingsGrid {
+    _showMenu: function(g,ri,e) {
+        e.stopEvent();
+        e.preventDefault();
+        this.menu.record = this.getStore().getAt(ri).data;
+        if (!this.getSelectionModel().isSelected(ri)) {
+            this.getSelectionModel().selectRow(ri);
+        }
+        this.menu.removeAll();
+
+        var m = [];
+        if (this.menu.record.menu) {
+            m = this.menu.record.menu;
+        } else {
+            m.push({
+                text: _('setting_update')
+                ,handler: this.updateSetting
+            },'-',{
+                text: _('setting_remove')
+                ,handler: this.remove.createDelegate(this,['setting_remove_confirm', 'security/user/setting/remove'])
+            });
+        }
+        if (m.length > 0) {
+            this.addContextMenuItem(m);
+            this.menu.showAt(e.xy);
+        }
+    }
+
+    ,updateSetting: function(btn,e) {
+        var r = this.menu.record;
+        r.fk = Ext.isDefined(this.config.fk) ? this.config.fk : 0;
+        var uss = MODx.load({
+            xtype: 'modx-window-setting-update'
+            ,action: 'security/user/setting/update'
+            ,record: r
+            ,grid: this
+            ,listeners: {
+                'success': {fn:function(r) {
+                    this.refresh();
+                },scope:this}
+            }
+        });
+        uss.reset();
+        uss.setValues(r);
+        uss.show(e.target);
+    }
+});
 Ext.reg('modx-grid-user-settings',MODx.grid.UserSettings);
