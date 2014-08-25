modxcms/revolution

commit a1ae0917b08b64b8bf735146366d52fe0933dcf4

modError is instantiated when sending fails

core/model/modx/mail/modphpmailer.class.php
@@ -179,6 +179,11 @@ class modPHPMailer extends modMail {
     public function send(array $attributes= array()) {
         $sent = parent :: send($attributes);
         $sent = $this->mailer->Send();
+        if ($send !== true) {
+            $this->modx->loadClass('error.modError');
+            $this->error = new modError($this->modx);
+            $this->error->addError($this->mailer->ErrorInfo);
+        }
         return $sent;
     }
 

commit 0f75f08bcb3cb4cc2e33d31994fb722a99c81105

Fix typo resulting in empty calls to modError::addError() for all sent emails
Typo introduced in commit a1ae0917b08b64b8bf735146366d52fe0933dcf4

core/model/modx/mail/modphpmailer.class.php
@@ -179,7 +179,7 @@ class modPHPMailer extends modMail {
     public function send(array $attributes= array()) {
         $sent = parent :: send($attributes);
         $sent = $this->mailer->Send();
-        if ($send !== true) {
+        if ($sent !== true) {
             $this->modx->loadClass('error.modError');
             $this->error = new modError($this->modx);
             $this->error->addError($this->mailer->ErrorInfo);