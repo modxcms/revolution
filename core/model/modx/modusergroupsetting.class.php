a4338230a1e83572d3d0eae85cac633ff400a866
Kireev Vitaly <argnist88@gmail.com>
2014-11-25 09:49:05 +0700

    Class based security/group/setting/update processor

--- core/model/modx/modusergroupsetting.class.php
+++ core/model/modx/modusergroupsetting.class.php
@@ -17,4 +17,47 @@
  *
  * @package modx
  */
-class modUserGroupSetting extends xPDOObject {}
+class modUserGroupSetting extends xPDOObject {
+    /**
+     * Update the translation for the setting
+     *
+     * @param string $key The key of the Setting to update
+     * @param string $value The value of the Setting to update
+     * @param array $options An array of options for the update
+     * @return bool
+     */
+    public function updateTranslation($key,$value = '',array $options = array()) {
+        if (!is_array($options) || empty($options)) return false;
+
+        $options['namespace'] = $this->xpdo->getOption('namespace',$options,'core');
+        $options['cultureKey'] = $this->xpdo->getOption('cultureKey',$options,'en');
+        $options['topic'] = $options['namespace'] == 'core' ? 'setting' : 'default';
+        $saved = false;
+
+        $entries = $this->xpdo->lexicon->getFileTopic($options['cultureKey'],$options['namespace'],$options['topic']);
+        $entry = $this->xpdo->getObject('modLexiconEntry',array(
+            'name' => $key,
+            'namespace' => $options['namespace'],
+            'language' => $options['cultureKey'],
+            'topic' => $options['topic'],
+        ));
+        if ((!empty($entries[$key]) && $entries[$key] == $value) || strcmp($key,$value) == 0 || empty($value)) {
+            if ($entry) {
+                $saved = $entry->remove();
+                $this->xpdo->lexicon->clearCache($options['cultureKey'].'/'.$options['namespace'].'/'.$options['topic'].'.cache.php');
+            }
+        } else {
+            if ($entry == null) {
+                $entry = $this->xpdo->newObject('modLexiconEntry');
+                $entry->set('name',$key);
+                $entry->set('namespace',$options['namespace']);
+                $entry->set('language',$options['cultureKey']);
+                $entry->set('topic',$options['topic']);
+            }
+            $entry->set('value',$value);
+            $saved = $entry->save();
+            $entry->clearCache();
+        }
+        return $saved;
+    }
+}
