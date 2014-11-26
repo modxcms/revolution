57fc01a1894926bb78958fac323119e84f3d55c7
Kireev Vitaly <argnist88@gmail.com>
2014-11-26 17:42:56 +0700

    Fix wrong error for namespace create

--- core/model/modx/processors/workspace/namespace/create.class.php
+++ core/model/modx/processors/workspace/namespace/create.class.php
@@ -10,7 +10,7 @@
  */
 class modNamespaceCreateProcessor extends modObjectCreateProcessor {
     public $classKey = 'modNamespace';
-    public $languageTopics = array('workspace','namespace','lexicon');
+    public $languageTopics = array('namespace');
     public $permission = 'namespaces';
     public $objectType = 'namespace';
     public $primaryKeyField = 'name';
@@ -18,13 +18,14 @@ class modNamespaceCreateProcessor extends modObjectCreateProcessor {
     public function beforeSave() {
         $name = $this->getProperty('name');
         if (empty($name)) {
-            $this->addFieldError('name',$this->modx->lexicon('namespace_err_ns_name'));
+            $this->addFieldError('name', $this->modx->lexicon($this->objectType.'_err_ns'));
         }
-        $this->object->set('name',$name);
+        $this->object->set('name', $name);
 
-        $this->object->set('path',trim($this->object->get('path')));
-        $this->object->set('assets_path',trim($this->object->get('assets_path')));
+        $this->object->set('path', trim($this->object->get('path')));
+        $this->object->set('assets_path', trim($this->object->get('assets_path')));
         return parent::beforeSave();
     }
 }
+
 return 'modNamespaceCreateProcessor';
