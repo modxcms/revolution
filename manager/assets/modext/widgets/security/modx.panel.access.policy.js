3c8fb13599ed11a5e3572cca1c55b2021bc1acfa
Kireev Vitaly <argnist88@gmail.com>
2014-11-27 11:21:39 +0700

    Fix javascript error after click on permission row

--- manager/assets/modext/widgets/security/modx.panel.access.policy.js
+++ manager/assets/modext/widgets/security/modx.panel.access.policy.js
@@ -193,7 +193,7 @@ MODx.grid.PolicyPermissions = function(config) {
 Ext.extend(MODx.grid.PolicyPermissions,MODx.grid.LocalGrid,{
     onPermRowClick: function(g,ri,e) {
         var s = this.getStore();
-        if (!s) { return; }
+        if (!s || typeof ri == 'undefined') { return; }
 
         var r = s.getAt(ri);
         r.set('enabled',r.get('enabled') ? false : true);
