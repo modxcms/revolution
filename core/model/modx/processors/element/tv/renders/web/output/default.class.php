4a1c2f2167f7b191647894d507cb4fe8b8755f64
Nikolay Lanets <info@newpg.ru>
2015-01-21 23:24:07 +0300

    Do not remove || from value
    http://joxi.ru/n2Y87DVh0pRpm6

--- core/model/modx/processors/element/tv/renders/web/output/default.class.php
+++ core/model/modx/processors/element/tv/renders/web/output/default.class.php
@@ -6,10 +6,6 @@
 class modTemplateVarOutputRenderDefault extends modTemplateVarOutputRender {
     public function process($value,array $params = array()) {
         $value= $this->tv->parseInput($value);
-        if ($this->tv->get('type') == 'checkbox' || $this->tv->get('type') == 'listbox-multiple') {
-            // remove delimiter from checkbox and listbox-multiple TVs
-            $value= str_replace('||', '', $value);
-        }
         $o= (string) $value;
         return $o;
     }
