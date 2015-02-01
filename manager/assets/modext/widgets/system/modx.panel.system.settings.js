16d7a49eaeb8b87902ae4d89355a9a3e46192b4e
Bert Oost <bert@oostdesign.nl>
2015-02-01 02:23:57 +0100

    System Events Manager page

--- manager/assets/modext/widgets/system/modx.panel.system.settings.js
+++ manager/assets/modext/widgets/system/modx.panel.system.settings.js
@@ -14,25 +14,51 @@ MODx.panel.SystemSettings = function(config) {
         ,bodyStyle: ''
         ,defaults: { collapsible: false ,autoHeight: true }
         ,items: [{
-            html: '<h2>'+_('system_settings')+'</h2>'
+            html: '<h2>'+_('system_settings')+' & '+_('events')+'</h2>'
             ,border: false
             ,id: 'modx-system-settings-header'
             ,cls: 'modx-page-header'
-        },{
-            layout: 'form'
+        },MODx.getPageStructure([{
+            title: _('system_settings')
             ,autoHeight: true
-            ,defaults: { border: false }
-            ,items: [{
-                html: '<p>'+_('settings_desc')+'</p>'
-               ,bodyCssClass: 'panel-desc'
-            },{
-                xtype: 'modx-grid-system-settings'
-                ,cls: 'main-wrapper'
-                ,preventSaveRefresh: true
-            },{
-                html: MODx.onSiteSettingsRender
-            }]
-        }]
+            ,layout: 'form'
+            ,defaults: { border: false ,msgTarget: 'side' }
+			,items:[{
+				layout: 'form'
+				,autoHeight: true
+				,defaults: { border: false }
+				,items: [{
+					html: '<p>'+_('settings_desc')+'</p>'
+				   ,bodyCssClass: 'panel-desc'
+				},{
+					xtype: 'modx-grid-system-settings'
+					,cls: 'main-wrapper'
+					,preventSaveRefresh: true
+				},{
+					html: MODx.onSiteSettingsRender
+				}]
+			}]
+		},{
+            title: _('system_events')
+            ,autoHeight: true
+            ,layout: 'form'
+            ,defaults: { border: false ,msgTarget: 'side' }
+			,items:[{
+				layout: 'form'
+				,autoHeight: true
+				,defaults: { border: false }
+				,items: [{
+					html: '<p>'+_('system_events.desc')+'</p>'
+				   ,bodyCssClass: 'panel-desc'
+				},{
+					xtype: 'modx-grid-system-event'
+					,cls: 'main-wrapper'
+					,preventSaveRefresh: true
+				}]
+			}]
+		}],{
+            id: 'modx-context-tabs'
+        })]
     });
     MODx.panel.SystemSettings.superclass.constructor.call(this,config);
 };
