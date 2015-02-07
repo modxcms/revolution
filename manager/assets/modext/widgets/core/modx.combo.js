3f5bb0495fb55e75bf1b81ae6f95cf6058f2ce98
Oliver Haase-Lobinger <modx@mindeffects.de>
2015-02-07 21:03:08 +0100

    Update modx.combo.js

--- manager/assets/modext/widgets/core/modx.combo.js
+++ manager/assets/modext/widgets/core/modx.combo.js
@@ -104,7 +104,7 @@ MODx.combo.ComboBox = function(config,getStore) {
     });
     this.store.on('load', function() {
         // Workaround to let the combobox know the store is loaded (to help hide/display the pagination if required)
-        this.loaded = true;
+        this.fireEvent('loaded', this);
     }, this, {
         single: true
     });
