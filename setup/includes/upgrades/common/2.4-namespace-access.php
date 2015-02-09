260b7b2fe40faa467f2805a0710ef6330e8267c9
Jan Peca <pecajan@gmail.com>
2015-02-09 15:37:38 +0100

    Update principal_targets system setting on setup

--- setup/includes/upgrades/common/2.4-namespace-access.php
+++ setup/includes/upgrades/common/2.4-namespace-access.php
@@ -6,3 +6,16 @@ $classes = array(
 if (!empty($classes)) {
     $this->createTable($classes);
 }
+
+/** @var modSystemSetting $setting */
+$setting = $modx->getObject('modSystemSetting',array(
+    'key' => 'principal_targets',
+));
+if ($setting) {
+    $value = $setting->get('value');
+    $value = explode(',',$value);
+    $value[] = 'modAccessNamespace';
+    $value = array_unique($value);
+    $setting->set('value',implode(',',$value));
+    $setting->save();
+}
