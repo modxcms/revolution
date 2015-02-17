0098443aa64466238c9be8abd7d5271acb2febbc
Jan Peca <pecajan@gmail.com>
2015-02-17 11:26:17 +0100

    Don't count children hidden from tree as children

--- core/model/modx/processors/resource/getnodes.class.php
+++ core/model/modx/processors/resource/getnodes.class.php
@@ -166,7 +166,7 @@ class modResourceGetNodesProcessor extends modProcessor {
         );
         $this->itemClass= 'modResource';
         $c= $this->modx->newQuery($this->itemClass);
-        $c->leftJoin('modResource', 'Child', array('modResource.id = Child.parent'));
+        $c->leftJoin('modResource', 'Child', array('modResource.id = Child.parent AND Child.show_in_tree = true'));
         $c->select($this->modx->getSelectColumns('modResource', 'modResource', '', $resourceColumns));
         $c->select(array(
             'childrenCount' => 'COUNT(Child.id)',
