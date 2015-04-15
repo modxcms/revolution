4d9ceb5c915c537292aef737f5ba4f053370956e
Mark Willis <mark.willis@adi.do>
2015-04-15 19:47:45 +0100

    Updated paths

--- _build/test/generateConfigs.sh
+++ _build/test/generateConfigs.sh
@@ -44,8 +44,8 @@ CONFIG="${CONFIG/\{\$baseUrl\}//}"
 echo $CONFIG > ../../setup/config.xml
 
 echo "make core/cache and change permissions"
-mkdir ../core/cache
-chmod 777 -R ../
+mkdir ../../core/cache
+chmod 777 -R ../../
 
 echo "Run MODX setup"
 php ../../setup/index.php --installmode=new --config=../../setup/config.xml
