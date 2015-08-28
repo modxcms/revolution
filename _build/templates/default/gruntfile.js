78cc8a373c5966e6af2bc32e93cd8e8631e0c240
JP DeVries <johnpaul.devries@gmail.com>
2015-08-28 09:35:27 -0700

    Grunt watch for modext js files
    Also removing modx.jsgrps.js as it is no longer needed

--- _build/templates/default/gruntfile.js
+++ _build/templates/default/gruntfile.js
@@ -1,3 +1,36 @@
+var coreJSFiles = [
+    '<%= dirs.manager %>assets/modext/core/modx.localization.js',
+    '<%= dirs.manager %>assets/modext/util/utilities.js',
+    '<%= dirs.manager %>assets/modext/util/datetime.js',
+    '<%= dirs.manager %>assets/modext/util/uploaddialog.js',
+    '<%= dirs.manager %>assets/modext/util/fileupload.js',
+    '<%= dirs.manager %>assets/modext/util/superboxselect.js',
+    '<%= dirs.manager %>assets/modext/core/modx.component.js',
+    '<%= dirs.manager %>assets/modext/core/modx.view.js',
+    '<%= dirs.manager %>assets/modext/widgets/core/modx.button.js',
+    '<%= dirs.manager %>assets/modext/widgets/core/modx.searchbar.js',
+    '<%= dirs.manager %>assets/modext/widgets/core/modx.panel.js',
+    '<%= dirs.manager %>assets/modext/widgets/core/modx.tabs.js',
+    '<%= dirs.manager %>assets/modext/widgets/core/modx.window.js',
+    '<%= dirs.manager %>assets/modext/widgets/core/modx.combo.js',
+    '<%= dirs.manager %>assets/modext/widgets/core/modx.grid.js',
+    '<%= dirs.manager %>assets/modext/widgets/core/modx.console.js',
+    '<%= dirs.manager %>assets/modext/widgets/core/modx.portal.js',
+    '<%= dirs.manager %>assets/modext/widgets/windows.js',
+    '<%= dirs.manager %>assets/fileapi/FileAPI.js',
+    '<%= dirs.manager %>assets/modext/util/multiuploaddialog.js',
+    '<%= dirs.manager %>assets/modext/widgets/core/tree/modx.tree.js',
+    '<%= dirs.manager %>assets/modext/widgets/core/tree/modx.tree.treeloader.js',
+    '<%= dirs.manager %>assets/modext/widgets/modx.treedrop.js',
+    '<%= dirs.manager %>assets/modext/widgets/core/modx.tree.asynctreenode.js',
+    '<%= dirs.manager %>assets/modext/widgets/resource/modx.tree.resource.js',
+    '<%= dirs.manager %>assets/modext/widgets/element/modx.tree.element.js',
+    '<%= dirs.manager %>assets/modext/widgets/system/modx.tree.directory.js',
+    '<%= dirs.manager %>assets/modext/widgets/system/modx.panel.filetree.js',
+    '<%= dirs.manager %>assets/modext/widgets/system/modx.browser.js',
+    '<%= dirs.manager %>assets/modext/core/modx.layout.js'
+];
+
 module.exports = function(grunt) {
 	// Project configuration.
 	grunt.initConfig({
@@ -127,6 +160,10 @@ module.exports = function(grunt) {
                 },
                 files: ['<%= dirs.css %>*.css'],
                 tasks: []
+            },
+            js : {
+                files:coreJSFiles,
+                tasks:['compress']
             }
 		},
 		clean: { /* take out the trash */
@@ -182,44 +219,17 @@ module.exports = function(grunt) {
                     sourceMap:true
 				},
 				files: {
-                    '<%= dirs.manager %>assets/modext/modx.jsgrps-min.js': [
-                        '<%= dirs.manager %>assets/modext/core/modx.localization.js',
-                        '<%= dirs.manager %>assets/modext/util/utilities.js',
-                        '<%= dirs.manager %>assets/modext/util/datetime.js',
-                        '<%= dirs.manager %>assets/modext/util/uploaddialog.js',
-                        '<%= dirs.manager %>assets/modext/util/fileupload.js',
-                        '<%= dirs.manager %>assets/modext/util/superboxselect.js',
-                        '<%= dirs.manager %>assets/modext/core/modx.component.js',
-                        '<%= dirs.manager %>assets/modext/core/modx.view.js',
-                        '<%= dirs.manager %>assets/modext/widgets/core/modx.button.js',
-                        '<%= dirs.manager %>assets/modext/widgets/core/modx.searchbar.js',
-                        '<%= dirs.manager %>assets/modext/widgets/core/modx.panel.js',
-                        '<%= dirs.manager %>assets/modext/widgets/core/modx.tabs.js',
-                        '<%= dirs.manager %>assets/modext/widgets/core/modx.window.js',
-                        '<%= dirs.manager %>assets/modext/widgets/core/modx.combo.js',
-                        '<%= dirs.manager %>assets/modext/widgets/core/modx.grid.js',
-                        '<%= dirs.manager %>assets/modext/widgets/core/modx.console.js',
-                        '<%= dirs.manager %>assets/modext/widgets/core/modx.portal.js',
-                        '<%= dirs.manager %>assets/modext/widgets/windows.js',
-                        '<%= dirs.manager %>assets/fileapi/FileAPI.js',
-                        '<%= dirs.manager %>assets/modext/util/multiuploaddialog.js',
-                        '<%= dirs.manager %>assets/modext/widgets/core/tree/modx.tree.js',
-                        '<%= dirs.manager %>assets/modext/widgets/core/tree/modx.tree.treeloader.js',
-                        '<%= dirs.manager %>assets/modext/widgets/modx.treedrop.js',
-                        '<%= dirs.manager %>assets/modext/widgets/core/modx.tree.asynctreenode.js',
-                        '<%= dirs.manager %>assets/modext/widgets/resource/modx.tree.resource.js',
-                        '<%= dirs.manager %>assets/modext/widgets/element/modx.tree.element.js',
-                        '<%= dirs.manager %>assets/modext/widgets/system/modx.tree.directory.js',
-                        '<%= dirs.manager %>assets/modext/widgets/system/modx.panel.filetree.js',
-                        '<%= dirs.manager %>assets/modext/widgets/system/modx.browser.js',
-                        '<%= dirs.manager %>assets/modext/core/modx.layout.js'
-                    ]
+                    '<%= dirs.manager %>assets/modext/modx.jsgrps-min.js': coreJSFiles
 				}
 			}
 		},
 		growl: {
 			sass: {
 				message: "Sass files created.",
+				title: "grunt"
+			},
+			js: {
+				message: "Core JS concatenated and minified.",
 				title: "grunt"
 			},
 			map: {
