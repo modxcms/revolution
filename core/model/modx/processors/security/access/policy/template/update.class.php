fa2982ce841d2e01ffb8d3f459edb91eea0a95eb
Jan Tezner <jan.tezner@gmail.com>
2015-09-16 16:44:57 +0200

    Syntax cleanup

--- core/model/modx/processors/security/access/policy/template/update.class.php
+++ core/model/modx/processors/security/access/policy/template/update.class.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Updates a policy template
  *
@@ -10,18 +11,22 @@
  * @package modx
  * @subpackage processors.security.access.policy.template
  */
-class modAccessPolicyTemplateUpdateProcessor extends modObjectUpdateProcessor {
+class modAccessPolicyTemplateUpdateProcessor extends modObjectUpdateProcessor
+{
     public $classKey = 'modAccessPolicyTemplate';
     public $languageTopics = array('policy');
     public $permission = 'policy_template_save';
     public $objectType = 'policy';
 
-    public function afterSave() {
+    public function afterSave()
+    {
         /* now store the permissions into the modAccessPermission table */
         /* and cache the data into the policy table */
-        $permissions = $this->getProperty('permissions',null);
+        $permissions = $this->getProperty('permissions', null);
         if ($permissions !== null) {
-            $permissions = is_array($permissions) ? $permissions : $this->modx->fromJSON($permissions);
+            if (is_array($permissions)) {
+                $permissions = $this->modx->fromJSON($permissions);
+            }
 
             /* first erase all prior permissions */
             $oldPermissions = $this->object->getMany('Permissions');
@@ -32,17 +37,25 @@ class modAccessPolicyTemplateUpdateProcessor extends modObjectUpdateProcessor {
 
             $added = array();
             foreach ($permissions as $permissionArray) {
-                if (in_array($permissionArray['name'],$added)) continue;
+                if (in_array($permissionArray['name'], $added)) {
+                    continue;
+                }
+
                 $permission = $this->modx->newObject('modAccessPermission');
-                $permission->set('template',$this->object->get('id'));
-                $permission->set('name',$permissionArray['name']);
-                $permission->set('description',$permissionArray['description']);
-                $permission->set('value',true);
+
+                $permission->set('template', $this->object->get('id'));
+                $permission->set('name', $permissionArray['name']);
+                $permission->set('description', $permissionArray['description']);
+                $permission->set('value', true);
+
                 $permission->save();
+
                 $added[] = $permissionArray['name'];
             }
         }
+
         return parent::afterSave();
     }
 }
+
 return 'modAccessPolicyTemplateUpdateProcessor';
