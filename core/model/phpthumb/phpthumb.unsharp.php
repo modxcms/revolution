7eb5034f4409a44d07432b2916b8d2aaf13f62e7
Thomas Jakobi <thomas.jakobi@partout.info>
2016-05-30 00:28:35 +0200

    Update phpThumb to 1.7.14 - Update the files with the latest phpThumb repository files - Changed the encoding of some files to UTF-8 - Modified some wrong encoded chars

--- core/model/phpthumb/phpthumb.unsharp.php
+++ core/model/phpthumb/phpthumb.unsharp.php
@@ -3,7 +3,7 @@
 ////
 ////            Unsharp Mask for PHP - version 2.1.1
 ////
-////    Unsharp mask algorithm by Torstein Hønsi 2003-07.
+////    Unsharp mask algorithm by Torstein HÃ¸nsi 2003-07.
 ////             thoensi_at_netcom_dot_no.
 ////               Please leave this notice.
 ////
