662f449bbbf58fd993faecb03997ef18c409b8be
Sebastian G. Marinescu <sebastian-marinescu@users.noreply.github.com>
2016-08-08 22:49:26 +0200

    package.grid.js // default_per_page items in Package-Overview
    ### What does it do?
    Updated package.grid.js, so that it not statically always show 10 entries, 
    but acknowledges the _default_per_page_ system-setting the user provided, 
    if it is larger than 10.
    
    ### Why is it needed?
    The Package-Overview statically shows 10 entries. 
    I was expecting it to behave as all the other grids in the manager.
    
    ### Related issue(s)/PR(s)
    None that I know of.

--- manager/assets/modext/workspace/package.grid.js
+++ manager/assets/modext/workspace/package.grid.js
@@ -83,7 +83,7 @@ MODx.grid.Package = function(config) {
                  ,'provider','provider_name','disabled','source','attributes','readme','menu'
                  ,'install','textaction','iconaction','updateable']
         ,plugins: [this.exp]
-        ,pageSize: 10
+        ,pageSize: MODx.config.default_per_page > 10 ? MODx.config.default_per_page : 10
         ,columns: cols
         ,primaryKey: 'signature'
         ,paging: true
