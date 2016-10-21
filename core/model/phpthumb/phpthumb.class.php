b098aa89523da0032038dc466c8b3edecec8cccf
Thomas Jakobi <thomas.jakobi@partout.info>
2016-10-21 11:27:14 +0200

    typo

--- core/model/phpthumb/phpthumb.class.php
+++ core/model/phpthumb/phpthumb.class.php
@@ -4342,7 +4342,7 @@ if (false) {
 
 	function InitializeTempDirSetting() {
 		$this->config_temp_directory = ($this->config_temp_directory ? $this->config_temp_directory : $this->realPathSafe((function_exists('sys_get_temp_dir') ? sys_get_temp_dir() : ''))); // sys_get_temp_dir added in PHP v5.2.1
-        $this->config_temp_directory = ($this->config_temp_directory ? $this->config_temp_directory : $this->realPathSafe(ini_get('upload_tmp_dir')));
+		$this->config_temp_directory = ($this->config_temp_directory ? $this->config_temp_directory : $this->realPathSafe(ini_get('upload_tmp_dir')));
 		$this->config_temp_directory = ($this->config_temp_directory ? $this->config_temp_directory : $this->realPathSafe(getenv('TMPDIR')));
 		$this->config_temp_directory = ($this->config_temp_directory ? $this->config_temp_directory : $this->realPathSafe(getenv('TMP')));
 		return true;
