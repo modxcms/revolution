e4bfa38f9f1a4d6c10707e964cd81f478444038e
Jan Peca <pecajan@gmail.com>
2017-01-03 17:22:04 +0100

    Update lexicons from crowdin
    Crowdin project: http://translate.modx.com
    Thanks to all translators and proofreaders for contribution!

--- core/lexicon/zh/about.inc.php
+++ core/lexicon/zh/about.inc.php
@@ -6,32 +6,33 @@
  * @package modx
  * @subpackage lexicon
  */
-$_lang['help_page_title'] = 'Help & Support';
-$_lang['help_main_head'] = 'MODX is Here for You';
-$_lang['help_main_subhead'] = 'See below to get the help you need for your MODX project<br>Find more details at <a href="http://modx.com/support">modx.com/support</a> or call +1 (469) 777-6639';
-$_lang['forums'] = 'Forums';
-$_lang['forums_title'] = 'MODX Community Forums';
-$_lang['forums_description'] = 'Free help from the Community';
+$_lang['help_page_title'] = '帮助 & 支持';
+$_lang['help_main_head'] = 'MODX在这里';
+$_lang['help_main_subhead'] = '为你的MODX项目得到帮助请看下面<br>查看详情 <a href="http://modx.com/support">modx.com/support</a> 或者致电 +1 (469) 777-6639';
+$_lang['forums'] = '论坛';
+$_lang['forums_title'] = 'MODX 社区论坛';
+$_lang['forums_description'] = '来自社区的免费帮助';
 $_lang['forums_link'] = 'http://forums.modx.com/';
-$_lang['docs'] = 'Documentation';
-$_lang['docs_title'] = 'Read the Fabulous (MODX) Manual';
-$_lang['docs_description'] = 'Read the official MODX manuals';
+$_lang['docs'] = '文档';
+$_lang['docs_title'] = '阅读精彩 (MODX)手册';
+$_lang['docs_description'] = '阅读官方MODX手册';
 $_lang['docs_link'] = 'http://rtfm.modx.com/';
-$_lang['bugs'] = 'Bug Tracker';
-$_lang['bugs_title'] = 'Report Bugs & Feature Requests';
-$_lang['bugs_description'] = 'Found a problem? Let us know!';
-$_lang['bugs_link'] = 'https://github.com/modxcms/revolution/issues';
-$_lang['support'] = 'Commercial Support';
-$_lang['support_title'] = 'Buy an Emergency Support Ticket Now';
-$_lang['support_description'] = 'Priority support from the source';
+$_lang['bugs'] = 'Bug 追踪器';
+$_lang['bugs_title'] = '报告 Bug & 功能请求';
+$_lang['bugs_description'] = '发现一个问题吗？让我们知道 ！';
+$_lang['bugs_link'] = 'https://github.com/modxcms/revolution/issues
+';
+$_lang['support'] = '商业支持';
+$_lang['support_title'] = '购买紧急支持票';
+$_lang['support_description'] = '从源码中优先支持';
 $_lang['support_link'] = 'http://modx.com/modx-support/';
 $_lang['email_placeholder'] = 'you@example.com';
-$_lang['email_sub'] = 'Stay Up to Date with MODX';
-$_lang['email_sub_description'] = 'Subscribe to the <b>MODX Newsletter</b> to receive all the important news about MODX. Just enter your email below—we promise to never send spam.';
-$_lang['email_sub_button'] = 'Sign up';
-$_lang['social_follows'] = 'You can also <b>follow MODX</b> on these channels.';
-$_lang['follow'] = 'Follow MODX';
-$_lang['like'] = 'Like MODX';
-$_lang['circle'] = 'Circle MODX';
-$_lang['help_about'] = 'The MODX® software you are using is the result of collaboration with an amazing community of users, supporters and developers. Since 2004, the team behind MODX has sponsored and managed it, funding ongoing work through commercial support and commissioned features. You can <a href="http://modx.com/support/" class="supportTicket">buy support from the source</a> to get one hour of emergency support (also usable for two hours non-priority support). Or <a href="mailto:hello@modx.com?subject=MODX Manager Inquiry ">email us</a> if you have another question about MODX in general.';
-$_lang['help_credit'] = 'MODX is free open source software licensed under the <a href="http://www.gnu.org/licenses/gpl-2.0.html" target="_blank">GPL version 2.0</a> or later. Copyright 2005-[[+current_year]] by MODX, LLC. “MODX” is a registered trademark. Do the right thing—please keep this credit and the other information on this page intact.';
+$_lang['email_sub'] = '与 MODX保持最新';
+$_lang['email_sub_description'] = '订阅 <b>MODX新闻</b>，接收所有关于 MODX 的重要新闻。请在下面输入您的电子邮件 — — 我们承诺永远不会发送垃圾邮件。';
+$_lang['email_sub_button'] = '注册';
+$_lang['social_follows'] = '您还可以在这些频道上 <b>跟随 MODX</b>。';
+$_lang['follow'] = '跟随MODX';
+$_lang['like'] = '赞MODX';
+$_lang['circle'] = '圈MODX';
+$_lang['help_about'] = '这 Modx 软件是一个很多人合作的结果，包括使用人，支持人，相似字。从2004，Modx的团队一直提供积极管理型，包括软件开发，新的形状，也包括新的软件模块和软件版本。你会 <a href="http://modx.com/support/" class="supportTicket">买软件支持 </a>(一个小时危急支持或者两个小时普通支持)。有另外的问题？ <a href="mailto:hello@modx.com?subject=MODX Manager Inquiry ">电邮给我们喽！</a>';
+$_lang['help_credit'] = 'MODX 是免费的开源软件，在 <a href="http://www.gnu.org/licenses/gpl-2.0.html"target="_blank"> GPL 版本 2.0</a> 或更高版本许可证下发布。版权所有 2005-[[+current_year]] MODX，LLC。"MODX"是一个注册的商标。做正确的事情 — — 请保持本信用证和这个页面上的其他信息不变。';
