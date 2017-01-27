bfa0f1d8acebbc6a64a6a5fd17266fe614d0e8da
Jason Coward <jason@opengeek.com>
2017-01-26 19:42:37 -0700

    Fix security issue with config_key in setup welcome controller

--- setup/controllers/welcome.php
+++ setup/controllers/welcome.php
@@ -10,13 +10,15 @@ $install->settings->check();
 $proceed = false;
 $writable = is_writable(MODX_SETUP_PATH . 'includes/config.core.php');
 $writableError = false;
-$config_key = isset($_POST['config_key']) && !empty($_POST['config_key']) ? $_POST['config_key'] : MODX_CONFIG_KEY;
+$config_key = isset($_POST['config_key']) && !empty($_POST['config_key'])
+    ? str_replace(array('{','}',"'",'"','\$'), '', $_POST['config_key'])
+    : MODX_CONFIG_KEY;
 if (!empty($_POST['proceed'])) {
     if ($config_key !== MODX_CONFIG_KEY) {
         if ($writable) {
             $content = file_get_contents(MODX_SETUP_PATH . 'includes/config.core.php');
             $pattern = "/define\s*\(\s*'MODX_CONFIG_KEY'\s*,.*\);/";
-            $replacement = "define ('MODX_CONFIG_KEY', '{$_POST['config_key']}');";
+            $replacement = "define ('MODX_CONFIG_KEY', '{$config_key}');";
             $content = preg_replace($pattern, $replacement, $content);
             file_put_contents(MODX_SETUP_PATH . 'includes/config.core.php', $content);
             $proceed = true;
