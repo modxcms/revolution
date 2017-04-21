0d162450ba408fe2b12c68d3deae8174070e30bb
wuuti <info@wuuti.org>
2017-04-21 18:27:51 +0200

    Added first version of trash CMP with ExtJs base.

---
+++ manager/assets/modext/sections/resource/trash/index.js
@@ -0,0 +1,24 @@
+/**
+ * Loads the trash page
+ *
+ * @class MODx.page.Trash
+ * @extends MODx.Component
+ * @param {Object} config An object of config properties
+ * @xtype modx-page-trash
+ */
+MODx.page.Trash = function(config) {
+    config = config || {};
+    Ext.applyIf(config,{
+        components: [{
+            xtype: 'modx-panel-trash'
+        }]
+        /*,buttons: [{
+            text: _('help_ex')
+            ,id: 'modx-abtn-help'
+            ,handler: MODx.loadHelpPane
+        }]*/
+    });
+    MODx.page.Trash.superclass.constructor.call(this,config);
+};
+Ext.extend(MODx.page.Trash,MODx.Component);
+Ext.reg('modx-page-trash',MODx.page.Trash);
