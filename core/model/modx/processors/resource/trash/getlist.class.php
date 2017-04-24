8fa33e82cce2189914cf84514b0483eac47f9a62
wuuti <info@wuuti.org>
2017-04-24 12:40:49 +0200

    Implemented bulk purge.

--- core/model/modx/processors/resource/trash/getlist.class.php
+++ core/model/modx/processors/resource/trash/getlist.class.php
@@ -1,24 +1,45 @@
 <?php
-
 /**
  * Gets a list of resources.
  *
  * @param integer $start (optional) The record to start at. Defaults to 0.
  * @param integer $limit (optional) The number of records to limit to. Defaults
- *                       to 10.
- * @param string  $sort  (optional) The column to sort by. Defaults to name.
- * @param string  $dir   (optional) The direction of the sort. Defaults to ASC.
- *
+ * to 10.
+ * @param string $sort (optional) The column to sort by. Defaults to name.
+ * @param string $dir (optional) The direction of the sort. Defaults to ASC.
  * @return array An array of modResources
- * @package    modx
+ *
+ * @package modx
  * @subpackage processors.resource
  */
 class modResourceTrashGetListProcessor extends modObjectGetListProcessor {
-
     public $classKey = 'modResource';
     public $languageTopics = array('resource');
     public $defaultSortField = 'pagetitle';
     public $permission = 'view';
+
+    /**
+     * @return string
+     */
+    public function getSortClassKey() {
+        return 'modResource';
+    }
+
+    /**
+     * @param array $list
+     *
+     * @return array
+     */
+    public function beforeIteration(array $list) {
+        /*if ($this->getProperty('showNone')) {
+            $list[] = array(
+                'id' => 0,
+                'name' => '('.$this->modx->lexicon('none').')',
+                'description' => '',
+            );
+        }*/
+        return $list;
+    }
 
     /**
      * @param xPDOQuery $c
@@ -27,59 +48,40 @@ class modResourceTrashGetListProcessor extends modObjectGetListProcessor {
      */
     public function prepareQueryBeforeCount(xPDOQuery $c) {
         $query = $this->getProperty('query');
-        $c->select([
-            $this->modx->getSelectColumns('modResource', 'modResource', 'modResource_'),
+        $c->select(array(
+            $this->modx->getSelectColumns('modResource','modResource', 'modResource_'),
             'modResource_deletedbyUser' => 'User.username',
-            'modResource_context_name'  => 'Context.name',
-        ]);
+            'modResource_context_name' => 'Context.name',
+        ));
         $c->leftJoin('modUser', 'User', 'modResource.deletedby = User.id');
         $c->leftJoin('modContext', 'Context', 'modResource.context_key = Context.key');
 
         // TODO add only resources if we have the save permission here (on the context!!)
-        // we need the following permissions:
-        // undelete_document - to restore the document
-        // delete_document - thats perhaps not necessary, because all documents are already deleted
-        // but we need the purge_deleted permission - for every single file
 
         if (!empty($query)) {
-            $c->where(array('modResource.pagetitle:LIKE' => '%' . $query . '%'));
-            $c->orCondition(array('modResource.longtitle:LIKE' => '%' . $query . '%'));
+            $c->where(array('modResource.pagetitle:LIKE' => '%'.$query.'%'));
+            $c->orCondition(array('modResource.longtitle:LIKE' => '%'.$query.'%'));
         }
         $c->where(array(
             'modResource.deleted' => true,
         ));
-        // $c->prepare();
-        // $this->modx->log(1,"Query: ".$c->toSQL());
+       // $c->prepare();
+       // $this->modx->log(1,"Query: ".$c->toSQL());
         return $c;
     }
 
     public function prepareRow(xPDOObject $object) {
-        $charset = $this->modx->getOption('modx_charset', null, 'UTF-8');
+        $charset = $this->modx->getOption('modx_charset',null,'UTF-8');
         $objectArray = $object->toArray();
-        $objectArray['pagetitle'] = htmlentities($objectArray['pagetitle'], ENT_COMPAT, $charset);
+        $objectArray['pagetitle'] = htmlentities($objectArray['pagetitle'],ENT_COMPAT,$charset);
 
-        // to enable a better detection of the resource's location, we also construct the
-        // parent-child path to the resource
+        $this->modx->log(3,print_r($objectArray,true));
 
-        $parents = array();
-        $parent = $objectArray['parent'];
+        $canEdit = $this->modx->hasPermission('source_edit');
+        $canSave = $this->modx->hasPermission('source_save');
+        $canRemove = $this->modx->hasPermission('source_delete');
 
-        while ($parent!=0) {
-            $parents[] = $this->modx->getObject('modResource', $parent);
-            $parent = end($parents)->get('parent');
-        }
-
-        $parentPath = "";
-        foreach ($parents as $parent) {
-            $parentPath = $parent->get('pagetitle') . " (".$parent->get('id') . ") > " . $parentPath;
-        }
-        $objectArray['parentPath'] =  "[" . $objectArray['context_key'] . "] " . $parentPath;
-
-//        $canEdit = $this->modx->hasPermission('');
-//       $canSave = $this->modx->hasPermission('source_save');
-//        $canRemove = $this->modx->hasPermission('source_delete');
-//        $canView = $this->modx->hasPermission('view_document');
-
+//        $objectArray = $object->toArray();
         $objectArray['iconCls'] = $this->modx->getOption('mgr_source_icon', null, 'icon-folder-open-o');
 
         $cls = array();
@@ -91,13 +93,11 @@ class modResourceTrashGetListProcessor extends modObjectGetListProcessor {
         if ($object->checkPolicy('remove') && $canRemove) $cls[] = 'premove';
         if ($object->checkPolicy('copy') && $canSave) $cls[] = 'pduplicate';
 */
-        $objectArray['cls'] = implode(' ', $cls);
-
+        $objectArray['cls'] = implode(' ',$cls);
         return $objectArray;
 
 
     }
 }
-
 return 'modResourceTrashGetListProcessor';
 
