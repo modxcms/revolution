dd445402f653657642461386b0fe797ac6386931
Jason Coward <jason@opengeek.com>
2017-06-01 19:16:26 -0600

    Change new installs to create tables with InnoDB engine

--- setup/includes/modinstall.class.php
+++ setup/includes/modinstall.class.php
@@ -162,6 +162,7 @@ class modInstall {
             if (!($this->xpdo instanceof xPDO)) { return $this->xpdo; }
 
             $this->xpdo->setOption('cache_path',MODX_CORE_PATH . 'cache/');
+            $this->xpdo->setOption(xPDO::OPT_OVERRIDE_TABLE_TYPE, 'InnoDB');
 
             if ($mode === modInstall::MODE_UPGRADE_REVO_ADVANCED) {
                 if ($this->xpdo->connect()) {
