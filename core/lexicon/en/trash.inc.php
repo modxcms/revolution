c4ee0b23db12458e4f784a08bef4c8803868dfbe
Jens Külzer <github@wuuti.org>
2017-07-07 15:24:56 +0200

    Added lexicon entries. Changed recycle bin icon action to open the trash manager. Changed recycle bin icon tooltip. Added autosave to trash grid.

--- core/lexicon/en/trash.inc.php
+++ core/lexicon/en/trash.inc.php
@@ -17,6 +17,8 @@ $_lang['trash.intro_msg'] = '
     Manage the deleted resources here. <br>
     <i>Before you restore any resource, check the publishing state</i> - you can unpublish resources here directly from the grid with a double-click on the published-cell of the resource.';
 
+$_lang['trash.manage_recycle_bin_tooltip'] = "Go to the trash bin manager";
+
 $_lang['trash.deletedon_title'] = 'Deleted on';
 $_lang['trash.deletedbyUser_title'] = 'Deleted by User';
 
