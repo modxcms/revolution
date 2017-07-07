ae59f72f327febea6df5d4fd102a5ca56119802f
Mark Hamstra <hello@markhamstra.com>
2017-07-07 11:58:11 +0100

    Load the dashboard feeds (news/security) over AJAX to prevent remote delays from slowing down the manager

--- manager/controllers/default/dashboard/widget.modx-security.php
+++ manager/controllers/default/dashboard/widget.modx-security.php
@@ -8,33 +8,13 @@
  * @subpackage dashboard
  */
 class modDashboardWidgetSecurityFeed extends modDashboardWidgetInterface {
-    /**
-     * @var modRSSParser $rss
-     */
-    public $rss;
-
     public function render() {
-        $url = $this->modx->getOption('feed_modx_security');
-        $feedHost = parse_url($url, PHP_URL_HOST);
-        if ($feedHost && function_exists('checkdnsrr') && !checkdnsrr($feedHost, 'A')) {
+        $enabled = $this->modx->getOption('feed_modx_security_enabled',null,true);
+        if (!$enabled) {
             return '';
         }
-        $this->modx->loadClass('xmlrss.modRSSParser','',false,true);
-        $this->rss = new modRSSParser($this->modx);
 
-        $o = array();
-        $newsEnabled = $this->modx->getOption('feed_modx_security_enabled',null,true);
-        if (!empty($url) && !empty($newsEnabled)) {
-            $rss = $this->rss->parse($url);
-            if (is_object($rss)) {
-                foreach (array_keys($rss->items) as $key) {
-                    $item= &$rss->items[$key];
-                    $item['pubdate'] = strftime('%c',$item['date_timestamp']);
-                    $o[] = $this->getFileChunk('dashboard/rssitem.tpl',$item);
-                }
-            }
-        }
-        return implode("\n",$o);
+        return '<div id="modx-security-feed-container" class="feed-loading" data-feed="news"><i class="icon icon-refresh icon-spin" aria-hidden="true"></i> ' . $this->modx->lexicon('loading') . '</div>';
     }
 }
 return 'modDashboardWidgetSecurityFeed';
