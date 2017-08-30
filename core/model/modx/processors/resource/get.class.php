822fe6dd19ddb2a337d54046e59d7ec62ef920cb
Jason Coward <jason@opengeek.com>
2017-08-30 14:59:05 -0600

    Skip date format check when using resource quick update

--- core/model/modx/processors/resource/get.class.php
+++ core/model/modx/processors/resource/get.class.php
@@ -17,7 +17,10 @@ class modResourceGetProcessor extends modObjectGetProcessor {
     public function process() {
         $resourceArray = $this->object->toArray();
         $resourceArray['canpublish'] = $this->modx->hasPermission('publish_document');
-        $this->formatDates($resourceArray);
+        if (!$this->getProperty('skipFormatDates') ||
+            ($this->getProperty('skipFormatDates') && $this->getProperty('skipFormatDates') == 'false')) {
+            $this->formatDates($resourceArray);
+        }
         return $this->success('',$resourceArray);
     }
 
