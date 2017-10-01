11b34cb79124b789baa8eca286370cf63bab816a
Thomas Jakobi <thomas.jakobi@partout.info>
2017-10-01 15:27:27 +0200

    Check $this->xpdo for instance of modX

--- core/model/modx/modresource.class.php
+++ core/model/modx/modresource.class.php
@@ -612,7 +612,7 @@ class modResource extends modAccessibleSimpleObject implements modResourceInterf
      * @return string The transformed string.
      */
     public function cleanAlias($alias, array $options = array()) {
-        if ($ctx = $this->xpdo->getContext($this->get('context_key'))) {
+        if ($this->xpdo instanceof modX && $ctx = $this->xpdo->getContext($this->get('context_key'))) {
             $options = ($ctx->getOption('friendly_alias_translit')) ?
                 array_merge(array(
                     'friendly_alias_translit' => $ctx->getOption('friendly_alias_translit')
