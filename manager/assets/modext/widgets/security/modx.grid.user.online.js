689ca8beab67771f3ef95aa857da941d07590b22
Jason Coward <jason@opengeek.com>
2017-10-12 13:55:52 -0600

    Add new Who's Online dashboard widget
    Squashed commit of the following:
    
    commit 854eb97ecb3331dce23014e84c0ba3a99e1eb295
    Author: Jason Coward <jason@opengeek.com>
    Date:   Thu Oct 12 13:53:47 2017 -0600
    
        Add changelog entry
    
    commit cf7fa3c00e01a788d07bea90939b5e13de7ae516
    Author: David Pede <david@tasian.media>
    Date:   Mon Jul 10 22:40:27 2017 +0100
    
        New getonline processor
    
        Update processor
    
        Update tpl
    
        Update controller
    
        New grid
    
        Add 'manager_date_format' and 'manager_time_format' setting support
    
        Fix hardcoded datetime zone
    
        Remove date from current time
    
        Add 'manager_use_fullname' setting support
    
        Lexicon load in addHtml
    
        Add pagination to grid

---
+++ manager/assets/modext/widgets/security/modx.grid.user.online.js
@@ -0,0 +1,62 @@
+/**
+ * Loads a grid of all users who are online.
+ *
+ * @class MODx.grid.WhoIsOnline
+ * @extends MODx.grid.Grid
+ * @param {Object} config An object of options.
+ * @xtype modx-grid-user-online
+ */
+MODx.grid.WhoIsOnline = function(config) {
+  config = config || {};
+  Ext.applyIf(config,{
+    title: _('onlineusers_title')
+    ,url: MODx.config.connector_url
+    ,baseParams: {
+      action: 'security/user/getonline'
+    }
+    ,autosave: false
+    ,save_action: ''
+    ,pageSize: 10
+    ,fields: ['user','username','occurred','action']
+    ,columns: [{
+      header: _('onlineusers_userid')
+      ,dataIndex: 'user'
+      ,width: 80
+      ,fixed: true
+    },{
+      header: _('onlineusers_user')
+      ,dataIndex: 'username'
+    },{
+      header: _('onlineusers_lasthit')
+      ,dataIndex: 'occurred'
+    },{
+      header: _('onlineusers_action')
+      ,dataIndex: 'action'
+    }]
+    ,paging: true
+    ,listeners: {
+      afterrender: this.onAfterRender
+      ,scope: this
+    }
+  });
+  MODx.grid.WhoIsOnline.superclass.constructor.call(this,config);
+};
+Ext.extend(MODx.grid.WhoIsOnline,MODx.grid.Grid,{
+  // Workaround to resize the grid when in a dashboard widget
+  onAfterRender: function() {
+    var cnt = Ext.getCmp('modx-content')
+    // Dashboard widget "parent" (renderTo)
+    ,parent = Ext.get('modx-grid-user-online');
+
+    if (cnt && parent) {
+      cnt.on('afterlayout', function(elem, layout) {
+        var width = parent.getWidth();
+        // Only resize when more than 500px (else let's use/enable the horizontal scrolling)
+        if (width > 500) {
+          this.setWidth(width);
+        }
+      }, this);
+    }
+  }
+});
+Ext.reg('modx-grid-user-online',MODx.grid.WhoIsOnline);
