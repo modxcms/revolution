44034933585687a45cc8579c581fa454a2a94c30
Thomas Jakobi <thomas.jakobi@partout.info>
2017-11-14 20:23:28 +0100

    Fixing 'Flush Your Permissions' in topmenu

--- core/model/modx/processors/security/access/flush.class.php
+++ core/model/modx/processors/security/access/flush.class.php
@@ -6,12 +6,19 @@
  * @subpackage processors.security.access
  */
 class modFlushPermissionsProcessor extends modProcessor {
+    public function getLanguageTopics() {
+        return array('topmenu');
+    }
+
     public function checkPermissions() {
         return $this->modx->hasPermission('access_permissions');
     }
 
     public function process() {
-        $this->modx->cacheManager->flushPermissions();
+        if (!$this->modx->cacheManager->flushPermissions()) {
+            return $this->failure($this->modx->lexicon('flush_sessions_err'));
+        }
+        return $this->success();
     }
 }
 return 'modFlushPermissionsProcessor';
