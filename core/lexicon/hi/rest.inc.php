b41662ee16fadf6cd7c4cbd37721e71b8d5075cb
Jason Coward <jason@opengeek.com>
2017-12-13 09:12:15 -0700

    Update lexicon translations

--- core/lexicon/hi/rest.inc.php
+++ core/lexicon/hi/rest.inc.php
@@ -8,7 +8,7 @@
  */
 $_lang['error'] = 'त्रुटि!';
 $_lang['rest.err_class_remove'] = '[[+class_key]] को दूर करने का प्रयास करते समय कोई त्रुटि उत्पन्न हुई';
-$_lang['rest.err_class_save'] = '[[+ class_key]] सहेजने का प्रयास करते समय कोई त्रुटि उत्पन्न हुई';
+$_lang['rest.err_class_save'] = '[[+class_key]] सहेजने का प्रयास करते समय कोई त्रुटि उत्पन्न हुई';
 $_lang['rest.err_field_ns'] = '[[+field]] निर्दिष्ट नहीं किया गया!';
 $_lang['rest.err_field_required'] = 'इस फ़ील्ड की आवश्यकता है।';
 $_lang['rest.err_fields_required'] = 'निम्न फ़ील्ड आवश्यक हैं: [[+fields]]';
