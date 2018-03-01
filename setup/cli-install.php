0e20cc8672099ce02d41e93d2d5b6ba021a92f74
Vasily Naumkin <bezumkin@yandex.ru>
2018-03-01 10:32:33 +0100

    - Added support of lexicons - Improved questions - More security on using shell_exec commands - The installation process now run by including install.php, without shell_exec. - Replaced system calls to built-in functions

--- setup/cli-install.php
+++ setup/cli-install.php
@@ -13,78 +13,73 @@ if (PHP_SAPI != 'cli') {
 $path = dirname(__FILE__) . '/';
 $config = $path . 'config.xml';
 $mode = 'new';
+$languages = array_slice(scandir($path . 'lang/'), 2);
 
 $variables = [
     'database_type' => 'mysql',
     'database_server' => [
-        'prompt' => 'Database server',
-        'default' => '127.0.0.1',
+        'prompt' => 'connection_database_host',
+        'default' => 'localhost',
     ],
     'database' => [
-        'prompt' => 'Database',
+        'prompt' => 'connection_database_name',
         'default' => '',
     ],
     'database_user' => [
-        'prompt' => 'Database user',
+        'prompt' => 'connection_database_login',
         'default' => '',
     ],
     'database_password' => [
-        'prompt' => 'Database password',
+        'prompt' => 'connection_database_pass',
         'default' => '',
     ],
     'database_connection_charset' => 'utf8',
     'database_charset' => 'utf8',
     'database_collation' => 'utf8_general_ci',
     'table_prefix' => [
-        'prompt' => 'Table prefix',
+        'prompt' => 'connection_table_prefix',
         'default' => 'modx_',
     ],
     'https_port' => 443,
-    'http_host' => [
-        'prompt' => 'Http host',
-        'default' => '',
-    ],
+    'http_host' => 'localhost',
     'cache_disabled' => 0,
     'inplace' => 0,
     'unpacked' => 0,
     'language' => [
-        'prompt' => 'Language',
+        'prompt' => 'choose_language',
         'default' => 'en',
     ],
     'cmsadmin' => [
-        'prompt' => 'Admin login',
+        'prompt' => 'connection_default_admin_login',
         'default' => 'admin',
     ],
     'cmspassword' => [
-        'prompt' => 'Admin password',
+        'prompt' => 'connection_default_admin_password',
         'default' => '',
     ],
     'cmsadminemail' => [
-        'prompt' => 'Admin email',
+        'prompt' => 'connection_default_admin_email',
         'default' => '',
-    ],
-    'core_path' => [
-        'prompt' => 'Path to core',
-        'default' => dirname($path) . '/core/',
-    ],
-    'context_mgr_path' => [
-        'prompt' => 'Path to manager',
-        'default' => dirname($path) . '/manager/',
-    ],
-    'context_mgr_url' => [
-        'prompt' => 'Manager url',
-        'default' => '/manager/',
-    ],
-    'context_connectors_path' => [
-        'prompt' => 'Path to connectors',
-        'default' => dirname($path) . '/connectors/',
-    ],
-    'context_connectors_url' => [
-        'prompt' => 'Url to connectors',
-        'default' => '/connectors/',
     ],
     'context_web_path' => dirname($path) . '/',
     'context_web_url' => '/',
+    'core_path' => dirname($path) . '/core/',
+    'context_mgr_path' => [
+        'prompt' => 'context_manager_path',
+        'default' => dirname($path) . '/manager/',
+    ],
+    'context_mgr_url' => [
+        'prompt' => 'context_manager_url',
+        'default' => '/manager/',
+    ],
+    'context_connectors_path' => [
+        'prompt' => 'context_connector_path',
+        'default' => dirname($path) . '/connectors/',
+    ],
+    'context_connectors_url' => [
+        'prompt' => 'context_connector_url',
+        'default' => '/connectors/',
+    ],
     'remove_setup_directory' => 1,
 ];
 
@@ -103,12 +98,18 @@ foreach ($args as $arg) {
     }
 }
 
+$lang = !empty($cli_variables['language']) && in_array($cli_variables['language'], $languages)
+    ? $cli_variables['language']
+    : 'en';
+/** @var array $_lang */
+require($path . 'lang/' . $lang . '/default.inc.php');
+
 // If it is upgrade - parse old variables
 $old_variables = [];
 if (file_exists($config)) {
     $use = ($mode == 'upgrade')
         ? 'Y'
-        : readline("There is an old config found at {$config}. Do you want to load this values? (Y): ");
+        : readline("There is an existing config file found at {$config}. Do you want to load this values? (Y): ");
     if (empty($use) || strtolower($use) == 'y') {
         $tmp = simplexml_load_file($config);
         $tmp = @json_decode(json_encode($tmp), true);
@@ -127,16 +128,19 @@ foreach ($variables as $key => $params) {
     if (!is_array($params)) {
         $data[$key] = $params;
     } elseif (!empty($params['prompt'])) {
-        $prompt = $params['prompt'];
+        $prompt = rtrim($_lang[$params['prompt']], ':');
         $res = '';
         if (!empty($params['default'])) {
+            if ($key == 'language') {
+                $prompt .= ' (' . implode('|', $languages) . ') ';
+            }
             $prompt .= " ({$params['default']})";
             if (!$res = readline($prompt . ': ')) {
                 $res = $params['default'];
             }
         } else {
             while (!$res = readline($prompt . ': ')) {
-                echo $prompt . " is required!\n";
+                // pass
             }
             readline_add_history(trim($res));
         }
@@ -148,6 +152,13 @@ foreach ($variables as $key => $params) {
 }
 
 // Check directories
+$rmdir = function ($dir) use (&$rmdir) {
+    $files = array_diff(scandir($dir), ['.', '..']);
+    foreach ($files as $file) {
+        is_dir("$dir/$file") ? $rmdir("$dir/$file") : unlink("$dir/$file");
+    }
+    rmdir($dir);
+};
 $dirs = [
     'core_path' => '/core/',
     'context_mgr_path' => '/manager/',
@@ -155,11 +166,12 @@ $dirs = [
 ];
 foreach ($dirs as $key => $value) {
     $target = dirname($path) . $value;
-    if (file_exists($target) && $data[$key] != $target) {
-        shell_exec("rm -rf {$data[$key]}");
-        shell_exec("mv {$target} {$data[$key]}");
+    if (file_exists($target) && $data[$key] != $target && strpos($data[$key], dirname($path)) === 0) {
+        $rmdir(rtrim($data[$key], DIRECTORY_SEPARATOR));
+        rename($target, $data[$key]);
     }
 }
+unset($rmdir);
 
 // Generate config file
 $xml = new SimpleXMLElement('<modx/>');
@@ -169,4 +181,11 @@ foreach ($data as $key => $value) {
 file_put_contents($config, $xml->asXML());
 
 // Run install!
-echo shell_exec("php {$path}index.php --installmode={$mode} --core_path={$data['core_path']} --config={$config}");
+echo "\n\033[32m{$_lang['thank_installing']}MODX!\n\n\033[0m";
+$argv = [
+    $path . 'index.php',
+    '--installmode=' . $mode,
+    '--core_path=' . $data['core_path'],
+    '--config=' . $config,
+];
+require($path . 'index.php');
