e61f4c6aedaa963994bd65794434e8c26572388b
Vasily Naumkin <bezumkin@yandex.ru>
2018-03-05 13:01:40 +0100

    Enabled modx_browser_tree_hide_files system setting by default Enabled caching of images from phpthumb connector by browser Lazy load of images in media browser

--- setup/includes/upgrades/sqlsrv/2.7.0-pl.php
+++ setup/includes/upgrades/sqlsrv/2.7.0-pl.php
@@ -9,3 +9,4 @@
 
 /* run upgrades common to all db platforms */
 include dirname(dirname(__FILE__)) . '/common/2.7-alias-visible.php';
+include dirname(dirname(__FILE__)) . '/common/2.7-browser-tree-hide-files.php';
