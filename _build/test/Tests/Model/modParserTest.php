cfddf5ef5bb57e005ffa406ddd581dc38483197f
Joshua Lückers <joshua@lmg.nl>
2018-05-18 15:29:50 +0200

    Added an example tag inside a 'HTML array key' in the dataprovider for the modParserTest.

--- _build/test/Tests/Model/modParserTest.php
+++ _build/test/Tests/Model/modParserTest.php
@@ -73,6 +73,7 @@ class modParserTest extends MODxTestCase {
             array("", '[[', ']]', array(), 0),
             array("[[tag]]", '[[', ']]', array(array("[[tag]]", "tag")), 1),
             array("[[[[*field:is=`0`:then=`Tag`:else=`Tag`]]]]", '[[', ']]', array(array("[[*field:is=`0`:then=`Tag`:else=`Tag`]]", "*field:is=`0`:then=`Tag`:else=`Tag`")), 1),
+            array("items[[[tag]]]", '[[', ']]', array(array("[[tag]]", "tag")), 1),
             array("[[tag]][[tag2]]", '[[', ']]', array(array("[[tag2]]", "tag"), array("[[tag2]]", "tag2")), 2),
             array("[[tag[[tag2]]]]", '[[', ']]', array(array("[[tag[[tag2]]]]", "tag[[tag2]]")), 1),
             array("[[tag[[tag2[[tag3]]]]]]", '[[', ']]', array(array("[[tag[[tag2[[tag3]]]]]]", "tag[[tag2[[tag3]]]]")), 1),
