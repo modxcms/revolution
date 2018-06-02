2ea0bd0d3f7cb0e8ade5ff751b0cfc3f06b7ab89
Christian Seel <cs@seda.digital>
2018-06-02 09:28:37 +0200

    use $file instead of $f in all processor to avoid confusions

--- core/model/modx/processors/system/errorlog/download.class.php
+++ core/model/modx/processors/system/errorlog/download.class.php
@@ -16,15 +16,15 @@ class modSystemErrorLogDownloadProcessor extends modProcessor {
         }
         $filename = $this->modx->getOption('filename', $logTarget['options'], 'error.log', true);
         $filepath = $this->modx->getOption('filepath', $logTarget['options'], $this->modx->getCachePath() . xPDOCacheManager::LOG_DIR, true);
-        $f = rtrim($filepath, '/').'/'.$filename;
-        if (!file_exists($f)) {
+        $file = rtrim($filepath, '/').'/'.$filename;
+        if (!file_exists($file)) {
             return $this->failure();
         }
         header('Content-Type: application/force-download');
-        header('Content-Length: ' . filesize($f));
+        header('Content-Length: ' . filesize($file));
         header('Content-Disposition: attachment; filename="error.'.time().'.log');
         ob_get_level() && @ob_end_flush();
-        readfile($f);
+        readfile($file);
         die();
     }
 }
--- core/model/modx/processors/system/errorlog/get.class.php
+++ core/model/modx/processors/system/errorlog/get.class.php
@@ -16,19 +16,19 @@ class modSystemErrorLogGetProcessor extends modProcessor {
         }
         $filename = $this->modx->getOption('filename', $logTarget['options'], 'error.log', true);
         $filepath = $this->modx->getOption('filepath', $logTarget['options'], $this->modx->getCachePath() . xPDOCacheManager::LOG_DIR, true);
-        $f = rtrim($filepath, '/').'/'.$filename;
+        $file = rtrim($filepath, '/').'/'.$filename;
         $content = '';
         $tooLarge = false;
-        if (file_exists($f)) {
-            $size = round(@filesize($f) / 1000 / 1000,2);
+        if (file_exists($file)) {
+            $size = round(@filesize($file) / 1000 / 1000,2);
             if ($size > 1) {
                 $tooLarge = true;
             } else {
-                $content = @file_get_contents($f);
+                $content = @file_get_contents($file);
             }
         }
         $la = array(
-            'name' => $f,
+            'name' => $file,
             'log' => $content,
             'tooLarge' => $tooLarge,
         );
