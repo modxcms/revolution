37aad49ac8b6b3dc5f523acda10f083489ae1765
Ivan Klimchuk <ivan.klimchuk@1pt.com>
2018-08-21 11:38:52 +0300

    Returned loading english lexicons as fallback

--- core/model/modx/modmenu.class.php
+++ core/model/modx/modmenu.class.php
@@ -112,7 +112,7 @@ class modMenu extends modAccessibleObject {
             $this->xpdo->getService('lexicon','modLexicon');
         }
 
-        $this->xpdo->lexicon->load('menu', 'topmenu');
+        $this->xpdo->lexicon->load('menu', 'en:menu', 'topmenu', 'en:topmenu');
 
         $c = $this->xpdo->newQuery('modMenu');
         $c->select($this->xpdo->getSelectColumns('modMenu', 'modMenu'));
