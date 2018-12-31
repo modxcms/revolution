79e8c21ff5088446d33ec5594f1d14cd3e277e15
Thomas Jakobi <>
2018-12-31 17:54:40 +0100

    Don't remove static files if automated static files are turned off

--- core/model/modx/processors/element/remove.class.php
+++ core/model/modx/processors/element/remove.class.php
@@ -16,6 +16,9 @@
  * @subpackage processors.element
  */
 abstract class modElementRemoveProcessor extends modObjectRemoveProcessor {
+    public $staticFilePath;
+    public $staticFile;
+
     public function cleanup() {
         $this->clearCache();
     }
@@ -24,18 +27,42 @@ abstract class modElementRemoveProcessor extends modObjectRemoveProcessor {
     }
 
     public function cleanupStaticFiles() {
-        /* Remove file. */
-        $count = $this->modx->getCount($this->classKey, array('static_file' => $this->staticFile));
-        if ($this->staticFilePath && $count === 0) {
-            @unlink($this->staticFilePath);
+        if ($this->isStaticFilesAutomated()) {
+            /* Remove file. */
+            $count = $this->modx->getCount($this->classKey, array('static_file' => $this->staticFile));
+            if ($this->staticFilePath && $count === 0) {
+                @unlink($this->staticFilePath);
+            }
+
+            /* Check if parent directory is empty, if so remove parent directory. */
+            $pathinfo = pathinfo($this->staticFilePath);
+
+            if (!empty($pathinfo['dirname'])) {
+                $this->cleanupStaticDirectories($pathinfo['dirname']);
+            }
+        }
+    }
+
+    /**
+     * Determine if static files should be automated for current element class.
+     *
+     * @return bool
+     */
+    protected function isStaticFilesAutomated()
+    {
+        $elements = array(
+            'modTemplate'    => 'templates',
+            'modTemplateVar' => 'tvs',
+            'modChunk'       => 'chunks',
+            'modSnippet'     => 'snippets',
+            'modPlugin'      => 'plugins'
+        );
+
+        if (!array_key_exists($this->classKey, $elements)) {
+            return false;
         }
 
-        /* Check if parent directory is empty, if so remove parent directory. */
-        $pathinfo = pathinfo($this->staticFilePath);
-
-        if (!empty($pathinfo['dirname'])) {
-            $this->cleanupStaticDirectories($pathinfo['dirname']);
-        }
+        return (bool) $this->modx->getOption('static_elements_automate_' . $elements[$this->classKey], null, false);
     }
 
     public function cleanupStaticDirectories($dirname) {
