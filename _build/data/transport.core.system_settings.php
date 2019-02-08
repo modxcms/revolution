83c2285e5d95ab33cb77e8477cc95fffd0ce6c02
Jens Külzer <jk@seda.digital>
2019-02-08 15:58:53 +0100

    Added first system settings for magic-login-links.

--- _build/data/transport.core.system_settings.php
+++ _build/data/transport.core.system_settings.php
@@ -2127,4 +2127,40 @@ $settings['error_log_filepath']->fromArray(array (
     'area' => 'system',
     'editedon' => null,
 ), '', true, true);
+$settings['passwordless_activated']= $xpdo->newObject('modSystemSetting');
+$settings['passwordless_activated']->fromArray(array (
+    'key' => 'passwordless_activated',
+    'value' => true,
+    'xtype' => 'combo-boolean',
+    'namespace' => 'core',
+    'area' => 'authentication',
+    'editedon' => null,
+), '', true, true);
+$settings['passwordless_confirm_email']= $xpdo->newObject('modSystemSetting');
+$settings['passwordless_confirm_email']->fromArray(array (
+    'key' => 'passwordless_confirm_email',
+    'value' => 'Confirm message',
+    'xtype' => 'textarea',
+    'namespace' => 'core',
+    'area' => 'authentication',
+    'editedon' => null,
+), '', true, true);
+$settings['passwordless_login_email']= $xpdo->newObject('modSystemSetting');
+$settings['passwordless_login_email']->fromArray(array (
+    'key' => 'passwordless_confirm_email',
+    'value' => 'Login message',
+    'xtype' => 'textarea',
+    'namespace' => 'core',
+    'area' => 'authentication',
+    'editedon' => null,
+), '', true, true);
+$settings['passwordless_expiration']= $xpdo->newObject('modSystemSetting');
+$settings['passwordless_expiration']->fromArray(array (
+    'key' => 'passwordless_expiration',
+    'value' => '3600',
+    'xtype' => 'textfield',
+    'namespace' => 'core',
+    'area' => 'authentication',
+    'editedon' => null,
+), '', true, true);
 return $settings;
