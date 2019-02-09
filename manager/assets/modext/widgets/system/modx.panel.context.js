44cc86fb0c841c079f3af17a2031f8885c7dc9f7
Bochkarev Ivan <ivanx86@gmail.com>
2019-02-10 03:33:13 +0600

    Fix #14342

--- manager/assets/modext/widgets/system/modx.panel.context.js
+++ manager/assets/modext/widgets/system/modx.panel.context.js
@@ -45,13 +45,15 @@ MODx.panel.Context = function(config) {
 					,fieldLabel: _('name')
 					,name: 'name'
 					,width: 300
-					,maxLength: 255
+          ,maxLength: 255
+          ,renderer: Ext.util.Format.htmlEncode
 				},{
 					xtype: 'textarea'
 					,fieldLabel: _('description')
 					,name: 'description'
 					,width: 300
-					,grow: true
+          ,grow: true
+          ,renderer: Ext.util.Format.htmlEncode
 				},{
 					xtype: 'numberfield'
 					,fieldLabel: _('rank')
