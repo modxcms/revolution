3f14eafa12df356d6be371492db692de50262049
Bochkarev Ivan <ivanx86@gmail.com>
2019-02-16 12:35:55 +0600

    Add upgrades scripts for 2.7.2

---
+++ setup/includes/upgrades/mysql/2.7.2-pl.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Specific upgrades for Revolution 2.7.2-pl
+ *
+ * @var modX $modx
+ * @package setup
+ * @subpackage upgrades
+ */
+
+/* run upgrades common to all db platforms */
+include dirname(dirname(__FILE__)) . '/common/2.7.2-update-upload_files-upload_images.php';
