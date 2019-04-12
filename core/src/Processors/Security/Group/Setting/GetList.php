9666ea17c7a6076bf0475c917248f96b935d22eb
Ivan Klimchuk <ivan@klimchuk.com>
2019-04-12 10:05:18 +0300

    Security/Group/Setting processors converted

---
+++ core/src/Processors/Security/Group/Setting/GetList.php
@@ -0,0 +1,52 @@
+<?php
+/*
+ * This file is part of MODX Revolution.
+ *
+ * Copyright (c) MODX, LLC. All Rights Reserved.
+ *
+ * For complete copyright and license information, see the COPYRIGHT and LICENSE
+ * files found in the top-level directory of this distribution.
+ */
+
+namespace MODX\Revolution\Processors\Security\Group\Setting;
+
+// @todo: replace by proper class when setting processors will be converted
+use modSystemSettingsGetListProcessor;
+use MODX\Revolution\modUserGroupSetting;
+
+/**
+ * Gets a list of user group settings
+ * @param integer $group The group to grab from
+ * @param integer $start (optional) The record to start at. Defaults to 0.
+ * @param integer $limit (optional) The number of records to limit to. Defaults to 10.
+ * @param string $sort (optional) The column to sort by. Defaults to key.
+ * @param string $dir (optional) The direction of the sort. Defaults to ASC.
+ * @package MODX\Revolution\Processors\Security\Group\Setting
+ */
+class GetList extends modSystemSettingsGetListProcessor
+{
+    public $classKey = modUserGroupSetting::class;
+
+    /**
+     * @return bool
+     */
+    public function initialize()
+    {
+        $this->setDefaultProperties(['group' => 0]);
+
+        return parent::initialize();
+    }
+
+    /**
+     * Filter by user group
+     * @return array
+     */
+    public function prepareCriteria()
+    {
+        $criteria = [];
+        $criteria[] = ['group' => (int)$this->getProperty('group')];
+
+        return $criteria;
+    }
+
+}
---
+++ core/src/Processors/Security/Group/Setting/Remove.php
@@ -0,0 +1,53 @@
+<?php
+/*
+ * This file is part of MODX Revolution.
+ *
+ * Copyright (c) MODX, LLC. All Rights Reserved.
+ *
+ * For complete copyright and license information, see the COPYRIGHT and LICENSE
+ * files found in the top-level directory of this distribution.
+ */
+
+namespace MODX\Revolution\Processors\Security\Group\Setting;
+
+// @todo: replace by proper class when setting processors will be converted
+use modSystemSettingsRemoveProcessor;
+use MODX\Revolution\modAccessibleObject;
+use MODX\Revolution\modUserGroupSetting;
+
+/**
+ * Remove a user group setting and its lexicon strings
+ * @param integer $group The group associated to the setting
+ * @param string $key The setting key
+ * @package MODX\Revolution\Processors\Security\Group\Setting
+ */
+class Remove extends modSystemSettingsRemoveProcessor
+{
+    public $classKey = modUserGroupSetting::class;
+
+    /**
+     * @return bool
+     */
+    public function initialize()
+    {
+        $key = $this->getProperty('key', '');
+        $group = (int)$this->getProperty('group', 0);
+
+        if (empty($key) || !$group) {
+            return $this->modx->lexicon($this->objectType . '_err_ns');
+        }
+
+        $primaryKey = ['key' => $key, 'group' => $group];
+        $this->object = $this->modx->getObject($this->classKey, $primaryKey);
+
+        if (!$this->object) {
+            return $this->modx->lexicon($this->objectType . '_err_nf');
+        }
+
+        if ($this->checkRemovePermission && $this->object instanceof modAccessibleObject && !$this->object->checkPolicy('remove')) {
+            return $this->modx->lexicon('access_denied');
+        }
+
+        return true;
+    }
+}
