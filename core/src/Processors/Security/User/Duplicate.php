10ae82d01a1eff9f61e67f149300d6606d8b0439
Ivan Klimchuk <ivan@klimchuk.com>
2019-04-16 16:29:17 +0300

    Security/User/Base processors converted

---
+++ core/src/Processors/Security/User/Duplicate.php
@@ -0,0 +1,98 @@
+<?php
+/*
+ * This file is part of MODX Revolution.
+ *
+ * Copyright (c) MODX, LLC. All Rights Reserved.
+ *
+ * For complete copyright and license information, see the COPYRIGHT and LICENSE
+ * files found in the top-level directory of this distribution.
+ */
+
+namespace MODX\Revolution\Processors\Security\User;
+
+use MODX\Revolution\modObjectDuplicateProcessor;
+use MODX\Revolution\modUser;
+use MODX\Revolution\modUserGroupMember;
+use MODX\Revolution\modUserProfile;
+use MODX\Revolution\modUserSetting;
+
+/**
+ * Duplicates a user.
+ * @param integer $id The user to duplicate
+ * @param string $new_username The name of the new user.
+ * @package MODX\Revolution\Processors\Security\User
+ */
+class Duplicate extends modObjectDuplicateProcessor
+{
+    public $classKey = modUser::class;
+    public $languageTopics = ['user'];
+    public $permission = 'new_user';
+    public $objectType = 'user';
+    public $nameField = 'username';
+    public $beforeSaveEvent = 'OnBeforeUserDuplicate';
+    public $afterSaveEvent = 'OnUserDuplicate';
+
+    /**
+     * @return mixed|string
+     */
+    public function getNewName()
+    {
+        $name = $this->getProperty('new_username', '');
+        return !empty($name) ? $name : $this->object->get('username') . '_copy';
+    }
+
+    /**
+     * @return bool
+     */
+    public function beforeSave()
+    {
+        /* copy profile */
+        $profile = $this->object->getOne('Profile');
+        if ($profile) {
+            // Reset some modUserProfile fields
+            $profileData = array_merge($profile->toArray(), [
+                'logincount' => '',
+                'lastlogin' => '',
+                'thislogin' => '',
+                'failedlogincount' => '',
+                'sessionid' => '',
+            ]);
+            unset($profileData['internalKey']);
+
+            /** @var modUserProfile $newProfile */
+            $newProfile = $this->modx->newObject(modUserProfile::class);
+            $newProfile->fromArray($profileData);
+            $this->newObject->addOne($newProfile);
+        }
+
+        /* copy user group memberships */
+        $memberships = $this->object->getMany('UserGroupMembers');
+        $newMemberships = [];
+        /** @var modUserGroupMember $membership */
+        foreach ($memberships as $membership) {
+            /** @var modUserGroupMember $newMembership */
+            $newMembership = $this->modx->newObject(modUserGroupMember::class);
+            $newMembership->fromArray($membership->toArray());
+            $newMemberships[] = $newMembership;
+        }
+        $this->newObject->addMany($newMemberships);
+
+        /* copy settings */
+        $settings = $this->object->getMany('UserSettings');
+        $newSettings = [];
+        /** @var modUserSetting $setting */
+        foreach ($settings as $setting) {
+            /** @var modUserSetting $newSetting */
+            $newSetting = $this->modx->newObject(modUserSetting::class);
+            $newSetting->fromArray($setting->toArray());
+            $newSetting->set('key', $setting->get('key'));
+            $newSettings[] = $newSetting;
+        }
+        $this->newObject->addMany($newSettings);
+
+        // Unset some modUser fields
+        $this->object->set('session_stale');
+
+        return parent::beforeSave();
+    }
+}
---
+++ core/src/Processors/Security/User/GetRecentlyEditedResources.php
@@ -0,0 +1,137 @@
+<?php
+/*
+ * This file is part of MODX Revolution.
+ *
+ * Copyright (c) MODX, LLC. All Rights Reserved.
+ *
+ * For complete copyright and license information, see the COPYRIGHT and LICENSE
+ * files found in the top-level directory of this distribution.
+ */
+
+namespace MODX\Revolution\Processors\Security\User;
+
+use MODX\Revolution\modManagerLog;
+use MODX\Revolution\modObjectGetListProcessor;
+use MODX\Revolution\modResource;
+use MODX\Revolution\modUser;
+use MODX\Revolution\modUserGroup;
+use PDO;
+use xPDO\Om\xPDOObject;
+use xPDO\Om\xPDOQuery;
+
+/**
+ * Gets a list of recently edited resources by a user
+ * @param integer $user The user to grab from
+ * @param integer $start (optional) The record to start at. Defaults to 0.
+ * @param integer $limit (optional) The number of records to limit to. Defaults to 10.
+ * @param string $sort (optional) The column to sort by. Defaults to name.
+ * @param string $dir (optional) The direction of the sort. Defaults to ASC.
+ * @package MODX\Revolution\Processors\Security\User
+ */
+class GetRecentlyEditedResources extends modObjectGetListProcessor
+{
+    public $classKey = modManagerLog::class;
+    public $permission = 'view_document';
+    public $languageTopics = ['resource', 'user'];
+    public $defaultSortField = 'occurred';
+    public $defaultSortDirection = 'DESC';
+    protected $classKeys = [];
+
+    /**
+     * @return bool|null|string
+     */
+    public function initialize()
+    {
+        $this->setDefaultProperties(['limit' => 10]);
+
+        $this->classKeys = $this->modx->getDescendants(modResource::class);
+        $this->classKeys[] = modResource::class;
+
+        return parent::initialize();
+    }
+
+
+    /**
+     * Filter resources by user
+     * @param xPDOQuery $c
+     * @return xPDOQuery
+     */
+    public function prepareQueryBeforeCount(xPDOQuery $c)
+    {
+        $q = $this->modx->newQuery($this->classKey, ['classKey:IN' => $this->classKeys]);
+        $q->select('MAX(id), item');
+        $q->groupby('item');
+        $q->limit($this->getProperty('limit', 10));
+        if ($q->prepare() && $q->stmt->execute()) {
+            if ($ids = $q->stmt->fetchAll(PDO::FETCH_COLUMN)) {
+                $c->where(['id:IN' => $ids]);
+            } else {
+                $c->where(['id' => -1]);
+            }
+        }
+
+        $c->select($this->modx->getSelectColumns(modManagerLog::class, 'modManagerLog'));
+
+        return $c;
+    }
+
+
+    /**
+     * Prepare the row for iteration
+     * @param xPDOObject $object
+     * @return array
+     */
+    public function prepareRow(xPDOObject $object)
+    {
+        $row = $object->toArray();
+
+        if (!$resource = $this->modx->getObject(modResource::class, ['id' => $row['item']])) {
+            return [];
+        }
+
+        $resourceArray = $object->get(['id', 'pagetitle', 'description', 'published', 'deleted', 'context_key']);
+        $resourceArray['pagetitle'] = htmlspecialchars($resourceArray['pagetitle'], ENT_QUOTES,
+            $this->modx->getOption('modx_charset', null, 'UTF-8'));
+
+        $row = array_merge($row, $resourceArray);
+
+        /** @var modUser $user */
+        if ($user = $object->getOne('User')) {
+            $row = array_merge($row, $user->get(['username']), $user->Profile->get(['fullname', 'email', 'photo']),
+                ['gravatar' => $user->getGravatar(64)]);
+            /** @var modUserGroup $group */
+            $row['group'] = ($group = $user->getOne('PrimaryGroup')) ? $group->get('name') : '';
+        }
+
+        $row['menu'] = [];
+        $row['menu'][] = [
+            'text' => $this->modx->lexicon('resource_overview'),
+            'params' => [
+                'a' => 'resource/data',
+                'id' => $object->get('id'),
+                'type' => 'view',
+            ],
+        ];
+        if ($this->modx->hasPermission('edit_document')) {
+            $row['menu'][] = [
+                'text' => $this->modx->lexicon('resource_edit'),
+                'params' => [
+                    'a' => 'resource/update',
+                    'id' => $object->get('id'),
+                    'type' => 'edit',
+                ],
+            ];
+        }
+        $row['menu'][] = '-';
+        $row['menu'][] = [
+            'text' => $this->modx->lexicon('resource_preview'),
+            'params' => [
+                'url' => $this->modx->makeUrl($object->get('id'), null, '', 'full'),
+                'type' => 'open',
+            ],
+            'handler' => 'this.preview',
+        ];
+
+        return $row;
+    }
+}
