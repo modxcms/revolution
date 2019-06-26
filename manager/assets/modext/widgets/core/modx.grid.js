bfeae3c25a87ce3d77a88e2d6b38e3f36250b8c1
Jake Scaltreto <jscaltreto@users.noreply.github.com>
2019-06-26 12:54:38 -0400

    Update manager/assets/modext/widgets/core/modx.grid.js
    Co-Authored-By: Joshua Lückers <joshualuckers@me.com>

--- manager/assets/modext/widgets/core/modx.grid.js
+++ manager/assets/modext/widgets/core/modx.grid.js
@@ -317,7 +317,7 @@ Ext.extend(MODx.grid.Grid,Ext.grid.EditorGridPanel,{
                 ,autoDestroy: true
                 ,listeners:{
                     load: function(){
-                        cmp = Ext.getCmp('modx-content');
+                        var cmp = Ext.getCmp('modx-content');
                         if(typeof cmp !== "undefined") {
                             cmp.doLayout(); /* Fix layout bug with absolute positioning */
                         }
