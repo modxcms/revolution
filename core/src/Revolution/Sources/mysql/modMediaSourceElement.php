7403d6152db6ad70337283314daed75384893d29
Jason Coward <jason@opengeek.com>
2019-09-13 08:51:39 -0600

    Update platform classes via changes to the model schema
    After making changes to the schema, these commands were executed to update the maps in the platform classes:
    
    ```
    core/vendor/bin/xpdo parse-schema --config=core/model/schema/config.php --update=1 --psr4=MODX\\ mysql core/model/schema/modx.sources.mysql.schema.xml core/src/
    core/vendor/bin/xpdo parse-schema --config=core/model/schema/config.php --update=1 --psr4=MODX\\ sqlsrv core/model/schema/modx.sources.sqlsrv.schema.xml core/src/
    ```

--- core/src/Revolution/Sources/mysql/modMediaSourceElement.php
+++ core/src/Revolution/Sources/mysql/modMediaSourceElement.php
@@ -12,16 +12,16 @@ class modMediaSourceElement extends \MODX\Revolution\Sources\modMediaSourceEleme
         'version' => '3.0',
         'table' => 'media_sources_elements',
         'extends' => 'xPDO\\Om\\xPDOObject',
-        'fields' =>
+        'fields' => 
         array (
             'source' => 0,
             'object_class' => 'MODX\\Revolution\\modTemplateVar',
             'object' => 0,
             'context_key' => 'web',
         ),
-        'fieldMeta' =>
+        'fieldMeta' => 
         array (
-            'source' =>
+            'source' => 
             array (
                 'dbtype' => 'int',
                 'precision' => '11',
@@ -31,7 +31,7 @@ class modMediaSourceElement extends \MODX\Revolution\Sources\modMediaSourceEleme
                 'default' => 0,
                 'index' => 'pk',
             ),
-            'object_class' =>
+            'object_class' => 
             array (
                 'dbtype' => 'varchar',
                 'precision' => '100',
@@ -40,7 +40,7 @@ class modMediaSourceElement extends \MODX\Revolution\Sources\modMediaSourceEleme
                 'default' => 'MODX\\Revolution\\modTemplateVar',
                 'index' => 'pk',
             ),
-            'object' =>
+            'object' => 
             array (
                 'dbtype' => 'int',
                 'precision' => '11',
@@ -50,7 +50,7 @@ class modMediaSourceElement extends \MODX\Revolution\Sources\modMediaSourceEleme
                 'default' => 0,
                 'index' => 'pk',
             ),
-            'context_key' =>
+            'context_key' => 
             array (
                 'dbtype' => 'varchar',
                 'precision' => '100',
@@ -60,35 +60,35 @@ class modMediaSourceElement extends \MODX\Revolution\Sources\modMediaSourceEleme
                 'index' => 'pk',
             ),
         ),
-        'indexes' =>
+        'indexes' => 
         array (
-            'PRIMARY' =>
+            'PRIMARY' => 
             array (
                 'alias' => 'PRIMARY',
                 'primary' => true,
                 'unique' => true,
                 'type' => 'BTREE',
-                'columns' =>
+                'columns' => 
                 array (
-                    'source' =>
+                    'source' => 
                     array (
                         'length' => '',
                         'collation' => 'A',
                         'null' => false,
                     ),
-                    'object' =>
+                    'object' => 
                     array (
                         'length' => '',
                         'collation' => 'A',
                         'null' => false,
                     ),
-                    'object_class' =>
+                    'object_class' => 
                     array (
                         'length' => '',
                         'collation' => 'A',
                         'null' => false,
                     ),
-                    'context_key' =>
+                    'context_key' => 
                     array (
                         'length' => '',
                         'collation' => 'A',
@@ -97,9 +97,9 @@ class modMediaSourceElement extends \MODX\Revolution\Sources\modMediaSourceEleme
                 ),
             ),
         ),
-        'aggregates' =>
+        'aggregates' => 
         array (
-            'Source' =>
+            'Source' => 
             array (
                 'class' => 'MODX\\Revolution\\Sources\\modMediaSource',
                 'local' => 'source',
@@ -107,7 +107,7 @@ class modMediaSourceElement extends \MODX\Revolution\Sources\modMediaSourceEleme
                 'cardinality' => 'one',
                 'owner' => 'foreign',
             ),
-            'Element' =>
+            'Element' => 
             array (
                 'class' => 'MODX\\Revolution\\modElement',
                 'local' => 'object',
@@ -115,7 +115,7 @@ class modMediaSourceElement extends \MODX\Revolution\Sources\modMediaSourceEleme
                 'cardinality' => 'one',
                 'owner' => 'foreign',
             ),
-            'Context' =>
+            'Context' => 
             array (
                 'class' => 'MODX\\Revolution\\modContext',
                 'local' => 'context_key',
