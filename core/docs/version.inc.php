fc522f869248f4a3718f5ed310604e4987029aaf
Jason Coward <jason@opengeek.com>
2019-09-24 15:41:33 -0600

    Update version to 2.7.2-dev
    In the future, this should always be done before any new commits after a release

--- core/docs/version.inc.php
+++ core/docs/version.inc.php
@@ -2,8 +2,8 @@
 $v= array ();
 $v['version']= '2'; // Current version.
 $v['major_version']= '7'; // Current major version.
-$v['minor_version']= '1'; // Current minor version.
-$v['patch_level']= 'pl'; // Current patch level.
+$v['minor_version']= '2'; // Current minor version.
+$v['patch_level']= 'dev'; // Current patch level.
 $v['code_name']= 'Revolution'; // Current codename.
 $v['distro']= '@git@';
 $v['full_version']= $v['version'] . ($v['major_version'] ? ".{$v['major_version']}" : ".0") . ($v['minor_version'] ? ".{$v['minor_version']}" : ".0") . ($v['patch_level'] ? "-{$v['patch_level']}" : "");
