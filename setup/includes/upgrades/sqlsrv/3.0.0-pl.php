00078bfb2b9d679a7b80147caa5e097421d262f5
Jan Peca <pecajan@gmail.com>
2019-11-21 17:37:40 +0100

    Change the value of default_media_source_type system setting

--- setup/includes/upgrades/sqlsrv/3.0.0-pl.php
+++ setup/includes/upgrades/sqlsrv/3.0.0-pl.php
@@ -19,3 +19,4 @@ include dirname(__DIR__) . '/common/3.0.0-update-xtypes-system-settings.php';
 include dirname(__DIR__) . '/common/3.0.0-update-upload_files-upload_images.php';
 include dirname(__DIR__) . '/common/3.0.0-trim-gender-field-size.php';
 include dirname(__DIR__) . '/common/3.0.0-update-menu-entries.php';
+include dirname(__DIR__) . '/common/3.0.0-update-default-media-source-setting.php';
