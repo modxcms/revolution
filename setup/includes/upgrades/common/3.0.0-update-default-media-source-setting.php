e2d950784a637f94491e7794f6c814a3255e81af
John Peca <pecajan@gmail.com>
2019-11-25 23:35:04 +0100

    Change the value of default_media_source_type system setting (#14836)
    * Change the value of default_media_source_type system setting
    
    * Use ::class instead of hardcoded string

---
+++ setup/includes/upgrades/common/3.0.0-update-default-media-source-setting.php
@@ -0,0 +1,21 @@
+<?php
+/** Sets the default_media_source_type system setting to a correct class */
+
+use MODX\Revolution\modSystemSetting;
+
+$defaultMediaSourceType = $modx->getObject(modSystemSetting::class, [
+  'key' => 'default_media_source_type'
+]);
+
+if ($defaultMediaSourceType) {
+    switch($defaultMediaSourceType->get('value')) {
+        case 'sources.modFileMediaSource':
+            $defaultMediaSourceType->set('value', MODX\Revolution\Sources\modFileMediaSource::class);
+            break;
+        case 'sources.modS3MediaSource':
+        $defaultMediaSourceType->set('value', MODX\Revolution\Sources\modS3MediaSource::class);
+            break;
+    }
+
+    $defaultMediaSourceType->save();
+}
