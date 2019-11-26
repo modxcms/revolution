4d7438f20b77cf3977b89ea6a7ef3c5eb336ec82
Ruslan Aleev <aleev.insmile@gmail.com>
2019-11-26 16:26:06 +0400

    Script to remove unused settings of the "Rich-Text Editor" section when updating

---
+++ setup/includes/upgrades/common/3.0.0-cleanup-richtext-editor-system-settings.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Remove unused system settings for "Rich-Text Editor" section
+ */
+
+use MODX\Revolution\modSystemSetting;
+
+$settings = [
+    'editor_css_path',
+    'editor_css_selectors'
+];
+
+$messageTemplate = '<p class="%s">%s</p>';
+
+foreach ($settings as $key) {
+    /** @var modSystemSetting $setting */
+    $setting = $modx->getObject(modSystemSetting::class, ['key' => $key]);
+    if ($setting instanceof modSystemSetting) {
+        if ($setting->remove()) {
+            $this->runner->addResult(modInstallRunner::RESULT_SUCCESS,
+                sprintf($messageTemplate, 'ok', $this->install->lexicon('system_setting_cleanup_success', ['key' => $key])));
+        } else {
+            $this->runner->addResult(modInstallRunner::RESULT_WARNING,
+                sprintf($messageTemplate, 'warning', $this->install->lexicon('system_setting_cleanup_failure', ['key' => $key])));
+        }
+    }
+}
