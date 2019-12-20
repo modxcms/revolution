f721e50f41e23e564df71cc8b9f12f287ee04e01
Jan Peca <pecajan@gmail.com>
2019-12-20 17:36:03 +0100

    Remove call_user_func_array

--- core/src/Revolution/modX.php
+++ core/src/Revolution/modX.php
@@ -1724,7 +1724,7 @@ class modX extends xPDO {
         // We're limiting to subclasses of Processor to prevent instantiating arbitrary classes
         if (static::isProcessorClass($action)) {
             /** @var Processor $processor */
-            $processor = call_user_func_array([$action, 'getInstance'], [&$this, $action, $scriptProperties]);
+            $processor = $action::getInstance($this, $action, $scriptProperties);
 
             return $processor->run();
         }
@@ -1738,7 +1738,7 @@ class modX extends xPDO {
         }
         if (static::isProcessorClass($legacyAction)) {
             /** @var Processor $processor */
-            $processor = call_user_func_array([$legacyAction, 'getInstance'], [&$this, $legacyAction, $scriptProperties]);
+            $processor = $legacyAction::getInstance($this, $legacyAction, $scriptProperties);
 
             return $processor->run();
         }
