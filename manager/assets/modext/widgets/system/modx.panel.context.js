961894b7281f5084f33db3899a677835a32f492e
Ruslan Aleev <aleev.insmile@gmail.com>
2020-01-31 17:20:52 +0400

    Corrected breadcrumbs in the Context panel

--- manager/assets/modext/widgets/system/modx.panel.context.js
+++ manager/assets/modext/widgets/system/modx.panel.context.js
@@ -12,7 +12,7 @@ MODx.panel.Context = function(config) {
             action: 'Context/Get'
         }
         ,id: 'modx-panel-context'
-		,cls: 'container'
+        ,cls: 'container'
         ,class_key: 'modContext'
         ,plugin: ''
         ,bodyStyle: ''
@@ -21,55 +21,55 @@ MODx.panel.Context = function(config) {
             ,autoHeight: true
             ,layout: 'form'
             ,defaults: { border: false ,msgTarget: 'side' }
-			,items:[{
-				xtype: 'panel'
-				,border: false
-				,cls:'main-wrapper'
-				,layout: 'form'
-				,items: [{
-					xtype: 'statictextfield'
-					,fieldLabel: _('key')
-					,name: 'key'
-					,width: 300
-					,maxLength: 100
-					,enableKeyEvents: true
-					,allowBlank: true
-					,value: config.context
-					,submitValue: true
-				},{
-					xtype: 'textfield'
-					,fieldLabel: _('name')
-					,name: 'name'
-					,width: 300
-					,maxLength: 255
-				},{
-					xtype: 'textarea'
-					,fieldLabel: _('description')
-					,name: 'description'
-					,width: 300
-					,grow: true
-				},{
-					xtype: 'numberfield'
-					,fieldLabel: _('rank')
-					,name: 'rank'
-					,allowBlank: true
-					,width: 300
-				},{
-					html: MODx.onContextFormRender
-					,border: false
-				}]
-			}]
+            ,items:[{
+                xtype: 'panel'
+                ,border: false
+                ,cls:'main-wrapper'
+                ,layout: 'form'
+                ,items: [{
+                    xtype: 'statictextfield'
+                    ,fieldLabel: _('key')
+                    ,name: 'key'
+                    ,width: 300
+                    ,maxLength: 100
+                    ,enableKeyEvents: true
+                    ,allowBlank: true
+                    ,value: config.context
+                    ,submitValue: true
+                },{
+                    xtype: 'textfield'
+                    ,fieldLabel: _('name')
+                    ,name: 'name'
+                    ,width: 300
+                    ,maxLength: 255
+                },{
+                    xtype: 'textarea'
+                    ,fieldLabel: _('description')
+                    ,name: 'description'
+                    ,width: 300
+                    ,grow: true
+                },{
+                    xtype: 'numberfield'
+                    ,fieldLabel: _('rank')
+                    ,name: 'rank'
+                    ,allowBlank: true
+                    ,width: 300
+                },{
+                    html: MODx.onContextFormRender
+                    ,border: false
+                }]
+            }]
         },{
             title: _('context_settings')
             ,autoHeight: true
-			,layout: 'form'
+            ,layout: 'form'
             ,items: [{
                 html: '<p>'+_('context_settings_desc')+'</p>'
                 ,id: 'modx-context-settings-desc'
                 ,xtype: 'modx-description'
             },{
                 xtype: 'modx-grid-context-settings'
-				,cls:'main-wrapper'
+                ,cls:'main-wrapper'
                 ,title: ''
                 ,preventRender: true
                 ,context_key: config.context
@@ -82,7 +82,7 @@ MODx.panel.Context = function(config) {
             ,autoHeight: true
             ,items:[{
                 xtype: 'modx-grid-access-context'
-				,cls:'main-wrapper'
+                ,cls:'main-wrapper'
                 ,title: ''
                 ,preventRender: true
                 ,context_key: config.context
@@ -104,6 +104,7 @@ MODx.panel.Context = function(config) {
 };
 Ext.extend(MODx.panel.Context,MODx.FormPanel,{
     initialized: false
+
     ,setup: function() {
         if (this.initialized || (this.config.context === '' || this.config.context === 0)) {
             this.fireEvent('ready');
@@ -116,16 +117,17 @@ Ext.extend(MODx.panel.Context,MODx.FormPanel,{
                 ,key: this.config.context
             }
             ,listeners: {
-            	'success': {fn:function(r) {
+                'success': {fn:function(r) {
                     this.getForm().setValues(r.object);
-                    Ext.getCmp('modx-header-breadcrumbs').updateHeader(r.object.key);
+                    Ext.getCmp('modx-header-breadcrumbs').updateHeader(Ext.util.Format.htmlEncode(r.object.key));
                     this.fireEvent('ready');
                     MODx.fireEvent('ready');
                     this.initialized = true;
-            	},scope:this}
+                },scope:this}
             }
         });
     }
+
     ,beforeSubmit: function(o) {
         var r = {};
 
@@ -134,6 +136,7 @@ Ext.extend(MODx.panel.Context,MODx.FormPanel,{
 
         Ext.apply(o.form.baseParams,r);
     }
+
     ,success: function(o) {
         var g = Ext.getCmp('modx-grid-context-settings');
         if (g) { g.getStore().commitChanges(); }
@@ -141,6 +144,7 @@ Ext.extend(MODx.panel.Context,MODx.FormPanel,{
         var t = parent.Ext.getCmp('modx-resource-tree');
         if (t) { t.refresh(); }
     }
+
     ,getPageHeader: function(config) {
         return MODx.util.getHeaderBreadCrumbs('modx-context-name', [{
             text: _('contexts'),
