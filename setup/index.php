825dbf951aaf3fdb67f4cafddd411e967f3d76b5
Joshua Lückers <joshua.luckers@springbokagency.com>
2020-02-08 11:14:31 +0100

    Check if the timezone is set properly without checking if we are running PHP 5.6 or later

--- setup/index.php
+++ setup/index.php
@@ -49,16 +49,14 @@ if (!function_exists('json_encode')) {
     die('<html><head><title></title></head><body><h1>FATAL ERROR: MODX Setup cannot continue.</h1><p>MODX requires the PHP JSON extension! You\'re PHP configuration at version '.MODX_SETUP_PHP_VERSION.' does not appear to have this extension enabled. This should be a standard extension on PHP 5.2+; it is available as a PECL extension in 5.1.</p></body></html>');
 }
 
-/* make sure date.timezone is set for PHP 5.3.0+ users */
-if (version_compare(MODX_SETUP_PHP_VERSION,'5.6.0') >= 0) {
-    $phptz = @ini_get('date.timezone');
-    if (empty($phptz)) {
-        date_default_timezone_set('UTC');
-    }
-    if (!date_default_timezone_get()) {
-        die('<html><head><title></title></head><body><h1>FATAL ERROR: MODX Setup cannot continue.</h1><p>To use PHP 5.6.0+, you must set the date.timezone setting in your php.ini (or have at least UTC in the list of supported timezones). Please do set it to a proper timezone before proceeding. A list can be found <a href="http://us.php.net/manual/en/timezones.php">here</a>.</p></body></html>');
-    }
+$phptz = @ini_get('date.timezone');
+if (empty($phptz)) {
+    date_default_timezone_set('UTC');
 }
+if (!date_default_timezone_get()) {
+    die('<html><head><title></title></head><body><h1>FATAL ERROR: MODX Setup cannot continue.</h1><p>To use PHP 5.6.0+, you must set the date.timezone setting in your php.ini (or have at least UTC in the list of supported timezones). Please do set it to a proper timezone before proceeding. A list can be found <a href="http://us.php.net/manual/en/timezones.php">here</a>.</p></body></html>');
+}
+
 if (!$isCommandLine) {
     $https = isset($_SERVER['HTTPS']) ? $_SERVER['HTTPS'] : false;
     $installBaseUrl= (!$https || strtolower($https) != 'on') ? 'http://' : 'https://';
