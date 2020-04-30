92c2fac03ed51986a51c2d88a2cc211def6f7880
Jason Coward <jason@opengeek.com>
2020-04-30 11:04:56 -0600

    Update version for 2.8.0-dev

--- core/docs/version.inc.php
+++ core/docs/version.inc.php
@@ -1,9 +1,9 @@
 <?php
 $v= array ();
 $v['version']= '2'; // Current version.
-$v['major_version']= '7'; // Current major version.
-$v['minor_version']= '3'; // Current minor version.
-$v['patch_level']= 'pl'; // Current patch level.
+$v['major_version']= '8'; // Current major version.
+$v['minor_version']= '0'; // Current minor version.
+$v['patch_level']= 'dev'; // Current patch level.
 $v['code_name']= 'Revolution'; // Current codename.
 $v['distro']= '@git@';
 $v['full_version']= $v['version'] . ($v['major_version'] ? ".{$v['major_version']}" : ".0") . ($v['minor_version'] ? ".{$v['minor_version']}" : ".0") . ($v['patch_level'] ? "-{$v['patch_level']}" : "");
