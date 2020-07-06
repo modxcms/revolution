4365f6fbf5cd2030d91a77a849e1d0235cbec68c
Ruslan-Aleev <aleev.insmile@gmail.com>
2020-07-06 16:51:12 +0300

    Added a menu update script

---
+++ setup/includes/upgrades/common/3.0.0-update-menu-main.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Common upgrade script for modify modMenu entries
+ *
+ * @var modX $modx
+ * @package setup
+ */
+
+use MODX\Revolution\modMenu;
+
+$menu = [
+    'site' => '<i class="icon-file-text-o icon icon-large"></i>',
+    'media' => '<i class="icon-file-image-o icon icon-large"></i>',
+    'components' => '<i class="icon-cube icon icon-large"></i>',
+    'manage' => '<i class="icon-sliders icon icon-large"></i>',
+    'user' => '<span id="user-avatar">{$userImage}</span><span id="user-username">{$username}</span>',
+    'admin' => '<i class="icon-gear icon icon-large"></i>',
+    'about' => '<i class="icon-question-circle icon icon-large"></i>',
+];
+
+foreach ($menu as $key => $value) {
+    /** @var modMenu $menu_item */
+
+    $menu_item = $modx->getObject(modMenu::class, ['text' => $key]);
+    $menu_item->set('description', '');
+    $menu_item->set('icon', $value);
+    $menu_item->save();
+
+}
