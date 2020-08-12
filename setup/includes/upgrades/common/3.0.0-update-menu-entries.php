f2ed73c8b33fe203936233dbbe80383c52db2849
John Peca <pecajan@gmail.com>
2020-08-12 22:03:04 +0200

    Recreate form customization menu entry (#15112)
    * Recreate form customization menu entry
    
    * Check if the new menu entry already exists

--- setup/includes/upgrades/common/3.0.0-update-menu-entries.php
+++ setup/includes/upgrades/common/3.0.0-update-menu-entries.php
@@ -10,8 +10,23 @@ use MODX\Revolution\modMenu;
 
 /** @var modMenu $formCustomization */
 $formCustomization = $modx->getObject(modMenu::class, ['text' => 'bespoke_manager']);
+
 if ($formCustomization) {
-    $formCustomization->set('text', 'form_customization');
-    $formCustomization->set('description', 'form_customization_desc');
-    $formCustomization->save();
+    $newData = $formCustomization->toArray();
+    $newData['text'] = 'form_customization';
+    $newData['description'] = 'form_customization_desc';
+
+    $created = true;
+    $newFormCustomization = $modx->getObject(modMenu::class, ['text' => 'form_customization']);
+    if (!$newFormCustomization) {
+        $newFormCustomization = $modx->newObject(modMenu::class);
+        $newFormCustomization->fromArray($newData, '', true, true);
+
+        $created = $newFormCustomization->save();
+    }
+
+    if ($created) {
+        $formCustomization->remove();
+    }
 }
+
