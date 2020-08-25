85c6a6acafbfe9096a345c9ceaa6ad735e928183
Bochkarev Ivan <ivanx86@gmail.com>
2020-08-25 20:27:22 +0600

    Register woff2 format in system settings (#15160)
    * Register woff2 format in system settings
    
    * Group the order of the extensions by file type
    
    Co-authored-by: Thomas Jakobi <thomas.jakobi@partout.info>

--- setup/includes/upgrades/mysql/2.8.0-pl.php
+++ setup/includes/upgrades/mysql/2.8.0-pl.php
@@ -1,3 +1,4 @@
 <?php
 /* run upgrades common to all db platforms */
 include dirname(__DIR__) . '/common/2.8.0-update-upload_files-upload_images.php';
+include dirname(__DIR__) . '/common/2.8.0-update-upload_files-woff2.php';
