649cccb76efe16782ea54b4ce5e22ecc38e7cc06
Thomas Jakobi <thomas.jakobi@partout.info>
2020-09-20 11:46:02 +0200

    Remove not needed checks

--- core/model/modx/mail/phpmailer/PHPMailerAutoload.php
+++ core/model/modx/mail/phpmailer/PHPMailerAutoload.php
@@ -30,20 +30,4 @@ function PHPMailerAutoload($classname)
     }
 }
 
-if (version_compare(PHP_VERSION, '5.1.2', '>=')) {
-    //SPL autoloading was introduced in PHP 5.1.2
-    if (version_compare(PHP_VERSION, '5.3.0', '>=')) {
-        spl_autoload_register('PHPMailerAutoload', true, true);
-    } else {
-        spl_autoload_register('PHPMailerAutoload');
-    }
-} else {
-    /**
-     * Fall back to traditional autoload for old PHP versions
-     * @param string $classname The name of the class to load
-     */
-    function __autoload($classname)
-    {
-        PHPMailerAutoload($classname);
-    }
-}
+spl_autoload_register('PHPMailerAutoload', true, true);
