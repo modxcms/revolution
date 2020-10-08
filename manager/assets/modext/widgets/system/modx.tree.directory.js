88a4f714512e288e84f42e06a8b00924cae2269d
Ilya Utkin <ilyautkin@mail.ru>
2020-10-08 16:37:14 +0300

    Fixed incorrect path setting when uploading files #15227

--- manager/assets/modext/widgets/system/modx.tree.directory.js
+++ manager/assets/modext/widgets/system/modx.tree.directory.js
@@ -272,6 +272,7 @@ Ext.extend(MODx.tree.Directory,MODx.tree.Tree,{
     }
 
     ,changeSource: function(sel) {
+        this.cm.activeNode = '';
         var s = sel.getValue();
         var rn = this.getRootNode();
         if (rn) { rn.setText(sel.getRawValue()); }
@@ -308,6 +309,9 @@ Ext.extend(MODx.tree.Directory,MODx.tree.Tree,{
         if (!n.expanded && !n.isRoot) {
             // Node has been collapsed, grab its parent
             n = n.parentNode;
+        }
+        if (n.id == this.config.openTo) {
+            n.select();
         }
         var p = n.getPath('text');
         Ext.state.Manager.set(this.treestate_id, p);
@@ -759,7 +763,7 @@ Ext.extend(MODx.tree.Directory,MODx.tree.Tree,{
     }
 
     ,beforeUpload: function() {
-        var path = this.config.rootId || '/';
+        var path = this.config.openTo || this.config.rootId || '/';
         if (this.cm.activeNode) {
             path = this.getPath(this.cm.activeNode);
             if(this.cm.activeNode.isLeaf()) {
