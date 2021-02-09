5fb05be2eaa118bd8df6d0168c3cdc82407fe925
Thomas Jakobi <thomas.jakobi@partout.info>
2021-02-09 17:34:45 +0100

    Remove flash based xtypes (#15312)
    * Remove flash based xtypes
    'chart', 'piechart', 'cartesianchart' and the derived xtypes
    
    * Add the chart components in an extra file.
    
    * Deactivate MODx.util.Clipboard
    
    * Remove the chart components finally
    
    * Disable flash fallback
    
    * Completely remove MODx.util.Clipboard
    
    A github search for MODx.util.Clipboard filename:utilities.js shows 459 results, most in the utilities.js file or in some cache files. So I think that the component is no longer in use.
    
    * grunt build
    
    Co-authored-by: Jason Coward <jason@opengeek.com>

--- manager/assets/fileapi/FileAPI.js
+++ manager/assets/fileapi/FileAPI.js
@@ -1840,7 +1840,7 @@
 				api.getDropFiles(evt, function (files, all){
 					onDrop.call(evt[currentTarget], files, all, evt);
 				});
-				
+
 				onHover.call(evt[currentTarget], false, evt);
 			});
 		}
@@ -3510,25 +3510,7 @@
 
 
 	api.support.flash = (function (){
-		var mime = navigator.mimeTypes, has = false;
-
-		if( navigator.plugins && typeof navigator.plugins['Shockwave Flash'] == 'object' ){
-			has	= navigator.plugins['Shockwave Flash'].description && !(mime && mime['application/x-shockwave-flash'] && !mime['application/x-shockwave-flash'].enabledPlugin);
-		}
-		else {
-			try {
-				has	= !!(window.ActiveXObject && new ActiveXObject('ShockwaveFlash.ShockwaveFlash'));
-			}
-			catch(er){
-				api.log('Flash -- does not supported.');
-			}
-		}
-
-		if( has && /^file:/i.test(location) ){
-			api.log('[warn] Flash does not work on `file:` protocol.');
-		}
-
-		return	has;
+		return	false;
 	})();
 
 
