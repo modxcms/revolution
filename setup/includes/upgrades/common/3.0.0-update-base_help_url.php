5299e627951996d323d05b1c98c9dc128873186b
Bochkarev Ivan <ivanx86@gmail.com>
2021-02-24 16:46:15 +0600

    Update setup/includes/upgrades/common/3.0.0-update-base_help_url.php
    Co-authored-by: Joshua Lückers <joshualuckers@me.com>

--- setup/includes/upgrades/common/3.0.0-update-base_help_url.php
+++ setup/includes/upgrades/common/3.0.0-update-base_help_url.php
@@ -4,12 +4,13 @@
 /**
  * Update the URL base_help_url
  */
+use MODX\Revolution\modSystemSetting;
 
 /** @var modSystemSetting $base_help_url */
-$base_help_url = $modx->getObject('modSystemSetting', array(
+$base_help_url = $modx->getObject(modSystemSetting::class, [
     'key' => 'base_help_url',
     'value' => '//docs.modx.com/display/revolution20/',
-));
+]);
 if ($base_help_url) {
     $base_help_url->set('value', '//docs.modx.com/3.x/en/index');
     $base_help_url->save();
