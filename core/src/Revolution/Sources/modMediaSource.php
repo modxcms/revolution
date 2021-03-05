1b74804cbf3430b29b22e318c060e65dec13a07b
Ilya Utkin <ilyautkin@mail.ru>
2021-03-05 18:46:17 +0300

    Check if function exif_read_data exists

--- core/src/Revolution/Sources/modMediaSource.php
+++ core/src/Revolution/Sources/modMediaSource.php
@@ -2184,13 +2184,15 @@ abstract class modMediaSource extends modAccessibleSimpleObject implements modMe
             } else {
                 $absolute_path = $path;
             }
-            $exif = @exif_read_data($absolute_path);
-            if (!empty($exif) && $exif['Orientation'] >= 5) {
-                // This image was rotated
-                $new_width = $size['height'];
-                $new_height = $size['width'];
-                $size['width'] = $new_width;
-                $size['height'] = $new_height;
+            if (function_exists('exif_read_data')) {
+                $exif = exif_read_data($absolute_path);
+                if (!empty($exif) && $exif['Orientation'] >= 5) {
+                    // This image was rotated
+                    $new_width = $size['height'];
+                    $new_height = $size['width'];
+                    $size['width'] = $new_width;
+                    $size['height'] = $new_height;
+                }
             }
             // get original image size for proportional scaling
             if ($size['width'] > $size['height']) {
