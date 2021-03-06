ce03f7746d3f623777db598ea30c72bf2989819f
Bochkarev Ivan <ivanx86@gmail.com>
2021-03-06 10:56:49 +0600

    Flatten the lexicon parameters by dot notation

--- core/src/Revolution/modLexicon.php
+++ core/src/Revolution/modLexicon.php
@@ -581,11 +581,35 @@ class modLexicon
             return $str;
         }
 
+        $params = $this->_flatten($params);
         foreach ($params as $k => $v) {
             $str = str_replace('[[+' . $k . ']]', $v, $str);
         }
 
         return $str;
+    }
+
+    /**
+     * Flattens an array by dot notation
+     *
+     * @access private
+     * @param array $array The array to flatten
+     * @return array The processed array
+     */
+    private function _flatten($array) {
+        $result = [];
+
+        if (is_array($array)) {
+            $iterator = new RecursiveIteratorIterator(new RecursiveArrayIterator($array));
+            foreach ($iterator as $value) {
+                $keys = array();
+                foreach (range(0, $iterator->getDepth()) as $depth) {
+                    $keys[] = $iterator->getSubIterator($depth)->key();
+                }
+                $result[ join('.', $keys) ] = $value;
+            }
+        }
+        return $result;
     }
 
     /**
