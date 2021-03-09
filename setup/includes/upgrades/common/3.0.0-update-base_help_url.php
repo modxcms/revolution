eafcda7d152b19bba7fa7d3d066bc12c8e7fa314
Bochkarev Ivan <ivanx86@gmail.com>
2021-03-09 22:29:33 +0600

    Updating url in system setting base_help_url (#15411)
    * Updating url in system setting base_help_url
    
    * Update setup/includes/upgrades/common/3.0.0-update-base_help_url.php
    
    Co-authored-by: Joshua Lückers <joshualuckers@me.com>
    
    Co-authored-by: Joshua Lückers <joshualuckers@me.com>

---
+++ setup/includes/upgrades/common/3.0.0-update-base_help_url.php
@@ -0,0 +1,17 @@
+
+@@ -0,0 +1,14 @@
+<?php
+/**
+ * Update the URL base_help_url
+ */
+use MODX\Revolution\modSystemSetting;
+
+/** @var modSystemSetting $base_help_url */
+$base_help_url = $modx->getObject(modSystemSetting::class, [
+    'key' => 'base_help_url',
+    'value' => '//docs.modx.com/display/revolution20/',
+]);
+if ($base_help_url) {
+    $base_help_url->set('value', '//docs.modx.com/3.x/en/index');
+    $base_help_url->save();
+}
