a2065d3bece97e405de91b13e73e402cba618518
Digital Penguin <digitalpenguin@users.noreply.github.com>
2021-03-17 23:02:18 +0800

    Improved check to see if the key "url" exists in media source $properties array. This fixes the preview thumbnail issue. (#15526)
    * Improved check to see if the key "url" exists in media source $properties array. This fixes the preview thumbnail issue.
    
    * Switched to using isset()

--- core/src/Revolution/Sources/modMediaSource.php
+++ core/src/Revolution/Sources/modMediaSource.php
@@ -1496,7 +1496,7 @@ abstract class modMediaSource extends modAccessibleSimpleObject implements modMe
         }
 
         $properties = $this->getPropertyList();
-        if ($properties['url']) {
+        if (isset($properties['url'])) {
             $src = $properties['url'] . DIRECTORY_SEPARATOR . ltrim($src, DIRECTORY_SEPARATOR);
         }
 
