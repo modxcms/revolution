08b061441d5729af3c58fe43a02892e5404af509
Ruslan-Aleev <aleev.insmile@gmail.com>
2021-03-22 12:47:40 +0300

    Changed buttons for FCs

--- manager/assets/modext/sections/fc/list.js
+++ manager/assets/modext/sections/fc/list.js
@@ -1,16 +1,16 @@
 MODx.page.FormCustomization = function(config) {
-	config = config || {};
-	Ext.applyIf(config,{
+    config = config || {};
+    Ext.applyIf(config,{
         components: [{
             xtype: 'modx-panel-fc-profiles'
         }]
         ,buttons: [{
-            text: _('help_ex')
+            text: '<i class="icon icon-question-circle"></i>'
             ,id: 'modx-abtn-help'
             ,handler: MODx.loadHelpPane
         }]
-	});
-	MODx.page.FormCustomization.superclass.constructor.call(this,config);
+    });
+    MODx.page.FormCustomization.superclass.constructor.call(this,config);
 };
 Ext.extend(MODx.page.FormCustomization,MODx.Component);
 Ext.reg('modx-page-form-customization',MODx.page.FormCustomization);
