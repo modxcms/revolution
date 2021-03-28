0bfcf37793403b633e53f6cc9e852b10bde2aa89
Bochkarev Ivan <ivanx86@gmail.com>
2021-03-29 02:01:19 +0600

    Update base_help_url system settings with a new link to the documentation (#15572)

--- _build/data/transport.core.system_settings.php
+++ _build/data/transport.core.system_settings.php
@@ -130,7 +130,7 @@ $settings['automatic_template_assignment']->fromArray([
 $settings['base_help_url']= $xpdo->newObject(modSystemSetting::class);
 $settings['base_help_url']->fromArray([
   'key' => 'base_help_url',
-  'value' => '//docs.modx.com/3.x/en/index',
+  'value' => '//docs.modx.com/help/',
   'xtype' => 'textfield',
   'namespace' => 'core',
   'area' => 'manager',
