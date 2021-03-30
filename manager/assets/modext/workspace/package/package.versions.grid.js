823caf1bcc82ad104256bf55606fa30c0dee6d2a
Ruslan-Aleev <aleev.insmile@gmail.com>
2021-03-30 17:57:51 +0300

    Fixed sorting in grid for grid.PackageVersions

--- manager/assets/modext/workspace/package/package.versions.grid.js
+++ manager/assets/modext/workspace/package/package.versions.grid.js
@@ -14,21 +14,34 @@ MODx.grid.PackageVersions = function(config) {
             ,signature: config.signature
             ,package_name: MODx.request.package_name
         }
-        ,fields: ['signature','name','version','release','created','updated','installed','state'
-                 ,'workspace','provider','provider_name','disabled','source'
-                 ,'readme','menu']
+        ,fields: ['signature','name','version','release','created','updated','installed','state','workspace','provider','provider_name','disabled','source','readme','menu']
         ,plugins: [this.exp]
         ,pageSize: 20
-        ,columns: [this.exp,{
-              header: _('name') ,dataIndex: 'name' }
-           ,{ header: _('version') ,dataIndex: 'version' }
-           ,{ header: _('release') ,dataIndex: 'release' }
-            ,{ header: _('installed') ,dataIndex: 'installed' ,renderer: this._rins }
-            ,{
-                header: _('provider')
-                ,dataIndex: 'provider_name'
-                ,editable: false
-            }]
+        ,columns: [
+        this.exp,
+        {
+            header: _('name')
+            ,dataIndex: 'name'
+            ,sortable: true
+        },{
+            header: _('version')
+            ,dataIndex: 'version'
+            ,sortable: true
+        },{
+            header: _('release')
+            ,dataIndex: 'release'
+            ,sortable: true
+        },{
+            header: _('installed')
+            ,dataIndex: 'installed'
+            ,sortable: true
+            ,renderer: this._rins
+        },{
+            header: _('provider')
+            ,dataIndex: 'provider_name'
+            ,sortable: true
+            ,editable: false
+        }]
         ,primaryKey: 'signature'
         ,paging: true
         ,autosave: true
