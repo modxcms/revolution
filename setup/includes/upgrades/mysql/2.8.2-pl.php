75d663008b639a8e97de38b0380c1bbb5d57979e
Mark Hamstra <hello@markhamstra.com>
2021-04-07 17:29:48 +0200

    Remove legacy file manager processors (#15646)
    Reported by Solar Security [SEC-3837]. These file/folder processors were once part of a file manager, predating media sources and more fine-grained access control to files. Any manager user with file_manager permission could use these to interact with existing files and create/remove directories.

--- setup/includes/upgrades/mysql/2.8.2-pl.php
+++ setup/includes/upgrades/mysql/2.8.2-pl.php
@@ -1,3 +1,4 @@
 <?php
 /* run upgrades common to all db platforms */
 include dirname(__DIR__) . '/common/2.8.2-update-base_help_url.php';
+include dirname(__DIR__) . '/common/2.8.2-remove-processors.php';
