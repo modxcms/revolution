fe9ac81afe750a3f5acf21e76e16da0995e64eab
Mark Hamstra <hello@markhamstra.com>
2021-05-17 17:12:24 +0200

    Adjust filtering of placeholders and accessible options for `@SELECT` (#15714)

--- core/model/modx/modx.class.php
+++ core/model/modx/modx.class.php
@@ -574,7 +574,7 @@ class modX extends xPDO {
             if (is_array ($this->config)) {
                 $c = $this->config;
                 if ((bool)$this->getOption('filter_config_placeholders', null, true)) {
-                    unset($c['password'], $c['username'], $c['mail_smtp_pass'], $c['mail_smtp_user'], $c['proxy_password'], $c['proxy_username'], $c['connections'], $c['connection_init'], $c['connection_mutable'], $c['dbname'], $c['database'], $c['table_prefix'], $c['driverOptions'], $c['dsn'], $c['session_name'], $c['assets_path'], $c['base_path'], $c['cache_path'], $c['connectors_path'], $c['core_path'], $c['friendly_alias_translit_class_path'], $c['manager_path'], $c['processors_path']);
+                    unset($c['password'], $c['username'], $c['mail_smtp_pass'], $c['mail_smtp_user'], $c['proxy_password'], $c['proxy_username'], $c['connections'], $c['connection_init'], $c['connection_mutable'], $c['dbname'], $c['database'], $c['table_prefix'], $c['driverOptions'], $c['dsn'], $c['session_name'], $c['cache_path'], $c['connectors_path'], $c['friendly_alias_translit_class_path'], $c['manager_path'], $c['processors_path']);
                 }
                 $this->setPlaceholders($c, '+');
             }
