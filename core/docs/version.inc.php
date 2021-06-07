db23a3ff7f76bddf6ccd1e66952a3fda49c324a8
Ilya Utkin <ilyautkin@mail.ru>
2021-06-07 15:47:42 +0300

    MODX 3S 2

--- core/docs/version.inc.php
+++ core/docs/version.inc.php
@@ -1,16 +1,16 @@
 <?php
 
-$fork_version = 1; // Change this when needed new version for MODX3s
+$f_version = 2; // Change this when needed new version for MODX 3S
 
 $v = [];
 $v['version']        = '3'; // Current version.
 $v['major_version']  = '0'; // Current major version.
 $v['minor_version']  = '0'; // Current minor version.
-$v['patch_level']    = 's' . $fork_version; // Patch level for display in SiteDash
+$v['patch_level']    = 's' . $f_version; // Patch level for display in SiteDash
 $v['code_name']      = 'Revolution'; // Current codename.
 $v['distro']         = '@git@';
 $v['full_version']   = $v['version'] . ($v['major_version'] ? ".{$v['major_version']}" : ".0") . ($v['minor_version'] ? ".{$v['minor_version']}" : ".0") . ($v['patch_level'] ? "-{$v['patch_level']}" : "");
 $v['full_appname']   = 'MODX' . ($v['code_name'] ? " {$v['code_name']} " : " ") . $v['full_version'] . ' (' . trim($v['distro'], '@') . ')';
-$v['modx3s_version'] = 'MODX 3S ' . $fork_version; // Version for MODX3s - Sterc fork
+$v['modx3s_version'] = 'MODX 3S ' . $f_version; // Version for MODX3s - Sterc fork
 
 return $v;
