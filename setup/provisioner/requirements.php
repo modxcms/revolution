17ef1acb0b569f3975aeeb52466ccf39133dd842
Joshua Lückers <joshualuckers@me.com>
2021-07-10 13:53:10 +0200

    Update minimum PHP version to 7.2 in setup requirements

--- setup/provisioner/requirements.php
+++ setup/provisioner/requirements.php
@@ -8,7 +8,7 @@
  * files found in the top-level directory of this distribution.
  */
 
-define('MODX_MINIMUM_REQUIRED_PHP_VERSION', '7.0.0');
+define('MODX_MINIMUM_REQUIRED_PHP_VERSION', '7.2.0');
 define('MODX_REQUIRED_EXTENSIONS', [
     "curl",
     "dom",
