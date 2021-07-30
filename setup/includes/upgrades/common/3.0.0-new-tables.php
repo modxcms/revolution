fe286f49a97e6c9902b719702f8ad5bc063c805f
Mark Hamstra <hello@markhamstra.com>
2021-07-30 19:21:50 +0200

    Create the new deprecated log tables on upgrade

---
+++ setup/includes/upgrades/common/3.0.0-new-tables.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Add new tables introduced in 3.0
+ *
+ * @var modX $modx
+ *
+ * @package setup
+ */
+
+$manager = $modx->getManager();
+
+$manager->createObjectContainer(\MODX\Revolution\modDeprecatedMethod::class);
+$manager->createObjectContainer(\MODX\Revolution\modDeprecatedCall::class);
