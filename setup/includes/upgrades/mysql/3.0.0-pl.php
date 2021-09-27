21068cc155196c340e56a69787814100d2eeeb1b
digitalpenguin <murray@digitalpenguin.hk>
2021-09-27 21:50:29 +0800

    Refactor PR #14057 - Remove obsolete contentType field

--- setup/includes/upgrades/mysql/3.0.0-pl.php
+++ setup/includes/upgrades/mysql/3.0.0-pl.php
@@ -34,3 +34,4 @@ include dirname(__DIR__) . '/common/3.0.0-policy-description.php';
 include dirname(__DIR__) . '/common/3.0.0-non-index-field-length.php';
 include dirname(__DIR__) . '/common/3.0.0-update-base_help_url.php';
 include dirname(__DIR__) . '/common/3.0.0-template-preview.php';
+include dirname(__DIR__) . '/common/3.0.0-remove-content-type-field.php';
