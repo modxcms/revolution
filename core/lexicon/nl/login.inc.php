e7ddd92e33895359acc41bcf90a6503b2479d83e
Sander Drenth <sander@sterc.nl>
2021-10-04 09:18:38 +0200

    Fixing Dutch translations

--- core/lexicon/nl/login.inc.php
+++ core/lexicon/nl/login.inc.php
@@ -18,7 +18,7 @@ $_lang['login_cannot_locate_account'] = 'De gebruikersnaam of het wachtwoord zij
 $_lang['login_copyright'] = '&copy; 2005-[[+current_year]] door het <a href="http://modxcms.com/" target="_blank" title="MODX.com website">MODX Team</a>. <strong>MODX</strong>&trade; valt onder de GPL v2.0 licentie of later.';
 $_lang['login_email_label'] = 'Account e-mail:';
 $_lang['login_err_unknown'] = 'Er is een onbekende fout opgetreden tijdens het inloggen.';
-$_lang['login_forget_your_login'] = 'Uw wachtwoord vergeten?';
+$_lang['login_forget_your_login'] = 'Wachtwoord vergeten?';
 $_lang['login_forget_your_login_note'] = 'Om een nieuw wachtwoord aan te maken, vul uw gebruikersnaam of email onderstaand in. Als u een account heeft, zal er activatie link worden verstuurd naar uw email adres.';
 $_lang['login_new_password'] = 'Nieuw wachtwoord';
 $_lang['login_new_password_note'] = 'Bevestig uw wachtwoord door hem tweemaal in te vullen.<br><br>&bull;&nbsp;<strong>[[+length]]</strong> karakters is het minimale wachtwoord lengte.';
@@ -28,7 +28,7 @@ $_lang['login_hostname_error'] = 'Je hostnaam wijst niet terug naar jouw IP adre
 $_lang['login_message'] = 'Vul je inloggegevens in om jouw Manager sessie te starten. Je gebruikersnaam en wachtwoord zijn hoofdlettergevoelig, dus vul het met zorg in!';
 $_lang['login_password'] = 'Wachtwoord';
 $_lang['login_password_reset_act_sent'] = 'Als de gebruiker of het e-mailadres bestaat, ontvangt u spoedig een e-mail.';
-$_lang['login_remember'] = 'Houdt mij ingelogd voor [[+lifetime]]';
+$_lang['login_remember'] = 'Houd mij ingelogd voor [[+lifetime]]';
 $_lang['login_send_activation_email'] = 'Stuur activatie e-mail';
 $_lang['login_title'] = 'Aanmelden';
 $_lang['login_user_err_nf_email'] = 'Als de gebruiker of het e-mailadres bestaat, ontvangt u spoedig een e-mail.';
@@ -39,10 +39,10 @@ $_lang['login_user_inactive'] = 'Je gebruikersaccount is uitgeschakeld. Neem con
 $_lang['login_email_subject'] = 'Je inloggegevens';
 $_lang['login_forgot_email'] = '<h2>Wachtwoord vergeten?</h2><p> We hebben een verzoek ontvangen om uw MODX Revolution wachtwoord aan te passen. U kunt uw wachtwoord vernieuwen door op de onderstaande knop te klikken en de instructie\'s te volgen.</p><p class="center"><a href="[[+url_scheme]][[+http_host]][[+manager_url]]?modhash=[[+hash]]" class="btn">Wachtwoord vernieuwen</a></p><p class="small">Als u dit verzoek niet heeft ingediend, negeer a.u.b. deze mail.</p>';
 $_lang['login_signup_email'] = '<p>Hallo, [[+username]]!</p><p>Er is een account geregistreerd voor de <strong>[[++site_name]]</strong> website. Mocht uw wachtwoord niet bekent zijn, kunt u [[++allow_manager_login_forgot_password:is=`1`:then=` het venieuwen via het inlog scherm`:else=`vragen aan de site beheerder`]].</p><p class="center"><a href="[[+url_scheme]][[+http_host]][[+manager_url]]" class="btn">Log in op [[++site_name]]</a></p>';
-$_lang['login_greeting_morning'] = '<strong>Goede morgen</strong>, welkom terug!';
-$_lang['login_greeting_afternoon'] = '<strong>Goede middag</strong>, welkom terug!';
-$_lang['login_greeting_evening'] = '<strong>Goede avond</strong>, welkom terug!';
-$_lang['login_greeting_night'] = '<strong>Goede nacht</strong>, welkom terug!';
+$_lang['login_greeting_morning'] = '<strong>Goedemorgen</strong>, welkom terug!';
+$_lang['login_greeting_afternoon'] = '<strong>Goedemiddag</strong>, welkom terug!';
+$_lang['login_greeting_evening'] = '<strong>Goedeavond</strong>, welkom terug!';
+$_lang['login_greeting_night'] = '<strong>Goedenacht</strong>, welkom terug!';
 $_lang['login_note'] = 'Meld je aan om toegang te krijgen tot de manager.';
 $_lang['login_help_button_text'] = 'Help';
 $_lang['login_help_title'] = 'Vraag om hulp voor MODX';
