1c203beffee46d2580d630bde13789bea69d3171
Bram Verstraten <bramverstraten@outlook.com>
2021-10-26 07:54:27 +0200

    Use pathinfo to get file extension

--- core/model/modx/processors/system/phpthumb.class.php
+++ core/model/modx/processors/system/phpthumb.class.php
@@ -55,7 +55,7 @@ class modSystemPhpThumbProcessor extends modProcessor {
         $src = $this->source->prepareSrcForThumb($src);
         if (empty($src)) return '';
 
-        if (strtolower(substr(strrchr($src, '.'), 1)) === 'svg') {
+        if (strtolower(pathinfo($src, PATHINFO_EXTENSION)) === 'svg') {
             /* Skip thumbnail generation for svg and output the file directly */
             header('Content-Type: image/svg+xml');
             echo @file_get_contents($src);
