57c9aecee8308dbb26260261328e4c3f37a9c703
Ilya Utkin <ilyautkin@mail.ru>
2021-12-17 05:12:32 +0000

    Set MODX version to 2.8.3 for package installer

--- core/docs/version.inc.php
+++ core/docs/version.inc.php
@@ -3,10 +3,10 @@
 $f_version = 4; // Change this when needed new version for MODX 3S
 
 $v = [];
-$v['version']        = '3'; // Current version.
-$v['major_version']  = '0'; // Current major version.
-$v['minor_version']  = '0'; // Current minor version.
-$v['patch_level']    = 's' . $f_version; // Patch level for display in SiteDash
+$v['version']        = '2'; // Current version.
+$v['major_version']  = '8'; // Current major version.
+$v['minor_version']  = '3'; // Current minor version.
+$v['patch_level']    = 'pl'; // Patch level for display in SiteDash
 $v['code_name']      = 'Revolution'; // Current codename.
 $v['distro']         = '@git@';
 $v['full_version']   = $v['version'] . ($v['major_version'] ? ".{$v['major_version']}" : ".0") . ($v['minor_version'] ? ".{$v['minor_version']}" : ".0") . ($v['patch_level'] ? "-{$v['patch_level']}" : "");
