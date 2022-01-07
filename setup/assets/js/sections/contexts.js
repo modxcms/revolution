78d485d50d5e3147a5f63a80add510b588ceac48
Mikhail <mishantrop@users.noreply.github.com>
2022-01-07 20:39:01 +0300

    Setup context improvements (#15868)
    * Simple bugfix, simple code cleanup, simple adaptive markup
    
    * grunt build
    
    Co-authored-by: Jason Coward <jason@opengeek.com>

--- setup/assets/js/sections/contexts.js
+++ setup/assets/js/sections/contexts.js
@@ -4,67 +4,35 @@ Ext.onReady(function() {
 });
 
 MODx.ctx = function() {
-    
     return {
         init: function() {
-            Ext.get('context_web_path').on('keydown',function() {
-                Ext.get('context_web_path_toggle').set({ checked: true });
-            });
-            Ext.get('context_web_path_toggle').on('click',function() {
-                if (!this.dom.checked) {
-                    Ext.get('context_web_path').dom.value = MODx.context_web_path;
-                }
-            });
-            
-            Ext.get('context_web_url').on('keydown',function() {
-                Ext.get('context_web_url_toggle').set({ checked: true });
-            });
-            Ext.get('context_web_url_toggle').on('click',function() {
-                if (!this.dom.checked) {
-                    Ext.get('context_web_url').dom.value = MODx.context_web_url;
-                }
-            });
-            
-            /* connectors */
-            Ext.get('context_connectors_path').on('keydown',function() {
-                Ext.get('context_connectors_path_toggle').set({ checked: true });
-            });
-            Ext.get('context_connectors_path_toggle').on('click',function() {
-                if (!this.dom.checked) {
-                    Ext.get('context_connectors_path').dom.value = MODx.context_connectors_path;
-                }
-            });
-            
-            Ext.get('context_connectors_url').on('keydown',function() {
-                Ext.get('context_connectors_url_toggle').set({ checked: true });
-            });
-            Ext.get('context_connectors_url_toggle').on('click',function() {
-                if (!this.dom.checked) {
-                    Ext.get('context_connectors_url').dom.value = MODx.context_connectors_url;
-                }
-            });
-            
-            /* mgr */
-            Ext.get('context_mgr_path').on('keydown',function() {
-                Ext.get('context_mgr_path_toggle').set({ checked: true });
-            });
-            Ext.get('context_mgr_path_toggle').on('click',function() {
-                if (!this.dom.checked) {
-                    Ext.get('context_mgr_path').dom.value = MODx.context_mgr_path;
-                }
-            });
-            
-            Ext.get('context_mgr_url').on('keydown',function() {
-                Ext.get('context_mgr_url_toggle').set({ checked: true });
-            });
-            Ext.get('context_mgr_url_toggle').on('click',function() {
-                if (!this.dom.checked) {
-                    Ext.get('context_mgr_url').dom.value = MODx.context_mgr_url;
-                }
-            });
-            
-            
-            
+            var contextProperties = [
+                'context_web_path',
+                'context_web_url',
+                'context_connectors_path',
+                'context_connectors_url',
+                'context_mgr_path',
+                'context_mgr_url',
+            ];
+
+            function handlePropertyChange(propertyName, input, checkbox) {
+                input.addEventListener('input', function() {
+                    checkbox.checked = true;
+                });
+
+                checkbox.addEventListener('change', function(event) {
+                    if (!event.target.checked) {
+                        input.value = MODx[propertyName];
+                    }
+                });
+            }
+
+            for (var i = 0; i < contextProperties.length; i++) {
+                var propertyName = contextProperties[i];
+                var checkboxId = propertyName + '_toggle';
+
+                handlePropertyChange(propertyName, document.getElementById(propertyName), document.getElementById(checkboxId));
+            }
         }
     };
 }();
