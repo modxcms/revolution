bc53490d09c31a0b6a504962519649fb0f6dbb5e
Jason Coward <jason@opengeek.com>
2022-01-25 12:25:43 -0700

    Fix phpcs formatting errors

--- _build/test/properties.sample.inc.php
+++ _build/test/properties.sample.inc.php
@@ -24,27 +24,27 @@ $properties['config_key'] = 'test';
 
 /* driver-specific connection properties */
 /* mysql */
-$properties['mysql_string_dsn_test']= 'mysql:host=127.0.0.1;dbname=revo_test;charset=utf8';
-$properties['mysql_string_dsn_nodb']= 'mysql:host=127.0.0.1;charset=utf8';
-$properties['mysql_string_dsn_error']= 'mysql:host= nonesuchhost;dbname=nonesuchdb';
-$properties['mysql_string_username']= '';
-$properties['mysql_string_password']= '';
-$properties['mysql_array_options']= [
+$properties['mysql_string_dsn_test'] = 'mysql:host=127.0.0.1;dbname=revo_test;charset=utf8';
+$properties['mysql_string_dsn_nodb'] = 'mysql:host=127.0.0.1;charset=utf8';
+$properties['mysql_string_dsn_error'] = 'mysql:host= nonesuchhost;dbname=nonesuchdb';
+$properties['mysql_string_username'] = '';
+$properties['mysql_string_password'] = '';
+$properties['mysql_array_options'] = [
     xPDO::OPT_HYDRATE_FIELDS => true,
     xPDO::OPT_HYDRATE_RELATED_OBJECTS => true,
     xPDO::OPT_HYDRATE_ADHOC_FIELDS => true,
 ];
-$properties['mysql_array_driverOptions']= [\PDO::ATTR_ERRMODE => \PDO::ERRMODE_SILENT];
+$properties['mysql_array_driverOptions'] = [\PDO::ATTR_ERRMODE => \PDO::ERRMODE_SILENT];
 
 /* PHPUnit test config */
-$properties['xpdo_driver']= 'mysql';
-$properties['logTarget']= [
+$properties['xpdo_driver'] = 'mysql';
+$properties['logTarget'] = [
     'target' => 'file',
     'options' => [
         'filename' => "unit_test_{$properties['runtime']}.log",
         'filepath' => dirname(__FILE__) . '/'
     ]
 ];
-$properties['logLevel']= xPDO::LOG_LEVEL_INFO;
+$properties['logLevel'] = xPDO::LOG_LEVEL_INFO;
 $properties['context'] = 'web';
 $properties['debug'] = false;
