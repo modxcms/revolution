4ae980fe4b2bdcda63dc9a1a621dc95dd9099898
Jim Graham <info@sparkmediagroup.com>
2022-01-30 16:22:12 -0500

    Update Plugin Panel

--- core/lexicon/en/plugin.inc.php
+++ core/lexicon/en/plugin.inc.php
@@ -10,15 +10,13 @@ $_lang['event'] = 'Event';
 $_lang['events'] = 'Events';
 $_lang['plugin'] = 'Plugin';
 $_lang['plugin_add'] = 'Add Plugin';
-$_lang['plugin_code'] = 'Plugin code (php)';
+$_lang['plugin_category_desc'] = 'Use to group Plugins within the Elements tree.';
+$_lang['plugin_code'] = 'Plugin Code (PHP)';
 $_lang['plugin_config'] = 'Plugin configuration';
-$_lang['plugin_desc'] = 'Description';
-$_lang['plugin_desc_category'] = 'The Category this Plugin belongs in.';
-$_lang['plugin_desc_description'] = 'A short description of this Plugin.';
-$_lang['plugin_desc_name'] = 'The name of this Plugin.';
+$_lang['plugin_description_desc'] = 'Usage information for this Plugin shown in search results and as a tooltip in the Elements tree.';
 $_lang['plugin_delete_confirm'] = 'Are you sure you want to delete this plugin?';
-$_lang['plugin_disabled'] = 'Inactive plugin';
-$_lang['plugin_disabled_msg'] = 'Plugin is deactivated and will not respond to events.';
+$_lang['plugin_disabled'] = 'Deactivate Plugin';
+$_lang['plugin_disabled_msg'] = 'When deactivated, this Plugin will not respond to events.';
 $_lang['plugin_duplicate_confirm'] = 'Are you sure you want to duplicate this plugin?';
 $_lang['plugin_err_create'] = 'An error occurred while creating the plugin.';
 $_lang['plugin_err_ae'] = 'A plugin already exists with the name "[[+name]]".';
@@ -37,14 +35,24 @@ $_lang['plugin_event_err_save'] = 'An error occurred while saving the plugin eve
 $_lang['plugin_event_msg'] = 'Select the events that you would like this plugin to listen to.';
 $_lang['plugin_event_plugin_remove_confirm'] = 'Are you sure you want to delete this plugin from this event?';
 $_lang['plugin_lock'] = 'Plugin locked for editing';
-$_lang['plugin_lock_msg'] = 'Users must have the edit_locked attribute in order to edit this plugin.';
+$_lang['plugin_lock_desc'] = 'Only users with “edit_locked” permissions can edit this Plugin.';
 $_lang['plugin_locked_message'] = 'This plugin is locked.';
 $_lang['plugin_management_msg'] = 'Here you can choose which plugin you wish to edit.';
-$_lang['plugin_msg'] = 'Here you can create/edit plugins. Plugins are \'raw\' PHP codes that are invoked whenever the selected System Events are triggered.';
-$_lang['plugin_name'] = 'Plugin name';
+$_lang['plugin_name_desc'] = 'The name of this Plugin.';
 $_lang['plugin_new'] = 'Create Plugin';
 $_lang['plugin_priority'] = 'Edit Plugin Execution Order by Event';
 $_lang['plugin_properties'] = 'Plugin Properties';
+$_lang['plugin_tab_general_desc'] = 'Here you can enter the basic attributes for this <em>Plugin</em> as well as its content. The content must be PHP, either placed in the <a href="#x-form-el-modx-plugin-plugincode">Plugin Code</a> field below or in a static external file. The PHP code entered runs in response to one or more MODX System Events that you specify.';
 $_lang['plugin_title'] = 'Create/edit plugin';
 $_lang['plugin_untitled'] = 'Untitled plugin';
 $_lang['plugins'] = 'Plugins';
+
+// Temporarily match old keys to new ones to ensure compatibility
+// --fields
+$_lang['plugin_desc_category'] = $_lang['plugin_category_desc'];
+$_lang['plugin_desc_description'] = $_lang['plugin_description_desc'];
+$_lang['plugin_desc_name'] = $_lang['plugin_name_desc'];
+$_lang['plugin_lock_msg'] = $_lang['plugin_lock_desc'];
+
+// --tabs
+$_lang['plugin_msg'] = $_lang['plugin_tab_general_desc'];
