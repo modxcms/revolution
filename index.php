d2aa4c9bc543265e6cb1e65e123a4b7e8c7a5909
Jason Coward <jason@opengeek.com>
2022-02-08 09:03:34 -0700

    Fix formatting issues in index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
  * files found in the top-level directory of this distribution.
  */
 
-$tstart= microtime(true);
+$tstart = microtime(true);
 
 /* define this as true in another entry file, then include this file to simply access the API
  * without executing the MODX request handler */
@@ -18,13 +18,17 @@ if (!defined('MODX_API_MODE')) {
 
 /* include custom core config and define core path */
 @include(dirname(__FILE__) . '/config.core.php');
-if (!defined('MODX_CORE_PATH')) define('MODX_CORE_PATH', dirname(__FILE__) . '/core/');
-if (!defined('MODX_CONFIG_KEY')) define('MODX_CONFIG_KEY', 'config');
+if (!defined('MODX_CORE_PATH')) {
+    define('MODX_CORE_PATH', dirname(__FILE__) . '/core/');
+}
+if (!defined('MODX_CONFIG_KEY')) {
+    define('MODX_CONFIG_KEY', 'config');
+}
 
 /* include the autoloader */
-if (!@require_once (MODX_CORE_PATH . "vendor/autoload.php")) {
+if (!@require_once MODX_CORE_PATH . "vendor/autoload.php") {
     $errorMessage = 'Site temporarily unavailable';
-    @include(MODX_CORE_PATH . 'error/unavailable.include.php');
+    @include MODX_CORE_PATH . 'error/unavailable.include.php';
     header($_SERVER['SERVER_PROTOCOL'] . ' 503 Service Unavailable');
     echo "<html><title>Error 503: Site temporarily unavailable</title><body><h1>Error 503</h1><p>{$errorMessage}</p></body></html>";
     exit();
@@ -34,18 +38,18 @@ if (!@require_once (MODX_CORE_PATH . "vendor/autoload.php")) {
 ob_start();
 
 /* Create an instance of the modX class */
-$modx= new \MODX\Revolution\modX();
+$modx = new \MODX\Revolution\modX();
 if (!is_object($modx) || !($modx instanceof \MODX\Revolution\modX)) {
     ob_get_level() && @ob_end_flush();
     $errorMessage = '<a href="setup/">MODX not installed. Install now?</a>';
-    @include(MODX_CORE_PATH . 'error/unavailable.include.php');
+    @include MODX_CORE_PATH . 'error/unavailable.include.php';
     header($_SERVER['SERVER_PROTOCOL'] . ' 503 Service Unavailable');
     echo "<html><title>Error 503: Site temporarily unavailable</title><body><h1>Error 503</h1><p>{$errorMessage}</p></body></html>";
     exit();
 }
 
 /* Set the actual start time */
-$modx->startTime= $tstart;
+$modx->startTime = $tstart;
 
 /* Initialize a context */
 $contextKey = 'web';
