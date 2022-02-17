779267dcf48c2f5ed0c414ce0b1358f5ebec5809
Ruslan-Aleev <aleev.insmile@gmail.com>
2022-02-17 16:38:07 +0400

    Delete unused lexicons

--- core/lexicon/en/source.inc.php
+++ core/lexicon/en/source.inc.php
@@ -7,14 +7,9 @@
  * @subpackage lexicon
  */
 $_lang['access'] = 'Access Permissions';
-$_lang['base_path'] = 'Base Path';
-$_lang['base_path_relative'] = 'Base Path Relative?';
-$_lang['base_url'] = 'Base URL';
-$_lang['base_url_relative'] = 'Base URL Relative?';
 $_lang['minimum_role'] = 'Minimum Role';
 $_lang['path_options'] = 'Path Options';
 $_lang['policy'] = 'Policy';
-$_lang['source'] = 'Media Source';
 $_lang['source_access_add'] = 'Add User Group';
 $_lang['source_access_remove'] = 'Delete Access';
 $_lang['source_access_remove_confirm'] = 'Are you sure you want to delete Access to this Source for this User Group?';
@@ -30,7 +25,6 @@ $_lang['source_name_desc'] = 'The name of the Media Source.';
 $_lang['source_properties.intro_msg'] = 'Manage the properties for this Source below.';
 $_lang['source_remove_confirm'] = 'Are you sure you want to delete this Media Source? This might break any TVs you have assigned to this source.';
 $_lang['source_remove_multiple_confirm'] = 'Are you sure you want to delete these Media Sources? This might break any TVs you have assigned to these sources.';
-$_lang['source_type'] = 'Source Type';
 $_lang['source_type_desc'] = 'The type, or driver, of the Media Source. The Source will use this driver to connect to when gathering its data. For example: File System will grab files from the file system. S3 will get files from an S3 bucket.';
 $_lang['source_type.file'] = 'File System';
 $_lang['source_type.file_desc'] = 'A filesystem-based source that navigates your server\'s files.';
@@ -46,31 +40,19 @@ $_lang['sources.intro_msg'] = 'Manage all your Media Sources here.';
 $_lang['user_group'] = 'User Group';
 
 /* file source type */
-$_lang['allowedFileTypes'] = 'allowedFileTypes';
 $_lang['prop_file.allowedFileTypes_desc'] = 'If set, will restrict the files shown to only the specified extensions. Please specify in a comma-separated list, without the dots preceding the extensions.';
-$_lang['basePath'] = 'basePath';
 $_lang['prop_file.basePath_desc'] = 'The file path to point the Source to, for example: assets/images/<br>The path may depend on the "basePathRelative" parameter';
-$_lang['basePathRelative'] = 'basePathRelative';
 $_lang['prop_file.basePathRelative_desc'] = 'If the Base Path setting above is not relative to the MODX install path, set this to No.';
-$_lang['baseUrl'] = 'baseUrl';
 $_lang['prop_file.baseUrl_desc'] = 'The URL that this source can be accessed from, for example: assets/images/<br>The path may depend on the "baseUrlRelative" parameter';
-$_lang['baseUrlPrependCheckSlash'] = 'baseUrlPrependCheckSlash';
-$_lang['prop_file.baseUrlPrependCheckSlash_desc'] = 'If true, MODX only will prepend the baseUrl if no forward slash (/) is found at the beginning of the URL when rendering the TV. Useful for setting a TV value outside the baseUrl.';
-$_lang['baseUrlRelative'] = 'baseUrlRelative';
 $_lang['prop_file.baseUrlRelative_desc'] = 'If the Base URL setting above is not relative to the MODX install URL, set this to No.';
-$_lang['imageExtensions'] = 'imageExtensions';
 $_lang['prop_file.imageExtensions_desc'] = 'A comma-separated list of file extensions to use as images. MODX will attempt to make thumbnails of files with these extensions.';
-$_lang['skipFiles'] = 'skipFiles';
 $_lang['prop_file.skipFiles_desc'] = 'A comma-separated list. MODX will skip over and hide files and folders that match any of these.';
-$_lang['thumbnailQuality'] = 'thumbnailQuality';
 $_lang['prop_file.thumbnailQuality_desc'] = 'The quality of the rendered thumbnails, in a scale from 0-100.';
-$_lang['thumbnailType'] = 'thumbnailType';
 $_lang['prop_file.thumbnailType_desc'] = 'The image type to render thumbnails as.';
 $_lang['prop_file.visibility_desc'] = 'Default visibility for new files and folders.';
 $_lang['no_move_folder'] = 'The Media Source driver does not support moving of folders at this time.';
 
 /* s3 source type */
-$_lang['bucket'] = 'Bucket';
 $_lang['prop_s3.bucket_desc'] = 'The S3 Bucket to load your data from.';
 $_lang['prop_s3.key_desc'] = 'The Amazon key for authentication to the bucket.';
 $_lang['prop_s3.imageExtensions_desc'] = 'A comma-separated list of file extensions to use as images. MODX will attempt to make thumbnails of files with these extensions.';
@@ -82,7 +64,6 @@ $_lang['prop_s3.url_desc'] = 'The URL of the Amazon S3 instance.';
 $_lang['prop_s3.endpoint_desc'] = 'Alternative S3-compatible endpoint URL, e.g., "https://s3.<region>.example.com". Review your S3-compatible provider’s documentation for the endpoint location. Leave empty for Amazon S3';
 $_lang['prop_s3.region_desc'] = 'Region of the bucket. Example: us-west-1';
 $_lang['prop_s3.prefix_desc'] = 'Optional path/folder prefix';
-$_lang['s3_no_move_folder'] = 'The S3 driver does not support moving of folders at this time.';
 
 /* ftp source type */
 $_lang['prop_ftp.host_desc'] = 'Server hostname or IP address';
@@ -94,8 +75,3 @@ $_lang['prop_ftp.root_desc'] = 'The root folder, it will be opened after connect
 $_lang['prop_ftp.passive_desc'] = 'Enable or disable passive ftp mode';
 $_lang['prop_ftp.ssl_desc'] = 'Enable or disable ssl connection';
 $_lang['prop_ftp.timeout_desc'] = 'Timeout for connection in seconds.';
-
-/* file type */
-$_lang['PNG'] = 'PNG';
-$_lang['JPG'] = 'JPG';
-$_lang['GIF'] = 'GIF';
