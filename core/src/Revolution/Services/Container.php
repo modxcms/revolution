ded54a01f7372555d8dbc37845fef25001b3cf7b
Jason Coward <jason@opengeek.com>
2022-03-08 13:08:36 -0700

    Update xPDO to 3.1.0 and fix Container method signatures
    This resolves issues with PHP 8.1 compatibility

--- core/src/Revolution/Services/Container.php
+++ core/src/Revolution/Services/Container.php
@@ -8,7 +8,14 @@ use Psr\Container\ContainerInterface;
 
 class Container extends \Pimple\Container implements ContainerInterface
 {
-    public function add($id, $value)
+    /**
+     * Add an entry to the container.
+     *
+     * @param string $id
+     * @param mixed $value
+     * @return void
+     */
+    public function add(string $id, $value)
     {
         $this->offsetSet($id, $value);
     }
@@ -16,7 +23,7 @@ class Container extends \Pimple\Container implements ContainerInterface
     /**
      * @inheritDoc
      */
-    public function get($id)
+    public function get(string $id)
     {
         if ($this->has($id)) {
             try {
@@ -31,7 +38,7 @@ class Container extends \Pimple\Container implements ContainerInterface
     /**
      * @inheritDoc
      */
-    public function has($id)
+    public function has(string $id): bool
     {
         return $this->offsetExists($id);
     }
