b8ef39093ab58cfe08541b7f1457fe299c2a13ab
Joshua Lückers <joshualuckers@me.com>
2022-04-26 09:23:32 +0200

    Add passing modInputFilter Unit Test

--- _build/test/Tests/Model/Filters/modInputFilterTest.php
+++ _build/test/Tests/Model/Filters/modInputFilterTest.php
@@ -12,6 +12,8 @@
 namespace MODX\Revolution\Tests\Model\Filters;
 
 
+use MODX\Revolution\Filters\modInputFilter;
+use MODX\Revolution\modFieldTag;
 use MODX\Revolution\MODxTestCase;
 
 /**
@@ -24,7 +26,46 @@ use MODX\Revolution\MODxTestCase;
  * @group modInputFilter
  */
 class modInputFilterTest extends MODxTestCase {
-    public function testExample() {
-        $this->assertTrue(true);
+
+    /**
+     * @dataProvider providerFilter
+     * @param $elementName
+     * @param $expectedElementName
+     * @param $expectedComands
+     * @param $expectedModifiers
+     * @return void
+     */
+    public function testFilter($elementName, $expectedElementName, $expectedComands, $expectedModifiers) {
+        $element = new modFieldTag($this->modx);
+        $element->set('name', $elementName);
+
+        $modInputFilter = new modInputFilter($this->modx);
+        $modInputFilter->filter($element);
+
+        $this->assertEquals($expectedElementName, $element->get('name'));
+        $this->assertEquals($expectedComands, $modInputFilter->getCommands());
+        $this->assertEquals($expectedModifiers, $modInputFilter->getModifiers());
+    }
+
+    /**
+     * dataProvider for testFilter
+     */
+    public function providerFilter() {
+        return [
+            [
+                'pagetitle:eq=`0`:then=`foo`:else=`bar`',
+                'pagetitle',
+                [
+                    'eq',
+                    'then',
+                    'else',
+                ],
+                [
+                    '0',
+                    'foo',
+                    'bar',
+                ]
+            ]
+        ];
     }
 }
