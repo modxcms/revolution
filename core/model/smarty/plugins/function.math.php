59d11a1247224fbccfafef5088fd469da09ed389
Bochkarev Ivan <ivanx86@gmail.com>
2022-09-07 10:10:21 +0600

    Update Smarty to 4.2.0

--- core/model/smarty/plugins/function.math.php
+++ core/model/smarty/plugins/function.math.php
@@ -12,7 +12,7 @@
  * Name:     math
  * Purpose:  handle math computations in template
  *
- * @link   http://www.smarty.net/manual/en/language.function.math.php {math}
+ * @link   https://www.smarty.net/manual/en/language.function.math.php {math}
  *           (Smarty online manual)
  * @author Monte Ohrt <monte at ohrt dot com>
  *
@@ -69,7 +69,7 @@ function smarty_function_math($params, $template)
     // Adapted from https://www.php.net/manual/en/function.eval.php#107377
     $number = '(?:\d+(?:[,.]\d+)?|pi|π)'; // What is a number
     $functionsOrVars = '((?:0x[a-fA-F0-9]+)|([a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*))';
-    $operators = '[+\/*\^%-]'; // Allowed math operators
+    $operators = '[,+\/*\^%-]'; // Allowed math operators
     $regexp = '/^(('.$number.'|'.$functionsOrVars.'|('.$functionsOrVars.'\s*\((?1)+\)|\((?1)+\)))(?:'.$operators.'(?1))?)+$/';
 
     if (!preg_match($regexp, $equation)) {
