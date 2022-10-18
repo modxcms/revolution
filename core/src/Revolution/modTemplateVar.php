476e1b2cd2b62923c7b0ec693ad7a38d97a5201f
halftrainedharry <85392304+halftrainedharry@users.noreply.github.com>
2022-10-18 10:19:18 +0200

    Update modTemplateVar.php

--- core/src/Revolution/modTemplateVar.php
+++ core/src/Revolution/modTemplateVar.php
@@ -1003,7 +1003,7 @@ class modTemplateVar extends modElement
             $regexp2 = '/(\S+)\s+(.+)/is'; /* Split binding on second whitespace to get properties */
 
             $properties = [];
-            if (strtoupper($match[1]) != 'SELECT' && preg_match($regexp2, $match[2] , $match2)) {
+            if (strtoupper($match[1]) != 'SELECT' && preg_match($regexp2, $match[2], $match2)) {
                 if (isset($match2[2])) {
                     $props = json_decode($match2[2],true);
                     $valid = json_last_error() === JSON_ERROR_NONE;
