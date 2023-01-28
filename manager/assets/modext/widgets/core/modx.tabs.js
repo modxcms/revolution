a476ba49cb48d86db1f3647e75f847103bc785fe
Jim Graham <info@sparkmediagroup.com>
2023-01-27 23:54:50 -0500

    Progress round 2, manual conflict resolution on base grid and tabs classes

--- manager/assets/modext/widgets/core/modx.tabs.js
+++ manager/assets/modext/widgets/core/modx.tabs.js
@@ -1,70 +1,96 @@
 MODx.Tabs = function(config) {
     config = config || {};
-    Ext.applyIf(config,{
-        enableTabScroll: true
-        ,layoutOnTabChange: true
-        ,plain: true
-        ,deferredRender: true
-        ,hideMode: 'offsets'
-        ,defaults: {
-            autoHeight: true
-            ,hideMode: 'offsets'
-            ,border: true
-            ,autoWidth: true
-            ,bodyCssClass: 'tab-panel-wrapper'
-        }
-        ,activeTab: 0
-        ,border: false
-        ,autoScroll: true
-        ,autoHeight: true
-        ,cls: 'modx-tabs'
+    Ext.applyIf(config, {
+        enableTabScroll: true,
+        layoutOnTabChange: true,
+        plain: true,
+        deferredRender: true,
+        hideMode: 'offsets',
+        defaults: {
+            autoHeight: true,
+            hideMode: 'offsets',
+            border: true,
+            autoWidth: true,
+            bodyCssClass: 'tab-panel-wrapper'
+        },
+        activeTab: 0,
+        border: false,
+        autoScroll: true,
+        autoHeight: true,
+        cls: 'modx-tabs'
     });
-    MODx.Tabs.superclass.constructor.call(this,config);
+    MODx.Tabs.superclass.constructor.call(this, config);
     this.config = config;
     this.on({
         afterrender: function(tabPanel) {
-            if (MODx.request && MODx.request.hasOwnProperty('tab')){
-                const tabId = parseInt(MODx.request.tab);
-                this.setActiveTab(tabId);
+            if (this.id !== 'modx-leftbar-tabpanel') {
+                console.log('tabs afterrender, activeTab: ', this.activeTab?.id);
+            }
+            if (MODx.request && Object.prototype.hasOwnProperty.call(MODx.request, 'tab')) {
+                const tabId = parseInt(MODx.request.tab, 10);
+                // console.log('tabs, afterrender, this', this);
+                // Ensure tab panels other than the main one are unaffected
+                if (this.id !== 'modx-leftbar-tabpanel') {
+                    console.log(`trying to set active tab to: ${tabId} for this:`, this);
+                    this.setActiveTab(tabId);
+                }
             }
             /* Placing listener here because we only want to listen after the initial panel has loaded */
             tabPanel.on({
-                beforetabchange: function(tabPanel, newTab, currentTab) {
+                beforetabchange: function(tabPanelCmp, newTab, currentTab) {
                     /*
                         Only proceed with the clearing process if the tab has changed.
                         This is needed to prevent clearing when a URL has been typed in.
 
                         NOTE: The currentTab is the previous one being navigated away from
                     */
+                    // console.log('request: ', MODx.request);
+                    console.log('tabs, beforetabchange, currentTab (prev): ', currentTab.id);
+                    console.log('tabs, beforetabchange, newTab: ', newTab?.id);
+
                     if (newTab && currentTab && newTab.id !== currentTab.id) {
                         const resetVerticalTabPanelFilters = (currentTab.items?.items[0]?.xtype === 'modx-vtabs') || currentTab.ownerCt?.xtype === 'modx-vtabs',
                               changedBetweenVtabs = newTab.ownerCt?.xtype === 'modx-vtabs' && currentTab.ownerCt?.xtype === 'modx-vtabs'
                         ;
+                        // console.log('tab changed...');
                         let itemsSource,
                             gridObj = null
                         ;
                         if (resetVerticalTabPanelFilters) {
-                            itemsSource = changedBetweenVtabs ? currentTab.items : currentTab.items.items[0].activeTab.items ;
+                            itemsSource = changedBetweenVtabs
+                                ? currentTab.items
+                                : currentTab.items.items[0].activeTab.items;
                         } else {
                             itemsSource = currentTab.items;
+                            // console.log('current tab:', currentTab);
+                            // console.log('current tab items: ', itemsSource);
                         }
                         if (itemsSource.length > 0) {
                             gridObj = itemsSource.find(obj => {
-                                return Object.entries(obj).find(([key, value]) => key == 'xtype' && value.includes('modx-grid'));
+                                // console.log('obj entries: ', Object.entries(obj));
+                                return Object.entries(obj).find(([key, value]) => key === 'xtype' && value.includes('modx-grid'));
                             });
+                            // test = itemsSource.filter('id', 'modx-grid');
+                            // console.log('gridObj: ', gridObj);
+                            // console.log('test filtered: ', test);
+                            if (!gridObj) {
+                                // keep?
+                            }
                         }
                         if (gridObj) {
                             const toolbar = gridObj.getTopToolbar(),
                                   filterIds = []
                             ;
+                            // console.log('tabs, toolbar:', toolbar);
                             if (toolbar && toolbar.items.items.length > 0) {
                                 toolbar.items.items.forEach(cmp => {
-                                    if (cmp.xtype && (cmp.xtype.includes('combo') || cmp.xtype == 'textfield') && cmp.itemId) {
+                                    if (cmp.xtype && (cmp.xtype.includes('combo') || cmp.xtype === 'textfield') && cmp.itemId) {
                                         filterIds.push(cmp.itemId);
                                     }
                                 });
                             }
                             if (filterIds.length > 0) {
+                                // console.log('clearing filters: ', filterIds);
                                 gridObj.clearGridFilters(filterIds);
                             }
                         }
@@ -74,31 +100,31 @@ MODx.Tabs = function(config) {
         }
     });
 };
-Ext.extend(MODx.Tabs,Ext.TabPanel);
-Ext.reg('modx-tabs',MODx.Tabs);
+Ext.extend(MODx.Tabs, Ext.TabPanel);
+Ext.reg('modx-tabs', MODx.Tabs);
 
 MODx.VerticalTabs = function(config) {
     config = config || {};
-    Ext.applyIf(config,{
-        cls: 'vertical-tabs-panel'
-        ,headerCfg: { tag: 'div', cls: 'x-tab-panel-header vertical-tabs-header' }
-        ,bwrapCfg: { tag: 'div', cls: 'x-tab-panel-bwrap vertical-tabs-bwrap' }
-        ,defaults: {
-            bodyCssClass: 'vertical-tabs-body'
-            ,autoScroll: true
-            ,autoHeight: true
-            ,autoWidth: true
-            ,layout: 'form'
+    Ext.applyIf(config, {
+        cls: 'vertical-tabs-panel',
+        headerCfg: { tag: 'div', cls: 'x-tab-panel-header vertical-tabs-header' },
+        bwrapCfg: { tag: 'div', cls: 'x-tab-panel-bwrap vertical-tabs-bwrap' },
+        defaults: {
+            bodyCssClass: 'vertical-tabs-body',
+            autoScroll: true,
+            autoHeight: true,
+            autoWidth: true,
+            layout: 'form'
         }
     });
-    MODx.VerticalTabs.superclass.constructor.call(this,config);
+    MODx.VerticalTabs.superclass.constructor.call(this, config);
     this.config = config;
     this.on('afterrender', function() {
-        if (MODx.request && MODx.request.hasOwnProperty('vtab')){
-            const tabId = parseInt(MODx.request.vtab);
+        if (MODx.request && Object.prototype.hasOwnProperty.call(MODx.request, 'vtab')) {
+            const tabId = parseInt(MODx.request.vtab, 10);
             this.setActiveTab(tabId);
         }
     });
 };
 Ext.extend(MODx.VerticalTabs, MODx.Tabs);
-Ext.reg('modx-vtabs',MODx.VerticalTabs);
+Ext.reg('modx-vtabs', MODx.VerticalTabs);
