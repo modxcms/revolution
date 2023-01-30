0dcb898a29106bfada1493dc0edc969a8a07291a
Jim Graham <info@sparkmediagroup.com>
2023-01-30 16:37:24 -0500

    Add resource alias in template picker as needed (#16361)
    * Add resource alias in template picker as needed
    
    When friendly URLs are enabled, a resource can not be saved without an alias. The current picker form does not include the alias field.
    
    * grunt build
    
    ---------
    
    Co-authored-by: Jason Coward <jason@opengeek.com>

--- manager/assets/modext/widgets/resource/modx.window.resource.js
+++ manager/assets/modext/widgets/resource/modx.window.resource.js
@@ -1,7 +1,69 @@
-MODx.window.CreateResource = function(config) {
-    config = config || {};
-    this.ident = config.ident || 'tplpick'+Ext.id();
-    const id = this.ident;
+MODx.window.CreateResource = function(config = {}) {
+    this.ident = config.ident || `tplpick${Ext.id()}`;
+    const id = this.ident,
+          requireAlias = parseInt(MODx.config.friendly_urls, 10) && !parseInt(MODx.config.automatic_alias, 10),
+          aliasLength = parseInt(MODx.config.friendly_alias_max_length, 10) || 0,
+          resourceDetail = [
+              {
+                  columnWidth: requireAlias ? 0.33 : 0.5,
+                  items: [
+                      {
+                          xtype: 'modx-combo-class-derivatives',
+                          fieldLabel: _('resource_type'),
+                          description: MODx.expandHelp ? '' : _('resource_type_help'),
+                          name: 'class_key',
+                          hiddenName: 'class_key',
+                          anchor: '100%',
+                          allowBlank: false,
+                          value: config.record.class_key || 'MODX\\Revolution\\modDocument',
+                      }
+                  ]
+              }, {
+                  columnWidth: requireAlias ? 0.33 : 0.5,
+                  items: [
+                      {
+                          xtype: 'modx-field-parent-change',
+                          fieldLabel: _('resource_parent'),
+                          description: `<b>[[*parent]]</b><br>${_('resource_parent_help')}`,
+                          name: 'parent-cmb',
+                          id: `modx-${id}-parent-change`,
+                          value: config.record.parent || 0,
+                          anchor: '100%',
+                          parentcmp: 'modx-template-picker-parent-id',
+                          contextcmp: 'modx-template-picker-parent-context',
+                          currentid: id
+                      }
+                  ]
+              }
+          ]
+    ;
+    if (requireAlias) {
+        resourceDetail.push({
+            columnWidth: 0.34,
+            defaults: {
+                anchor: '100%',
+                enableKeyEvents: true,
+                validationEvent: 'change',
+                validateOnBlur: false,
+                msgTarget: 'under'
+            },
+            items: [
+                {
+                    xtype: 'textfield',
+                    fieldLabel: _('resource_alias'),
+                    name: 'alias',
+                    allowBlank: false,
+                    maxLength: (aliasLength > 191 || aliasLength === 0) ? 191 : aliasLength,
+                    value: config.record.alias || '',
+                    listeners: {
+                        afterrender: function() {
+                            this.clearInvalid();
+                        }
+                    }
+                }
+            ]
+        });
+    }
     Ext.applyIf(config, {
         autoHeight: true,
         title: _('document_new'),
@@ -15,50 +77,33 @@ MODx.window.CreateResource = function(config) {
             fieldLabel: _('resource_pagetitle'),
             description: MODx.expandHelp ? '' : _('resource_pagetitle_help'),
             name: 'pagetitle',
+            maxLength: 191,
             anchor: '100%',
-            allowBlank: false
-        },{
+            allowBlank: false,
+            validationEvent: 'change',
+            validateOnBlur: false,
+            value: config.record.pagetitle || '',
+            listeners: {
+                afterrender: function() {
+                    this.clearInvalid();
+                }
+            }
+        }, {
             xtype: 'hidden',
             name: 'parent',
-            id: 'modx-template-picker-parent-id',
-        },{
+            id: 'modx-template-picker-parent-id'
+        }, {
             xtype: 'hidden',
             name: 'context_key',
-            id: 'modx-template-picker-parent-context',
-        },{
+            id: 'modx-template-picker-parent-context'
+        }, {
             layout: 'column',
             defaults: {
                 layout: 'form',
                 labelSeparator: ''
             },
-            items: [{
-                columnWidth: .5,
-                items: [{
-                    xtype: 'modx-combo-class-derivatives',
-                    fieldLabel: _('resource_type'),
-                    description: MODx.expandHelp ? '' : _('resource_type_help'),
-                    name: 'class_key',
-                    hiddenName: 'class_key',
-                    anchor: '100%',
-                    allowBlank: false,
-                    value: config.record.class_key || 'MODX\\Revolution\\modDocument'
-                }]
-            },{
-                columnWidth: .5,
-                items: [{
-                    xtype: 'modx-field-parent-change'
-                    ,fieldLabel: _('resource_parent')
-                    ,description: '<b>[[*parent]]</b><br />'+_('resource_parent_help')
-                    ,name: 'parent-cmb'
-                    ,id: 'modx-'+id+'-parent-change'
-                    ,value: config.record.parent || 0
-                    ,anchor: '100%'
-                    ,parentcmp: 'modx-template-picker-parent-id'
-                    ,contextcmp: 'modx-template-picker-parent-context'
-                    ,currentid: id
-                }]
-            }]
-        },{
+            items: resourceDetail
+        }, {
             xtype: 'modx-panel-template-picker',
             fieldLabel: _('resource_template'),
             description: MODx.expandHelp ? '' : _('resource_template_help'),
@@ -66,7 +111,6 @@ MODx.window.CreateResource = function(config) {
             value: config.record.template || MODx.config.default_template
         }]
     });
-
     MODx.window.CreateResource.superclass.constructor.call(this, config);
 };
 
@@ -87,21 +131,21 @@ MODx.panel.TemplatePicker = function(config) {
                 labelSeparator: ''
             },
             items: [{
-                columnWidth: .4,
+                columnWidth: 0.4,
                 items: [{
                     xtype: 'modx-combo-template-picker',
                     id: 'modx-resource-template-picker',
                     name: 'template',
                     value: config.record || MODx.config.default_template,
                     listeners: {
-                        'select': {
+                        select: {
                             fn: this.setPreview,
                             scope: this
                         }
                     }
                 }]
             }, {
-                columnWidth: .6,
+                columnWidth: 0.6,
                 items: [{
                     xtype: 'modx-panel-template-preview',
                     id: 'modx-resource-template-preview'
@@ -163,9 +207,9 @@ MODx.combo.TemplatePicker = function(config) {
                     scope: this
                 },
                 'loadexception': {fn: function(o,trans,resp) {
-                        const status = _('code') + ': ' + resp.status + ' ' + resp.statusText + '<br/>';
-                        MODx.msg.alert(_('error'), status + resp.responseText);
-                    }}
+                    var status = _('code') + ': ' + resp.status + ' ' + resp.statusText + '<br/>';
+                    MODx.msg.alert(_('error'), status + resp.responseText);
+                }}
             }
         })
     });
@@ -175,11 +219,10 @@ MODx.combo.TemplatePicker = function(config) {
 
 Ext.extend(MODx.combo.TemplatePicker, Ext.Panel, {
     loadItems: function(store, data) {
-        console.log('loadItems');
-        const value = this.value;
+        var value = this.value;
 
-        const items = [];
-        let category = '';
+        var items = [];
+        var category = '';
 
         Ext.each(data, function(record) {
             if (category !== record.data.category_name) {
@@ -215,8 +258,7 @@ Ext.extend(MODx.combo.TemplatePicker, Ext.Panel, {
             listeners: {
                 'render': {
                     fn: function(tf) {
-                        console.log('render');
-                        const value = tf.getValue();
+                        var value = tf.getValue();
 
                         if (value.record) {
                             this.fireEvent('select', value.record);
@@ -226,8 +268,7 @@ Ext.extend(MODx.combo.TemplatePicker, Ext.Panel, {
                 },
                 'change': {
                     fn: function(tf) {
-                        console.log('change');
-                        const value = tf.getValue();
+                        var value = tf.getValue();
 
                         if (value.record) {
                             this.fireEvent('select', value.record);
@@ -241,14 +282,12 @@ Ext.extend(MODx.combo.TemplatePicker, Ext.Panel, {
         this.doLayout();
     },
     filterItems: function(tf) {
-        console.log('filterItems')
-        const panel = Ext.getCmp('modx-template-picker-templates')
-        if (panel) {
+        if (undefined !== (panel = Ext.getCmp('modx-template-picker-templates'))) {
             panel.items.each(function(object) {
                 if (!Ext.isEmpty(tf.getValue()) && object.record) {
-                    const regex = new RegExp(tf.getValue(), 'i');
+                    var regex = new RegExp(tf.getValue(), 'i');
 
-                    if (object.record.data.templatename.search(regex) === -1) {
+                    if (-1 === object.record.data.templatename.search(regex)) {
                         object.hide();
                     } else {
                         object.show();
@@ -275,19 +314,15 @@ MODx.panel.TemplatePreview = function(config) {
 
 Ext.extend(MODx.panel.TemplatePreview, Ext.Panel, {
     setPreview: function(record) {
-        console.log('setPreview');
-        console.log(record);
         this.removeAll();
-
-        let html = '';
-        if (record.data.preview == '' || undefined === record.data.preview) {
+        if ('' == record.data.preview || undefined === record.data.preview) {
             this.addClass('x-form-template-preview-empty');
 
-            html = '';
+            var html = '';
         } else {
             this.removeClass('x-form-template-preview-empty');
 
-            html = '<img src="' + record.data.preview + '" alt="' + record.data.templatename + '" />';
+            var html = '<img src="' + record.data.preview + '" alt="' + record.data.templatename + '" />';
         }
 
         this.add({
@@ -297,7 +332,7 @@ Ext.extend(MODx.panel.TemplatePreview, Ext.Panel, {
                 cls: 'x-form-template-preview-image',
                 html: html
             },
-            hidden: record.data.image == ''
+            hidden: '' == record.data.image ? true : false
         }, {
             xtype: 'box',
             autoEl: {
@@ -305,7 +340,7 @@ Ext.extend(MODx.panel.TemplatePreview, Ext.Panel, {
                 cls: 'x-form-template-preview-desc',
                 html: '<p>' + record.data.description + '</p>'
             },
-            hidden: record.data.description == ''
+            hidden: '' == record.data.description ? true : false
         });
 
         this.doLayout();
