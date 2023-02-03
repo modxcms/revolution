f7008518d6e4ea4a49620afa6e62dd10c046e857
Jason Coward <jason@opengeek.com>
2023-02-03 10:59:18 -0700

    Upgrade legacy modSessionHandler class references in settings

---
+++ setup/includes/upgrades/mysql/3.0.4-pl.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Specific upgrades for Revolution 3.0.4-pl
+ *
+ * @var modX $modx
+ * @package setup
+ * @subpackage upgrades
+ */
+
+/* run upgrades common to all db platforms */
+include dirname(__DIR__) . '/common/3.0.4-update-session-handler-class.php';
