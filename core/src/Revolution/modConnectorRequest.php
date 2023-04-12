021461b762779048607cca9cdd23f498c0d8d45c
Thomas Jakobi <thomas.jakobi@partout.info>
2023-04-12 13:54:40 +0200

    Fix undefined global variable $_SESSION in a CLI connector

--- core/src/Revolution/modConnectorRequest.php
+++ core/src/Revolution/modConnectorRequest.php
@@ -38,7 +38,7 @@ class modConnectorRequest extends modManagerRequest
         if ($this->modx && is_object($this->modx->context) && $this->modx->context instanceof modContext) {
             $ctx = $this->modx->context->get('key');
             if (!empty($ctx) && $ctx == 'mgr') {
-                $ml = $this->modx->getOption('manager_language', $_SESSION,
+                $ml = $this->modx->getOption('manager_language', $_SESSION ?? [],
                     $this->modx->getOption('cultureKey', null, 'en'));
                 if (!empty($ml)) {
                     $this->modx->setOption('cultureKey', $ml);
