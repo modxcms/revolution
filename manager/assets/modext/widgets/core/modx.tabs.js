09676ea96fc0a7b0f8782a7c9584becc50f25a59
Jason Coward <jason@opengeek.com>
2023-08-29 14:14:25 -0600

    Settings grid adjustments for Extras support (#16414)
    Squashed commit of the following:
    
    commit 9121214b1d1f1c093de9779fafb2004497a38a28
    Author: Jason Coward <jason@opengeek.com>
    Date:   Tue Aug 29 14:13:36 2023 -0600
    
        grunt build
    
    commit 3a32455b58247735ab67adf362eeb2bebfbc8122
    Author: Jim Graham <info@sparkmediagroup.com>
    Date:   Tue Apr 4 16:25:45 2023 -0400
    
        Settings grid adjustments for Extras support

--- manager/assets/modext/widgets/core/modx.tabs.js
+++ manager/assets/modext/widgets/core/modx.tabs.js
@@ -23,13 +23,17 @@ MODx.Tabs = function(config) {
     this.config = config;
     this.on({
         afterrender: function(tabPanel) {
-            if (MODx.request && MODx.request.hasOwnProperty('tab')){
-                const tabId = parseInt(MODx.request.tab);
-                this.setActiveTab(tabId);
+            if (MODx.request && Object.prototype.hasOwnProperty.call(MODx.request, 'tab')) {
+                const tabId = parseInt(MODx.request.tab, 10);
+                // Ensure tab panels other than the main one are unaffected
+                if (this.id !== 'modx-leftbar-tabpanel') {
+                    this.setActiveTab(tabId);
+                }
             }
+
             /* Placing listener here because we only want to listen after the initial panel has loaded */
             tabPanel.on({
-                beforetabchange: function(tabPanel, newTab, currentTab) {
+                beforetabchange: function(tabPanelCmp, newTab, currentTab) {
                     /*
                         Only proceed with the clearing process if the tab has changed.
                         This is needed to prevent clearing when a URL has been typed in.
@@ -44,14 +48,22 @@ MODx.Tabs = function(config) {
                             gridObj = null
                         ;
                         if (resetVerticalTabPanelFilters) {
-                            itemsSource = changedBetweenVtabs ? currentTab.items : currentTab.items.items[0].activeTab.items ;
+                            itemsSource = changedBetweenVtabs
+                                ? currentTab.items
+                                : currentTab.items.items[0].activeTab.items;
                         } else {
                             itemsSource = currentTab.items;
                         }
                         if (itemsSource.length > 0) {
-                            gridObj = itemsSource.find(obj => {
-                                return Object.entries(obj).find(([key, value]) => key == 'xtype' && value.includes('modx-grid'));
-                            });
+                            gridObj = this.findGridObject(itemsSource);
+                            /*
+                                Grids placed in an atypical structure, such as the ACLs User Group grid that
+                                is activated via the User Groups tree, require further searching
+                            */
+                            if (!gridObj && itemsSource?.map['modx-tree-panel-usergroup']) {
+                                itemsSource = itemsSource.map['modx-tree-panel-usergroup'].items;
+                                gridObj = this.findGridObject(itemsSource);
+                            }
                         }
                         if (gridObj) {
                             const toolbar = gridObj.getTopToolbar(),
@@ -59,7 +71,7 @@ MODx.Tabs = function(config) {
                             ;
                             if (toolbar && toolbar.items.items.length > 0) {
                                 toolbar.items.items.forEach(cmp => {
-                                    if (cmp.xtype && (cmp.xtype.includes('combo') || cmp.xtype == 'textfield') && cmp.itemId) {
+                                    if (cmp.xtype && (cmp.xtype.includes('combo') || cmp.xtype === 'textfield') && cmp.itemId) {
                                         filterIds.push(cmp.itemId);
                                     }
                                 });
@@ -74,8 +86,26 @@ MODx.Tabs = function(config) {
         }
     });
 };
-Ext.extend(MODx.Tabs,Ext.TabPanel);
-Ext.reg('modx-tabs',MODx.Tabs);
+Ext.extend(MODx.Tabs, Ext.TabPanel, {
+    /**
+     * @property {Function} findGridObject - Search for and return a grid object with a given items array
+     *
+     * @param {String} itemsSource - The config items array to search within
+     * @return {MODx.grid.Grid|undefined}
+     */
+    findGridObject: function(itemsSource) {
+        const grid = itemsSource.find(obj => Object.entries(obj).find(([key, value]) => key === 'xtype' && value.includes('-grid-')));
+        if (grid) {
+            return grid;
+        }
+        const nextItemsSource = itemsSource?.items;
+        if (nextItemsSource) {
+            this.findGridObject(nextItemsSource);
+        }
+        return undefined;
+    }
+});
+Ext.reg('modx-tabs', MODx.Tabs);
 
 MODx.VerticalTabs = function(config) {
     config = config || {};
@@ -94,8 +124,8 @@ MODx.VerticalTabs = function(config) {
     MODx.VerticalTabs.superclass.constructor.call(this,config);
     this.config = config;
     this.on('afterrender', function() {
-        if (MODx.request && MODx.request.hasOwnProperty('vtab')){
-            const tabId = parseInt(MODx.request.vtab);
+        if (MODx.request && Object.prototype.hasOwnProperty.call(MODx.request, 'vtab')) {
+            const tabId = parseInt(MODx.request.vtab, 10);
             this.setActiveTab(tabId);
         }
     });
