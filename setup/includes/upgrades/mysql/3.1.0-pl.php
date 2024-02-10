c417c0ead7e977f9990ac685f3d32e3eedf2362f
Mark Hamstra <hello@markhamstra.com>
2024-02-10 12:43:56 +0100

    Fix modManagerLog MySQL > 5.7.8
    Add right default value for datatime-type column in modx_manager_log table
    
    Re-up of #15736 with migration
    
    Satisfy phpcs

---
+++ setup/includes/upgrades/mysql/3.1.0-pl.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Specific upgrades for Revolution 3.1.0-pl
+ *
+ * @var modX $modx
+ * @package setup
+ * @subpackage upgrades
+ */
+
+/* run upgrades common to all db platforms */
+include dirname(__DIR__) . '/common/3.1.0-db-changes.php';
