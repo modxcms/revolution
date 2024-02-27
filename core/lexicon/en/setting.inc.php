0b08eedd84daa1dc047879f6d4d0a22b632ce376
Sebastian G. Marinescu <sebastian-marinescu@users.noreply.github.com>
2024-02-27 21:18:53 +0100

    Pin Installer entry to top of Extras menu (#14482)
    Due to the default-value of menuindex in menu-entries of newly installed
    extras being most of the time "0", the installer entry will jump up
    and/or down after an extra gets installed/updated.
    
    Therefore it should at least be fixed at the bottom of the
    dropdown-menu.
    
    Fixes #11694 pragmatically
    
    ---------
    
    Co-authored-by: Jim Graham <info@sparkmediagroup.com>

--- core/lexicon/en/setting.inc.php
+++ core/lexicon/en/setting.inc.php
@@ -505,6 +505,9 @@ $_lang['setting_new_file_permissions_desc'] = 'When uploading a new file in the 
 $_lang['setting_new_folder_permissions'] = 'New Folder Permissions';
 $_lang['setting_new_folder_permissions_desc'] = 'When creating a new folder in the File Manager, the File Manager will attempt to change the folder permissions to those entered in this setting. This may not work on some setups, such as IIS, in which case you will need to manually change the permissions.';
 
+$_lang['setting_package_installer_at_top'] = 'Pin Package-Installer at top';
+$_lang['setting_package_installer_at_top_desc'] = 'If enabled, the Installer entry will be pinned to the top of the Extras menu. Otherwise it will be positioned according to its menuindex.';
+
 $_lang['setting_parser_recurse_uncacheable'] = 'Delay Uncacheable Parsing';
 $_lang['setting_parser_recurse_uncacheable_desc'] = 'If disabled, uncacheable elements may have their output cached inside cacheable element content. Disable this ONLY if you are having problems with complex nested parsing which stopped working as expected.';
 
