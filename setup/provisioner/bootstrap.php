ae0364c2ba35d3b247929c9e6fe53ad777f56adf
Dimasites <dimasites@yandex.com>
2024-03-23 00:18:24 +0300

    Fix working with non-standart ports
    In default config, setup process and setup wizard

--- setup/provisioner/bootstrap.php
+++ setup/provisioner/bootstrap.php
@@ -17,12 +17,11 @@ define('MODX_INSTALL_PATH', $installPath);
 if (!MODX_SETUP_INTERFACE_IS_CLI) {
     $https = $_SERVER['HTTP_X_FORWARDED_PROTO'] ?? $_SERVER['HTTPS'] ?? 'off';
     $https = in_array(strtolower((string)$https), ['https', 'on', 'ssl', '1'], true);
-    $installBaseUrl = $https ? 'https://' : 'http://';
-    $installBaseUrl .= $_SERVER['HTTP_HOST'];
-    if (isset($_SERVER['SERVER_PORT']) && (string)$_SERVER['SERVER_PORT'] !== '' && $_SERVER['SERVER_PORT'] !== 80) {
-        $installBaseUrl = str_replace(':' . $_SERVER['SERVER_PORT'], '', $installBaseUrl);
-    }
-    $installBaseUrl .= in_array($_SERVER['SERVER_PORT'], [80, 443]) ? '' : ':' . $_SERVER['SERVER_PORT'];
+    $url_scheme = $https ? 'https://' : 'http://';
+    $installBaseUrl = $url_scheme;
+    $installBaseUrl .= parse_url($url_scheme . $_SERVER['HTTP_HOST'], PHP_URL_HOST);
+    $url_port = parse_url($url_scheme . $_SERVER['HTTP_HOST'], PHP_URL_PORT);
+    $installBaseUrl .= in_array($url_port, [null , 80, 443]) ? '' : ':' . $url_port;
     $installBaseUrl .= $_SERVER['SCRIPT_NAME'];
     $installBaseUrl = htmlspecialchars($installBaseUrl, ENT_QUOTES, 'utf-8');
     define('MODX_SETUP_URL', $installBaseUrl);
