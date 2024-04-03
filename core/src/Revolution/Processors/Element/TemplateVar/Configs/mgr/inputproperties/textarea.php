46f9e795a3124e1ba2e8cdcd4bacadecb2f0fe65
Jim Graham <jim@pixelsandstrings.com>
2024-04-03 15:52:01 -0400

    Enhance textarea tv type
    Adds new input options to control input height

--- core/src/Revolution/Processors/Element/TemplateVar/Configs/mgr/inputproperties/textarea.php
+++ core/src/Revolution/Processors/Element/TemplateVar/Configs/mgr/inputproperties/textarea.php
@@ -13,8 +13,19 @@
  * @package MODX\Revolution\Processors\Element\TemplateVar\Configs\mgr\inputproperties
  */
 
+# Set values
+$defaultHeight = 140;
+$inputHeight = !empty($params['inputHeight']) ? $params['inputHeight'] : $defaultHeight ;
+$textareaGrow = $params['textareaGrow'] === 'true' || $params['textareaGrow'] == 1 ? 'true' : 'false' ;
+$textareaResizable = $params['textareaResizable'] === 'true' || $params['textareaResizable'] == 1 ? 'true' : 'false' ;
+
 # Set help descriptions
-$descKeys = ['required_desc'];
+$descKeys = [
+    'required_desc',
+    'input_height_desc',
+    'textarea_grow_desc',
+    'textarea_resizable_desc'
+];
 $this->setHelpContent($descKeys, $expandHelp);
 
 $optsJS = <<<OPTSJS
@@ -41,11 +52,95 @@ $optsJS = <<<OPTSJS
                     name: 'inopt_allowBlank',
                     hiddenName: 'inopt_allowBlank',
                     id: 'inopt_allowBlank{$tvId}',
+                    tabIndex: 1,
                     value: {$allowBlank}
                 },{
                     xtype: '{$helpXtype}',
                     forId: 'inopt_allowBlank{$tvId}',
                     html: {$this->helpContent['required_desc']},
+                    cls: 'desc-under'
+                }]
+            }
+        ]
+    },
+    {
+        defaults: {
+            layout: 'form',
+            labelSeparator: ''
+        },
+        items: [
+            {
+                xtype: 'panel',
+                columnWidth: 0.34,
+                autoHeight: true,
+                labelAlign: 'top',
+                defaults: {
+                    anchor: '100%',
+                    msgTarget: 'under'
+                },
+                items: [{
+                    xtype: 'numberfield',
+                    fieldLabel: _('input_height'),
+                    description: {$this->helpContent['eh_input_height_desc']},
+                    name: 'inopt_inputHeight',
+                    id: 'inopt_inputHeight{$tvId}',
+                    tabIndex: 2,
+                    allowDecimals: false,
+                    allowNegative: false,
+                    value: {$inputHeight},
+                    emptyText: {$defaultHeight}
+                },{
+                    xtype: '{$helpXtype}',
+                    forId: 'inopt_inputHeight{$tvId}',
+                    html: {$this->helpContent['input_height_desc']},
+                    cls: 'desc-under'
+                }]
+            },{
+                xtype: 'panel',
+                columnWidth: 0.33,
+                autoHeight: true,
+                labelAlign: 'top',
+                defaults: {
+                    anchor: '100%',
+                    msgTarget: 'under'
+                },
+                items: [{
+                    xtype: 'combo-boolean',
+                    fieldLabel: _('textarea_grow'),
+                    description: {$this->helpContent['eh_textarea_grow_desc']},
+                    name: 'inopt_textareaGrow',
+                    hiddenName: 'inopt_textareaGrow',
+                    id: 'inopt_textareaGrow{$tvId}',
+                    tabIndex: 3,
+                    value: {$textareaGrow}
+                },{
+                    xtype: '{$helpXtype}',
+                    forId: 'inopt_textareaGrow{$tvId}',
+                    html: {$this->helpContent['textarea_grow_desc']},
+                    cls: 'desc-under'
+                }]
+            },{
+                xtype: 'panel',
+                columnWidth: 0.33,
+                autoHeight: true,
+                labelAlign: 'top',
+                defaults: {
+                    anchor: '100%',
+                    msgTarget: 'under'
+                },
+                items: [{
+                    xtype: 'combo-boolean',
+                    fieldLabel: _('textarea_resizable'),
+                    description: {$this->helpContent['eh_textarea_resizable_desc']},
+                    name: 'inopt_textareaResizable',
+                    hiddenName: 'inopt_textareaResizable',
+                    id: 'inopt_textareaResizable{$tvId}',
+                    tabIndex: 4,
+                    value: {$textareaResizable}
+                },{
+                    xtype: '{$helpXtype}',
+                    forId: 'inopt_textareaResizable{$tvId}',
+                    html: {$this->helpContent['textarea_resizable_desc']},
                     cls: 'desc-under'
                 }]
             }
--- manager/templates/default/element/tv/renders/input/textarea.tpl
+++ manager/templates/default/element/tv/renders/input/textarea.tpl
@@ -1,25 +1,37 @@
-<textarea id="tv{$tv->id}" name="tv{$tv->id}" rows="15">{$tv->get('value')|escape}</textarea>
+<textarea id="tv{$tv->id}" name="tv{$tv->id}">{$tv->get('value')|escape}</textarea>
 
 <script>
 // <![CDATA[
 document.getElementById('tv{$tv->id}').setAttribute('autocomplete', globalAutoCompleteSetting);
 {literal}
 Ext.onReady(function() {
-    const fld = MODx.load({
-    {/literal}
-        xtype: 'textarea'
-        ,itemId: 'tv{$tv->id}'
-        ,applyTo: 'tv{$tv->id}'
+    const 
+        defaultHeight = 140,
+        fld = MODx.load({
+        {/literal}
+        xtype: 'textarea',
+        itemId: 'tv{$tv->id}',
+        applyTo: 'tv{$tv->id}',
         {if $tv->get('value') != ''}
-        ,value: '{$tv->get('value')|escape:'javascript'}'
+            value: '{$tv->get('value')|escape:'javascript'}',
         {/if}
-        ,height: 140
-        ,width: '99%'
-        ,enableKeyEvents: true
-        ,msgTarget: 'under'
-        ,allowBlank: {if $params.allowBlank == 1 || $params.allowBlank == 'true'}true{else}false{/if}
+        {if $params.textareaGrow == 1 || $params.textareaGrow == 'true'}
+            boxMinHeight: {if $params.inputHeight != ''}{$params.inputHeight}{else}{literal}defaultHeight{/literal}{/if},
+            grow: true,
+            growMin: {if $params.inputHeight != ''}{$params.inputHeight}{else}{literal}defaultHeight{/literal}{/if},
+            growMax: 1200,
+        {else}
+            height: {if $params.inputHeight != ''}{$params.inputHeight}{else}{literal}defaultHeight{/literal}{/if},
+        {/if}
+        {if $params.textareaResizable == 1 || $params.textareaResizable == 'true'}
+            ctCls: 'resizable',
+        {/if}
+        width: '99%',
+        enableKeyEvents: true,
+        msgTarget: 'under',
+        allowBlank: {if $params.allowBlank == 1 || $params.allowBlank == 'true'}true{else}false{/if},
     {literal}
-        ,listeners: {
+        listeners: {
             keydown: {
                 fn: MODx.fireResourceFormChange,
                 scope: this
