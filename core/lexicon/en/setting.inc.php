be5078e213b7f8252c451f0795088b38dd4e3a51
Warfork <51063252+Warfork@users.noreply.github.com>
2024-04-22 13:17:28 -0400

    [en] Correct typo placehodlers -> placeholders

--- core/lexicon/en/setting.inc.php
+++ core/lexicon/en/setting.inc.php
@@ -806,7 +806,7 @@ $_lang['setting_error_log_filename'] = 'Error log filename';
 $_lang['setting_error_log_filename_desc'] = 'Customize the filename of the MODX error log file (includes file extension).';
 
 $_lang['setting_error_log_filepath'] = 'Error log path';
-$_lang['setting_error_log_filepath_desc'] = 'Optionally set a absolute path the a custom error log location. You might use placehodlers like {cache_path}.';
+$_lang['setting_error_log_filepath_desc'] = 'Optionally set a absolute path the a custom error log location. You might use placeholders like {cache_path}.';
 
 $_lang['setting_passwordless_activated'] = 'Activate passwordless login';
 $_lang['setting_passwordless_activated_desc'] = 'When enabled, users will enter their email address to receive a one-time login link, rather than entering a username and password.';
