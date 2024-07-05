e0f83db1ed91ce1a0953560973f52eb971678009
Jim Graham <jim@pixelsandstrings.com>
2024-07-05 14:26:30 -0400

    Fix Role Authority Editability (#16568)
    ### What does it do?
    Add new listener routine to restrict editing based on whether a Role is
    assigned to an ACL entry. Also:
    
    1. Adds new UI feedback, both a lock icon (instead of pencil) on hover
    of authority as well as an alert dialog when double-clicking on locked
    authority
    2. Ensures authority cannot be deleted in grid editor
    3. Remove pencil icon for cells of non-editable Roles
    
    ### Why is it needed?
    The ability to edit an assigned Role's authority leads to orphaned ACL
    rules that not longer show up in the manager, yet remain in the
    database. See the referenced issue below.
    
    ### How to test
    
    1. Rebuild template (grunt build) and clear manager and browser caches
    2. Create a few Roles under the Access Control Lists section
    3. Assign at least one Role to any ACL entry of your choice
    4. Verify that the assigned Roles' authority is locked in the Roles grid
    5. Verify that unassigned Roles remain fully editable in the Roles grid
    (authority is unlocked)
    
    ### Note
    The initial commit contains all substantive changes, while the follow up
    is just code-style/optimization oriented.
    
    ### Related issue(s)/PR(s)
    Resolves #16565
    
    ---------
    
    Co-authored-by: Jason Coward <jason@opengeek.com>

--- manager/assets/modext/widgets/security/modx.grid.role.js
+++ manager/assets/modext/widgets/security/modx.grid.role.js
@@ -7,83 +7,138 @@
  * @param {Object} config An object of options.
  * @xtype modx-grid-role
  */
-MODx.grid.Role = function(config) {
-    config = config || {};
-    Ext.applyIf(config,{
-        title: _('roles')
-        ,id: 'modx-grid-role'
-        ,url: MODx.config.connector_url
-        ,baseParams: {
+MODx.grid.Role = function(config = {}) {
+    Ext.applyIf(config, {
+        title: _('roles'),
+        id: 'modx-grid-role',
+        url: MODx.config.connector_url,
+        baseParams: {
             action: 'Security/Role/GetList'
-        }
-        ,fields: ['id','name','description','authority','perm']
-        ,paging: true
-        ,autosave: true
-        ,save_action: 'Security/Role/UpdateFromGrid'
-        ,columns: [{
-            header: _('id')
-            ,dataIndex: 'id'
-            ,width: 50
-            ,sortable: true
-        },{
-            header: _('name')
-            ,dataIndex: 'name'
-            ,width: 150
-            ,sortable: true
-            ,editor: { xtype: 'textfield' }
-        },{
-            header: _('description')
-            ,dataIndex: 'description'
-            ,width: 350
-            ,editor: { xtype: 'textarea' }
-        },{
-            header: _('authority')
-            ,dataIndex: 'authority'
-            ,width: 60
-            ,editor: { xtype: 'textfield' }
-            ,sortable: true
-        }]
-        ,tbar: [{
-            text: _('create')
-            ,cls:'primary-button'
-            ,handler: this.createRole
-            ,scope: this
+        },
+        fields: [
+            'id',
+            'name',
+            'description',
+            'authority',
+            'perm'
+        ],
+        paging: true,
+        autosave: true,
+        save_action: 'Security/Role/UpdateFromGrid',
+        columns: [{
+            header: _('id'),
+            dataIndex: 'id',
+            width: 50,
+            sortable: true
+        }, {
+            header: _('name'),
+            dataIndex: 'name',
+            width: 150,
+            sortable: true,
+            editor: {
+                xtype: 'textfield'
+            },
+            renderer: {
+                fn: function(value, metaData, record, rowIndex, colIndex, store) {
+                    metaData.css = this.setEditableCellClasses(record);
+                    return Ext.util.Format.htmlEncode(value);
+                },
+                scope: this
+            }
+        }, {
+            header: _('description'),
+            dataIndex: 'description',
+            width: 350,
+            editor: { xtype: 'textarea' },
+            renderer: {
+                fn: function(value, metaData, record, rowIndex, colIndex, store) {
+                    metaData.css = this.setEditableCellClasses(record);
+                    return Ext.util.Format.htmlEncode(value);
+                },
+                scope: this
+            }
+        }, {
+            header: _('authority'),
+            dataIndex: 'authority',
+            width: 60,
+            sortable: true,
+            editor: {
+                xtype: 'numberfield',
+                allowNegative: false,
+                allowDecimals: false,
+                allowBlank: false,
+                blankText: _('role_err_ns_authority'),
+                maxValue: 9999
+            },
+            renderer: {
+                fn: function(value, metaData, record, rowIndex, colIndex, store) {
+                    metaData.css = this.setEditableCellClasses(record, [record.json.isAssigned]);
+                    return value;
+                },
+                scope: this
+            },
+            listeners: {
+                dblclick: {
+                    fn: function(column, grid, rowIndex, e) {
+                        const
+                            selectedRecord = grid.getSelectionModel().getSelected(),
+                            roleIsAssigned = selectedRecord.json.isAssigned === 1
+                        ;
+                        if (roleIsAssigned) {
+                            Ext.Msg.show({
+                                title: _('warning'),
+                                msg: _('role_warn_authority_locked'),
+                                buttons: Ext.Msg.OK,
+                                icon: Ext.MessageBox.WARNING,
+                                maxWidth: 400
+                            });
+                        }
+                    },
+                    scope: this
+                }
+            }
+        }],
+        tbar: [{
+            text: _('create'),
+            cls: 'primary-button',
+            handler: this.createRole,
+            scope: this
         }]
     });
-    MODx.grid.Role.superclass.constructor.call(this,config);
-    this.on('beforeedit',this.checkEditable,this);
+    MODx.grid.Role.superclass.constructor.call(this, config);
+    this.on('beforeedit', this.checkCellIsEditable, this);
 };
-Ext.extend(MODx.grid.Role,MODx.grid.Grid,{
-    checkEditable: function(o) {
-        var p = o.record.data.perm || '';
-        return p.indexOf('edit') != -1;
-    }
-
-    ,getMenu: function() {
-        var r = this.getSelectionModel().getSelected();
-        var p = r.data.perm || '';
-        var m = [];
-        if (p.indexOf('remove') != -1) {
-            m.push({
-                text: _('delete')
-                ,handler: this.remove.createDelegate(this,['role_remove_confirm', 'Security/Role/Remove'])
+Ext.extend(MODx.grid.Role, MODx.grid.Grid, {
+    getMenu: function() {
+        const
+            record = this.getSelectionModel().getSelected(),
+            permissions = record.data.perm || '',
+            menu = []
+        ;
+        if (permissions.indexOf('remove') !== -1) {
+            menu.push({
+                text: _('delete'),
+                handler: this.remove.createDelegate(this, ['role_remove_confirm', 'Security/Role/Remove'])
             });
         }
-        return m;
-    }
+        return menu;
+    },
 
-    ,createRole: function(btn,e) {
-        this.loadWindow(btn,e,{
-            xtype: 'modx-window-role-create'
-            ,listeners: {
-                'success': {fn: function() {
-                    this.refresh();
-                },scope:this}
+    createRole: function(btn, e) {
+        this.loadWindow(btn, e, {
+            xtype: 'modx-window-role-create',
+            listeners: {
+                success: {
+                    fn: function() {
+                        this.refresh();
+                    },
+                    scope: this
+                }
             }
         });
     }
 });
-Ext.reg('modx-grid-role',MODx.grid.Role);
+Ext.reg('modx-grid-role', MODx.grid.Role);
 
 /**
  * @class MODx.window.CreateRole
@@ -91,55 +146,47 @@ Ext.reg('modx-grid-role',MODx.grid.Role);
  * @param {Object} config An object of options.
  * @xtype modx-window-role-create
  */
-MODx.window.CreateRole = function(config) {
-    config = config || {};
-    this.ident = config.ident || 'crole'+Ext.id();
-    Ext.applyIf(config,{
-        title: _('create')
-        ,url: MODx.config.connector_url
-        ,action: 'Security/Role/Create'
-        ,fields: [{
-            name: 'name'
-            ,fieldLabel: _('name')
-            ,id: 'modx-'+this.ident+'-name'
-            ,xtype: 'textfield'
-            ,allowBlank: false
-            ,anchor: '100%'
-        },{
-            xtype: MODx.expandHelp ? 'label' : 'hidden'
-            ,forId: 'modx-'+this.ident+'-name'
-            ,html: _('role_desc_name')
-            ,cls: 'desc-under'
-        },{
-            name: 'authority'
-            ,fieldLabel: _('authority')
-            ,xtype: 'textfield'
-            ,id: 'modx-'+this.ident+'-authority'
-            ,allowBlank: false
-            ,allowNegative: false
-            ,value: 0
-            ,anchor: '100%'
-        },{
-            xtype: MODx.expandHelp ? 'label' : 'hidden'
-            ,forId: 'modx-'+this.ident+'-authority'
-            ,html: _('role_desc_authority')
-            ,cls: 'desc-under'
-        },{
-            name: 'description'
-            ,fieldLabel: _('description')
-            ,id: 'modx-'+this.ident+'-description'
-            ,xtype: 'textarea'
-            ,anchor: '100%'
-            ,grow: true
-        },{
-            xtype: MODx.expandHelp ? 'label' : 'hidden'
-            ,forId: 'modx-'+this.ident+'-description'
-            ,html: _('role_desc_description')
-            ,cls: 'desc-under'
-        }]
-        ,keys: []
+MODx.window.CreateRole = function(config = {}) {
+    Ext.applyIf(config, {
+        title: _('create'),
+        url: MODx.config.connector_url,
+        action: 'Security/Role/Create',
+        formDefaults: {
+            allowBlank: false,
+            anchor: '100%'
+        },
+        fields: [{
+            name: 'name',
+            fieldLabel: _('name'),
+            xtype: 'textfield'
+        }, {
+            xtype: MODx.expandHelp ? 'box' : 'hidden',
+            html: _('role_desc_name'),
+            cls: 'desc-under'
+        }, {
+            name: 'authority',
+            fieldLabel: _('authority'),
+            xtype: 'textfield',
+            allowNegative: false,
+            value: 0
+        }, {
+            xtype: MODx.expandHelp ? 'box' : 'hidden',
+            html: _('role_desc_authority'),
+            cls: 'desc-under'
+        }, {
+            name: 'description',
+            fieldLabel: _('description'),
+            xtype: 'textarea',
+            allowBlank: true,
+            grow: true
+        }, {
+            xtype: MODx.expandHelp ? 'box' : 'hidden',
+            html: _('role_desc_description'),
+            cls: 'desc-under'
+        }],
+        keys: []
     });
-    MODx.window.CreateRole.superclass.constructor.call(this,config);
+    MODx.window.CreateRole.superclass.constructor.call(this, config);
 };
-Ext.extend(MODx.window.CreateRole,MODx.Window);
-Ext.reg('modx-window-role-create',MODx.window.CreateRole);
+Ext.extend(MODx.window.CreateRole, MODx.Window);
+Ext.reg('modx-window-role-create', MODx.window.CreateRole);
