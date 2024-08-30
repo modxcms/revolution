eb62cea5a88bb55d5c95858d4aed5ba6e29e0da6
Ruslan-Aleev <aleev.insmile@gmail.com>
2024-08-30 13:35:57 +0400

    Fix menu update script

--- setup/includes/upgrades/common/3.0.0-update-menu-main.php
+++ setup/includes/upgrades/common/3.0.0-update-menu-main.php
@@ -8,35 +8,60 @@
 
 use MODX\Revolution\modMenu;
 
-$menu = [
-    'site' => '<i class="icon-file-text-o icon icon-large"></i>',
-    'media' => '<i class="icon-file-image-o icon icon-large"></i>',
-    'components' => '<i class="icon-cube icon icon-large"></i>',
-    'manage' => '<i class="icon-sliders icon icon-large"></i>',
+// Update menu icons
+$menuItemsUpdateIcons = [
+    'site' => '<i class="icon-file-text-o icon"></i>',
+    'media' => '<i class="icon-file-image-o icon"></i>',
+    'components' => '<i class="icon-cube icon"></i>',
+    'manage' => '<i class="icon-sliders icon"></i>',
     'user' => '<span id="user-avatar">{$userImage}</span><span id="user-username">{$username}</span>',
-    'admin' => '<i class="icon-gear icon icon-large"></i>',
-    'about' => '<i class="icon-question-circle icon icon-large"></i>',
+    'admin' => '<i class="icon-gear icon"></i>',
+    'about' => '<i class="icon-question-circle icon"></i>',
 ];
 
-foreach ($menu as $key => $value) {
-    /** @var modMenu $menu_item */
+foreach ($menuItemsUpdateIcons as $key => $value) {
+    /** @var modMenu $menuItem */
 
-    $menu_item = $modx->getObject(modMenu::class, ['text' => $key]);
-    if ($menu_item instanceof modMenu) {
-        $menu_item->set('description', '');
-        $menu_item->set('icon', $value);
-        $menu_item->save();
+    $menuItem = $modx->getObject(modMenu::class, ['text' => $key]);
+    if ($menuItem instanceof modMenu) {
+        $menuItem->set('description', '');
+        $menuItem->set('icon', $value);
+
+        if ($menuItem->save()) {
+            $this->runner->addResult(
+                modInstallRunner::RESULT_SUCCESS,
+                sprintf($messageTemplate, 'ok', $this->install->lexicon('menu_update_success', ['text' => $key]))
+            );
+        } else {
+            $this->runner->addResult(
+                modInstallRunner::RESULT_WARNING,
+                sprintf($messageTemplate, 'warning', $this->install->lexicon('menu_update_failed', ['text' => $key]))
+            );
+        }
     }
 }
 
-$removed = [
+// Remove menu items
+$menuItemsRemove = [
     'import_site',
     'import_resources',
 ];
 
-foreach ($removed as $key) {
-    $menu_item = $modx->getObject(modMenu::class, ['text' => $key]);
-    if ($menu_item instanceof modMenu) {
-        $menu_item->remove();
+foreach ($menuItemsRemove as $key) {
+    /** @var modMenu $menuItem */
+
+    $menuItem = $modx->getObject(modMenu::class, ['text' => $key]);
+    if ($menuItem instanceof modMenu) {
+        if ($menuItem->remove()) {
+            $this->runner->addResult(
+                modInstallRunner::RESULT_SUCCESS,
+                sprintf($messageTemplate, 'ok', $this->install->lexicon('menu_remove_success', ['text' => $key]))
+            );
+        } else {
+            $this->runner->addResult(
+                modInstallRunner::RESULT_WARNING,
+                sprintf($messageTemplate, 'warning', $this->install->lexicon('menu_remove_failed', ['text' => $key]))
+            );
+        }
     }
 }
