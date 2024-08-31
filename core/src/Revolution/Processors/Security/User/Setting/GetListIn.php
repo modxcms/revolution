d2ab7a17345818448011b419319d14478f50de1c
Jim Graham <jim@pixelsandstrings.com>
2024-08-30 23:46:49 -0400

    Update GetListIn.php
    Code QC

--- core/src/Revolution/Processors/Security/User/Setting/GetListIn.php
+++ core/src/Revolution/Processors/Security/User/Setting/GetListIn.php
@@ -11,16 +11,13 @@
 
 namespace MODX\Revolution\Processors\Security\User\Setting;
 
-use MODX\Revolution\Processors\Model\GetProcessor;
 use MODX\Revolution\modUserSetting;
 
 /**
  * Gets a list of user settings given an array of keys to search for
- * @param integer $user The user to grab from
- * @param integer $start (optional) The record to start at. Defaults to 0.
- * @param integer $limit (optional) The number of records to limit to. Defaults to 10.
- * @param string $sort (optional) The column to sort by. Defaults to key.
- * @param string $dir (optional) The direction of the sort. Defaults to ASC.
+ *
+ * @property int $user The user to filter by
+ * @property string $keys A json-formatted list of settings keys to additionally filter by
  * @package MODX\Revolution\Processors\Security\User\Setting
  */
 class GetListIn extends \MODX\Revolution\Processors\System\Settings\GetList
@@ -45,20 +42,11 @@ class GetListIn extends \MODX\Revolution\Processors\System\Settings\GetList
     {
         $criteria = [];
         $criteria[] = ['user' => (int)$this->getProperty('user')];
-        // $msg = "\r\n prepareCriteria, \$properties:\r\n" . print_r($this->getProperties(), true);
-        // $this->modx->log(\modX::LOG_LEVEL_ERROR, $msg, '', __CLASS__);
 
         if ($keys = $this->getProperty('keys', '')) {
             $keys = json_decode($keys);
-            // $this->modx->log(
-            //     \modX::LOG_LEVEL_ERROR,
-            //     "\r\t prepareCriteria:
-            //     \t\t\$var1: {none}
-            //     \t\t\$keys: " . print_r($keys, true)
-            // );
             $criteria[] = ['key:IN' => $keys];
         }
         return $criteria;
     }
-
 }
