a4e3c9fd41cf1a3c912448c80dbc03da46c8db16
Jason Coward <jason@opengeek.com>
2024-11-14 15:28:51 -0700

    Update welcome screen setting

--- _build/data/transport.core.system_settings.php
+++ _build/data/transport.core.system_settings.php
@@ -2127,7 +2127,7 @@ $settings['welcome_screen']->fromArray(array (
 $settings['welcome_screen_url']= $xpdo->newObject('modSystemSetting');
 $settings['welcome_screen_url']->fromArray(array (
   'key' => 'welcome_screen_url',
-  'value' => '//misc.modx.com/revolution/welcome.28.html',
+  'value' => '//misc.modx.com/revolution/welcome.29.html',
   'xtype' => 'textfield',
   'namespace' => 'core',
   'area' => 'manager',
