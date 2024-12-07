06ea9f740dcb14120541543b13c1f72aa0e2ee92
Jim Graham <jim@pixelsandstrings.com>
2024-12-07 00:01:17 -0500

    TV Resource Group Access formatting update
    Code style changes only

--- core/src/Revolution/Processors/Element/TemplateVar/ResourceGroup/GetList.php
+++ core/src/Revolution/Processors/Element/TemplateVar/ResourceGroup/GetList.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
  * This file is part of the MODX Revolution package.
  *
@@ -9,7 +10,6 @@
  */
 
 namespace MODX\Revolution\Processors\Element\TemplateVar\ResourceGroup;
-
 
 use MODX\Revolution\Processors\Processor;
 use MODX\Revolution\modResourceGroup;
--- manager/assets/modext/widgets/element/modx.grid.tv.security.js
+++ manager/assets/modext/widgets/element/modx.grid.tv.security.js
@@ -6,44 +6,54 @@
  * @param {Object} config An object of options.
  * @xtype modx-grid-tv-security
  */
-MODx.grid.TVSecurity = function(config) {
-    config = config || {};
-    var tt = new Ext.ux.grid.CheckColumn({
-        header: _('access')
-        ,dataIndex: 'access'
-        ,width: 40
-        ,sortable: false
+MODx.grid.TVSecurity = function(config = {}) {
+    const accessCheckboxCol = new Ext.ux.grid.CheckColumn({
+        header: _('access'),
+        dataIndex: 'access',
+        width: 40,
+        sortable: false
     });
-    Ext.applyIf(config,{
-        id: 'modx-grid-tv-security'
-        ,url: MODx.config.connector_url
-        ,showActionsColumn: false
-        ,fields: ['id','name','access','menu']
-        ,baseParams: {
-            action: 'Element/TemplateVar/ResourceGroup/GetList'
-            ,tv: config.tv
-        }
-        ,saveParams: {
+    Ext.applyIf(config, {
+        id: 'modx-grid-tv-security',
+        url: MODx.config.connector_url,
+        showActionsColumn: false,
+        fields: [
+            'id',
+            'name',
+            'access',
+            'menu'
+        ],
+        baseParams: {
+            action: 'Element/TemplateVar/ResourceGroup/GetList',
             tv: config.tv
-        }
-        ,width: 800
-        ,paging: true
-        ,remoteSort: true
-        ,plugins: tt
-        ,columns: [{
-            header: _('name')
-            ,dataIndex: 'name'
-            ,width: 200
-            ,sortable: true
-            ,renderer: { fn: function(v,md,record) {
-                return this.renderLink(v, {
-                    href: '?a=security/resourcegroup'
-                    ,target: '_blank'
-                });
-            }, scope: this }
-        },tt]
+        },
+        saveParams: {
+            tv: config.tv
+        },
+        width: 800,
+        paging: true,
+        remoteSort: true,
+        plugins: accessCheckboxCol,
+        columns: [
+            {
+                header: _('name'),
+                dataIndex: 'name',
+                width: 200,
+                sortable: true,
+                renderer: {
+                    fn: function(value, metaData, record) {
+                        return this.renderLink(value, {
+                            href: '?a=security/resourcegroup',
+                            target: '_blank'
+                        });
+                    },
+                    scope: this
+                }
+            },
+            accessCheckboxCol
+        ]
     });
-    MODx.grid.TVSecurity.superclass.constructor.call(this,config);
+    MODx.grid.TVSecurity.superclass.constructor.call(this, config);
 };
-Ext.extend(MODx.grid.TVSecurity,MODx.grid.Grid);
-Ext.reg('modx-grid-tv-security',MODx.grid.TVSecurity);
+Ext.extend(MODx.grid.TVSecurity, MODx.grid.Grid);
+Ext.reg('modx-grid-tv-security', MODx.grid.TVSecurity);
