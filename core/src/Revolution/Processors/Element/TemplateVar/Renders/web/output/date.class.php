505f0c46fd95fd07e30dd1cf14485da57f17aad7
Jim Graham <jim@pixelsandstrings.com>
2025-05-31 22:53:05 -0400

    Update date.class.php
    Code style/quality fixes

--- core/src/Revolution/Processors/Element/TemplateVar/Renders/web/output/date.class.php
+++ core/src/Revolution/Processors/Element/TemplateVar/Renders/web/output/date.class.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
  * This file is part of MODX Revolution.
  *
@@ -19,24 +20,27 @@ use MODX\Revolution\modTemplateVarOutputRender;
  * @package modx
  * @subpackage processors.element.tv.renders.mgr.output
  */
-class modTemplateVarOutputRenderDate extends modTemplateVarOutputRender {
-    public function process($value,array $params = []) {
+class modTemplateVarOutputRenderDate extends modTemplateVarOutputRender
+{
+    public function process($value, array $params = [])
+    {
         $formatter = $this->modx->services->get(modManagerDateFormatter::class);
         /* default properties */
         $params['format'] = !empty($params['format']) ? $params['format'] : 'l, d F Y';
         /* fix for 2.0.0-pl bug where 1=yes and 0=no */
         $params['default'] = !empty($params['default']) && in_array($params['default'], ['yes',1,'1']) ? 1 : 0;
 
-        $value= $this->tv->parseInput($value);
+        $value = $this->tv->parseInput($value);
 
         /* if not using current time and no value, return */
-        if (empty($value) && empty($params['default'])) return '';
-
+        if (empty($value) && empty($params['default'])) {
+            return '';
+        }
         /* if using current, and value empty, get current time */
         if (!empty($params['default']) && empty($value)) {
             $timestamp = time();
         } else { /* otherwise get timestamp */
-            $timestamp= strtotime($value);
+            $timestamp = strtotime($value);
         }
 
         /* return formatted time */
