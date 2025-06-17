cfea01200b0a0658f619d7f1dd544079e78abb06
Ali Çömez | Slaweally <sys@rootali.net>
2025-06-17 16:58:07 +0300

    Update dashboard.inc.php
    Update Turkish Lang.

--- core/lexicon/tr/dashboard.inc.php
+++ core/lexicon/tr/dashboard.inc.php
@@ -1,45 +1,43 @@
 <?php
 /**
- * Welcome Page English lexicon topic
+ * Welcome Page Turkish lexicon topic
  *
- * @language en
+ * @language tr
  * @package modx
  * @subpackage lexicon
  */
 $_lang['active_users_none'] = 'Şu anda aktif kullanıcı yok.';
-$_lang['add_user'] = 'Kullanıcı Ekle';
-$_lang['create_resource'] = 'Kaynak Oluştur';
-$_lang['dashboard'] = 'Kontrol Paneli';
+$_lang['add_user'] = 'Kullanıcı ekle';
+$_lang['create_resource'] = 'Kaynak oluştur';
+$_lang['dashboard'] = 'Kontrol paneli';
 $_lang['documentation'] = 'Dökümantasyon';
-$_lang['file_bug'] = 'Bir hatayı dosyala';
+$_lang['file_bug'] = 'Hata bildir';
 $_lang['forums'] = 'Forumlar';
 $_lang['help'] = 'Yardım';
-$_lang['modx_news'] = 'MODX Haberleri';
+$_lang['modx_news'] = 'MODX haberleri';
 $_lang['onlineusers_action'] = 'Eylem';
-$_lang['onlineusers_actionid'] = 'Eylem - Kimliği';
-$_lang['onlineusers_ipaddress'] = 'Kullanıcının IP adresi';
-$_lang['onlineusers_lasthit'] = 'Son vuruş';
-$_lang['onlineusers_message'] = 'This list shows all users active within the last 20 minutes:';
+$_lang['onlineusers_actionid'] = 'Eylem ID';
+$_lang['onlineusers_ipaddress'] = 'IP adresi';
+$_lang['onlineusers_lasthit'] = 'Son etkinlik';
+$_lang['onlineusers_message'] = 'Bu liste son 20 dakika içinde aktif olan tüm kullanıcıları gösterir:';
 $_lang['onlineusers_title'] = 'Çevrimiçi kullanıcılar';
 $_lang['onlineusers_user'] = 'Kullanıcı';
-$_lang['onlineusers_userid'] = 'Kullanıcı Kimliği';
-
-$_lang['updates_type'] = 'Type';
+$_lang['onlineusers_userid'] = 'Kullanıcı ID';
+$_lang['updates_type'] = 'Tür';
 $_lang['updates_status'] = 'Durum';
 $_lang['updates_action'] = 'Eylem';
-$_lang['updates_available'] = 'Updates available';
-$_lang['updates_update'] = 'Güncelleştirme';
-$_lang['updates_ok'] = 'Up to date';
-$_lang['updates_extras'] = 'Ekstralar';
-
-$_lang['quicklinks'] = 'Hızlı Linkler';
+$_lang['updates_available'] = 'Güncellemeler mevcut';
+$_lang['updates_update'] = 'Güncelle';
+$_lang['updates_ok'] = 'Güncel';
+$_lang['updates_extras'] = 'Eklentiler';
+$_lang['quicklinks'] = 'Hızlı linkler';
 $_lang['security_notices'] = 'Güvenlik uyarıları';
-$_lang['view_tree'] = 'Ağacı Görüntüle';
-$_lang['welcome_back'] = 'Tekrar hoşgeldin, [[+name]].';
-$_lang['welcome_messages'] = 'Sizin gelen kutunuz <strong>%s</strong> okunmamış, <strong>%d</strong> mesaj(lar) içeriyor.';
+$_lang['view_tree'] = 'Ağaç görünümü';
+$_lang['welcome_back'] = 'Tekrar hoş geldin, [[+name]].';
+$_lang['welcome_messages'] = 'Gelen kutunuzda <strong>%s</strong> adet okunmamış mesajınız var.';
 $_lang['welcome_title'] = 'MODX içerik yöneticisine hoş geldiniz';
 $_lang['yourinfo_message'] = 'Bu bölüm sizin hakkınızda bazı bilgiler gösterir:';
 $_lang['yourinfo_previous_login'] = 'Son giriş:';
 $_lang['yourinfo_title'] = 'Bilgileriniz';
 $_lang['yourinfo_total_logins'] = 'Toplam giriş sayısı:';
-$_lang['yourinfo_username'] = 'Olarak oturum açtınız:';
+$_lang['yourinfo_username'] = 'Giriş yaptığınız kullanıcı:';
