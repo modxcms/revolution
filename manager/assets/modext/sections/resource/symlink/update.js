1076de8812e072da8774470b1ad4e087a3ca2bd9
Jim Graham <jim@pixelsandstrings.com>
2025-09-25 12:04:59 -0400

    JS file formatting (batch 1)
    For the smaller files: Fix format, modernize

--- manager/assets/modext/sections/resource/symlink/update.js
+++ manager/assets/modext/sections/resource/symlink/update.js
@@ -6,22 +6,21 @@
  * @param {Object} config An object of config properties
  * @xtype modx-page-symlink-update
  */
-MODx.page.UpdateSymLink = function(config) {
-    config = config || {};
-    Ext.applyIf(config,{
+MODx.page.UpdateSymLink = function(config = {}) {
+    Ext.applyIf(config, {
         components: [{
-            xtype: 'modx-panel-symlink'
-            ,renderTo: 'modx-panel-symlink-div'
-            ,resource: config.resource
-            ,record: config.record || {}
-            ,publish_document: config.publish_document
-            ,show_tvs: config.show_tvs
-            ,url: config.url
-            ,canDelete: config.canDelete
-            ,locked: config.locked
+            xtype: 'modx-panel-symlink',
+            renderTo: 'modx-panel-symlink-div',
+            resource: config.resource,
+            record: config.record || {},
+            publish_document: config.publish_document,
+            show_tvs: config.show_tvs,
+            url: config.url,
+            canDelete: config.canDelete,
+            locked: config.locked
         }]
     });
-    MODx.page.UpdateSymLink.superclass.constructor.call(this,config);
+    MODx.page.UpdateSymLink.superclass.constructor.call(this, config);
 };
-Ext.extend(MODx.page.UpdateSymLink,MODx.page.UpdateResource);
-Ext.reg('modx-page-symlink-update',MODx.page.UpdateSymLink);
+Ext.extend(MODx.page.UpdateSymLink, MODx.page.UpdateResource);
+Ext.reg('modx-page-symlink-update', MODx.page.UpdateSymLink);
--- manager/assets/modext/sections/resource/weblink/update.js
+++ manager/assets/modext/sections/resource/weblink/update.js
@@ -6,22 +6,21 @@
  * @param {Object} config An object of config properties
  * @xtype modx-page-weblink-update
  */
-MODx.page.UpdateWebLink = function(config) {
-    config = config || {};
-    Ext.applyIf(config,{
+MODx.page.UpdateWebLink = function(config = {}) {
+    Ext.applyIf(config, {
         components: [{
-            xtype: 'modx-panel-weblink'
-            ,renderTo: 'modx-panel-weblink-div'
-            ,resource: config.resource
-            ,record: config.record || {}
-            ,publish_document: config.publish_document
-            ,show_tvs: config.show_tvs
-            ,url: config.url
-            ,canDelete: config.canDelete
-            ,locked: config.locked
+            xtype: 'modx-panel-weblink',
+            renderTo: 'modx-panel-weblink-div',
+            resource: config.resource,
+            record: config.record || {},
+            publish_document: config.publish_document,
+            show_tvs: config.show_tvs,
+            url: config.url,
+            canDelete: config.canDelete,
+            locked: config.locked
         }]
     });
-    MODx.page.UpdateWebLink.superclass.constructor.call(this,config);
+    MODx.page.UpdateWebLink.superclass.constructor.call(this, config);
 };
-Ext.extend(MODx.page.UpdateWebLink,MODx.page.UpdateResource);
-Ext.reg('modx-page-weblink-update',MODx.page.UpdateWebLink);
+Ext.extend(MODx.page.UpdateWebLink, MODx.page.UpdateResource);
+Ext.reg('modx-page-weblink-update', MODx.page.UpdateWebLink);
