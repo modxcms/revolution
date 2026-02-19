ad99c7e70152d9cdcfbf01f124b9e49bb25848d2
Ivan Bochkarev <ivanx86@gmail.com>
2026-02-19 15:16:32 +0600

    Fix: convert refresh_site to modManagerController class (#16282)
    Replace script-style refresh_site.php with SystemRefreshSiteManagerController
    extending modManagerController. The action /manager/?a=system/refresh_site
    now loads properly and clears cache correctly (e.g. frontendManager).

---
+++ manager/controllers/default/system/refresh_site.class.php
@@ -0,0 +1,85 @@
+<?php
+/*
+ * This file is part of MODX Revolution.
+ *
+ * Copyright (c) MODX, LLC. All Rights Reserved.
+ *
+ * For complete copyright and license information, see the COPYRIGHT and LICENSE
+ * files found in the top-level directory of this distribution.
+ */
+
+use MODX\Revolution\modManagerController;
+
+/**
+ * Refreshes the site cache
+ *
+ * @package modx
+ * @subpackage manager.controllers
+ */
+class SystemRefreshSiteManagerController extends modManagerController
+{
+    /**
+     * Check for any permissions or requirements to load page
+     * @return bool
+     */
+    public function checkPermissions()
+    {
+        return $this->modx->hasPermission('empty_cache');
+    }
+
+    /**
+     * Custom logic code here for setting placeholders, etc
+     *
+     * @param array $scriptProperties
+     * @return string
+     */
+    public function process(array $scriptProperties = [])
+    {
+        /* invoke OnBeforeCacheUpdate event */
+        $this->modx->invokeEvent('OnBeforeCacheUpdate');
+
+        $results = [];
+        $this->modx->cacheManager->refresh([], $results);
+
+        /* invoke OnSiteRefresh event */
+        $this->modx->invokeEvent('OnSiteRefresh');
+
+        $num_rows_pub = isset($results['publishing']['published']) ? $results['publishing']['published'] : 0;
+        $num_rows_unpub = isset($results['publishing']['unpublished']) ? $results['publishing']['unpublished'] : 0;
+        $this->modx->smarty->assign('published', $this->modx->lexicon('refresh_published', ['num' => $num_rows_pub]));
+        $this->modx->smarty->assign('unpublished', $this->modx->lexicon('refresh_unpublished', ['num' => $num_rows_unpub]));
+        $this->modx->smarty->assign('results', $results);
+
+        $this->checkFormCustomizationRules();
+
+        return $this->modx->smarty->fetch('system/refresh_site.tpl');
+    }
+
+    /**
+     * Return the pagetitle
+     *
+     * @return string
+     */
+    public function getPageTitle()
+    {
+        return $this->modx->lexicon('empty_cache');
+    }
+
+    /**
+     * Register custom CSS/JS for the page
+     * @return void
+     */
+    public function loadCustomCssJs()
+    {
+        return;
+    }
+
+    /**
+     * Return the location of the template file
+     * @return string|null
+     */
+    public function getTemplateFile()
+    {
+        return null;
+    }
+}
