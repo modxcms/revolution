55c2361512ccf7e8a39193f9cf1f5f3134d89f39
Ivan Bochkarev <ivanx86@gmail.com>
2026-02-24 23:29:17 +0600

    fix(setup): extend modUser password/cachepwd to 255 chars in 3.2.1 upgrade
    Add dedicated 3.2.1-pl upgrade that runs common script to alter
    modUser.password and modUser.cachepwd to varchar(255) for future
    hash algorithm support. Idempotent and safe when 2.7-native-password-hash
    did not run.
    
    Fixes #13929

---
+++ setup/includes/upgrades/mysql/3.2.1-pl.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Specific upgrades for Revolution 3.2.1-pl
+ *
+ * @var modX $modx
+ * @package setup
+ * @subpackage upgrades
+ */
+
+/* run upgrades common to all db platforms */
+include dirname(__DIR__) . '/common/3.2.1-password-cachepwd-255.php';
