f3be51e581f1e28f6db77d88106142120128ab6a
Bochkarev Ivan <ivanx86@gmail.com>
2026-07-06 21:52:40 +0600

    Update core/lexicon/en/system_info.inc.php
    Co-authored-by: Jim Graham <jim@pixelsandstrings.com>

--- core/lexicon/en/system_info.inc.php
+++ core/lexicon/en/system_info.inc.php
@@ -28,7 +28,7 @@ $_lang['database_table_totalsize'] = 'Total size';
 $_lang['database_table_totals'] = 'Totals:';
 $_lang['database_type'] = 'Database type';
 $_lang['database_version'] = 'Database version';
-$_lang['database_query_err_dbname_empty'] = 'Query for current database name returned an empty value.';
+$_lang['database_dbname_err_empty'] = 'Can not query current database because the cached name used to access it is empty.';
 $_lang['database_query_err_table_stat'] = 'Query for TABLE STATUS from [[+db]] returned a null result.';
 $_lang['extjs_version'] = '<a href="http://extjs.com/" target="_blank">ExtJS</a> Version';
 $_lang['localtime'] = 'Local Time';
