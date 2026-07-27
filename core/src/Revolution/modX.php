7b40c039420b6900378fb1675e9358f565a4e8f3
Ivan Bochkarev <ivanx86@gmail.com>
2026-07-27 20:11:39 +0600

    style(core): phpcs operator spacing near _initError change
    Blame-scoped phpcs flagged author-owned lines in adjacent regClient*
    helpers when modX.php was touched.

--- core/src/Revolution/modX.php
+++ core/src/Revolution/modX.php
@@ -1580,18 +1580,19 @@ class modX extends xPDO {
      * @param string $media all, aural, braille, embossed, handheld, print, projection, screen, tty, tv
      * @return void
      */
-    public function regClientCSS($src, $media = null) {
+    public function regClientCSS($src, $media = null)
+    {
         if (isset ($this->loadedjscripts[$src]) && $this->loadedjscripts[$src]) {
             return;
         }
-        $this->loadedjscripts[$src]= true;
+        $this->loadedjscripts[$src] = true;
         if (strpos(strtolower($src), "<style") !== false || strpos(strtolower($src), "<link") !== false) {
-            $this->sjscripts[count($this->sjscripts)]= $src;
+            $this->sjscripts[count($this->sjscripts)] = $src;
         } else {
             if (!empty($media)) {
                 $media = ' media="' . $media .'"';
             }
-            $this->sjscripts[count($this->sjscripts)]= '<link rel="stylesheet" href="' . $src . '" type="text/css"' . $media . ' />';
+            $this->sjscripts[count($this->sjscripts)] = '<link rel="stylesheet" href="' . $src . '" type="text/css"' . $media . ' />';
         }
     }
 
@@ -1604,17 +1605,18 @@ class modX extends xPDO {
      * rather than assuming it is JavaScript.
      * @return void
      */
-    public function regClientStartupScript($src, $plaintext= false) {
+    public function regClientStartupScript($src, $plaintext = false)
+    {
         if (!empty ($src) && !array_key_exists($src, $this->loadedjscripts)) {
             if (isset ($this->loadedjscripts[$src]))
                 return;
-            $this->loadedjscripts[$src]= true;
+            $this->loadedjscripts[$src] = true;
             if ($plaintext == true) {
-                $this->sjscripts[count($this->sjscripts)]= $src;
+                $this->sjscripts[count($this->sjscripts)] = $src;
             } elseif (strpos(strtolower($src), "<script") !== false) {
-                $this->sjscripts[count($this->sjscripts)]= $src;
+                $this->sjscripts[count($this->sjscripts)] = $src;
             } else {
-                $this->sjscripts[count($this->sjscripts)]= '<script src="' . $src . '"></script>';
+                $this->sjscripts[count($this->sjscripts)] = '<script src="' . $src . '"></script>';
             }
         }
     }
@@ -1628,16 +1630,17 @@ class modX extends xPDO {
      * rather than assuming it is JavaScript.
      * @return void
      */
-    public function regClientScript($src, $plaintext= false) {
+    public function regClientScript($src, $plaintext = false)
+    {
         if (isset ($this->loadedjscripts[$src]))
             return;
-        $this->loadedjscripts[$src]= true;
+        $this->loadedjscripts[$src] = true;
         if ($plaintext == true) {
-            $this->jscripts[count($this->jscripts)]= $src;
+            $this->jscripts[count($this->jscripts)] = $src;
         } elseif (strpos(strtolower($src), "<script") !== false) {
-            $this->jscripts[count($this->jscripts)]= $src;
+            $this->jscripts[count($this->jscripts)] = $src;
         } else {
-            $this->jscripts[count($this->jscripts)]= '<script src="' . $src . '"></script>';
+            $this->jscripts[count($this->jscripts)] = '<script src="' . $src . '"></script>';
         }
     }
 
